58a3a3b83d7085d441d6f80d86843f39a7cf89b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:15 +0000

    Add package tox-lambda-autodiscovery/0.1.1/tox-lambda-autodiscovery-0.1.1.tar.gz

---
+++ packages/tox-lambda-autodiscovery/tox-lambda-autodiscovery-0.1.1.tar.gz/tox-lambda-autodiscovery-0.1.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='tox-lambda-autodiscovery',
+    description='autodiscovery and autocreate tox testenv to aws lambda function',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    version='0.1.1',
+    author='Fabio C. Barrionuevo da Luz',
+    author_email='bnafta@gmail.com',
+    maintainer='Fabio C. Barrionuevo da Luz',
+    maintainer_email='bnafta@gmail.com',
+    url='https://github.com/luzfcb/tox-lambda-autodiscovery',
+    packages=find_packages(),
+    python_requires='>=3.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=['tox>=3.3.0'],
+    entry_points={'tox': ['lambda-autodiscovery = tox_lambda_autodiscovery.plugin']},
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: tox',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
