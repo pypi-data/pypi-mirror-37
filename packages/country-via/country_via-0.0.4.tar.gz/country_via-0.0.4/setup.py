11cb5c4f0d0a603fa8a4e752d93c4c161263f445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:52 +0000

    Add package country-via/0.0.4/country_via-0.0.4.tar.gz

---
+++ packages/country-via/country_via-0.0.4.tar.gz/country_via-0.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name="country_via",
+    version="0.0.4",
+    description="Route Configuring Tool for Country",
+    author="Hui Yiqun",
+    author_email="huiyiqun@gmail.com",
+    license='MIT',
+    url="https://github.com/huiyiqun/country_via",
+    packages=["country_via"],
+    install_requires=[
+        'requests',
+        'jinja2',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+
+        'Intended Audience :: System Administrators',
+
+        'Environment :: Console',
+    ],
+    keywords='network route',
+    package_data={
+        'country_via': ['templates/*.j2']
+    },
+    entry_points={
+        'console_scripts': {
+            'country-via=country_via.cli:country_via'
+        }
+    }
+)
