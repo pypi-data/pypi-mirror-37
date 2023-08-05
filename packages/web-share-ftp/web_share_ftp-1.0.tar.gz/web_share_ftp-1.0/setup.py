a3918ddf921ec9c51b5e7c71379a73eff809f480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package web-share-ftp/1.0/web_share_ftp-1.0.tar.gz

---
+++ packages/web-share-ftp/web_share_ftp-1.0.tar.gz/web_share_ftp-1.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "web_share_ftp",
+    version = '1.00',
+    author = 'Ove Bepari',
+    author_email = 'ovebepari@gmail.com',
+    description = "A Flask App to Share Files Within the Same Network",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ovebepari/webshare",
+    packages = setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    install_requires=[
+        'flask',
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'web_share_ftp = web_share_ftp.cli:main',
+        ],
+    },
+)
