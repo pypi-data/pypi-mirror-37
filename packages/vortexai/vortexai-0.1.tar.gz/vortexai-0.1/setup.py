c6e78e1e874eafd88b5b07ecf4d8281bbdc47fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:21 +0000

    Add package vortexai/0.1/vortexai-0.1.tar.gz

---
+++ packages/vortexai/vortexai-0.1.tar.gz/vortexai-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='vortexai',
+    packages=['mypackage'],  # this must be the same as the name above
+    version='0.1',
+    description='A random test lib',
+    author='Peter Downs',
+    author_email='peterldowns@gmail.com',
+    url='https://github.com/peterldowns/mypackage',
+    download_url='https://github.com/alitokur/python-package/archive/0.1.tar.gz',
+    keywords=['testing', 'logging', 'example'],
+    classifiers=[],
+)
