5bc4c856ebb90c86e733d10706a463c8338ae1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:03 +0000

    Add package happy-pandas/0.1/happy_pandas-0.1.tar.gz

---
+++ packages/happy-pandas/happy_pandas-0.1.tar.gz/happy_pandas-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='happy_pandas',
+    version='0.1',
+    packages=['happy_pandas'],
+    install_requires=["pandas", "happybase", "pyspark"],
+    url='',
+    license='',
+    author='voyager',
+    author_email='',
+    description=''
+)
