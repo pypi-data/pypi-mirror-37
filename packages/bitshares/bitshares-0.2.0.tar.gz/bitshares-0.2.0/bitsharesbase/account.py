df0b5a3c12142a530be4ca8fc63784ce97857a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:58 +0000

    Add package bitshares/0.2.0/bitshares-0.2.0.tar.gz

---
+++ packages/bitshares/bitshares-0.2.0.tar.gz/bitshares-0.2.0/bitsharesbase/account.py
@@ -0,0 +1,11 @@
+from graphenebase.account import (
+    PasswordKey,
+    BrainKey,
+    Address,
+    PublicKey,
+    PrivateKey,
+    Prefix
+)
+
+# Redefine default prefix
+Prefix.prefix = "BTS"
