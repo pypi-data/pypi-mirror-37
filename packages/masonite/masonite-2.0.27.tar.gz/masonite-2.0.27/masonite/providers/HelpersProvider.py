76eea8e28ec7233e97a6eedd397559f1030608c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package masonite/2.0.27/masonite-2.0.27.tar.gz

---
+++ packages/masonite/masonite-2.0.27.tar.gz/masonite-2.0.27/masonite/providers/HelpersProvider.py
@@ -0,0 +1,35 @@
+""" A SassProvider Service Provider """
+
+import builtins
+import os
+
+from masonite.helpers.view_helpers import back, set_request_method
+from masonite.provider import ServiceProvider
+
+
+class HelpersProvider(ServiceProvider):
+
+    wsgi = False
+
+    def register(self):
+        pass
+
+    def boot(self, View, ViewClass, Request):
+        """ Add helper functions to Masonite """
+        builtins.view = View
+        builtins.request = Request.helper
+        builtins.auth = Request.user
+        builtins.container = self.app.helper
+        builtins.env = os.getenv
+        builtins.resolve = self.app.resolve
+        builtins.route = Request.route
+
+        ViewClass.share(
+            {
+                'request': Request.helper,
+                'auth': Request.user,
+                'request_method': set_request_method,
+                'route': Request.route,
+                'back': back
+            }
+        )
