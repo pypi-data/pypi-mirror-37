b97c9c7c9fb2276a65391a8e698e485af4bda5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:51 +0000

    Add package opencap/0.0.2/opencap-0.0.2.tar.gz

---
+++ packages/opencap/opencap-0.0.2.tar.gz/opencap-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="opencap",
+    version="0.0.2",
+    description="Tools to help abstract and standardize OpenCAP common functions",
+    author="Lane Wagner",
+    packages=['opencap'],
+    url="https://github.com/opencap/python-opencap",
+    license="MIT",
+)
