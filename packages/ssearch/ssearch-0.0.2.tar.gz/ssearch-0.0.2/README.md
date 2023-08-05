f9f53383f486c87dea556f5c7ef262e9cec0fd66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package ssearch/0.0.2/ssearch-0.0.2.tar.gz

---
+++ packages/ssearch/ssearch-0.0.2.tar.gz/ssearch-0.0.2/README.md
@@ -0,0 +1,28 @@
+# ssearch
+
+v0.0.2
+
+命令行单词搜索工具
+
+
+feature:
+
+- 目前使用搜狗翻译
+
+todo：
+
+- 单词音频播放 
+- 搜索引擎切换
+
+## install
+
+```shell
+pip install ssearch
+```
+
+## usage
+
+```
+s -h
+```
+
---
+++ packages/ssearch/ssearch-0.0.2.tar.gz/ssearch-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+        name='ssearch',
+        version='0.0.2',
+        packages=['ssearch'],
+        url='https://github.com/xyok/ssearch.git',
+        license='MIT',
+        author='xyok',
+        author_email='xy___ok@163.com',
+        description='sougo translate cli tool',
+        keywords=['cli', 'translate', "chinese"],
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        install_requires=[
+            'requests'
+        ],
+        entry_points={
+            'console_scripts': [
+                's = ssearch.ssearch:main'
+            ]
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 2.7",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+)
