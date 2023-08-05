71bcbad796f26da738b313fe172e1e05603428b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:22 +0000

    Add package google-alerts/0.1.8/google_alerts-0.1.8-py3.6.egg

---
+++ packages/google-alerts/google_alerts-0.1.8-py3.6.egg/google_alerts/cli/manage.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+"""Perform administrative actions on Google Alerts."""
+import base64
+import json
+import os
+from google_alerts import GoogleAlerts
+from argparse import ArgumentParser
+import sys
+
+PY2 = False
+if sys.version_info[0] < 3:
+    PY2 = True
+
+__author__ = "Brandon Dixon"
+__copyright__ = "Copyright, Brandon Dixon"
+__credits__ = ["Brandon Dixon"]
+__license__ = "MIT"
+__maintainer__ = "Brandon Dixon"
+__email__ = "brandon@9bplus.com"
+__status__ = "BETA"
+
+
+CONFIG_PATH = os.path.expanduser('~/.config/google_alerts')
+CONFIG_FILE = os.path.join(CONFIG_PATH, 'config.json')
+CONFIG_DEFAULTS = {'email': '', 'password': ''}
+
+
+def obfuscate(p, action):
+    """Obfuscate the auth details to avoid easy snatching.
+
+    It's best to use a throw away account for these alerts to avoid having
+    your authentication put at risk by storing it locally.
+    """
+    key = "ru7sll3uQrGtDPcIW3okutpFLo6YYtd5bWSpbZJIopYQ0Du0a1WlhvJOaZEH"
+    s = list()
+    if action == 'store':
+        if PY2:
+            for i in range(len(p)):
+                kc = key[i % len(key)]
+                ec = chr((ord(p[i]) + ord(kc)) % 256)
+                s.append(ec)
+                return base64.urlsafe_b64encode("".join(s))
+        else:
+                return base64.urlsafe_b64encode(p.encode()).decode()
+    else:
+        if PY2:
+            e = base64.urlsafe_b64decode(p)
+            for i in range(len(e)):
+                kc = key[i % len(key)]
+                if PY2:
+                    dc = chr((256 + ord(e[i]) - ord(kc)) % 256)
+                s.append(dc)
+            return "".join(s)
+        else:
+            e = base64.urlsafe_b64decode(p)
+            return e.decode()
+
+
+def main():
+    """Run the core."""
+    parser = ArgumentParser()
+    subs = parser.add_subparsers(dest='cmd')
+    setup_parser = subs.add_parser('setup')
+    setup_parser.add_argument('-e', '--email', dest='email', required=True,
+                              help='Email of the Google user.', type=str)
+    setup_parser.add_argument('-p', '--password', dest='pwd', required=True,
+                              help='Password of the Google user.', type=str)
+    setup_parser = subs.add_parser('list')
+    setup_parser = subs.add_parser('create')
+    setup_parser.add_argument('-t', '--term', dest='term', required=True,
+                              help='Term to store.', type=str)
+    setup_parser.add_argument('--exact', dest='exact', action='store_true',
+                              help='Exact matches only for term.')
+    setup_parser.add_argument('-d', '--delivery', dest='delivery',
+                              required=True, choices=['rss', 'mail'],
+                              help='Delivery method of results.')
+    setup_parser.add_argument('-f', '--frequency', dest='frequency',
+                              default="realtime", choices=['realtime', 'daily', 'weekly'],
+                              help='Frequency to send results. RSS only allows for realtime alerting')
+    setup_parser = subs.add_parser('delete')
+    setup_parser.add_argument('--id', dest='term_id', required=True,
+                              help='ID of the term to find for deletion.',
+                              type=str)
+    args = parser.parse_args()
+
+    if args.cmd == 'setup':
+        if not os.path.exists(CONFIG_PATH):
+            os.makedirs(CONFIG_PATH)
+        if not os.path.exists(CONFIG_FILE):
+            json.dump(CONFIG_DEFAULTS, open(CONFIG_FILE, 'w'), indent=4,
+                      separators=(',', ': '))
+        config = CONFIG_DEFAULTS
+        config['email'] = args.email
+        config['password'] = str(obfuscate(args.pwd, 'store'))
+        json.dump(config, open(CONFIG_FILE, 'w'), indent=4,
+                  separators=(',', ': '))
+
+    config = json.load(open(CONFIG_FILE))
+    if config['password'] == '':
+        raise Exception("Run setup before any other actions!")
+
+    if args.cmd == 'list':
+        config['password'] = obfuscate(str(config['password']), 'fetch')
+        ga = GoogleAlerts(config['email'], config['password'])
+        ga.authenticate()
+        print(json.dumps(ga.list(), indent=4))
+
+    if args.cmd == 'create':
+        config['password'] = obfuscate(str(config['password']), 'fetch')
+        ga = GoogleAlerts(config['email'], config['password'])
+        ga.authenticate()
+        alert_frequency = 'as_it_happens'
+        if args.frequency == 'realtime':
+            alert_frequency = 'as_it_happens'
+        elif args.frequency == 'daily':
+            alert_frequency = 'at_most_once_a_day'
+        else:
+            alert_frequency = 'at_most_once_a_week'
+
+        monitor = ga.create(args.term, {'delivery': args.delivery.upper(),
+                                        'alert_frequency': alert_frequency.upper(),
+                                        'exact': args.exact})
+        print(json.dumps(monitor, indent=4))
+
+    if args.cmd == 'delete':
+        config['password'] = obfuscate(str(config['password']), 'fetch')
+        ga = GoogleAlerts(config['email'], config['password'])
+        ga.authenticate()
+        result = ga.delete(args.term_id)
+        if result:
+            print("%s was deleted" % args.term_id)
+
+
+if __name__ == '__main__':
+    main()
