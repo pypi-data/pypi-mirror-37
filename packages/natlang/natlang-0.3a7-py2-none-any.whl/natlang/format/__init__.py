98c1e6e0f29c317b64113de39d6dda1da669e52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:42 +0000

    Add package natlang/0.3a7/natlang-0.3a7-py2-none-any.whl

---
+++ packages/natlang/natlang-0.3a7-py2-none-any.whl/natlang/format/__init__.py
@@ -0,0 +1,31 @@
+import unittest
+from . import AMR
+from . import pyCode
+from . import semanticFrame
+from . import tree
+from . import txt
+from . import txtFiles
+from . import txtOrTree
+from . import conll
+
+
+modules = {
+    AMR,
+    pyCode,
+    semanticFrame,
+    tree,
+    txt,
+    txtFiles,
+    txtOrTree,
+    conll
+}
+
+
+def testSuite():
+    loader = unittest.TestLoader()
+    suite = unittest.TestSuite()
+
+    # add tests to the test suite
+    for module in modules:
+        suite.addTests(loader.loadTestsFromModule(module))
+    return suite
