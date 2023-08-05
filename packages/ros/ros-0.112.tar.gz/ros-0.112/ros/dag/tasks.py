49086db735b18735ce1dd9d974bf3aba177b5404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:18 +0000

    Add package ros/0.112/ros-0.112.tar.gz

---
+++ packages/ros/ros-0.112.tar.gz/ros-0.112/ros/dag/tasks.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+import json
+import time
+#from ros.dag.celery_app import app
+from ros.workflow import Workflow
+from ros.router import Router
+
+def json2workflow(json):
+    model = Workflow (spec={ 'x' : 'y' })
+    model.uuid = json['uuid']
+    model.spec = json['spec']
+    model.inputs = json['inputs']
+    model.dependencies = json['dependencies']
+    model.topsort = json['topsort']
+    model.running = json['running']
+    model.failed = json['failed']
+    model.done = json['done']
+    return model
+
+'''
+@app.task(bind=True, queue="rosetta")
+def calc_dag (self, workflow_spec, inputs):
+    return Workflow (workflow_spec, inputs=inputs).json ()
+
+@app.task(bind=True, queue="rosetta")
+def exec_operator(self, model, job_name):
+    result = None
+    wf = json2model (model)
+    op_node = wf.spec.get("workflow",{}).get(job_name,{})
+    if op_node:
+        router = Router (wf)
+        result = router.route (wf, job_name, op_node, op_node['code'], op_node['args'])
+        wf.set_result (job_name, result)
+    return result
+
+def calc_dag (workflow_spec, inputs):
+    return Workflow (workflow_spec, inputs=inputs).json ()
+'''
+
+def exec_operator(model, job_name):
+    result = None
+    wf = json2workflow (model)
+    op_node = wf.spec.get("workflow",{}).get(job_name,{})
+    if op_node:
+        router = Router (wf)
+        result = router.route (wf, job_name, op_node, op_node['code'], op_node['args'])
+        wf.set_result (job_name, result)
+    return result
