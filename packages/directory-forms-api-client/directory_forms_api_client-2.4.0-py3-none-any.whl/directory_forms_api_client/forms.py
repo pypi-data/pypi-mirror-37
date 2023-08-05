fce70915c0d70683cc9a219a93c6b8fed9141697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:55 +0000

    Add package directory-forms-api-client/2.4.0/directory_forms_api_client-2.4.0-py3-none-any.whl

---
+++ packages/directory-forms-api-client/directory_forms_api_client-2.4.0-py3-none-any.whl/directory_forms_api_client/forms.py
@@ -0,0 +1,77 @@
+from django.forms import Form
+
+from directory_forms_api_client import actions
+
+
+class EmailActionMixin:
+    action_class = actions.EmailAction
+
+    def save(self, recipients, subject, reply_to, *args, **kwargs):
+        action = self.action_class(
+            recipients=recipients,
+            subject=subject,
+            reply_to=reply_to,
+        )
+        return action.save(self.serialized_data)
+
+    @property
+    def serialized_data(self):
+        return {
+            'text_body': self.text_body,
+            'html_body': self.html_body,
+        }
+
+    @property
+    def text_body(self):
+        raise NotImplementedError()
+
+    @property
+    def html_body(self):
+        raise NotImplementedError()
+
+
+class ZendeskActionMixin:
+    action_class = actions.ZendeskAction
+
+    def save(
+        self, email_address, full_name, subject, subdomain=None, *args,
+        **kwargs
+    ):
+        action = self.action_class(
+            email_address=email_address,
+            full_name=full_name,
+            subject=subject,
+            subdomain=subdomain
+        )
+        return action.save(self.serialized_data)
+
+    @property
+    def serialized_data(self):
+        return self.cleaned_data
+
+
+class GovNotifyActionMixin:
+    action_class = actions.GovNotifyAction
+
+    def save(self, template_id, email_address, *args, **kwargs):
+        action = self.action_class(
+            template_id=template_id,
+            email_address=email_address,
+        )
+        return action.save(self.serialized_data)
+
+    @property
+    def serialized_data(self):
+        return self.cleaned_data
+
+
+class EmailAPIForm(EmailActionMixin, Form):
+    pass
+
+
+class ZendeskAPIForm(ZendeskActionMixin, Form):
+    pass
+
+
+class GovNotifyAPIForm(GovNotifyActionMixin, Form):
+    pass
