8c5e757c9b799fa3f819e24d607e7c00f13618a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:46 +0000

    Add package vector-2d/1.1.2/vector_2d-1.1.2-py2-none-any.whl

---
+++ packages/vector-2d/vector_2d-1.1.2-py2-none-any.whl/vector_2D/__init__.py
@@ -0,0 +1,7 @@
+from vector import *
+from vectorPolar import *
+
+__all__ = ["vector", "vectorPolar", "VectorPolar", "Vector",  "round_vector", "angle", "distance_point_line",
+           "distance_point_segment"]
+
+
