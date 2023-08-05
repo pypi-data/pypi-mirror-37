dce69e3e19260d3e3219e94c21fbc6bd29ae0dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:51 +0000

    Add package ybc-download/1.0.9/ybc_download-1.0.9.tar.gz

---
+++ packages/ybc-download/ybc_download-1.0.9.tar.gz/ybc_download-1.0.9/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_download',
+  packages = ['ybc_download'],
+  version = '1.0.9',
+  description = 'Down The Resource',
+  long_description='Down The Resource For Level1 Courses',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python'],
+  license='MIT',
+  # install_requires=['requests','ybc_art','ybc_bot','ybc_animal','ybc_box','ybc_food','ybc_speech','ybc_funny','ybc_face']
+)
---
+++ packages/ybc-download/ybc_download-1.0.9.tar.gz/ybc_download-1.0.9/ybc_download/ybc_download.py
@@ -0,0 +1,42 @@
+import os
+import urllib.request as rq
+import zipfile
+
+def create_my_world():
+    url = 'https://www.yuanfudao.com/tutor-ybc-course-api/res.zip'
+    filename = 'res.zip'
+    rq.urlretrieve(url,filename)
+    zip_file = zipfile.ZipFile(filename)
+    for files in zip_file.namelist():
+        zip_file.extract(files,'./')
+    zip_file.close()
+    os.remove(filename)
+
+def create_my_world_L2():
+    url = 'https://www.yuanfudao.com/tutor-ybc-course-api/res2.zip'
+    filename = 'res2.zip'
+    rq.urlretrieve(url,filename)
+    zip_file = zipfile.ZipFile(filename)
+    for files in zip_file.namelist():
+        zip_file.extract(files,'./')
+    zip_file.close()
+    os.remove(filename)
+
+def create_my_world_l2_bak():
+    url = 'https://www.yuanfudao.com/tutor-ybc-course-api/res2.zip'
+    filename = 'res2.zip'
+    rq.urlretrieve(url,filename)
+    zip_file = zipfile.ZipFile(filename)
+    for files in zip_file.namelist():
+        filename = files.encode('cp437').decode('utf8')
+        zip_file.extract(files,'./')
+        os.rename(files,filename)
+    zip_file.close()
+    os.remove(filename)
+
+def main():
+    create_my_world()
+    create_my_world_l2()
+
+if __name__ == '__main__':
+    main()
