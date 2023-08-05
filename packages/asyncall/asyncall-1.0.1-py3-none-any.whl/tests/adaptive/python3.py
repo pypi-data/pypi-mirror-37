b2c44b8633a3bd3633730a88cb03c521fc2f72a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package asyncall/1.0.1/asyncall-1.0.1-py3-none-any.whl

---
+++ packages/asyncall/asyncall-1.0.1-py3-none-any.whl/tests/adaptive/python3.py
@@ -0,0 +1,24 @@
+# /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from tests.context import Result
+import asyncio
+
+# @asyncio.coroutine
+# def demo(name):
+#     import random
+#     asleep = random.randint(0, 1)
+#     print('---start, %s  %s' % (name, asleep))
+#     yield from asyncio.sleep(asleep)
+#     print('---stop, %s' % name)
+#     # raise Result(name)
+#     return name
+
+async def demo(name):
+    import random
+    asleep = random.randint(0, 1)
+    print('---start, %s  %s' % (name, asleep))
+    await asyncio.sleep(asleep)
+    print('---stop, %s' % name)
+    # raise Result(name)
+    return name
