0b2e4e20685943e35400406858afff1fb4da2e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:50 +0000

    Add package pywikiscraper/0.0.3/pywikiscraper-0.0.3.tar.gz

---
+++ packages/pywikiscraper/pywikiscraper-0.0.3.tar.gz/pywikiscraper-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pywikiscraper",
+    version="0.0.3",
+    author="sam-iau",
+    author_email="sam-iau@outlook.com",
+    description="A lite package to easy scrape any wikipedia page",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
