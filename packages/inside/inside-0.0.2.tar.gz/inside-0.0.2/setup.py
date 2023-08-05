d38d1189654dbc49ad54540d4fd729a2e285d4b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:42 +0000

    Add package inside/0.0.2/inside-0.0.2.tar.gz

---
+++ packages/inside/inside-0.0.2.tar.gz/inside-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
+    name="inside",
+    version="0.0.2",
+    author="William Wyatt",
+    author_email="wwyatt@ucsc.edu",
+    description="Collision detection with polygons.",
+    long_description="Collision detection with polygons.",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Tsangares/overlap",
+    #package_data={'lgad': ['plates.json']},
+    scripts=['inside/parse.py'],
+    #install_requires=["matplotlib", "numpy"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
