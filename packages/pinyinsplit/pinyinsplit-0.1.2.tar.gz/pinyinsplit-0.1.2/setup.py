1909eca8018ac42d70a6e2cc8fb924cc4c87e2a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:07 +0000

    Add package pinyinsplit/0.1.2/pinyinsplit-0.1.2.tar.gz

---
+++ packages/pinyinsplit/pinyinsplit-0.1.2.tar.gz/pinyinsplit-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='pinyinsplit',
+    version='0.1.2',
+    description='A Python library to split a Chinese Pinyin phrase into possible permutations of Chinese Pinyin words',
+    long_description=readme(),
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+    ],
+    keywords='chinese pinyin',
+    url='https://github.com/throput/pinyinsplit',
+    author='Thomas Lee',
+    author_email='thomaslee@throput.com',
+    install_requires=[
+        'pygtrie>=2.3',
+    ],
+)
