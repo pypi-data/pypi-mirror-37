e90a0b98f59a991a1d4fa8f63fb3e4a5e3e4a621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:41 +0000

    Add package amipwnd/0.1.1/amipwnd-0.1.1.tar.gz

---
+++ packages/amipwnd/amipwnd-0.1.1.tar.gz/amipwnd-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="amipwnd",
+    version='0.1.1',
+    py_modules=['amipwnd'],
+    install_requires=[
+        'Click',
+        'ascii-graph',
+        'numpy',
+        'requests',
+        'tqdm',
+    ],
+    entry_points='''
+        [console_scripts]
+        amipwnd-cli=amipwnd.amipwnd_cli:cli
+    ''',
+)
