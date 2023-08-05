c2afe04ba66ffaa4026a805a0ad3c2a6889fb1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:42 +0000

    Add package shortesttrack-sdk/1.2.2/shortesttrack-sdk-1.2.2.tar.gz

---
+++ packages/shortesttrack-sdk/shortesttrack-sdk-1.2.2.tar.gz/shortesttrack-sdk-1.2.2/shortesttrack/library/iterative/sync/base_solver.py
@@ -0,0 +1,36 @@
+from abc import ABCMeta, abstractmethod
+
+
+class BaseSyncSolver(metaclass=ABCMeta):
+    """
+    An abstract class that the user needs to implement to run the code in ShortestTrack Company API.
+    """
+
+    @abstractmethod
+    def iterate(self, **kwargs):
+        """
+        This method will be called in a loop to process input data.
+        Synchronous and asynchronous method calls are possible.
+
+        args:
+            kwargs:
+                Input data for processing.
+        """
+        pass
+
+    @abstractmethod
+    def close(self):
+        """
+        This method will be guaranteed to be called when the script finishes working.
+        """
+        pass
+
+    @abstractmethod
+    def is_ready(self):
+        """
+        This method is required to determine the status of the user script (readiness to work).
+
+        returns:
+            bool: is ready
+        """
+        pass
