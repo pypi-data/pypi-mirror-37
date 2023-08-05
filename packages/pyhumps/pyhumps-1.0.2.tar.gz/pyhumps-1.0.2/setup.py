4c719dfd72abf27c3e13d417499da99f17973bf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:33 +0000

    Add package pyhumps/1.0.2/pyhumps-1.0.2.tar.gz

---
+++ packages/pyhumps/pyhumps-1.0.2.tar.gz/pyhumps-1.0.2/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module contains setup instructions for pyhumps."""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('LICENSE') as readme_file:
+    license = readme_file.read()
+
+setup(
+    name='pyhumps', version='1.0.2', author='Nick Ficano',
+    author_email='nficano@gmail.com', packages=['humps'],
+    url='https://github.com/nficano/humps', license=license,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console', 'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English', 'Operating System :: MacOS',
+        'Operating System :: Microsoft', 'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python', 'Topic :: Utilities', ],
+    description=(
+        '🐫  Convert strings (and dictionary keys) between snake case, camel '
+        'case and pascal case in Python. Inspired by Humps for Node'
+    ),
+    long_description=readme, zip_safe=True,
+)
