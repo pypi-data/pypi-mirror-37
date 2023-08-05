30ef21870703b4fcf48941c4fd93425b6eef66e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:24 +0000

    Add package vsmstreamer/0.1.1/vsmstreamer-0.1.1.tar.gz

---
+++ packages/vsmstreamer/vsmstreamer-0.1.1.tar.gz/vsmstreamer-0.1.1/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vsmstreamer",
+    version="0.1.1",
+    author="Praveen Kumar",
+    author_email="praveen+pypi@kumar.in",
+    description="Cisco Video Surveillance Manager Streamer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kprav33n/vsmstreamer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: MacOS",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        'PyCocoa',
+        'configparser',
+        'python-vlc',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vsmstreamer=vsmstreamer:main',
+        ],
+    }
+)
