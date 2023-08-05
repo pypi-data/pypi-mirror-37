451fc5fd071c0e38e079f68025e966aa14277df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:13 +0000

    Add package natlang/0.3a15/natlang-0.3a15-py2-none-any.whl

---
+++ packages/natlang/natlang-0.3a15-py2-none-any.whl/natlang/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from natlang import format
+
+from natlang import exporter
+from natlang import fileConverter
+from natlang import loader
+
+from natlang import version
+
+name = "natlang"
