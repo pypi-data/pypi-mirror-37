6923f48cca1555bba84396983d2f6fb740ab6a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:45 +0000

    Add package git-remote/0.0.1/git_remote-0.0.1-py2.py3-none-any.whl

---
+++ packages/git-remote/git_remote-0.0.1-py2.py3-none-any.whl/git_remote/__init__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import runcmd
+from public import public
+
+"""
+https://git-scm.com/docs/git-remote
+"""
+
+
+@public
+def run(args):
+    return runcmd.run(["git", "remote"] + list(args))._raise().out
+
+
+@public
+def add(name, url):
+    run(["add", name, url])
+
+
+@public
+def remove(name):
+    run(["remove", name])
+
+
+@public
+def rm(name):
+    run(["rm", name])
+
+
+@public
+def rename(old, new):
+    run(["rename", old, new])
+
+
+@public
+def set_url(name, url):
+    return run(["set-url", name, url])
+
+
+@public
+def remotes():
+    result = []
+    for l in run(["-v"]).splitlines():
+        name, url, role = l.split()
+        if "fetch" in role:
+            result.append([name, url])
+    return result
+
+
+@public
+def names():
+    return list(lambda name, url: name, remotes())
+
+
+@public
+def urls():
+    return list(lambda name, url: url, remotes())
