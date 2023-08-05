764cde26d12c86ac81c65eeefa014727003e607e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:25 +0000

    Add package json-store/3.0/json_store-3.0-py2.py3-none-any.whl

---
+++ packages/json-store/json_store-3.0-py2.py3-none-any.whl/json_store/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+from .json_store import *
+
+__version__ = "3.0"
+
+open = JSONStore
---
+++ packages/json-store/json_store-3.0-py2.py3-none-any.whl/json_store/shelve2json.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# encoding: utf-8
+from __future__ import absolute_import, print_function, unicode_literals
+"""Naïvely create a json_store file from a shelve DB."""
+
+import os
+import sys
+import shelve
+
+import json_store
+
+
+def convert(oldfile):
+    if not os.path.isfile(oldfile):
+        raise ValueError("No such file: {}".format(oldfile))
+
+    data = shelve.open(oldfile)
+
+    newfile = oldfile.rsplit(".db")[0] + ".json"
+    store = json_store.open(newfile)
+    store.update(data)
+    store.sync()
+
+
+def main(argv):
+    if len(argv) < 2:
+        print("Usage: {0[0]} <shelve.db>".format(sys.argv))
+        return 1
+
+    try:
+        convert(argv[1])
+    except Exception as e:
+        print(str(e), file=sys.stderr)
+        return 1
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
