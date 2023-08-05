d7a1233ddf8407a285e926878696da7018e26046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:46 +0000

    Add package gae-sdk-path-fixer/0.1.1/gae-sdk-path-fixer-0.1.1.tar.gz

---
+++ packages/gae-sdk-path-fixer/gae-sdk-path-fixer-0.1.1.tar.gz/gae-sdk-path-fixer-0.1.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='gae-sdk-path-fixer',
+    version='0.1.1',
+    description='GAE SDK Path Fixer: detect AppEngine SDK and fix Python sys.path',
+    author='Karl Kroening',
+    author_email='karlk@kralnet.us',
+    url='https://github.com/kkroening/gae-sdk-path-fixer',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        'console_scripts': [
+            'download-gae-sdk=gae_sdk_path_fixer.download_gae_sdk:main',
+        ],
+    },
+
+)
