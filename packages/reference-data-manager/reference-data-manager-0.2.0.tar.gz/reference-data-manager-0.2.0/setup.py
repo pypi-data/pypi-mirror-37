8ca5d219f04aed02209294d67040eaf19ec48fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:51 +0000

    Add package reference-data-manager/0.2.0/reference-data-manager-0.2.0.tar.gz

---
+++ packages/reference-data-manager/reference-data-manager-0.2.0.tar.gz/reference-data-manager-0.2.0/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+import os.path
+
+readme_file = 'README.md'
+readme = 'Cannot find the file:'+readme_file
+if os.path.exists(readme_file):
+    with open(readme_file) as f:
+        readme = f.read()
+
+license_file = 'LICENSE'
+license ='Cannot find the file:'+license_file
+if os.path.exists(license_file):
+    with open(license_file) as f:
+        license = f.read()
+
+requires_file = 'requirements.txt'
+requires = 'Cannot find the file:'+requires_file
+if os.path.exists(requires_file):
+    with open(requires_file) as f:
+        requires = f.read()
+
+
+setup(
+    name="reference-data-manager",
+    version="0.2.0",
+
+    description='''Bioinformatics Reference Data Manager is an application used to download, backup and update of \
+    reference data, required for various bioinformatics analysis''',
+    
+    long_description=readme,
+    
+    author='Oksana Korol, Chunfang Zheng',
+    author_email='aafc.bice-ceib.aac@canada.ca',
+
+    url='https://github.com/AAFC-BICoE/reference-data-manager',
+    
+    license=license,
+    
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ),
+
+
+    packages=find_packages(exclude=['test*', 'Test*']),
+
+    package_data={
+        '': ['README.md', 'LICENSE'],
+        'brdm': ['config.yaml.sample']
+      },
+
+
+    scripts=['main.py'],
+
+    entry_points={
+          'console_scripts': [
+              'brdm = main:main',
+          ],
+      },
+
+
+)
