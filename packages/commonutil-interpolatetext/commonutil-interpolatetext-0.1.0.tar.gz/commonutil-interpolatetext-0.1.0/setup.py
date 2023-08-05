970145704688167d770850ccd3f63356813f6c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:14 +0000

    Add package commonutil-interpolatetext/0.1.0/commonutil-interpolatetext-0.1.0.tar.gz

---
+++ packages/commonutil-interpolatetext/commonutil-interpolatetext-0.1.0.tar.gz/commonutil-interpolatetext-0.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup  # pylint: disable=import-error
+
+setup(
+		name="commonutil-interpolatetext",
+		version="0.1.0",  # REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
+		description="Interpolate text template engine",
+		packages=[
+				"commonutil_interpolatetext",
+		],
+		classifiers=[
+				"Development Status :: 5 - Production/Stable",
+				"Intended Audience :: Developers",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: POSIX",
+				"Programming Language :: Python :: 2.7",
+		],
+		license="MIT License",
+)
