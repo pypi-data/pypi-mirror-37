178149277c887d496a466c785a9154fa62692d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:29 +0000

    Add package WISE-PaaS-SCADA-Python-SDK/1.0.0/WISE_PaaS_SCADA_Python_SDK-1.0.0-py3-none-any.whl

---
+++ packages/WISE-PaaS-SCADA-Python-SDK/WISE_PaaS_SCADA_Python_SDK-1.0.0-py3-none-any.whl/WISE-PaaS-SCADA-Python-SDK/Common/Converter.py
@@ -0,0 +1,102 @@
+from WISEPaaS-SCADA-Python-SDK.Model.MQTTMessage import *
+import WISEPaaS-SCADA-Python-SDK.Common.Constants as constant
+from WISEPaaS-SCADA-Python-SDK.Model.Edge import *
+
+def convertData(data = None):
+  try:
+    if data is None:
+      return (False, None)
+    payloads = []
+    count = 0
+    dataMessage = None
+    tagList = data.tagList
+    tagList = sorted(tagList, key = lambda tag: tag.deviceId)
+
+    for tag in tagList:
+      if dataMessage is None:
+        dataMessage = DataMessage()
+      dataMessage.setTagValue(tag.deviceId, tag.tagName, tag.value)
+      count += 1
+      if count == constant.DataMaxTagCount:
+        payloads.append(dataMessage.getJson())
+        dataMessage = None
+    payloads.append(dataMessage.getJson())
+    return (True, payloads)
+  except Exception as error:
+    print('convert data fail', str(error))
+    return (False, None)
+    
+
+def convertDeviceStatus(status = None):
+  try:
+    if not status:
+      return (False, None)
+    deviceList = status.deviceList
+    payload = DeviceStatusMessage()
+    for device in deviceList:
+      payload.setDeviceStatus(device.id, device.status)
+    return (True, payload.getJson())
+  except Exception as error:
+    print('convert status fail', str(error))
+    return (False, None)
+
+def convertCreateorUpdateConfig(action = None, scadaId = None, config = None, heartbeat = constant.HeartbeatInterval):
+  try:
+    if not config or not scadaId:
+      return (False, None)
+    payload = ConfigMessage(action, scadaId)
+
+    scada = config.scada
+    if not type(scada) is ScadaConfig:
+      raise ValueError('config.scada type is invalid')
+    scada.heartbeat = heartbeat
+    (result, error) = scada.isValid()
+    if action == constant.ActionType['Create'] and not result:
+      raise error
+    payload.addScadaConfig(scadaId, scada)
+    for device in scada.deviceList:
+      if not type(device) is DeviceConfig:
+        raise ValueError('config.scada.device type is invalid')
+      (result, error) = device.isValid()
+      if action == constant.ActionType['Create'] and not result:
+        raise error
+      payload.addDeviceConfig(scadaId, deviceId = device.id, config = device)
+      for tag in device.analogTagList:
+        tag.type = constant.TagType['Analog']
+        payload.addTagConfig(scadaId, deviceId = device.id, tagName = tag.name, config = tag)
+      for tag in device.discreteTagList:
+        tag.type = constant.TagType['Discrete']
+        payload.addTagConfig(scadaId, deviceId = device.id, tagName = tag.name, config = tag)
+      for tag in device.textTagList:
+        tag.type = constant.TagType['Text']
+        payload.addTagConfig(scadaId, deviceId = device.id, tagName = tag.name, config = tag)
+    return (True, payload.getJson())
+  except Exception as error:
+    print('convert create config fail', str(error))
+    return (False, None)
+
+def convertDeleteConfig(action = None, scadaId = None, config = None):
+  try:
+    if not (config or scadaId):
+      return (False, None)
+    payload = ConfigMessage(action, scadaId)
+
+    scada = config.scada
+    if not type(scada) is ScadaConfig:
+      raise ValueError('config.scada type is invalid')
+    payload.deleteScadaConfig(scadaId)
+    for device in scada.deviceList:
+      payload.deleteDeviceConfig(scadaId, deviceId = device.id)
+      for listName in ['analogTagList', 'discreteTagList', 'textTagList']:
+        for tag in getattr(device, listName):
+          payload.deleteDeviceConfig(scadaId, deviceId = device.id)
+        for tag in device.analogTagList:
+          payload.deleteTagConfig(scadaId, deviceId = device.id, tagName = tag.name)
+        for tag in device.discreteTagList:
+          payload.deleteTagConfig(scadaId, deviceId = device.id, tagName = tag.name)
+        for tag in device.textTagList:
+          payload.deleteTagConfig(scadaId, deviceId = device.id, tagName = tag.name)
+    return (True, payload.getJson())
+  except Exception as error:
+    print('convert create config fail', str(error))
+    return (False, None)
---
+++ packages/WISE-PaaS-SCADA-Python-SDK/WISE_PaaS_SCADA_Python_SDK-1.0.0-py3-none-any.whl/WISEPaaSSCADA/Model/MQTTMessage.py
@@ -0,0 +1,141 @@
+import json
+import datetime
+import WISEPaaSSCADA.Common.Constants as constant
+
+format = '%Y-%m-%dT%H:%M:%S%z'
+
+class Message(dict):
+  def __init__(self):
+    self.message = {}
+    self.message['d'] = {}
+    self.message['ts'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+  def __repr__(self):
+    return repr(self.__dict__)
+  def getJson(self):
+    return json.dumps(self.message)
+
+class LastWillMessage(Message):
+  def __init__(self):
+    super().__init__()
+    self.message['d'] = {
+      'UeD': 1
+    }  
+
+class DisconnectMessage(Message):
+  def __init__(self):
+    super().__init__()
+    self.message['d'] = {
+      'DsC': 1
+    }
+
+class HeartbeatMessage(Message):
+  def __init__(self):
+    super().__init__()
+    self.message['d'] = {
+      'Hbt': 1
+    }
+    
+class ConnectMessage(Message):
+  def __init__(self):
+    super().__init__()
+    self.message['d'] = {
+      'Con': 1
+    }
+
+class DataMessage(Message):
+  def __init__(self):
+    super().__init__()
+
+  def setTagValue(self, deviceId, tagName, value):
+    if not (deviceId in self.message['d']):
+      self.message['d'][deviceId] = {}
+    self.message['d'][deviceId][tagName] = value
+
+class DeviceStatusMessage(Message):
+  def __init__(self):
+    super().__init__()
+    self.message['d'] = {
+      'Dev': {}
+    }
+  
+  def setDeviceStatus(self, deviceId, status):
+    self.message['d']['Dev'][deviceId] = status
+
+class ConfigMessage(Message):
+  def __init__(self, action = None, scadaId = None):
+    super().__init__()
+    if not action in constant.ActionType.values():
+      raise ValueError('action is invalid')
+    if scadaId is None or scadaId == '':
+      raise ValueError('action is necessary')
+    self.message['d'] = {
+      'Action': action,
+      'Scada': {}
+    }
+    self.message['d']['Scada'][scadaId] = {}
+  
+  def addScadaConfig(self, scadaId, config):
+    _config = {}
+    mapper = constant.ScadaConfigMapper
+    for key in mapper:
+      value = getattr(config, key, None)
+      if not value is None:
+        _config[mapper[key]] = value
+    self.message['d']['Scada'][scadaId] = _config
+
+  def deleteScadaConfig(self, scadaId):
+    self.message['d']['Scada'][scadaId] = {}
+  
+  def addDeviceConfig(self, scadaId, deviceId, config):
+    _config = {}
+    mapper = constant.DeviceConfigMapper
+    for key in mapper:
+      value = getattr(config, key, None)
+      if not value is None:
+        _config[mapper[key]] = value
+
+    if not scadaId in self.message['d']['Scada']:
+      self.message['d']['Scada'][scadaId] = {}
+    if not 'Device' in self.message['d']['Scada'][scadaId]:
+      self.message['d']['Scada'][scadaId]['Device'] = {}
+    self.message['d']['Scada'][scadaId]['Device'][deviceId] = _config
+  
+  def deleteDeviceConfig(self, scadaId, deviceId):
+    if not scadaId in self.message['d']['Scada']:
+      self.message['d']['Scada'][scadaId] = {}
+    if not 'Device' in self.message['d']['Scada'][scadaId]:
+      self.message['d']['Scada'][scadaId]['Device'] = {}
+    self.message['d']['Scada'][scadaId]['Device'][deviceId] = {}
+
+  def addTagConfig(self, scadaId, deviceId, tagName, config):
+    _config = {}
+    mapper = constant.TagConfigMapper
+    boolMapper = ['readOnly']
+    for key in mapper:
+      value = getattr(config, key, None)
+      if not value is None:
+        if key in boolMapper:
+          value = 1 if value == True else 0
+        _config[mapper[key]] = value
+
+    if not scadaId in self.message['d']['Scada']:
+      self.message['d']['Scada'][scadaId] = {}
+    if not 'Device' in self.message['d']['Scada'][scadaId]:
+      self.message['d']['Scada'][scadaId]['Device'] = {}
+    if not deviceId in self.message['d']['Scada'][scadaId]['Device']:
+      self.message['d']['Scada'][scadaId]['Device'][deviceId] = {}
+    if not 'Tag' in self.message['d']['Scada'][scadaId]['Device'][deviceId]:
+      self.message['d']['Scada'][scadaId]['Device'][deviceId]['Tag'] = {}
+    self.message['d']['Scada'][scadaId]['Device'][deviceId]['Tag'][tagName] = _config
+
+  def deleteTagConfig(self, scadaId, deviceId, tagName):
+    if not scadaId in self.message['d']['Scada']:
+      self.message['d']['Scada'][scadaId] = {}
+    if not 'Device' in self.message['d']['Scada'][scadaId]:
+      self.message['d']['Scada'][scadaId]['Device'] = {}
+    if not deviceId in self.message['d']['Scada'][scadaId]['Device']:
+      self.message['d']['Scada'][scadaId]['Device'][deviceId] = {}
+    if not 'Tag' in self.message['d']['Scada'][scadaId]['Device'][deviceId]:
+      self.message['d']['Scada'][scadaId]['Device'][deviceId]['Tag'] = {}
+    self.message['d']['Scada'][scadaId]['Device'][deviceId]['Tag'][tagName] = {}
+
