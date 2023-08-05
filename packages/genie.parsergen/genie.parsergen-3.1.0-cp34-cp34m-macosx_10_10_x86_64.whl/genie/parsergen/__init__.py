a75b4a8bc160554189e2d7fb623aeb69b9e06940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:31 +0000

    Add package genie.parsergen/3.1.0/genie.parsergen-3.1.0-cp34-cp34m-macosx_10_10_x86_64.whl

---
+++ packages/genie.parsergen/genie.parsergen-3.1.0-cp34-cp34m-macosx_10_10_x86_64.whl/genie/parsergen/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = '3.1.0'
+__author__  = """ Myles Dear <mdear@cisco.com>
+                  Karim Mohamed (karmoham) <karmoham@cisco.com>
+              """
+__contact__ = 'pyats-support-ext@cisco.com'
+__copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
+
+from ._parsergen import *
