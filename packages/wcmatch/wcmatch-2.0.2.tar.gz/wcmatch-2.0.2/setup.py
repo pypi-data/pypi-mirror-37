5c397d4ab7e5c18f0b1e473d20bff0b8b1476a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:21 +0000

    Add package wcmatch/2.0.2/wcmatch-2.0.2.tar.gz

---
+++ packages/wcmatch/wcmatch-2.0.2.tar.gz/wcmatch-2.0.2/setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup package."""
+from setuptools import setup, find_packages
+import os
+import imp
+import traceback
+
+
+def get_version():
+    """Get version and version_info without importing the entire module."""
+
+    devstatus = {
+        'alpha': '3 - Alpha',
+        'beta': '4 - Beta',
+        'candidate': '4 - Beta',
+        'final': '5 - Production/Stable'
+    }
+    path = os.path.join(os.path.dirname(__file__), 'wcmatch')
+    fp, pathname, desc = imp.find_module('__version__', [path])
+    try:
+        v = imp.load_module('__version__', fp, pathname, desc)
+        return v.version, devstatus[v.version_info[3]]
+    except Exception:
+        print(traceback.format_exc())
+    finally:
+        fp.close()
+
+
+def get_requirements(req):
+    """Load list of dependencies."""
+
+    install_requires = []
+    with open(req) as f:
+        for line in f:
+            if not line.startswith("#"):
+                install_requires.append(line.strip())
+    return install_requires
+
+
+def get_description():
+    """Get long description."""
+
+    with open("README.md", 'r') as f:
+        desc = f.read()
+    return desc
+
+
+VER, DEVSTATUS = get_version()
+
+setup(
+    name='wcmatch',
+    python_requires=">=3.4",
+    version=VER,
+    keywords='glob fnmatch search wildcard',
+    description='Wildcard/glob file name matcher.',
+    long_description=get_description(),
+    long_description_content_type='text/markdown',
+    author='Isaac Muse',
+    author_email='Isaac.Muse@gmail.com',
+    url='https://github.com/facelessuser/wcmatch',
+    packages=find_packages(exclude=['tests', 'tools']),
+    install_requires=get_requirements("requirements/setup.txt"),
+    license='MIT License',
+    classifiers=[
+        'Development Status :: %s' % DEVSTATUS,
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
