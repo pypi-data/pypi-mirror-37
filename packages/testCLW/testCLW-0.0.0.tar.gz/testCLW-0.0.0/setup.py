27fc27ffe53a41e1f0de6369b9b014356a22c5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package testCLW/0.0.0/testCLW-0.0.0.tar.gz

---
+++ packages/testCLW/testCLW-0.0.0.tar.gz/testCLW-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='testCLW',
+    version='0.0.0',
+    packages=['installTest'],
+    url='',
+    license='hello',
+    author='chenlw_ZCX',
+    author_email='',
+    description=''
+)
