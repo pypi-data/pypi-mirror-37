9f6cdc5a0c6288bff9d3424da5ac59ef55a44ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:27 +0000

    Add package landlab/1.5.4/landlab-1.5.4-cp27-cp27m-win32.whl

---
+++ packages/landlab/landlab-1.5.4-cp27-cp27m-win32.whl/landlab/layers/__init__.py
@@ -0,0 +1,5 @@
+from .eventlayers import EventLayers, EventLayersMixIn
+from .materiallayers import MaterialLayers, MaterialLayersMixIn
+
+__all__ = ['EventLayers', 'EventLayersMixIn',
+           'MaterialLayers', 'MaterialLayersMixIn']
