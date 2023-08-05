067c529470e18b292497aaa5f0d49afcf812edd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:12 +0000

    Add package cbpro2/1.0.2/cbpro2-1.0.2.tar.gz

---
+++ packages/cbpro2/cbpro2-1.0.2.tar.gz/cbpro2-1.0.2/cbpro/__init__.py
@@ -0,0 +1,5 @@
+from cbpro.authenticated_client import AuthenticatedClient
+from cbpro.cbpro_auth import CBProAuth
+from cbpro.order_book import OrderBook
+from cbpro.public_client import PublicClient
+from cbpro.websocket_client import WebsocketClient
