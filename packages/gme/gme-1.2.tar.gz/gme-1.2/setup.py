50cad91ff28697b02eb129c0b7b15c034ebf4eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:35 +0000

    Add package gme/1.2/gme-1.2.tar.gz

---
+++ packages/gme/gme-1.2.tar.gz/gme-1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gme',
+    version='1.2',
+    description='USITC\'s Gravity Modeling Environment',
+    long_description=open('README.txt').read(),
+    keywords='gravity estimation ppml',
+    url='https://gravity.usitc.gov',
+    author='USITC Gravity Modeling Group',
+    author_email='gravity@usitc.gov',
+    license='',
+    packages=find_packages(),
+    install_requires=['pandas','statsmodels','scipy','patsy'],
+    zip_safe=False
+)
