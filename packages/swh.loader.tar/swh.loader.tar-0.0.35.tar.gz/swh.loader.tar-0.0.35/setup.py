cca4c84a5e11e46e2017fe59a69377e36077f46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:05 +0000

    Add package swh.loader.tar/0.0.35/swh.loader.tar-0.0.35.tar.gz

---
+++ packages/swh.loader.tar/swh.loader.tar-0.0.35.tar.gz/swh.loader.tar-0.0.35/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+def parse_requirements():
+    requirements = []
+    for reqf in ('requirements.txt', 'requirements-swh.txt'):
+        with open(reqf) as f:
+            for line in f.readlines():
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                requirements.append(line)
+    return requirements
+
+
+setup(
+    name='swh.loader.tar',
+    description='Software Heritage Tarball Loader',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/DLDTAR',
+    packages=find_packages(),
+    scripts=[],
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)
