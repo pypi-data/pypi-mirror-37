a178c29d20859152322020dead50f88e70e43a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:40 +0000

    Add package raiden/0.13.0/raiden-0.13.0-py3-none-any.whl

---
+++ packages/raiden/raiden-0.13.0-py3-none-any.whl/raiden/routing.py
@@ -0,0 +1,138 @@
+from heapq import heappop, heappush
+from typing import List
+
+import networkx
+import structlog
+
+from raiden.transfer import channel, views
+from raiden.transfer.state import (
+    CHANNEL_STATE_OPENED,
+    NODE_NETWORK_REACHABLE,
+    NODE_NETWORK_UNKNOWN,
+    ChainState,
+    RouteState,
+)
+from raiden.utils import pex, typing
+
+log = structlog.get_logger(__name__)  # pylint: disable=invalid-name
+
+
+def get_ordered_partners(
+        network_graph: networkx.Graph,
+        from_address: typing.Address,
+        to_address: typing.Address,
+) -> List:
+    paths = list()
+
+    try:
+        all_neighbors = networkx.all_neighbors(network_graph, from_address)
+    except networkx.NetworkXError:
+        # If `our_address` is not in the graph, no channels opened with the
+        # address
+        return []
+
+    for neighbor in all_neighbors:
+        try:
+            length = networkx.shortest_path_length(
+                network_graph,
+                neighbor,
+                to_address,
+            )
+            heappush(paths, (length, neighbor))
+        except (networkx.NetworkXNoPath, networkx.NodeNotFound):
+            pass
+
+    return paths
+
+
+def get_best_routes(
+        chain_state: ChainState,
+        token_network_id: typing.Address,
+        from_address: typing.Address,
+        to_address: typing.Address,
+        amount: int,
+        previous_address: typing.Address,
+) -> List[RouteState]:
+    """ Returns a list of channels that can be used to make a transfer.
+
+    This will filter out channels that are not open and don't have enough
+    capacity.
+    """
+    # TODO: Route ranking.
+    # Rate each route to optimize the fee price/quality of each route and add a
+    # rate from in the range [0.0,1.0].
+
+    available_routes = list()
+
+    token_network = views.get_token_network_by_identifier(
+        chain_state,
+        token_network_id,
+    )
+
+    network_statuses = views.get_networkstatuses(chain_state)
+
+    neighbors_heap = get_ordered_partners(
+        token_network.network_graph.network,
+        from_address,
+        to_address,
+    )
+
+    if not neighbors_heap:
+        log.warning(
+            'No routes available',
+            from_address=pex(from_address),
+            to_address=pex(to_address),
+        )
+
+    while neighbors_heap:
+        _, partner_address = heappop(neighbors_heap)
+
+        channel_state = views.get_channelstate_by_token_network_and_partner(
+            chain_state,
+            token_network_id,
+            partner_address,
+        )
+
+        # don't send the message backwards
+        if partner_address == previous_address:
+            continue
+
+        assert channel_state is not None
+
+        if channel.get_status(channel_state) != CHANNEL_STATE_OPENED:
+            log.info(
+                'channel is not opened, ignoring',
+                from_address=pex(from_address),
+                partner_address=pex(partner_address),
+            )
+            continue
+
+        distributable = channel.get_distributable(
+            channel_state.our_state,
+            channel_state.partner_state,
+        )
+
+        if amount > distributable:
+            log.info(
+                'channel doesnt have enough funds, ignoring',
+                from_address=pex(from_address),
+                partner_address=pex(partner_address),
+                amount=amount,
+                distributable=distributable,
+            )
+            continue
+
+        network_state = network_statuses.get(partner_address, NODE_NETWORK_UNKNOWN)
+        if network_state != NODE_NETWORK_REACHABLE:
+            log.info(
+                'partner for channel state isn\'t reachable, ignoring',
+                from_address=pex(from_address),
+                partner_address=pex(partner_address),
+                status=network_state,
+            )
+            continue
+
+        route_state = RouteState(partner_address, channel_state.identifier)
+        available_routes.append(route_state)
+
+    return available_routes
---
+++ packages/raiden/raiden-0.13.0-py3-none-any.whl/raiden/tests/integration/fixtures/raiden_network.py
@@ -0,0 +1,199 @@
+import gevent
+import pytest
+
+from raiden.tests.utils.network import (
+    CHAIN,
+    create_apps,
+    create_network_channels,
+    create_sequential_channels,
+    payment_channel_open_and_deposit,
+    wait_for_alarm_start,
+    wait_for_channels,
+)
+from raiden.tests.utils.tests import shutdown_apps_and_cleanup_tasks
+
+
+@pytest.fixture
+def raiden_chain(
+        token_addresses,
+        token_network_registry_address,
+        channels_per_node,
+        deposit,
+        settle_timeout,
+        chain_id,
+        blockchain_services,
+        endpoint_discovery_services,
+        raiden_udp_ports,
+        reveal_timeout,
+        database_paths,
+        retry_interval,
+        retries_before_backoff,
+        throttle_capacity,
+        throttle_fill_rate,
+        nat_invitation_timeout,
+        nat_keepalive_retries,
+        nat_keepalive_timeout,
+        network_type,
+        local_matrix_server,
+        private_rooms,
+):
+
+    if len(token_addresses) != 1:
+        raise ValueError('raiden_chain only works with a single token')
+
+    assert channels_per_node in (0, 1, 2, CHAIN), (
+        'deployed_network uses create_sequential_network that can only work '
+        'with 0, 1 or 2 channels'
+    )
+
+    raiden_apps = create_apps(
+        chain_id=chain_id,
+        blockchain_services=blockchain_services.blockchain_services,
+        endpoint_discovery_services=endpoint_discovery_services,
+        token_network_registry_address=token_network_registry_address,
+        secret_registry_address=blockchain_services.secret_registry.address,
+        raiden_udp_ports=raiden_udp_ports,
+        reveal_timeout=reveal_timeout,
+        settle_timeout=settle_timeout,
+        database_paths=database_paths,
+        retry_interval=retry_interval,
+        retries_before_backoff=retries_before_backoff,
+        throttle_capacity=throttle_capacity,
+        throttle_fill_rate=throttle_fill_rate,
+        nat_invitation_timeout=nat_invitation_timeout,
+        nat_keepalive_retries=nat_keepalive_retries,
+        nat_keepalive_timeout=nat_keepalive_timeout,
+        network_type=network_type,
+        local_matrix_url=local_matrix_server,
+        private_rooms=private_rooms,
+    )
+
+    start_tasks = [gevent.spawn(app.raiden.start) for app in raiden_apps]
+    gevent.joinall(start_tasks, raise_error=True)
+
+    from_block = 0
+    for app in raiden_apps:
+        app.raiden.install_all_blockchain_filters(
+            app.raiden.default_registry,
+            app.raiden.default_secret_registry,
+            from_block,
+        )
+
+    app_channels = create_sequential_channels(
+        raiden_apps,
+        channels_per_node,
+    )
+
+    channel_greenlets = []
+    for token_address in token_addresses:
+        for app_pair in app_channels:
+            channel_greenlets.append(gevent.spawn(
+                payment_channel_open_and_deposit,
+                app_pair[0],
+                app_pair[1],
+                token_address,
+                deposit,
+                settle_timeout,
+            ))
+    gevent.joinall(channel_greenlets, raise_error=True)
+
+    exception = RuntimeError('`raiden_chain` fixture setup failed, nodes are unreachable')
+    with gevent.Timeout(seconds=30, exception=exception):
+        wait_for_channels(
+            app_channels,
+            blockchain_services.deploy_registry.address,
+            token_addresses,
+            deposit,
+        )
+
+    yield raiden_apps
+
+    shutdown_apps_and_cleanup_tasks(raiden_apps)
+
+
+@pytest.fixture
+def raiden_network(
+        token_addresses,
+        token_network_registry_address,
+        channels_per_node,
+        deposit,
+        settle_timeout,
+        chain_id,
+        blockchain_services,
+        endpoint_discovery_services,
+        raiden_udp_ports,
+        reveal_timeout,
+        database_paths,
+        retry_interval,
+        retries_before_backoff,
+        throttle_capacity,
+        throttle_fill_rate,
+        nat_invitation_timeout,
+        nat_keepalive_retries,
+        nat_keepalive_timeout,
+        network_type,
+        local_matrix_server,
+        private_rooms,
+):
+
+    raiden_apps = create_apps(
+        chain_id=chain_id,
+        blockchain_services=blockchain_services.blockchain_services,
+        endpoint_discovery_services=endpoint_discovery_services,
+        token_network_registry_address=token_network_registry_address,
+        secret_registry_address=blockchain_services.secret_registry.address,
+        raiden_udp_ports=raiden_udp_ports,
+        reveal_timeout=reveal_timeout,
+        settle_timeout=settle_timeout,
+        database_paths=database_paths,
+        retry_interval=retry_interval,
+        retries_before_backoff=retries_before_backoff,
+        throttle_capacity=throttle_capacity,
+        throttle_fill_rate=throttle_fill_rate,
+        nat_invitation_timeout=nat_invitation_timeout,
+        nat_keepalive_retries=nat_keepalive_retries,
+        nat_keepalive_timeout=nat_keepalive_timeout,
+        network_type=network_type,
+        local_matrix_url=local_matrix_server,
+        private_rooms=private_rooms,
+    )
+
+    start_tasks = [gevent.spawn(app.raiden.start) for app in raiden_apps]
+    gevent.joinall(start_tasks, raise_error=True)
+
+    app_channels = create_network_channels(
+        raiden_apps,
+        channels_per_node,
+    )
+
+    greenlets = []
+    for token_address in token_addresses:
+        for app_pair in app_channels:
+            greenlets.append(gevent.spawn(
+                payment_channel_open_and_deposit,
+                app_pair[0],
+                app_pair[1],
+                token_address,
+                deposit,
+                settle_timeout,
+            ))
+    gevent.joinall(greenlets, raise_error=True)
+
+    exception = RuntimeError('`raiden_network` fixture setup failed, nodes are unreachable')
+    with gevent.Timeout(seconds=30, exception=exception):
+        wait_for_channels(
+            app_channels,
+            blockchain_services.deploy_registry.address,
+            token_addresses,
+            deposit,
+        )
+
+    # Force blocknumber update
+    exception = RuntimeError('Alarm failed to start and set up start_block correctly')
+
+    with gevent.Timeout(seconds=5, exception=exception):
+        wait_for_alarm_start(raiden_apps)
+
+    yield raiden_apps
+
+    shutdown_apps_and_cleanup_tasks(raiden_apps)
