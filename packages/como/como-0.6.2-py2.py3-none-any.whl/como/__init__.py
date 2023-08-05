76dd5f276679bdc8bc30b04ce5df5cccf1ed92f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:53 +0000

    Add package como/0.6.2/como-0.6.2-py2.py3-none-any.whl

---
+++ packages/como/como-0.6.2-py2.py3-none-any.whl/como/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+como: batteries complete
+"""
+
+__title__ = 'como'
+__author__ = 'cwoebker'
+__version__ = '0.6.2'
+__license__ = 'BSD'
+__copyright__ = '© 2012-2018 Cecil Wöbker'
