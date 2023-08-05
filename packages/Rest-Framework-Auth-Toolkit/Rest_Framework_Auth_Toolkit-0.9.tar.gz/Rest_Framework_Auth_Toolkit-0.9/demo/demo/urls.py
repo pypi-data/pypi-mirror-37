85de32438e17cb38d1752689475c5aaafcecd27d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package Rest-Framework-Auth-Toolkit/0.9/Rest_Framework_Auth_Toolkit-0.9.tar.gz

---
+++ packages/Rest-Framework-Auth-Toolkit/Rest_Framework_Auth_Toolkit-0.9.tar.gz/Rest_Framework_Auth_Toolkit-0.9/demo/demo/urls.py
@@ -0,0 +1,26 @@
+"""demo URL Configuration."""
+
+from django.urls import include, path
+from django.contrib import admin
+
+from rest_auth_toolkit.views import FacebookLoginView, LoginView, LogoutView, SignupView
+
+
+auth_urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('fb-login/', FacebookLoginView.as_view(), name='fb-login'),
+]
+
+api_urlpatterns = [
+    path('account/', include('demo.accounts.urls')),
+    path('', include((auth_urlpatterns, 'auth'))),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(api_urlpatterns)),
+    path('', include('demo.pages.urls')),
+    path('', include('demo.pages.auth_urls')),
+]
