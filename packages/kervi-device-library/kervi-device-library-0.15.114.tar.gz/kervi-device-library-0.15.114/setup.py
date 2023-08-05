c429fe12e9aacc714dda901ffbb69d2b745d7233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:52 +0000

    Add package kervi-device-library/0.15.114/kervi-device-library-0.15.114.tar.gz

---
+++ packages/kervi-device-library/kervi-device-library-0.15.114.tar.gz/kervi-device-library-0.15.114/setup.py
@@ -0,0 +1,39 @@
+import distutils
+from setuptools import setup
+
+import sys
+
+VERSION = "0.0.0"
+if '--version' in sys.argv:
+    index = sys.argv.index('--version')
+    sys.argv.pop(index)
+    VERSION = sys.argv.pop(index)
+
+
+try:
+    distutils.dir_util.remove_tree("dist")
+except:
+    pass
+
+setup(
+    name='kervi-device-library',
+    version=VERSION,
+    author='Tim Wentzlau',
+    author_email='tim.wentzlau@gmail.com',
+    url='https://github.com/kervi/kervi-components',
+    description="""Hardware component library for the Kervi automation framework""",
+    packages=[
+        "kervi/devices",
+        "kervi/devices/displays",
+        "kervi/devices/gpio",
+        "kervi/devices/motors",
+        #"kervi/devices/platforms",
+        #"kervi/devices/platforms/common",
+        #"kervi/devices/platforms/common/sensors",
+        "kervi/devices/pwm",
+        "kervi/devices/sensors"
+    ],
+    install_requires=[
+        'psutil'
+    ]
+)
