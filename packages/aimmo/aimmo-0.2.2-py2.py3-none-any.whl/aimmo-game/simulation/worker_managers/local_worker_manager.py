f338440b8932318fc1fe3e62a0d355ab35e29a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:05 +0000

    Add package aimmo/0.2.2/aimmo-0.2.2-py2.py3-none-any.whl

---
+++ packages/aimmo/aimmo-0.2.2-py2.py3-none-any.whl/aimmo-game/simulation/worker_managers/local_worker_manager.py
@@ -0,0 +1,47 @@
+import atexit
+import itertools
+import logging
+import os
+import subprocess
+
+from .worker_manager import WorkerManager
+
+LOGGER = logging.getLogger(__name__)
+
+
+class LocalWorkerManager(WorkerManager):
+    """Relies on them already being created already."""
+
+    host = '127.0.0.1'
+    worker_directory = os.path.join(
+        os.path.dirname(__file__),
+        '../../../aimmo-game-worker/',
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.workers = {}
+        game_id = os.environ['GAME_ID']
+        self.port_counter = itertools.count(1989 + int(game_id) * 10000)
+        super(LocalWorkerManager, self).__init__(*args, **kwargs)
+
+    def create_worker(self, player_id):
+        assert(player_id not in self.workers)
+        port = self.port_counter.next()
+
+        data_url = 'http://{}:{}/player/{}'.format(self.host, self.port, player_id)
+
+        process = subprocess.Popen(['python', 'service.py', self.host, str(port), data_url],
+                                   cwd=self.worker_directory)
+        atexit.register(process.kill)
+        self.workers[player_id] = process
+        worker_url = 'http://%s:%d' % (
+            self.host,
+            port,
+        )
+        LOGGER.info("Worker started for %s, listening at %s", player_id, worker_url)
+        return worker_url
+
+    def remove_worker(self, player_id):
+        if player_id in self.workers:
+            self.workers[player_id].kill()
+            del self.workers[player_id]
