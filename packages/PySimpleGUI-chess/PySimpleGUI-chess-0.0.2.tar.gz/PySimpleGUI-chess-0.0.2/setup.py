6aec38949fc87887b033082c676115a98074eb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:52 +0000

    Add package PySimpleGUI-chess/0.0.2/PySimpleGUI-chess-0.0.2.tar.gz

---
+++ packages/PySimpleGUI-chess/PySimpleGUI-chess-0.0.2.tar.gz/PySimpleGUI-chess-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PySimpleGUI-chess",
+    version="0.0.2",
+    author="PySimpleGUI.org",
+    author_email="info@PySimpleGUI.org",
+    description="A PGN file playback of chess games",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['python-chess', 'pysimplegui'],
+    package_data={
+        'PySimpleGUI-chess': ['*.png', '*.pgn'],
+                },
+    url="https://github.com/MikeTheWatchGuy/PySimpleGUI/tree/master/Chess",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
