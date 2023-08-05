af5457c6542849fa44590f1127ff4de9697bc72f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:53 +0000

    Add package slack-actions/0.2.1/slack-actions-0.2.1.tar.gz

---
+++ packages/slack-actions/slack-actions-0.2.1.tar.gz/slack-actions-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='slack-actions',
+    packages=['slack_actions'],
+    version='0.2.1',
+    description='Build custom slackbots with ease',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Eddy Hintze',
+    author_email="eddy@hintze.co",
+    url="https://github.com/xtream1101/slack-actions",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
+    install_requires=['slackclient',
+                      'falcon',
+                      'gunicorn',
+                      ],
+)
