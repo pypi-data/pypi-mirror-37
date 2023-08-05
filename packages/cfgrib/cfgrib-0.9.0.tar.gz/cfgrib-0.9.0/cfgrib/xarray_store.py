dd5fb0a80145a537d2efa6472e93a2a4ceb09caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:16 +0000

    Add package cfgrib/0.9.0/cfgrib-0.9.0.tar.gz

---
+++ packages/cfgrib/cfgrib-0.9.0.tar.gz/cfgrib-0.9.0/cfgrib/xarray_store.py
@@ -0,0 +1,72 @@
+#
+# Copyright 2017-2018 European Centre for Medium-Range Weather Forecasts (ECMWF).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors:
+#   Alessandro Amici - B-Open - https://bopen.eu
+#
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+import typing as T  # noqa
+import warnings
+
+from xarray.backends import api
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+def open_dataset(path, flavour_name='ecmwf', filter_by_keys={}, errors='ignore', **kwargs):
+    # type: (str, str, T.Mapping[str, T.Any], str, T.Any) -> xr.Dataset
+    """
+    Return a ``xr.Dataset`` with the requested ``flavor`` from a GRIB file.
+    """
+    # validate Dataset keys, DataArray names, and attr keys/values
+    from . import cfgrib_
+    overrides = {
+        'flavour_name': flavour_name,
+        'filter_by_keys': filter_by_keys,
+        'errors': errors,
+    }
+    for k in list(kwargs):  # copy to allow the .pop()
+        if k.startswith('encode_'):
+            overrides[k] = kwargs.pop(k)
+    store = cfgrib_.CfGribDataStore(path, **overrides)
+    return api.open_dataset(store, **kwargs)
+
+
+def open_datasets(path, flavour_name='ecmwf', filter_by_keys={}, no_warn=False, **kwargs):
+    # type: (str, str, T.Dict[str, T.Any], bool, T.Any) -> T.List[xr.Dataset]
+    """
+    Open a GRIB file groupping incompatible hypercubes to different datasets via simple heuristics.
+    """
+    import cfgrib
+
+    if not no_warn:
+        warnings.warn("open_datasets is an experimental API, DO NOT RELY ON IT!", FutureWarning)
+
+    fbks = []
+    datasets = []
+    try:
+        datasets.append(open_dataset(path, flavour_name, filter_by_keys, **kwargs))
+    except cfgrib.DatasetBuildError as ex:
+        fbks.extend(ex.args[1])
+    # NOTE: the recursive call needs to stay out of the exception handler to avoid showing
+    #   to the user a confusing error message due to exception chaining
+    # OPTIMIZE: we need a way to cache the index
+    for fbk in fbks:
+        datasets.extend(open_datasets(path, flavour_name, fbk, no_warn=True, **kwargs))
+    return datasets
---
+++ packages/cfgrib/cfgrib-0.9.0.tar.gz/cfgrib-0.9.0/tests/test_30_dataset.py
@@ -0,0 +1,112 @@
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os.path
+
+import pytest
+import numpy as np
+
+from cfgrib import cfmessage
+from cfgrib import messages
+from cfgrib import dataset
+
+SAMPLE_DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'sample-data')
+TEST_DATA = os.path.join(SAMPLE_DATA_FOLDER, 'era5-levels-members.grib')
+
+
+def test_dict_merge():
+    master = {'one': 1}
+    dataset.dict_merge(master, {'two': 2})
+    assert master == {'one': 1, 'two': 2}
+    dataset.dict_merge(master, {'two': 2})
+    assert master == {'one': 1, 'two': 2}
+
+    with pytest.raises(dataset.DatasetBuildError):
+        dataset.dict_merge(master, {'two': 3})
+
+
+def test_build_data_var_components_no_encode():
+    index = messages.FileStream(path=TEST_DATA).index(dataset.ALL_KEYS).subindex(paramId=130)
+    dims, data_var, coord_vars = dataset.build_data_var_components(index=index)
+    assert dims == {'number': 10, 'dataDate': 2, 'dataTime': 2, 'level': 2, 'i': 7320}
+    assert data_var.data.shape == (10, 2, 2, 2, 7320)
+
+    # equivalent to not np.isnan without importing numpy
+    assert data_var.data[:, :, :, :, :].mean() > 0.
+
+
+def test_build_data_var_components_encode_geography():
+    stream = messages.FileStream(path=TEST_DATA, message_class=cfmessage.CfMessage)
+    index = stream.index(dataset.ALL_KEYS).subindex(paramId=130)
+    dims, data_var, coord_vars = dataset.build_data_var_components(
+        index=index, encode_geography=True,
+    )
+    assert dims == {
+        'number': 10, 'dataDate': 2, 'dataTime': 2,
+        'level': 2, 'latitude': 61, 'longitude': 120,
+    }
+    assert data_var.data.shape == (10, 2, 2, 2, 61, 120)
+
+    # equivalent to not np.isnan without importing numpy
+    assert data_var.data[:, :, :, :, :, :].mean() > 0.
+
+
+def test_Dataset():
+    res = dataset.Dataset.from_path(TEST_DATA)
+    assert 'history' in res.attributes
+    assert res.attributes['GRIB_edition'] == 1
+    assert tuple(res.dimensions.keys()) == \
+        ('number', 'time', 'air_pressure', 'latitude', 'longitude')
+    assert len(res.variables) == 9
+
+
+def test_Dataset_no_encode():
+    res = dataset.Dataset.from_path(
+        TEST_DATA, encode_time=False, encode_vertical=False, encode_geography=False,
+    )
+    assert 'history' in res.attributes
+    assert res.attributes['GRIB_edition'] == 1
+    assert tuple(res.dimensions.keys()) == ('number', 'dataDate', 'dataTime', 'level', 'i')
+    assert len(res.variables) == 9
+
+
+def test_Dataset_encode_time():
+    res = dataset.Dataset.from_path(TEST_DATA, encode_vertical=False, encode_geography=False)
+    assert 'history' in res.attributes
+    assert res.attributes['GRIB_edition'] == 1
+    assert tuple(res.dimensions.keys()) == ('number', 'time', 'level', 'i')
+    assert len(res.variables) == 9
+
+    # equivalent to not np.isnan without importing numpy
+    assert res.variables['t'].data[:, :, :, :].mean() > 0.
+
+
+def test_Dataset_encode_geography():
+    res = dataset.Dataset.from_path(TEST_DATA, encode_time=False, encode_vertical=False)
+    assert 'history' in res.attributes
+    assert res.attributes['GRIB_edition'] == 1
+    assert tuple(res.dimensions.keys()) == \
+        ('number', 'dataDate', 'dataTime', 'level', 'latitude', 'longitude')
+    assert len(res.variables) == 9
+
+    # equivalent to not np.isnan without importing numpy
+    assert res.variables['t'].data[:, :, :, :, :, :].mean() > 0.
+
+
+def test_Dataset_encode_vertical():
+    res = dataset.Dataset.from_path(TEST_DATA, encode_time=False, encode_geography=False)
+    assert 'history' in res.attributes
+    assert res.attributes['GRIB_edition'] == 1
+    assert tuple(res.dimensions.keys()) == ('number', 'dataDate', 'dataTime', 'air_pressure', 'i')
+    assert len(res.variables) == 9
+
+    # equivalent to not np.isnan without importing numpy
+    assert res.variables['t'].data[:, :, :, :, :].mean() > 0.
+
+
+def test_Dataset_reguler_gg_surface():
+    path = os.path.join(SAMPLE_DATA_FOLDER, 'regular_gg_sfc.grib')
+    res = dataset.Dataset.from_path(path)
+
+    assert res.dimensions == {'latitude': 96, 'longitude': 192}
+    assert np.allclose(res.variables['latitude'].data[:2], [88.57216851, 86.72253095])
