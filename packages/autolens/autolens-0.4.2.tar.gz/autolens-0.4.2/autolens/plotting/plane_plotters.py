f5dce3768accc3969a1e44b8e8344a407f885c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package autolens/0.4.2/autolens-0.4.2.tar.gz

---
+++ packages/autolens/autolens-0.4.2.tar.gz/autolens-0.4.2/autolens/plotting/plane_plotters.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+from autolens import conf
+from autolens import exc
+from autolens.plotting import array_plotters
+
+def plot_plane_grid(plane, xmin=None, xmax=None, ymin=None, ymax=None):
+
+    plt.figure(figsize=(12, 8))
+    plt.scatter(y=plane.grids.image[:, 0], x=plane.grids.image[:, 1], marker='.', s=1)
+    array_plotters.set_title(title='Plane Grid', titlesize=36)
+    plt.xlabel('x (arcsec)', fontsize=20)
+    plt.ylabel('y (arcsec)', fontsize=20)
+    plt.tick_params(labelsize=20)
+    if xmin is not None and xmax is not None and ymin is not None and ymax is not None:
+        plt.axis([xmin, xmax, ymin, ymax])
+    plt.show()
+    plt.close()
---
+++ packages/autolens/autolens-0.4.2.tar.gz/autolens-0.4.2/autolens/plotting/profile_plotters.py
@@ -0,0 +1,81 @@
+from matplotlib import pyplot as plt
+
+from autolens import conf
+from autolens.plotting import array_plotters
+
+def plot_intensities(light_profile, grid, output_path=None, output_filename='intensities', output_format='show'):
+
+    intensities = light_profile.intensities_from_grid(grid=grid)
+    intensities = grid.scaled_array_from_array_1d(intensities)
+
+    array_plotters.plot_array(
+        array=intensities, points=None, grid=None, as_subplot=False,
+        units='arcsec', kpc_per_arcsec=None,
+        xticks=intensities.xticks, yticks=intensities.yticks, xyticksize=16,
+        norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
+        figsize=None, aspect='auto', cmap='jet', cb_ticksize=16,
+        title='Intensities', titlesize=16, xlabelsize=16, ylabelsize=16,
+        output_path=output_path, output_filename=output_filename, output_format=output_format)
+
+def plot_surface_density(mass_profile, grid, output_path=None, output_filename='surface_density', output_format='show'):
+
+    surface_density = mass_profile.surface_density_from_grid(grid=grid)
+    surface_density = grid.scaled_array_from_array_1d(surface_density)
+
+    array_plotters.plot_array(
+        array=surface_density, points=None, grid=None, as_subplot=False,
+        units='arcsec', kpc_per_arcsec=None,
+        xticks=surface_density.xticks, yticks=surface_density.yticks, xyticksize=16,
+        norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
+        figsize=None, aspect='auto', cmap='jet', cb_ticksize=16,
+        title='Surface density', titlesize=16, xlabelsize=16, ylabelsize=16,
+        output_path=output_path, output_filename=output_filename, output_format=output_format)
+    
+def plot_potential(mass_profile, grid, output_path=None, output_filename='potential', output_format='show'):
+
+    potential = mass_profile.potential_from_grid(grid=grid)
+    potential = grid.scaled_array_from_array_1d(potential)
+
+    array_plotters.plot_array(
+        array=potential, points=None, grid=None, as_subplot=False,
+        units='arcsec', kpc_per_arcsec=None,
+        xticks=potential.xticks, yticks=potential.yticks, xyticksize=16,
+        norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
+        figsize=None, aspect='auto', cmap='jet', cb_ticksize=16,
+        title='Potential', titlesize=16, xlabelsize=16, ylabelsize=16,
+        output_path=output_path, output_filename=output_filename, output_format=output_format)
+
+def plot_deflections(mass_profile, grid, output_path=None, output_filename='deflections',
+                     output_format='show'):
+
+    deflections = mass_profile.deflections_from_grid(grid)
+
+    deflections_y = grid.scaled_array_from_array_1d(deflections[:,0])
+    deflections_x = grid.scaled_array_from_array_1d(deflections[:,1])
+
+    plt.figure(figsize=(14, 6))
+    plt.subplot(1, 2, 1)
+
+    array_plotters.plot_array(
+        array=deflections_y, points=None, grid=None, as_subplot=True,
+        units='arcsec', kpc_per_arcsec=None,
+        xticks=deflections_y.xticks, yticks=deflections_y.yticks, xyticksize=16,
+        norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
+        figsize=None, aspect='auto', cmap='jet', cb_ticksize=16,
+        title='Deflection angles (y)', titlesize=16, xlabelsize=16, ylabelsize=16,
+        output_path=output_path, output_filename=None, output_format=output_format)
+
+    plt.subplot(1, 2, 2)
+
+    array_plotters.plot_array(
+        array=deflections_x, points=None, grid=None, as_subplot=True,
+        units='arcsec', kpc_per_arcsec=None,
+        xticks=deflections_x.xticks, yticks=deflections_x.yticks, xyticksize=16,
+        norm='linear', norm_min=None, norm_max=None, linthresh=0.05, linscale=0.01,
+        figsize=None, aspect='auto', cmap='jet', cb_ticksize=16,
+        title='Deflection angles (x)', titlesize=16, xlabelsize=16, ylabelsize=16,
+        output_path=output_path, output_filename=None, output_format=output_format)
+
+    array_plotters.output_subplot_array(output_path=output_path, output_filename=output_filename,
+                                        output_format=output_format)
+    plt.close()
