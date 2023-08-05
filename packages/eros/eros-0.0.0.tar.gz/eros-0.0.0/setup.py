56cd3974eeb0e34a9c397ac8e6c11da120982607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:01 +0000

    Add package eros/0.0.0/eros-0.0.0.tar.gz

---
+++ packages/eros/eros-0.0.0.tar.gz/eros-0.0.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='eros',
+      version='0.0.0',
+      description='A plarform for reproducible deep learning and machine learning at scale.',
+      maintainer='Polyaxon',
+      maintainer_email='contact@polyaxon.com',
+      author='Polyaxon',
+      author_email='contact@polyaxon.com',
+      url='https://github.com/polyaxon/plutus',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+      ],
+      install_requires=[
+          'six==1.11.0',
+      ],
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Internet :: WWW/HTTP',
+      ],
+      tests_require=[
+          "pytest",
+      ],
+      cmdclass={'test': PyTest})
