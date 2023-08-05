e86eaae0e335a47acd1458a14a00bfb7f31b2e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:11 +0000

    Add package dsnd-distributions-gw/0.1/dsnd_distributions_gw-0.1.tar.gz

---
+++ packages/dsnd-distributions-gw/dsnd_distributions_gw-0.1.tar.gz/dsnd_distributions_gw-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_gw',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='Geoffrey Wong',
+      author_email='geoffreycswong@outlook.com',
+      zip_safe=False)
