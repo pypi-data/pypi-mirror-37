327bad61a78c8e403b10473161f79cd21f06cb55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:36 +0000

    Add package mds-timesheet-invoiced/4.6.1/mds_timesheet_invoiced-4.6.1-py3-none-any.whl

---
+++ packages/mds-timesheet-invoiced/mds_timesheet_invoiced-4.6.1-py3-none-any.whl/trytond/modules/timesheet_invoiced/__init__.py
@@ -0,0 +1,10 @@
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.pool import Pool
+from .timesheet import TimesheetLine
+
+def register():
+    Pool.register(
+        TimesheetLine,
+        module='timesheet_invoiced', type_='model')
