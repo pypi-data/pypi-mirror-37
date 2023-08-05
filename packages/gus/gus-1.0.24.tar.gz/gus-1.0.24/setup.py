d65f53c97058faa32a58d8dd33d2d7c9fdcff1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:24 +0000

    Add package gus/1.0.24/gus-1.0.24.tar.gz

---
+++ packages/gus/gus-1.0.24.tar.gz/gus-1.0.24/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+ver = '1.0.24'
+setup(
+    name='gus',
+    packages=['gus'],  # this must be the same as the name above
+    version=ver,
+    description='a python2 git utils',
+    author='chang',
+    author_email='zhchang@gmail.com',
+    url='https://github.com/zhchang/gutils',  # use the URL to the github repo
+    download_url='https://github.com/zhchang/gutils/tarball/' + ver,
+    keywords=['python', 'git', 'util'],  # arbitrary keywords
+    scripts=['scripts/gus'],
+    install_requires=[
+          'cliapp>=1.0.9',
+      ],
+    classifiers=[],
+)
