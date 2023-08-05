9ece993fc3a4f436e2aa0d795904d1b49c781276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:43 +0000

    Add package fonduer/0.3.4/fonduer-0.3.4.tar.gz

---
+++ packages/fonduer/fonduer-0.3.4.tar.gz/fonduer-0.3.4/src/fonduer/learning/__init__.py
@@ -0,0 +1,7 @@
+from fonduer.learning.disc_models.logistic_regression import LogisticRegression
+from fonduer.learning.disc_models.lstm import LSTM
+from fonduer.learning.disc_models.sparse_logistic_regression import (
+    SparseLogisticRegression,
+)
+
+__all__ = ["LogisticRegression", "LSTM", "SparseLogisticRegression"]
