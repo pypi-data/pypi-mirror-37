4f28df52b5e2be6b6ef7b53d7ea8a20bd2401d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:33 +0000

    Add package pysimplegui-exemaker/1.0.0/pysimplegui-exemaker-1.0.0.tar.gz

---
+++ packages/pysimplegui-exemaker/pysimplegui-exemaker-1.0.0.tar.gz/pysimplegui-exemaker-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysimplegui-exemaker",
+    version="1.0.0",
+    author="PySimpleGUI.org",
+    author_email="info@PySimpleGUI.org",
+    description="A GUI Front-end to PyInstaller",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['PyInstaller', 'pysimplegui'],
+    url="https://github.com/MikeTheWatchGuy/PySimpleGUI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows ",
+    ],
+)
