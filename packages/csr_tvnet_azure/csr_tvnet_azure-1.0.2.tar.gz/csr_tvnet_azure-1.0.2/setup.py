740b2fc5d6d6a10689f65a07dee44b8cd8178c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:35 +0000

    Add package csr_tvnet_azure/1.0.2/csr_tvnet_azure-1.0.2.tar.gz

---
+++ packages/csr_tvnet_azure/csr_tvnet_azure-1.0.2.tar.gz/csr_tvnet_azure-1.0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+project_name = 'csr_tvnet_azure'
+project_ver = '1.0.2'
+setup(
+    name=project_name,
+    packages=[],
+    version=project_ver,
+    description='Wrapper pypi package to install csr_tvnet and csr_azure_utils pypi packages',
+    author='Saravanakumar Periyaswamy',
+    author_email='sarperiy@cisco.com',
+    # use the URL to the github repo
+    url='https://github4-chn.cisco.com/csr1000v-azure/' + project_name,
+    download_url='https://github4-chn.cisco.com/csr1000v-azure/' + project_name + '/archive/' + \
+        project_ver + '.tar.gz',
+    keywords=['cisco', 'azure', 'guestshell', 'csr1000v'],
+    classifiers=[],
+    license="MIT",
+    install_requires=[
+        'azure-storage-file==1.1.0',
+        'csr_azure_utils==1.0.0',
+        'csr_tvnet==1.0.1',
+        'paramiko'
+    ]
+)
+
