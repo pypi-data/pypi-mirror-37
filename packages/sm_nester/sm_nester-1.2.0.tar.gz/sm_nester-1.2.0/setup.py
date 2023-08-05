2a941680d5aaa43c30d4aa718598bc1415980713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:54 +0000

    Add package sm_nester/1.2.0/sm_nester-1.2.0.tar.gz

---
+++ packages/sm_nester/sm_nester-1.2.0.tar.gz/sm_nester-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name            = 'sm_nester',
+    
+    version         = '1.2.0',
+    
+    py_modules      = ['sm_nester'],
+    
+    author          = 'Silencemoment',
+    
+    author_email    = '532088799@qq.com',
+    
+    
+    description     = 'A simple printer of sm_nested lists',
+    
+    )
