2fa1c12f6706dc4c06d4ac4b742c046b8dab4000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:58 +0000

    Add package primordial/0.0.11/primordial-0.0.11.tar.gz

---
+++ packages/primordial/primordial-0.0.11.tar.gz/primordial-0.0.11/primordial/equations/inflation.py
@@ -0,0 +1,28 @@
+""" Base classes for inflationary solvers"""
+import numpy
+from primordial.equations.equations import Equations as _Equations
+
+class Equations(_Equations):
+
+    def __init__(self, K, potential):
+        super(Equations, self).__init__()
+        self.potential = potential
+        self.K = K
+
+    def H(self, t, y):
+        """ Hubble parameter"""
+        return numpy.sqrt(self.H2(t, y))
+
+    def V(self, t, y):
+        """ Potential """
+        return self.potential(self.phi(t, y))
+
+    def dVdphi(self, t, y):
+        """ Potential derivative """
+        return self.potential.d(self.phi(t, y))
+
+    def sol(self, sol, **kwargs):
+        """ Post-process solution of solve_ivp """
+        sol.H = self.interp1d(sol.t, self.H(sol.t, sol.y), **kwargs)
+        sol = super(Equations, self).sol(sol, **kwargs)
+        return sol
---
+++ packages/primordial/primordial-0.0.11.tar.gz/primordial-0.0.11/primordial/equations/inflation_potentials.py
@@ -0,0 +1,15 @@
+class Potential(object):
+    pass
+
+class ChaoticPotential(Potential):
+    def __init__(self, m=1):
+        self.m = m
+
+    def __call__(self, phi):
+        return self.m**2*phi**2/2.
+
+    def d(self, phi):
+        return self.m**2*phi
+
+    def dd(self, phi):
+        return self.m**2
