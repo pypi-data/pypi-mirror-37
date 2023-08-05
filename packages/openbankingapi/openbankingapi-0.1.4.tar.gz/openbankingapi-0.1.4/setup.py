1085ce714d76a2e5356fbc59cc87cd3cad98b2b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:49 +0000

    Add package openbankingapi/0.1.4/openbankingapi-0.1.4.tar.gz

---
+++ packages/openbankingapi/openbankingapi-0.1.4.tar.gz/openbankingapi-0.1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='openbankingapi',
+    version='0.1.4',
+    description='Simple wrapper for openbanking',
+    long_description=readme,
+    author='Funding Options',
+    author_email='techsupport@fundingoptions.com',
+    url='https://github.com/FundingOptions/openbanking-api',
+    license='GPLv3',
+    py_modules=['openbankingapi'],
+    install_requires=['requests']
+)
