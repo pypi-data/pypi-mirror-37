23522c04b3cde08c945023c0706c67f942e2a080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:56 +0000

    Add package face_recognition_util/0.0.9/face_recognition_util-0.0.9.tar.gz

---
+++ packages/face_recognition_util/face_recognition_util-0.0.9.tar.gz/face_recognition_util-0.0.9/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: reventhao
+# Mail: ren895873@gmail.com
+# Created Time:  2018-10-15 11:47:34
+#############################################
+
+
+from setuptools import setup
+
+setup(
+    name="face_recognition_util",
+    version="0.0.9",
+    keywords=("face_recognition_util"),
+    description="time and path tool",
+    long_description="time and path tool",
+    license="MIT Licence",
+
+    url="https://github.com/reventhao/face_recognition_util",
+    author="reventhao",
+    author_email="ren895873@gmail.com",
+
+    packages=[
+        'face_recognition_util',
+    ],
+    package_dir={'face_recognition_util': 'face_recognition_util'},
+    include_package_data=True,
+    platforms="any",
+    install_requires=["numpy", "dlib", "gevent", "flask", "opencv-python", "face_recognition"]
+)
