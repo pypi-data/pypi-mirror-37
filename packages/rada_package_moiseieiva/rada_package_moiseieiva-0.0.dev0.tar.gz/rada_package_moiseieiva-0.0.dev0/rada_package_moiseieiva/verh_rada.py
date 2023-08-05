aa48ef156c89b7aca0d5cc42f0d9c044fa58697a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:02 +0000

    Add package rada_package_moiseieiva/0.0.dev0/rada_package_moiseieiva-0.0.dev0.tar.gz

---
+++ packages/rada_package_moiseieiva/rada_package_moiseieiva-0.0.dev0.tar.gz/rada_package_moiseieiva-0.0.dev0/rada_package_moiseieiva/verh_rada.py
@@ -0,0 +1,10 @@
+from rada_package_moiseieiva.rada_factories import UkraineRadaFactory, PolandRadaFactory
+
+simulation_config_file = {'rada': 'Ukraine'}
+
+
+if __name__=="__main__":
+    if simulation_config_file['rada'] == 'Ukraine':
+        UkraineRadaFactory()().run()
+    else:
+        PolandRadaFactory()().run()
