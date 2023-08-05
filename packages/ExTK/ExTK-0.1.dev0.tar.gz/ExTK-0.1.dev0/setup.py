6603e95b35775c2018988612f4ab4069bc416ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:09 +0000

    Add package ExTK/0.1.dev0/ExTK-0.1.dev0.tar.gz

---
+++ packages/ExTK/ExTK-0.1.dev0.tar.gz/ExTK-0.1.dev0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ExTK',
+    version='0.1dev',
+    packages=['extk'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description="ExTK: a widget pack for tkinter"
+)
