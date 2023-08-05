e8bf529a0e28681feed32e0b0f106d045c79cef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:13 +0000

    Add package zymptest/0.2.2/zymptest-0.2.2.tar.gz

---
+++ packages/zymptest/zymptest-0.2.2.tar.gz/zymptest-0.2.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup
+import setuptools
+
+setup(name="zymptest",
+      version="0.2.2",
+      author="Yimin Zhang",
+      author_email="1018575661@qq.com",
+      description="test",
+      long_description=open("README.rst").read(),
+      license="MIT",
+      url="",
+      packages=['zymptest'],
+      install_requires=[],
+      classifiers=[ "Environment :: Web Environment",
+                    "Intended Audience :: Developers",
+                    "Operating System :: Microsoft :: Windows",
+                    "Topic :: Text Processing :: Indexing",
+                    "Topic :: Utilities",
+                    "Topic :: Internet",
+                    "Topic :: Software Development :: Libraries :: Python Modules",
+                    "Programming Language :: Python",
+                    "Programming Language :: Python :: 2",
+                    "Programming Language :: Python :: 2.6",
+                    "Programming Language :: Python :: 2.7",
+                    ],
+      )
+
+
+
+
+
