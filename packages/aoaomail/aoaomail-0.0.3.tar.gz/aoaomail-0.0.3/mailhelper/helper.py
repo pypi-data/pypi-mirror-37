abba5a391265d27fe25e980ef80856c0b5093106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:53 +0000

    Add package aoaomail/0.0.3/aoaomail-0.0.3.tar.gz

---
+++ packages/aoaomail/aoaomail-0.0.3.tar.gz/aoaomail-0.0.3/mailhelper/helper.py
@@ -0,0 +1,24 @@
+from email.mime.text import MIMEText
+from smtplib import SMTP_SSL
+from email.header import Header
+
+
+class MailHelper():
+	def __init__(self,host,username,password):
+		self.host = host
+		self.username = username
+		self.password = password
+
+	def qq_sender(self,email_from, recipient, subject, message):
+		message = MIMEText(message,'plain','utf-8')
+		message['From'] = Header(email_from, 'utf-8')
+		message['To'] = Header(recipient, 'utf-8')
+		message['Subject'] = Header(subject, 'utf-8')
+
+		try:
+			server = smtplib.SMTP_SSL(smtp_server, 465)
+			server.login(self.username, self.password)
+			server.sendmail(email_from,recipient, message.as_string())
+			server.quit()
+		except smtplib.SMTPException:
+			raise Exception("Email Send Failed")
