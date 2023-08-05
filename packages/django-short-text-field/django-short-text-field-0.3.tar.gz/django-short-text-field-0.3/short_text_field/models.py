78a103ca588342557083c00963d02ce7332bfe81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:02 +0000

    Add package django-short-text-field/0.3/django-short-text-field-0.3.tar.gz

---
+++ packages/django-short-text-field/django-short-text-field-0.3.tar.gz/django-short-text-field-0.3/short_text_field/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+
+class ShortTextField(models.TextField):
+	"""Like TextField in the database, like CharField in forms. No
+	max_length argument required in initializer."""
+	description = _('String')
+
+	def formfield(self, **kwargs):
+		defaults = {'widget': forms.TextInput}
+		defaults.update(kwargs)
+		return super().formfield(**defaults)
