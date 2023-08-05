c1b078a4369ac454d4235b8382900a54d9d4d372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:57 +0000

    Add package rummage/4.3.1/rummage-4.3.1.tar.gz

---
+++ packages/rummage/rummage-4.3.1.tar.gz/rummage-4.3.1/rummage/__init__.py
@@ -0,0 +1,5 @@
+"""Rummage app."""
+from .lib import __version__
+
+version = __version__.version
+version_info = __version__.version_info
---
+++ packages/rummage/rummage-4.3.1.tar.gz/rummage-4.3.1/rummage/lib/__meta__.py
@@ -0,0 +1,12 @@
+"""Version info."""
+from __future__ import unicode_literals
+from .__version__ import version, version_info
+
+__version__ = version
+__version_info__ = version_info
+__app__ = "Rummage"
+__status__ = version_info[3]
+__maintainers__ = [("Isaac Muse", "IsaacMuse@gmail.com")]
+__help__ = "https://github.com/facelessuser/Rummage/issues"
+__manual__ = "http://facelessuser.github.io/Rummage/"
+__changelog__ = "https://facelessuser.github.io/Rummage/changelog/"
