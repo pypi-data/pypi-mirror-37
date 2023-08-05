e44b454de6a03a17a7c1ce618f0bff874f076927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:41 +0000

    Add package peltak/0.20.5/peltak-0.20.5.tar.gz

---
+++ packages/peltak/peltak-0.20.5.tar.gz/peltak-0.20.5/src/peltak/extra/appengine/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+""" Commands related to Google AppEngine.
+
+Only useful on appengine projects. If you're not using AppEngine, do not
+import inside `pelconf.py`.
+"""
+from __future__ import absolute_import
+from . import cli
