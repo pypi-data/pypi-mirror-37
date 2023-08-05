fd58d13b10935808889078bfd7aa8c1a33882600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:39 +0000

    Add package cvtools/0.1.3/cvtools-0.1.3.tar.gz

---
+++ packages/cvtools/cvtools-0.1.3.tar.gz/cvtools-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'cvtools',
+    packages = ['cvtools'], 
+    version = '0.1.3',
+    description = 'OpenCV tools for CV applications',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'Samson Wang',
+    author_email = 'samson.c.wang@gmail.com',
+    url = 'https://github.com/samson-wang/cvtools.git', 
+    keywords = ['opencv', 'image', 'http'], # arbitrary keywords
+    classifiers = [],
+)
+
