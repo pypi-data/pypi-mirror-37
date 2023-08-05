73d27b7bd33b600c503efe855481eb410130e177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:26 +0000

    Add package memorious/0.7.19/memorious-0.7.19.tar.gz

---
+++ packages/memorious/memorious-0.7.19.tar.gz/memorious-0.7.19/memorious/tests/conftest.py
@@ -0,0 +1,49 @@
+import os
+import uuid
+
+import pytest
+
+from memorious import settings
+from memorious.logic.manager import CrawlerManager
+from memorious.logic.context import Context
+from memorious.logic.http import ContextHttp
+
+
+@pytest.fixture(scope="module")
+def crawler_dir():
+    file_path = os.path.realpath(__file__)
+    crawler_dir = os.path.normpath(os.path.join(
+        file_path, "../testdata/config"
+    ))
+    return crawler_dir
+
+
+@pytest.fixture(scope="module")
+def manager():
+    manager = CrawlerManager()
+    manager.load_path(crawler_dir())
+    settings._manager = manager
+    return manager
+
+
+@pytest.fixture(scope="module")
+def crawler():
+    return manager().get("occrp_web_site")
+
+
+@pytest.fixture(scope="module")
+def stage():
+    cr = crawler()
+    return cr.get(cr.init_stage)
+
+
+@pytest.fixture(scope="module")
+def context():
+    ctx = Context(crawler(), stage(), {"foo": "bar"})
+    ctx.run_id = str(uuid.uuid4())
+    return ctx
+
+
+@pytest.fixture(scope="module")
+def http():
+    return ContextHttp(context())
