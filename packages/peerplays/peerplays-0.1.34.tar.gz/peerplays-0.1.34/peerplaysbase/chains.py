be4b2b307835ad02cd5ba13b9774e62b5288cc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:27 +0000

    Add package peerplays/0.1.34/peerplays-0.1.34.tar.gz

---
+++ packages/peerplays/peerplays-0.1.34.tar.gz/peerplays-0.1.34/peerplaysbase/chains.py
@@ -0,0 +1,22 @@
+known_chains = {
+    "ALICE": {
+        "chain_id": "6b6b5f0ce7a36d323768e534f3edb41c6d6332a541a95725b98e28d140850134",
+        "core_symbol": "PPY",
+        "prefix": "PPY"},
+    "ALPHA": {
+        "chain_id": "80df3c34bff8469bdaf47e55a2fb6fb6f1eb4a49d6375aaa5689d3ca9e51bff5",
+        "core_symbol": "PPY",
+        "prefix": "PPY"},
+    "BAXTER": {
+        "chain_id": "be6b79295e728406cbb7494bcb626e62ad278fa4018699cf8f75739f4c1a81fd",
+        "core_symbol": "PPYTEST",
+        "prefix": "PPYTEST"},
+    "CHARLIE": {
+        "chain_id": "ff13188de4d86b9d53d1cd4452b56d672562c4ceae3f7b59847e982220fe260f",
+        "core_symbol": "TEST",
+        "prefix": "TEST"},
+    "ELIZABETH": {
+        "chain_id": "bfd04981389d7ac80f9e9739a183808ff9d99c948f65510a0c08b4979e3538ae",
+        "core_symbol": "TEST",
+        "prefix": "TEST"}
+}
