f54ae164445425231d2a22ba986c691d93bf233a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:17 +0000

    Add package vetrr/0.1/vetrr-0.1.tar.gz

---
+++ packages/vetrr/vetrr-0.1.tar.gz/vetrr-0.1/README.md
@@ -0,0 +1,15 @@
+# vetrr
+Repo for CGM^2 (PI Werk)
+
+# Requirements
+
+
+* python
+* numpy
+* scipy
+* matplotlib
+* astropy
+* h5py
+* future
+* PyYAML
+* linetools
---
+++ packages/vetrr/vetrr-0.1.tar.gz/vetrr-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vetrr',
+      version='0.1',
+      description='vet redrock outputs',
+      url='http://github.com/mattcwilde/vetrr',
+      author='Matthew Wilde',
+      author_email='mattcwilde@gmail.com',
+      license='MIT',
+      packages=['vetrr'],
+      zip_safe=True)
