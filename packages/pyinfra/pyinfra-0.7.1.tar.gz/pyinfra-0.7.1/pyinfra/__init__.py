f9b185095bf4b8d3934f677b1daafd4496c0dc3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:25 +0000

    Add package pyinfra/0.7.1/pyinfra-0.7.1.tar.gz

---
+++ packages/pyinfra/pyinfra-0.7.1.tar.gz/pyinfra-0.7.1/pyinfra/__init__.py
@@ -0,0 +1,28 @@
+# pyinfra
+# File: pyinfra/__init__.py
+# Desc: some global state for pyinfra
+
+'''
+Welcome to pyinfra.
+'''
+
+import logging
+
+
+# Global flag set True by `pyinfra_cli.__main__`
+is_cli = False
+
+# Global pyinfra logger
+logger = logging.getLogger('pyinfra')
+
+# Setup package level version
+from .version import __version__  # noqa
+
+# Trigger pseudo_* creation
+from . import pseudo_modules  # noqa
+
+# Trigger fact index creation
+from . import facts  # noqa
+
+# Trigger module imports
+from . import modules  # noqa
