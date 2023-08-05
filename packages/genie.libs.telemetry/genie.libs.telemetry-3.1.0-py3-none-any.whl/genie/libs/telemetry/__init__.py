65fe835198e96c0ab0fd244db5bf9c5e71f58e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:29 +0000

    Add package genie.libs.telemetry/3.1.0/genie.libs.telemetry-3.1.0-py3-none-any.whl

---
+++ packages/genie.libs.telemetry/genie.libs.telemetry-3.1.0-py3-none-any.whl/genie/libs/telemetry/__init__.py
@@ -0,0 +1,28 @@
+'''
+    Module:
+        genie.libs.telemetry
+
+    Description:
+        This is the library sub-component of Genie for `genie.telemetry`.
+
+'''
+
+# metadata
+__version__ = '3.1.0'
+__author__ = 'Cisco Systems Inc.'
+__contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
+__copyright__ = 'Copyright (c) 2018, Cisco Systems Inc.'
+
+try:
+    from ats.cisco.stats import CesMonitor
+    CesMonitor(action = 'genietelemetrylibs', application='Genie').post()
+except Exception:
+    try:
+        from ats.utils.stats import CesMonitor
+        CesMonitor(action = 'genietelemetrylibs', application='Genie').post()
+    except Exception:
+        pass
+
+# Enable abstraction; This is the root package.
+from genie import abstract
+abstract.declare_package(__name__)
