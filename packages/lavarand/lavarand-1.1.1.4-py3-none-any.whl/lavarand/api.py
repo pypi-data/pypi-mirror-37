9ae2a962e6ebcd58a27fd1b471d0a521bf659c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:25 +0000

    Add package lavarand/1.1.1.4/lavarand-1.1.1.4-py3-none-any.whl

---
+++ packages/lavarand/lavarand-1.1.1.4-py3-none-any.whl/lavarand/api.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import warnings
+from base64 import b64encode
+
+import requests
+
+"""
+>>> import lavarand
+>>> random = lavarand.random()
+
+"""
+
+API_URL = "https://csprng.xyz/v1/api"
+
+RANDMAX = 65535
+RANDMIN = 1
+
+
+def random(size=32):
+
+    assert isinstance(size, int)
+
+    size = 32 if size < RANDMIN else min([RANDMAX, size])
+
+    try:
+        return requests.get(API_URL, params={"length": size}).json()["Data"]
+    except Exception as e:  # try catch all exceptions
+        warnings.warn("Was unable to use lava random fallback to os urandom")
+        return b64encode(os.urandom(size)).decode("utf-8")
+
+
+if __name__ == "__main__":
+    assert len(random(32)) == 44
