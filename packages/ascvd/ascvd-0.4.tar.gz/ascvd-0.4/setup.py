8505d328b96e9f70978b8ce4b7ad5d202c62e9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:48 +0000

    Add package ascvd/0.4/ascvd-0.4.tar.gz

---
+++ packages/ascvd/ascvd-0.4.tar.gz/ascvd-0.4/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(
+    name="ascvd",
+    version="0.4",
+    description="Module for calculating ASCVD 10-year estimate",
+    author="Brandon Istenes",
+    author_email="brandonesbox@gmail.com",
+    url="https://github.com/brandones/ascvd",
+    packages=["ascvd"],
+    project_urls={"Source": "https://github.com/brandones/ascvd"},
+)
