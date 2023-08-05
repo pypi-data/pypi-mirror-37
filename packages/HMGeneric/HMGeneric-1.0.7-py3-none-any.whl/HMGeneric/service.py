555f281153fdb3fe0c55c7ccde74395b9906f084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:34 +0000

    Add package HMGeneric/1.0.7/HMGeneric-1.0.7-py3-none-any.whl

---
+++ packages/HMGeneric/HMGeneric-1.0.7-py3-none-any.whl/HMGeneric/service.py
@@ -0,0 +1,80 @@
+import os
+
+from enum import Enum
+
+
+class ServiceManager:
+    def __init__(self):
+        self.services = []
+        self.service : self.ServiceByOs
+    
+    def get_service_by_os(self):
+        self.service = [x for x in self.services if x.os == os.name][0]
+
+    def check_service_status(self):
+        self.get_service_by_os()
+        if(self.service.serviceType == ServiceTypes.windowsService):
+            from win32serviceutil import QueryServiceStatus
+            if(QueryServiceStatus(self.service.attribute)[1] == 4):
+                return True
+            else:
+                return False
+        elif(self.service.serviceType == ServiceTypes.LinuxBoolFile):
+            try:
+                file = open(self.service.attribute, 'r')
+                line = file.readline()
+                file.close()
+            finally:
+                if(line == "1"):
+                    return True
+                else:
+                    return False
+        elif(self.service.serviceType == ServiceTypes.NotWorking):
+            print("The " + self.service.attribute + " service doesn't work currently on this operating system")
+
+    def change_service_status(self):
+        status = self.check_service_status()
+        if(self.service.serviceType == ServiceTypes.windowsService):
+            from win32serviceutil import QueryServiceStatus, StopService, StartService
+            if(status == False):
+                while True:
+                    try:
+                        StartService(self.service.attribute)
+                    except:
+                        os.system('sc config "' + self.service.attribute + '" start= auto')
+                        continue
+                    break
+            else:
+                if(input('Do you want to disable the service ? (y|n) [y]\n') != "n"):
+                    os.system('sc config "' + self.service.attribute + '" start= disabled')
+                StopService(self.service.attribute)
+            if(self.service.waitForChange):
+                if(status == True):
+                    while(QueryServiceStatus(self.service.attribute)[1] != 1):
+                        continue
+                else:
+                    while(QueryServiceStatus(self.service.attribute)[1] != 4):
+                        continue
+
+        elif(self.service.serviceType == ServiceTypes.LinuxBoolFile):
+            file = open(self.service.attribute, 'w')
+            if(status == False):
+                file.write("1")
+            else:
+                file.write("0")
+            file.close()
+
+    def add_service(self, os, attribute, serviceType, waitForChange = False):
+        self.services.append(self.ServiceByOs(os, attribute, serviceType, waitForChange))
+    
+    class ServiceByOs:
+        def __init__(self, os, attribute, serviceType, waitForChange):
+            self.os = os
+            self.attribute = attribute
+            self.serviceType = serviceType
+            self.waitForChange = waitForChange
+
+class ServiceTypes(Enum):
+    windowsService = 1
+    LinuxBoolFile = 2
+    NotWorking = 3
---
+++ packages/HMGeneric/HMGeneric-1.0.7-py3-none-any.whl/HMGeneric/terminal.py
@@ -0,0 +1,102 @@
+import subprocess, os, sys, glob
+
+from pyreadline import Readline
+
+from HMGeneric.array import count_layers
+from HMGeneric.parse import get_object_unbyte
+
+def get_terminal_lines(CMD, startLine=0):
+    '''Run terminal command and get its output'''
+    if(count_layers(CMD)>1):
+        procs = []
+        for i in range(0,len(CMD)):
+            if(i==0):
+                proc = subprocess.Popen(CMD[i], stdout=subprocess.PIPE)
+                procs.append(proc)
+            else:
+                proc = subprocess.Popen(CMD[i], stdin=procs[i-1].stdout,
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                procs.append(proc)
+
+        for i in range(0,len(procs)-1):
+            procs[i].stdout.close()
+
+        out, err = procs[-1].communicate()
+        return(bytes(out).decode())
+    else:
+        CMDOutLines = []
+        with subprocess.Popen(CMD, stdout=subprocess.PIPE, bufsize=1,
+                            universal_newlines=True) as p:
+            for line in p.stdout:
+                CMDOutLines.append((get_object_unbyte(line)))
+        return CMDOutLines[startLine:len(CMDOutLines)]
+
+def check_admin_rights(exitIfNot = False):
+    import ctypes
+    try:
+        is_admin = os.getuid() == 0
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    
+    if(exitIfNot == True):
+        if(is_admin == False):
+            print("Please start this code with admin rights")
+            sys.exit(0)
+    return is_admin
+
+class TabCompleter:
+    """
+    A tab completer that can either complete from
+    the filesystem or from a list.
+    Partially taken from:
+    http://stackoverflow.com/questions/5637124/tab-completion-in-pythons-raw
+    """
+    def __init__(self):
+        self.readline = Readline()
+    def completerController(self, ListOrPath, QPhrase):
+        """
+        This is the tab completer controller.
+        """
+        self.readline.set_completer_delims('\t')
+        self.readline.parse_and_bind('tab: complete')
+        if(ListOrPath == 'list'):
+            self.readline.set_completer(self.listCompleter)
+        else:
+            self.readline.set_completer(self.pathCompleter)
+        return input(QPhrase)
+
+    def pathCompleter(self, text, state):
+        """
+        This is the tab completer for systems paths.
+        Only tested on *nix systems
+        """
+        line = self.readline.get_line_buffer().split()
+
+        if '~' in text:
+            text = os.path.expanduser('~')
+
+        if os.path.isdir(text):
+            text += '/'
+
+        return [x for x in glob.glob(text + '*')][state]
+
+    def createListCompleter(self, listValues):
+        """
+        This is a closure that creates a method that autocompletes from
+        the given list.
+        Since the autocomplete function can't be given a list to complete from
+        a closure is used to create the listCompleter function with a list
+        to complete
+        from.
+        """
+
+        def listCompleter(text, state):
+            line = self.readline.get_line_buffer()
+
+            if not line:
+                return [c + " " for c in listValues][state]
+
+            else:
+                return [c + " " for c in listValues if c.startswith(line)][state]
+
+        self.listCompleter = listCompleter
