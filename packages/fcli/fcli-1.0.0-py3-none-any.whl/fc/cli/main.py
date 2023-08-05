3818b67ab427063c15edb00c3328df564067caf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:33 +0000

    Add package fcli/1.0.0/fcli-1.0.0-py3-none-any.whl

---
+++ packages/fcli/fcli-1.0.0-py3-none-any.whl/fc/cli/main.py
@@ -0,0 +1,30 @@
+import click
+from ..jira.task import Task
+from requests.exceptions import HTTPError
+from ..auth.combo import ComboAuth
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('title')
+@click.argument('description')
+@click.argument('parent_story', required=False)
+@click.option('--username')
+@click.option('--in-progress', is_flag=True)
+def task(title, description, parent_story, username, in_progress):
+    click.echo('Adding task {}; {}'.format(title, description))
+
+    auth = ComboAuth(username)
+
+    new_task = Task(title, description, parent_story, in_progress, auth)
+    try:
+        task_id, url = new_task.create()
+        click.echo('{} task {} added at {}'.format(new_task.type_str(), task_id, url))
+        if in_progress:
+            click.echo('Triage task put into In Progress')
+    except HTTPError as exception:
+        click.echo('{} task creation failed with {}'.format(new_task.type_str(), exception))
