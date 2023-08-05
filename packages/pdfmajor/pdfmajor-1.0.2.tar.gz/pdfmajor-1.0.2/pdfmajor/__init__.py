0bda5b5c485ada9895f4440b9ed5fa789ac6f16d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:50 +0000

    Add package pdfmajor/1.0.2/pdfmajor-1.0.2.tar.gz

---
+++ packages/pdfmajor/pdfmajor-1.0.2.tar.gz/pdfmajor-1.0.2/pdfmajor/__init__.py
@@ -0,0 +1,10 @@
+"""
+Fork of PDFMiner.six using latest python support and a more trim down approach
+
+PDFMajor is a tool for extracting information from PDF documents.
+It's main focus is on obtaining as closely related results that reflect the bare-bone structure of the 
+pdf document.
+It includes a PDF converter that can transform PDF files into other text
+formats (such as HTML, XML, JSON). It has an extensible PDF parser that can be used for
+other purposes instead of text analysis.
+"""
