beed7a487f1f2ff946f4169076d63926e167226c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:24 +0000

    Add package PYGravity/0.1/PYGravity-0.1.tar.gz

---
+++ packages/PYGravity/PYGravity-0.1.tar.gz/PYGravity-0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='PYGravity',
+    version='0.1',
+    packages=['PYGravity'],
+    url='https://github.com/harish123456798/PYGravity',
+    license='MIT',
+    author='Harishankar',
+    author_email='harishgaddanakeri@gmail.com',
+    description=''
+)
