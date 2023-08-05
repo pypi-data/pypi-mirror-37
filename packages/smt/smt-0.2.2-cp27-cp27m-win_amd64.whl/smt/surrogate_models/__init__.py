fca44828c79b386a9a809a81197ad77e46450567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:11 +0000

    Add package smt/0.2.2/smt-0.2.2-cp27-cp27m-win_amd64.whl

---
+++ packages/smt/smt-0.2.2-cp27-cp27m-win_amd64.whl/smt/surrogate_models/__init__.py
@@ -0,0 +1,13 @@
+from .ls import LS
+from .qp import QP
+from .kpls import KPLS
+from .krg import KRG
+from .gekpls import GEKPLS
+from .kplsk import KPLSK
+try:
+    from .idw import IDW
+    from .rbf import RBF
+    from .rmtc import RMTC
+    from .rmtb import RMTB
+except:
+    pass
