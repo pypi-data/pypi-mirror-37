6dd28cff31f0ae6f4133b5e3e461e197743a6693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:15 +0000

    Add package PyExport/0.0.0/PyExport-0.0.0-py2-none-any.whl

---
+++ packages/PyExport/PyExport-0.0.0-py2-none-any.whl/pyexport/html.py
@@ -0,0 +1,21 @@
+"""
+Create a html document and it's components programatically 
+with python.
+
+Usage:
+>> d = Document("This is an Html Page")
+>> d.section()
+>> d.div()
+>> d.paragraph()
+>> d.header()
+>> d.table()
+>> d.image()
+>> d.url()
+"""
+
+class Document:
+    def __init__(self, header):
+        return 
+
+if __name__=="__main__":
+    exit
