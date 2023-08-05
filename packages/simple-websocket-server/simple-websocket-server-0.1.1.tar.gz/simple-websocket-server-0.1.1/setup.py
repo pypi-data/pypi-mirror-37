df81c0e3151f38c6f1aadee244f065346fd898b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:39 +0000

    Add package simple-websocket-server/0.1.1/simple-websocket-server-0.1.1.tar.gz

---
+++ packages/simple-websocket-server/simple-websocket-server-0.1.1.tar.gz/simple-websocket-server-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='simple-websocket-server',
+    version='0.1.1',
+    author='Dave',
+    packages=['SimpleWebSocketServer'],
+    url='https://github.com/pikhovkin/simple-websocket-server/',
+    description='A Simple Websocket Server written in Python'
+)
