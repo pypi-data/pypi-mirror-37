82bd8d08989add6619b5eed985b2d08419288d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:35 +0000

    Add package kinesyslog/0.9.5/kinesyslog-0.9.5-py2.py3-none-any.whl

---
+++ packages/kinesyslog/kinesyslog-0.9.5-py2.py3-none-any.whl/kinesyslog/sink.py
@@ -0,0 +1,122 @@
+import logging
+import math
+import time
+from asyncio import get_event_loop
+from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
+from gzip import compress
+
+from boto3 import Session
+
+import ujson
+
+from . import constant
+
+logger = logging.getLogger(__name__)
+
+
+class MessageSink(object):
+    __slots__ = ['spool', 'loop', 'size', 'count', 'messages', 'flushed', 'message_class', 'account', 'group_prefix']
+
+    def __init__(self, spool, message_class, group_prefix):
+        self.spool = spool
+        self.message_class = message_class
+        self.group_prefix = group_prefix
+        self.loop = get_event_loop()
+        self._schedule_flush()
+        self.clear()
+        self.account = '000000000000'
+        try:
+            session = Session(profile_name=spool.profile_name)
+            client = session.client('sts', config=spool.config)
+            self.account = client.get_caller_identity()['Account']
+        except Exception:
+            logger.warn('Unable to determine AWS Account ID; using default value.')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.flush()
+
+    async def write(self, source, dest, message, timestamp):
+        self.messages[(source, dest)].append((message, timestamp))
+        self.size += len(message)
+        self.count += 1
+        if self.size > constant.FLUSH_SIZE:
+            await self.flush_async()
+        return len(message)
+
+    def clear(self):
+        self.size = 0
+        self.count = 0
+        self.messages = defaultdict(list)
+        self.flushed = time.time()
+
+    async def flush_async(self):
+        args = [self.spool, self.messages, self.size, self.message_class, self.account, self.group_prefix]
+        self.clear()
+        with ProcessPoolExecutor(max_workers=1) as executor:
+            return self.loop.run_in_executor(executor, self._spool_messages, *args)
+
+    def flush(self):
+        self._spool_messages(self.spool, self.messages, self.size, self.message_class, self.account, self.group_prefix)
+        self.clear()
+
+    def _schedule_flush(self):
+        self.loop.call_later(constant.TIMER_INTERVAL, self._flush_timer)
+
+    def _flush_timer(self):
+        logger.debug('flush timer: messages={0} size={1} age={2}'.format(self.count, self.size, time.time() - self.flushed))
+        if self.messages and time.time() - self.flushed >= constant.FLUSH_TIME:
+            self.loop.create_task(self.flush_async())
+        self._schedule_flush()
+
+    @classmethod
+    def _spool_messages(cls, spool, messages, size, message_class, account, group_prefix):
+        for (source, dest), values in messages.items():
+            group = '{0}/{1}/{2}'.format(group_prefix, message_class.name, dest)
+            events = message_class.create_events(source, values)
+            record = cls._prepare_record(account, group, source, events)
+            compressed_record = cls._compress_record(record)
+            logger.debug('Events for {0} > {1} compressed from {2} to {3} bytes (with JSON framing)'.format(group, source, size, len(compressed_record)))
+
+            if len(compressed_record) > constant.MAX_RECORD_SIZE:
+                # This approach naievely hopes that splitting a record into even parts will put it
+                # below the max record size. Further tuning may be required.
+                split_count = math.ceil(len(compressed_record) / constant.MAX_RECORD_SIZE)
+                logger.warning('Compressed record size of {0} bytes exceeds maximum Firehose record size of {1} bytes; splitting into {2} records'.format(
+                    len(compressed_record),
+                    constant.MAX_RECORD_SIZE,
+                    split_count
+                ))
+                start = 0
+                size = int(len(record['logEvents']) / split_count)
+                while start < len(record['logEvents']):
+                    record_part = cls._prepare_record(account, group, source, record['logEvents'][start:start+size])
+                    compressed_record = cls._compress_record(record_part)
+                    spool.write(compressed_record)
+                    start += size
+            else:
+                spool.write(compressed_record)
+
+    @classmethod
+    def _prepare_record(cls, owner, group, stream, events, filters=[], type='DATA_MESSAGE'):
+        if not isinstance(filters, list):
+            filters = [filters]
+
+        if not filters:
+            filters = [group]
+
+        return {
+            'owner': owner,
+            'logGroup': group,
+            'logStream': stream,
+            'subscriptionFilters': filters,
+            'messageType': type,
+            'logEvents': events,
+        }
+
+    @classmethod
+    def _compress_record(cls, record):
+        return compress(ujson.dumps(record, escape_forward_slashes=False).encode())
---
+++ packages/kinesyslog/kinesyslog-0.9.5-py2.py3-none-any.whl/kinesyslog/spool.py
@@ -0,0 +1,102 @@
+import logging
+import os
+import time
+from glob import glob
+from multiprocessing import Lock
+from tempfile import NamedTemporaryFile
+
+from boto3 import Session
+from botocore.config import Config
+
+from .util import get_region
+
+FLUSH_TIME = 60
+MAX_REQUEST_SIZE = 1024 * 1024 * 4
+MAX_RECORD_COUNT = 500
+logger = logging.getLogger(__name__)
+lock = Lock()
+
+
+class EventSpool(object):
+    __slots__ = ['delivery_stream', 'spool_dir', 'profile_name', 'config', 'flushed']
+    PREFIX = 'firehose_event-'
+
+    def __init__(self, delivery_stream, spool_dir, region_name=None, profile_name=None):
+        self.delivery_stream = delivery_stream
+        self.spool_dir = spool_dir
+        self.profile_name = profile_name
+        self.config = Config(retries={'max_attempts': 10}, region_name=get_region(region_name, profile_name))
+        self.flushed = 0
+        self._validate_stream()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.flush()
+
+    def write(self, record):
+        with NamedTemporaryFile(prefix=self.PREFIX, dir=self.spool_dir, delete=False) as temp:
+            logger.debug('Writing {0} byte record to {1}'.format(len(record), temp.name))
+            temp.write(record)
+        if time.time() - self.flushed >= FLUSH_TIME:
+            self.flush()
+
+    def flush(self):
+        if lock.acquire(block=False):
+            try:
+                while True:
+                    record_files = glob(os.path.join(self.spool_dir, self.PREFIX) + '*')
+                    request_kwargs = {'DeliveryStreamName': self.delivery_stream, 'Records': []}
+                    request_size = 0
+                    request_files = []
+
+                    for path in record_files:
+                        file_size = os.path.getsize(path)
+                        if request_size + file_size <= MAX_REQUEST_SIZE and len(request_files) < MAX_RECORD_COUNT:
+                            logger.debug('Including {0} ({1} bytes) in batch'.format(path, file_size))
+                            request_size += file_size
+                            request_files.append(path)
+                        else:
+                            break
+
+                    for request_file in request_files:
+                        try:
+                            with open(request_file, 'rb') as fh:
+                                request_kwargs['Records'].append({'Data': fh.read()})
+                        except Exception:
+                            logger.error('Failed to read file {0} from spool'.format(request_file), exc_info=True)
+                            return
+
+                    if request_kwargs['Records']:
+                        logger.info('Batch has {0} bytes in {1} files'.format(request_size, len(request_files)))
+                        try:
+                            session = Session(profile_name=self.profile_name)
+                            client = session.client('firehose', config=self.config)
+                            response = client.put_record_batch(**request_kwargs)
+                        except Exception:
+                            logger.error('Firehose put_record_batch failed', exc_info=True)
+                            return
+
+                        for (i, status) in enumerate(response['RequestResponses']):
+                            if 'RecordId' in status:
+                                logger.debug('Firehose record succeeded: {0}'.format(request_files[i]))
+                                try:
+                                    os.unlink(request_files[i])
+                                except Exception:
+                                    logger.error('Failed to unlink successfully processed record: {0}'.format(request_files[i]))
+                            else:
+                                logger.warning('Firehose record failed: [{ErrorCode}] {ErrorMessage}'.format(**status))
+                    else:
+                        logger.debug('Batch is empty')
+                        return
+            finally:
+                self.flushed = time.time()
+                lock.release()
+
+    def _validate_stream(self):
+        session = Session(profile_name=self.profile_name)
+        client = session.client('firehose', config=self.config)
+        response = client.describe_delivery_stream(DeliveryStreamName=self.delivery_stream)
+        if not response['DeliveryStreamDescription']['DeliveryStreamStatus'] == 'ACTIVE':
+            raise Exception('Firehose Delivery Stream is not active')
