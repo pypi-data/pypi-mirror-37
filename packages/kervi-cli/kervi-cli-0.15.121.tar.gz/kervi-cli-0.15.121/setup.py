a3e111c200e3b20ec7a279d95ae1329d35712392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:06 +0000

    Add package kervi-cli/0.15.121/kervi-cli-0.15.121.tar.gz

---
+++ packages/kervi-cli/kervi-cli-0.15.121.tar.gz/kervi-cli-0.15.121/setup.py
@@ -0,0 +1,29 @@
+import distutils
+from setuptools import setup
+import sys
+
+try:
+    distutils.dir_util.remove_tree("dist")
+except:
+    pass
+
+from kervi_cli.version import VERSION
+setup(
+    name='kervi-cli',
+    version=VERSION,
+    packages=[
+        "kervi_cli",
+        "kervi_cli/scripts",
+        "kervi_cli/templates",
+        "kervi_cli/scripts/commands"
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'psutil'
+    ],
+    entry_points='''
+        [console_scripts]
+        kervi=kervi_cli.scripts.kervi:cli
+    ''',
+)
