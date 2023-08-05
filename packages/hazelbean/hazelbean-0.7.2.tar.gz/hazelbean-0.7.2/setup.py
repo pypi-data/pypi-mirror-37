15588f7b6d48470bd0695406eb7fdce933c3df96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:01 +0000

    Add package hazelbean/0.7.2/hazelbean-0.7.2.tar.gz

---
+++ packages/hazelbean/hazelbean-0.7.2.tar.gz/hazelbean-0.7.2/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+packages=find_packages()
+include_package_data=True
+
+setup(
+  name = 'hazelbean',
+  packages = packages,
+  version = '0.7.2',
+  description = 'Geospatial research tools',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Justin Andrew Johnson',
+  url = 'https://github.com/jandrewjohnson/hazelbean',
+  download_url = 'https://github.com/jandrewjohnson/hazelbean',
+  keywords = ['geospatial', 'raster', 'shapefile', 'sustainability science'],
+  classifiers = ["Programming Language :: Python :: 3"],
+)
