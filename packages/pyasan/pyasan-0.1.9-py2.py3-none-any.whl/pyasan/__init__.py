1fcec77325581da630e5084eb2de32bfe58dea36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package pyasan/0.1.9/pyasan-0.1.9-py2.py3-none-any.whl

---
+++ packages/pyasan/pyasan-0.1.9-py2.py3-none-any.whl/pyasan/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pyasan."""
+
+__author__ = """Jeorry Balasabas"""
+__email__ = 'jeorry@gmail.com'
+__version__ = '0.1.0'
+
+from . import helpers
+from . import patents
+from .patents import Patents
