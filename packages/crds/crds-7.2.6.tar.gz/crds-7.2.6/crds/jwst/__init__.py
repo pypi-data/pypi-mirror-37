300fc5fb97c93c3530225596d883773ad86b7903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:47 +0000

    Add package crds/7.2.6/crds-7.2.6.tar.gz

---
+++ packages/crds/crds-7.2.6.tar.gz/crds-7.2.6/crds/jwst/__init__.py
@@ -0,0 +1,28 @@
+import os.path
+
+from crds.certify import reftypes
+
+HERE  = os.path.abspath(os.path.dirname(__file__) or ".")
+
+TYPES = reftypes.from_package_file("jwst", __file__)
+
+OBSERVATORY = TYPES.observatory
+INSTRUMENTS = TYPES.instruments
+EXTENSIONS = TYPES.extensions
+TEXT_DESCR = TYPES.text_descr
+FILEKINDS = TYPES.filekinds
+
+UNDEFINED_PARKEY_SUBST_VALUE = "UNDEFINED"
+
+INSTRUMENT_FIXERS = {
+}
+
+TYPE_FIXERS = {
+}
+
+PROVENANCE_KEYWORDS = ("META.DESCRIPTION", "META.PEDIGREE", "META.USEAFTER","META.HISTORY", "META.AUTHOR", "META.MODEL_TYPE")
+# PROVENANCE_KEYWORDS = ("DESCRIP", "PEDIGREE", "USEAFTER","HISTORY", "AUTHOR")
+USEAFTER_KEYWORDS = ("META.OBSERVATION.DATE", "META.OBSERVATION.TIME") # Dataset keywords matching in UseAfter selectors
+
+DEFAULT_SELECTORS = ("Match", "UseAfter")   # Normal selector hierarchy in rmap
+
