31512d68c0fc0e5de4c949312ec5926270bec12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:48 +0000

    Add package rainbow-bridge-logger/0.2/rainbow-bridge-logger-0.2.tar.gz

---
+++ packages/rainbow-bridge-logger/rainbow-bridge-logger-0.2.tar.gz/rainbow-bridge-logger-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='rainbow-bridge-logger',
+    version='0.2',
+    description='Improved logger based on Python\'s logging module',
+    packages=['rainbow',],
+    license='MIT',
+    url = 'https://github.com/mamerisawesome/rainbow-bridge-logger',
+    download_url='https://github.com/mamerisawesome/rainbow-bridge-logger/archive/v_0.2.tar.gz',
+    long_description=open('long-description.txt').read(),
+    author='Almer Mendoza',
+    author_email='atmalmer23@gmail.com'
+)
