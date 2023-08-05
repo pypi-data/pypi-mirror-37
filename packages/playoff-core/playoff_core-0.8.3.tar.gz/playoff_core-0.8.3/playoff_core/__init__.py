e3d034ee2bc29e5ecf087fd9bf36c2839a10da01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package playoff-core/0.8.3/playoff_core-0.8.3.tar.gz

---
+++ packages/playoff-core/playoff_core-0.8.3.tar.gz/playoff_core-0.8.3/playoff_core/__init__.py
@@ -0,0 +1,9 @@
+from playoff_core.data_handler import GetPlayoffData, PostPlayoffData, \
+    DeletePlayoffData, PatchPlayoffData
+from playoff_core.design_handler import GetPlayoffDesign, PostPlayoffDesign, \
+    DeletePlayoffDesign
+from playoff_core.import_export_playoff import ImportData, ImportDesign, \
+    ExportRawDesign, ExportRawData, ExportData, ExportDesign
+from playoff_core.utility import Utility
+
+name = "playoff_core"
