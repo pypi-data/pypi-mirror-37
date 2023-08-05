5eaa9769ed60c1758f9f4011e40375b275e80a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:48 +0000

    Add package pmagpy/4.2.0/pmagpy-4.2.0.tar.gz

---
+++ packages/pmagpy/pmagpy-4.2.0.tar.gz/pmagpy-4.2.0/pmag_env/set_env.py
@@ -0,0 +1,17 @@
+import matplotlib
+import sys
+isServer = False
+verbose = True
+IS_WIN = True if sys.platform in ['win32', 'win64'] else False
+
+def set_backend(wx=True):
+    if wx:
+        matplotlib.use('WXAgg')
+    else:
+        matplotlib.use('TKAgg')
+
+def set_server(boolean=True):
+    isServer = boolean
+
+def set_verbose(boolean=True):
+    verbose = boolean
