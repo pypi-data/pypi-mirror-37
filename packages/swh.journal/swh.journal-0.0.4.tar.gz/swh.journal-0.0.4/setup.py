9b411fcc77e98493a0153dc02490ba559719ead9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:05 +0000

    Add package swh.journal/0.0.4/swh.journal-0.0.4.tar.gz

---
+++ packages/swh.journal/swh.journal-0.0.4.tar.gz/swh.journal-0.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+def parse_requirements():
+    requirements = []
+    for reqf in ('requirements.txt', 'requirements-swh.txt'):
+        with open(reqf) as f:
+            for line in f.readlines():
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                requirements.append(line)
+    return requirements
+
+setup(
+    name='swh.journal',
+    description='Software Heritage Journal utilities',
+    author='Software Heritage developers',
+    author_email='swh-devel@inria.fr',
+    url='https://forge.softwareheritage.org/diffusion/DJNL/',
+    packages=find_packages(),  # packages's modules
+    scripts=[],   # scripts to package
+    install_requires=parse_requirements(),
+    setup_requires=['vcversioner'],
+    vcversioner={},
+    include_package_data=True,
+)
