94c263fadf1c06c0b7ccacf6b16dcc43e69b16e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package pylava-pylint/0.0.3/pylava_pylint-0.0.3.tar.gz

---
+++ packages/pylava-pylint/pylava_pylint-0.0.3.tar.gz/pylava_pylint-0.0.3/pylava_pylint/__init__.py
@@ -0,0 +1,12 @@
+"""Support pylint in Pylama."""
+
+# Module information
+# ==================
+
+
+__version__ = "0.0.3"
+__project__ = "pylava_pylint"
+__author__ = "Pylava Developers"
+__license__ = "MIT"
+
+from .main import Linter  # noqa
---
+++ packages/pylava-pylint/pylava_pylint-0.0.3.tar.gz/pylava_pylint-0.0.3/setup.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+""" Support pylint code checker.
+
+pylava_pylint
+-------------
+
+pylava_pylint -- Pylint integration to pylava library.
+
+"""
+import re
+import sys
+from os import path as op
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+def _read(fname):
+    try:
+        return open(op.join(op.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+_meta = _read('pylava_pylint/__init__.py')
+_license = re.search(r'^__license__\s*=\s*"(.*)"', _meta, re.M).group(1)
+_project = re.search(r'^__project__\s*=\s*"(.*)"', _meta, re.M).group(1)
+_version = re.search(r'^__version__\s*=\s*"(.*)"', _meta, re.M).group(1)
+
+
+class __PyTest(TestCommand):
+
+    test_args = []
+    test_suite = True
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['tests.py']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+setup(
+    name=_project,
+    version=_version,
+    license=_license,
+    description=_read('DESCRIPTION').strip(),
+    long_description=_read('README.rst'),
+    platforms=('Any'),
+
+    maintainer='Susam Pal',
+    maintainer_email='susam@susam.in',
+    url='http://github.com/pylava/pylava_pylint',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    entry_points={
+        'pylava.linter': [
+            'pylint = pylava_pylint.main:Linter',
+        ],
+    },
+    packages=find_packages(),
+    package_data={'pylava_pylint': ['pylint.rc']},
+    install_requires=[
+        l for l in _read('requirements.txt').split('\n')
+        if l and not l.startswith('#')],
+    tests_require=['pytest'],
+    cmdclass={'test': __PyTest},
+)
