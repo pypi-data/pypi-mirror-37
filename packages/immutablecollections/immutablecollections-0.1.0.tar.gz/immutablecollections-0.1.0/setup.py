b01a1e9b74116c5613828813e43a14837f4691f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package immutablecollections/0.1.0/immutablecollections-0.1.0.tar.gz

---
+++ packages/immutablecollections/immutablecollections-0.1.0.tar.gz/immutablecollections-0.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+export_packages =  find_packages(where='.', include=('immutablecollections'))
+
+setup(name='immutablecollections',
+      version='0.1.0',
+      author='Ruan Gabbard <gabbard@isi.edi> and Constantine Lignos <lignos@isi.edu>',
+      author_email='gabbard@isi.edu',
+      description='Immutable Collections (inspired by Google Guava)',
+      url='https://github.com/isi-vista/immutablecollections',
+      packages=export_packages,
+      # 3.6 and up, but not Python 4
+      python_requires='~=3.6',
+      install_requires=[
+          'attrs>=17.3.0',
+          'frozendict',
+          'typing_extensions',
+          'sortedcontainers>=1.5.9'
+      ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+      )
