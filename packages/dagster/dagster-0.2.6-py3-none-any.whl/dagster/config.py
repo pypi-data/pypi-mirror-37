7c2d5e4e04672b9714cc39795ca724b7ee8ecb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:34 +0000

    Add package dagster/0.2.6/dagster-0.2.6-py3-none-any.whl

---
+++ packages/dagster/dagster-0.2.6-py3-none-any.whl/dagster/config.py
@@ -0,0 +1,6 @@
+from .core.config import (
+    Context,
+    Environment,
+    Expectations,
+    Solid,
+)
---
+++ packages/dagster/dagster-0.2.6-py3-none-any.whl/dagster/utils/__init__.py
@@ -0,0 +1,25 @@
+import inspect
+import os
+import yaml
+
+from dagster import check
+
+
+def script_relative_path(file_path):
+    '''
+    Useful for testing with local files. Use a path relative to where the
+    test resides and this function will return the absolute path
+    of that file. Otherwise it will be relative to script that
+    ran the test
+    '''
+    # from http://bit.ly/2snyC6s
+
+    check.str_param(file_path, 'file_path')
+    scriptdir = inspect.stack()[1][1]
+    return os.path.join(os.path.dirname(os.path.abspath(scriptdir)), file_path)
+
+
+def load_yaml_from_path(path):
+    check.str_param(path, 'path')
+    with open(path, 'r') as ff:
+        return yaml.load(ff)
