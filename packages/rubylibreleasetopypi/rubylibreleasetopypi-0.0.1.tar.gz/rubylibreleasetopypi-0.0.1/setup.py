f94e884c81c894531936b4aca344655c2900825a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package rubylibreleasetopypi/0.0.1/rubylibreleasetopypi-0.0.1.tar.gz

---
+++ packages/rubylibreleasetopypi/rubylibreleasetopypi-0.0.1.tar.gz/rubylibreleasetopypi-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'rubylibreleasetopypi',
+    version = '0.0.1',
+    keywords = ('rubylib'),
+    description = 'only for test private pypi repo',
+    license = '',
+    install_requires = [
+        ],
+
+    scripts = [],
+
+    author = 'http://www.liaohuqiu.net',
+    author_email = 'liaohuqiu@gmail.com',
+    url = '',
+
+    packages = find_packages(),
+    platforms = 'any',
+)
