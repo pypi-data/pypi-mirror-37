453838a0ad20f819fb01ca9d8b4851b75a5719dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:14 +0000

    Add package arango-orm/0.4.1/arango-orm-0.4.1.tar.gz

---
+++ packages/arango-orm/arango-orm-0.4.1.tar.gz/arango-orm-0.4.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'six',
+    'python-arango>=4.0',
+    'marshmallow>=2.10.0'
+]
+
+setup(
+    name='arango-orm',
+    version='0.4.1',
+    description='A SQLAlchemy like ORM implementation for arangodb',
+    long_description=("A SQLAlchemy like ORM implementation using "
+                      " python-arangoas the backend library"),
+    classifiers=[
+        "Programming Language :: Python"
+    ],
+    author='Kashif Iftikhar',
+    author_email='kashif@compulife.com.pk',
+    url="https://github.com/threatify/arango-orm",
+    download_url="https://github.com/threatify/arango-orm/archive/v4.0.tar.gz",
+    license="GNU General Public License v3 (GPLv3)",
+    keywords='arangodb orm python',
+    packages=find_packages(),
+    install_requires=requires
+)
