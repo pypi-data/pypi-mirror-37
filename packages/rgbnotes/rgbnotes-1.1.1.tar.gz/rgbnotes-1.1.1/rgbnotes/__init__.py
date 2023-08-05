4f689392b35e42a17da83e35fb9ff302b3bacfa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:49 +0000

    Add package rgbnotes/1.1.1/rgbnotes-1.1.1.tar.gz

---
+++ packages/rgbnotes/rgbnotes-1.1.1.tar.gz/rgbnotes-1.1.1/rgbnotes/__init__.py
@@ -0,0 +1,9 @@
+''' 
+The RGB Notes Python library provides convenient access to the RGB Notes API 
+from applications written in the Python language. It includes a pre-defined 
+RGB_API class with methods that talk to the rgbnotes API.
+
+'''
+
+from rgbnotes.api import * # noqa
+from rgbnotes.api_config import * # noqa
