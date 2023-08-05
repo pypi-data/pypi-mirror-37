a1152ea377c9491e1b64e730b7995d50cfbbeeac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:16 +0000

    Add package pyairmore/0.1.0a9/pyairmore-0.1.0a9.tar.gz

---
+++ packages/pyairmore/pyairmore-0.1.0a9.tar.gz/pyairmore-0.1.0a9/pyairmore/__init__.py
@@ -0,0 +1,13 @@
+"""
+Pyairmore package
+"""  # todo [1] package doc
+
+__version__ = "0.1.0a9"
+__author__ = "Eray Erdin"
+
+
+class Refreshable:
+    # todo doc
+
+    def refresh(self) -> None:
+        raise NotImplementedError("This method needs to be overriden by children.")
