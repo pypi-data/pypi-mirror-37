32235f9ca849934bd9de95c3086385e320e032a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:58 +0000

    Add package fabricasdk/0.1.1/fabricasdk-0.1.1.tar.gz

---
+++ packages/fabricasdk/fabricasdk-0.1.1.tar.gz/fabricasdk-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+   name="fabricasdk",
+   version="0.1.1",
+   description="fabrica sdk",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   author="Fabrica Inc.",
+   author_email="info@fabrica.city",
+   packages=find_packages(),
+   package_data={
+       "fabricasdk": ["resources/blockchain/**/*.json", "contracts/*"]
+   },
+   install_requires=[
+       "ecdsa==0.13",
+       "ipfsapi==1.11.0",
+       "requests==2.19.1",
+       "rfc3986==1.1.0",
+       "web3==4.6.0"
+   ]
+)
