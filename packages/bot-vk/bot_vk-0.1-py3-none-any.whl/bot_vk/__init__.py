866b7c49d91616e29e2ae428ec6a46c9238a00ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:33 +0000

    Add package bot-vk/0.1/bot_vk-0.1-py3-none-any.whl

---
+++ packages/bot-vk/bot_vk-0.1-py3-none-any.whl/bot_vk/__init__.py
@@ -0,0 +1,14 @@
+from .keyboard_for_bot import keyboard
+from .bot_python import internet_on,auth_vk,dele,vk_bot,parse
+from .bot_longpoll import longpoll
+from .chat_bot import chat
+bot_longpoll,bot_python,keyboard_for_bot,chat_bot = None,None,None,None
+globals().pop("bot_longpoll"),globals().pop("chat_bot"),globals().pop("bot_python"),globals().pop("keyboard_for_bot")
+
+
+__version__ = "1.0"
+__email__ = "imartemy1524@gmail.com"
+__vk__ = "https://vk.com/imartemy"
+__doc__="https://vk.com/@bot_vk_python-doc-1-0"
+__url__="https://vk.com/@bot_vk_python-doc-1-0"
+name="bot_vk"
