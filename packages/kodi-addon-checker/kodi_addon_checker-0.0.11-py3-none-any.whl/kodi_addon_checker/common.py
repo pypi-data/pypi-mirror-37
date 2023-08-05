8a1b8b45f051d5ae3f055ad229f37c6314b3b0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:16 +0000

    Add package kodi-addon-checker/0.0.11/kodi_addon_checker-0.0.11-py3-none-any.whl

---
+++ packages/kodi-addon-checker/kodi_addon_checker-0.0.11-py3-none-any.whl/kodi_addon_checker/common.py
@@ -0,0 +1,34 @@
+import importlib
+import os
+import pkgutil
+import sys
+
+REL_PATH = ""
+
+
+def has_transparency(im):
+    try:
+        if im.mode == "RGBA":
+            alpha = im.split()[-1]
+            listdata = list(alpha.getdata())
+            first_transparent_pixel = next(x[0]
+                                           for x in enumerate(listdata) if x[1] < 255)
+            if first_transparent_pixel is not None:
+                return True
+        else:
+            return False
+    except StopIteration:
+        return False
+
+
+def load_plugins():
+    plugins_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "plugins")
+    sys.path.append(plugins_dir)
+    for _, package_name, _ in pkgutil.iter_modules([plugins_dir]):
+        if "test_" not in package_name:
+            importlib.import_module(package_name)
+
+
+def relative_path(file_path):
+    path_to_print = file_path[len(REL_PATH):]
+    return ".{}".format(path_to_print)
