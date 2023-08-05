01a5bedb35f895babefb6510f4f07987d7b30f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package rtmbot-mattermost/0.5/rtmbot-mattermost-0.5.tar.gz

---
+++ packages/rtmbot-mattermost/rtmbot-mattermost-0.5.tar.gz/rtmbot-mattermost-0.5/rtmbot_mattermost/bin/run_rtmbot.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+from websockets.exceptions import ConnectionClosed
+from logging import getLogger
+import sys
+import os
+import yaml
+
+from rtmbot_mattermost import RtmBot
+
+logger = getLogger(__name__)
+sys.path.append(os.getcwd())
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-c',
+        '--config',
+        help='Full path to config file.',
+        metavar='path'
+    )
+    return parser.parse_args()
+
+
+def main(args=None):
+    # load args with config path if not specified
+    if not args:
+        args = parse_args()
+
+    config = yaml.load(open(args.config or 'rtmbot.conf', 'r'))
+    bot = RtmBot(config)
+
+    try:
+        bot.start()
+    except ConnectionClosed:
+        while True:
+            try:
+                bot.connect()
+            except ConnectionClosed:
+                continue
+            except KeyboardInterrupt:
+                sys.exit(0)
+            except Exception as e:
+                logger.error(repr(e))
+                sys.exit(0)
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        logger.error(repr(e))
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
