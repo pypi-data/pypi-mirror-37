570ec692e2320669c765fe1a993ad38fce768dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:16 +0000

    Add package tornado-aws/1.2.0/tornado-aws-1.2.0.tar.gz

---
+++ packages/tornado-aws/tornado-aws-1.2.0.tar.gz/tornado-aws-1.2.0/tornado_aws/__init__.py
@@ -0,0 +1,11 @@
+"""
+Low-level AWS client for Tornado
+
+"""
+from tornado_aws.client import AWSClient
+from tornado_aws.client import AsyncAWSClient
+from tornado_aws.client import exceptions
+
+__version__ = '1.2.0'
+
+__all__ = ['AWSClient', 'AsyncAWSClient', 'exceptions']
