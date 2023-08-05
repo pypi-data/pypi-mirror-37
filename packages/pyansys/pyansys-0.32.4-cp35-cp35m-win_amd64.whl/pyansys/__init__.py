4584248cfb5a685455d0b94fcd31d1f4a9bb2f15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:17 +0000

    Add package pyansys/0.32.4/pyansys-0.32.4-cp35-cp35m-win_amd64.whl

---
+++ packages/pyansys/pyansys-0.32.4-cp35-cp35m-win_amd64.whl/pyansys/__init__.py
@@ -0,0 +1,25 @@
+import warnings
+from pyansys._version import __version__
+
+from pyansys.archive_reader import *
+from pyansys.binary_reader import *
+from pyansys.cyclic_reader import *
+from pyansys.binary_reader import FullReader
+from pyansys.cellquality import *
+from pyansys.convert import ConvertFile
+from pyansys.write_archive import WriteNBLOCK, WriteArchive, WriteCMBLOCK
+
+try:
+    from pyansys.ansys import ANSYS
+    from pyansys.ansys import ChangeDefaultANSYSPath
+except Exception as e:
+    try:
+        from pyansys.ansysbase import ANSYS
+    except:
+        warnings.warn('Unable to load interactive ANSYS APDL module:\n\n%s' % str(e))
+
+try:
+    from pyansys import ansys
+    has_ansys = ansys.CheckValidANSYS()
+except:
+    has_ansys = False
