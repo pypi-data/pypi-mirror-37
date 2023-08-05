1f8189c9d4ce7f5c083a573db13cbf49bc569eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:58 +0000

    Add package euclidean/1.0.0b3/euclidean-1.0.0b3.tar.gz

---
+++ packages/euclidean/euclidean-1.0.0b3.tar.gz/euclidean-1.0.0b3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.rst", "r") as f:
+        return f.read()
+
+
+CLASSIFIERS = []
+KEYWORDS = ["euclidean", "geometry", "vector", "point", "polygon"]
+PROJECT_URLS = {}
+
+
+INSTALL_REQUIRES = ["sortedcontainers>=2", "multipledispatch>=0.6"]
+TESTS_REQUIRE = ["pytest", "coverage"]
+EXTRAS_REQUIRE = {
+    "docs": [],
+    "tests": TESTS_REQUIRE,
+    "siquant": ["siquant==4.0.0b6"],
+    "dev": TESTS_REQUIRE + ["pre-commit"],
+}
+
+setup(
+    name="euclidean",
+    version="1.0.0b3",
+    description="Euclidean Geometry Library",
+    long_description=readme(),
+    url="https://github.com/keystonetowersystems/euclidean",
+    project_urls=PROJECT_URLS,
+    author="Greg Echelberger",
+    author_email="greg@keystonetowersystems.com",
+    packages=find_packages(exclude=("tests",)),
+    setup_requires=[],
+    extras_require=EXTRAS_REQUIRE,
+    install_requires=INSTALL_REQUIRES,
+    zip_safe=True,
+)
