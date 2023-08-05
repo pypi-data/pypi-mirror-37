b1b6893263b7fcff5aa20037f68224b374df87e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:52 +0000

    Add package predictivecontrol/1.0/predictivecontrol-1.0.tar.gz

---
+++ packages/predictivecontrol/predictivecontrol-1.0.tar.gz/predictivecontrol-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='predictivecontrol',
+      version='1.0',
+      description='Python package which implements Predictive Control techniques (e.g., MPC, E-MPC)',
+      url='https://github.com/rgmaidana/predictiveControl',
+      author='Renan Maidana',
+      author_email='renan.maidana@acad.pucrs.br',
+      license='MIT',
+      packages=['predictivecontrol'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'cvxopt'
+      ],
+      zip_safe=False
+    )
