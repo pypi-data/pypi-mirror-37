a087a4d0d48150b3264a6153c8bdbd79ddca598a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:16 +0000

    Add package apimaticdemonstration1234/1.0/apimaticdemonstration1234-1.0.tar.gz

---
+++ packages/apimaticdemonstration1234/apimaticdemonstration1234-1.0.tar.gz/apimaticdemonstration1234-1.0/apimaticdemonstration1234/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'send_messages_request',
+    'send_messages_response',
+    'cancel_scheduled_message_request',
+    'check_replies_response',
+    'confirm_replies_as_received_request',
+    'confirm_replies_as_received_request_9',
+    'check_delivery_reports_response',
+    'confirm_delivery_reports_as_received_request',
+    'confirm_delivery_reports_as_received_request_12',
+]
