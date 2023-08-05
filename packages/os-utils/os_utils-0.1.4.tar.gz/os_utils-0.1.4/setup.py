0cdfb7c9cc80f04ed88969594d2be2187243f70b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:33 +0000

    Add package os-utils/0.1.4/os_utils-0.1.4.tar.gz

---
+++ packages/os-utils/os_utils-0.1.4.tar.gz/os_utils-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='os_utils',
+    packages=find_packages(include=('os_utils', )),
+    version='0.1.4',
+    description='os utils to reduce code copy paste',
+    author='Alex Buchkovsky',
+    author_email='olex.buchkovsky@gmail.com',
+    url='https://github.com/ahcub/os_utils',
+    keywords=['logging', 'path', 'dirs', 'files'],
+)
