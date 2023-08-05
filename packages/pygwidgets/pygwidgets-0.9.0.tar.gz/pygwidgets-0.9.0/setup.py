3515200c95f89f00cac406d86dc4e7cd733debfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:04 +0000

    Add package pygwidgets/0.9.0/pygwidgets-0.9.0.tar.gz

---
+++ packages/pygwidgets/pygwidgets-0.9.0.tar.gz/pygwidgets-0.9.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pygwidgets',
+    version='0.9.0',
+    author='Irv Kalb',
+    author_email='Irv@furrypants.com',
+    description='User interface widgets for use with Pygame',
+    py_modules=['pygwidgets'],
+    package_dir = {'': 'src'},
+    license="BSD",
+    url='https://github.com/IrvKalb/pygwidgets',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+      ]
+    )
