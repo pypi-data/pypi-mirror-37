02fade7f3e941a2e72c1d9bcb20fe099ddd5acd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:28 +0000

    Add package doo/0.4.0/doo-0.4.0.tar.gz

---
+++ packages/doo/doo-0.4.0.tar.gz/doo-0.4.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="doo",
+    version="0.4.0",
+    author="Leo Tong",
+    author_email="tonglei@qq.com",
+    description="doo",
+    long_description=open("README.rst").read(),
+    license="Apache License, Version 2.0",
+    url="https://github.com/tonglei100/xiaomi",
+    packages=['doo'],
+    package_data={'doo': ['*.py', '*.xlsx', '*.json', '*.yml']},
+    install_requires=[
+        'xlrd',
+        'xlsxwriter',
+        'apistar<0.6.0'
+        ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3"
+    ],
+    entry_points={
+        'console_scripts': [
+            'doo=doo:doo'
+        ]    
+
+    }
+)
