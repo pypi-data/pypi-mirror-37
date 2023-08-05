cf7b71d7928651febf16e9fcad3df2ca2b99dbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:46 +0000

    Add package ged4py/0.1.10/ged4py-0.1.10-py2.py3-none-any.whl

---
+++ packages/ged4py/ged4py-0.1.10-py2.py3-none-any.whl/ged4py/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for GEDCOM parser for Python."""
+
+__author__ = """Andy Salnikov"""
+__email__ = 'ged4py@py-dev.com'
+__version__ = '0.1.10'
+
+from . import codecs  # noqa: F401, needed to register ANSEL codec
+from .parser import GedcomReader  # noqa: F401
