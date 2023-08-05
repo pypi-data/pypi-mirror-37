d50ebd3c8edbeb79fefd2b5474f42b471971ed75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:08 +0000

    Add package yt-astro-analysis/1.0.0/yt_astro_analysis-1.0.0-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/yt-astro-analysis/yt_astro_analysis-1.0.0-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/yt_astro_analysis/sunyaev_zeldovich/api.py
@@ -0,0 +1,13 @@
+"""
+API for sunyaev_zeldovich
+"""
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2017, yt Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#-----------------------------------------------------------------------------
+
+from yt_astro_analysis.sunyaev_zeldovich.projection import \
+    SZProjection
