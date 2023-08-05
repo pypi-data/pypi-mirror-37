50c12835bf50e03b22b37d7502780dc08a3e4377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:08 +0000

    Add package collective.tiles.collection/0.1.1/collective.tiles.collection-0.1.1.tar.gz

---
+++ packages/collective.tiles.collection/collective.tiles.collection-0.1.1.tar.gz/collective.tiles.collection-0.1.1/src/collective/tiles/collection/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Init and utils."""
+
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory('collective.tiles.collection')
+
+import logging
+logger = logging.getLogger('collective.tiles.collection')
