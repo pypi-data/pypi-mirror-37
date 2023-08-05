6640a1bf27b42220a8f39881da4e60b958088ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:24 +0000

    Add package pyFrotz/0.1.3/pyFrotz-0.1.3.tar.gz

---
+++ packages/pyFrotz/pyFrotz-0.1.3.tar.gz/pyFrotz-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyFrotz',
+    version='0.1.3',
+    packages=['pyfrotz'],
+    url='https://github.com/JarbasAl/pyFrotz',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='minimal wrapper around frotz'
+)
