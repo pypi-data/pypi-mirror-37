0427f8811cdafe33dc36b20ce4b19f0e113e45a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:51 +0000

    Add package followthemoney/1.3.7/followthemoney-1.3.7-py3-none-any.whl

---
+++ packages/followthemoney/followthemoney-1.3.7-py3-none-any.whl/followthemoney/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+from followthemoney.model import Model
+from followthemoney.util import set_model_locale
+
+__version__ = '1.3.7'
+
+model_path = os.path.dirname(__file__)
+model_path = os.path.join(model_path, 'schema')
+
+# Data model singleton
+model = Model(model_path)
+
+__all__ = [model, set_model_locale]
