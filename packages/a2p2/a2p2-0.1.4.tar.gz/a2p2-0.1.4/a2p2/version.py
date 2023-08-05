42858e2f97c02c31e5c492e3524d723282bd5e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:07 +0000

    Add package a2p2/0.1.4/a2p2-0.1.4.tar.gz

---
+++ packages/a2p2/a2p2-0.1.4.tar.gz/a2p2-0.1.4/a2p2/version.py
@@ -0,0 +1,40 @@
+__version__ = "0.1.4"
+
+__release_notes__ = { 
+#"0.1.5": {
+#    "A2P2": [
+#        ""
+#        ],
+#    "VLTI": [
+#        ""
+#        ],
+#    "CHARA": [
+#        ""
+#        ]
+#    },
+"0.1.4": { 
+    "A2P2": [
+        "fix order of returned fluxes in OB.getFluxes()"
+        ]
+    },
+"0.1.3": { 
+    "VLTI": [
+        "fix telescope mode computation"
+        ]
+    },
+"0.1.2": { 
+    "A2P2": [
+        "bugfix for SPLIT polarisation mode detection on GRAVITY",
+        "bugfix that displays warning message during DIT calculation in GRAVITY LR mode",
+        "enhancement of out of bound exception message of DIT calculation method"
+        ]
+    },
+"0.1.1": { 
+    "A2P2": [
+        "muti-faciliy",
+        "multi-VltiInstruments",
+        "json VltiConfig (templates+dit tables)"
+        ]
+    }
+}
+
