531ace912d5bd4732f519d27cb1212d51f12924c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:17 +0000

    Add package myFirstPiPy/0.0.2/myFirstPiPy-0.0.2.tar.gz

---
+++ packages/myFirstPiPy/myFirstPiPy-0.0.2.tar.gz/myFirstPiPy-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="myFirstPiPy",
+    version="0.0.2",
+    author="Daniel Bunzendahl",
+    author_email="StudentDanBu@gmail.com",
+    description="My first PiP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StudentESE/myFirstPiP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
