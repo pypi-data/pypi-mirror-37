ecfedf316dcc58a91210b3f8658ad025552fcf8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package pysilk/0.0.1/pysilk-0.0.1.tar.gz

---
+++ packages/pysilk/pysilk-0.0.1.tar.gz/pysilk-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pysilk',
+    version='0.0.1',
+    description='',
+    long_description='',
+    author='CindyMoon',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
