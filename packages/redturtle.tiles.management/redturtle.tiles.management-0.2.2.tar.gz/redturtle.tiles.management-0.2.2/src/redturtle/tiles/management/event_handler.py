7b8842fc8cb99448febdcfccc3b67e3e127d4d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:53 +0000

    Add package redturtle.tiles.management/0.2.2/redturtle.tiles.management-0.2.2.tar.gz

---
+++ packages/redturtle.tiles.management/redturtle.tiles.management-0.2.2.tar.gz/redturtle.tiles.management-0.2.2/src/redturtle/tiles/management/event_handler.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from zope.annotation.interfaces import IAnnotations
+from plone import api
+import re
+
+
+def tileCreated(tile, event):
+    context = event.newParent
+    tile_id = event.newName
+    if not context:
+        return
+    annotations = IAnnotations(context)
+    if "tiles_list" not in annotations:
+        annotations['tiles_list'] = []
+
+    new_tile = {
+        'tile_id': tile_id,
+        }
+    try:
+        tile_type = re.search("@@(.*?)/", tile.url).group(1)
+    except AttributeError:
+        tile_type = ""
+    if tile_type:
+        new_tile['tile_type'] = tile_type
+    annotations['tiles_list'].append(new_tile)
+
+
+def tileDeleted(tile, event):
+    annotations = IAnnotations(tile.context)
+    for tile_info in annotations.get('tiles_list', []):
+        if tile_info.get('tile_id') == tile.id:
+            annotations['tiles_list'].remove(tile_info)
