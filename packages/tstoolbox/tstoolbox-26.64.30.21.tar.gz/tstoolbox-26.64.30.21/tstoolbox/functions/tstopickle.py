ab90b202e621b7ceb7a1b1d0251dc6018d0199d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:26 +0000

    Add package tstoolbox/26.64.30.21/tstoolbox-26.64.30.21.tar.gz

---
+++ packages/tstoolbox/tstoolbox-26.64.30.21.tar.gz/tstoolbox-26.64.30.21/tstoolbox/functions/tstopickle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+"""Collection of functions for the manipulation of time series."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import warnings
+
+import mando
+from mando.rst_text_formatter import RSTHelpFormatter
+
+from .. import tsutils
+
+warnings.filterwarnings('ignore')
+
+
+@mando.command(formatter_class=RSTHelpFormatter, doctype='numpy')
+@tsutils.doc(tsutils.docstrings)
+def tstopickle(filename,
+               input_ts='-',
+               columns=None,
+               start_date=None,
+               end_date=None,
+               round_index=None,
+               dropna='no',
+               skiprows=None,
+               index_type='datetime',
+               names=None,
+               clean=False):
+    """Pickle the data into a Python pickled file.
+
+    Can be brought back into Python with 'pickle.load' or 'numpy.load'.
+    See also 'tstoolbox read'.
+
+    Parameters
+    ----------
+    filename : str
+         The filename to store the pickled data.
+    {input_ts}
+    {columns}
+    {start_date}
+    {end_date}
+    {dropna}
+    {skiprows}
+    {index_type}
+    {names}
+    {clean}
+    {round_index}
+
+    """
+    tsd = tsutils.common_kwds(tsutils.read_iso_ts(input_ts,
+                                                  skiprows=skiprows,
+                                                  names=names,
+                                                  index_type=index_type),
+                              start_date=start_date,
+                              end_date=end_date,
+                              pick=columns,
+                              round_index=round_index,
+                              dropna=dropna,
+                              clean=clean)
+    tsd.to_pickle(filename)
