7257078989f2b9e0023330ecc9bab762b1132146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:30 +0000

    Add package natlang/0.3a12/natlang-0.3a12-py3-none-any.whl

---
+++ packages/natlang/natlang-0.3a12-py3-none-any.whl/natlang/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+from natlang import format
+
+from natlang import exporter
+from natlang import fileConverter
+from natlang import loader
+
+name = "natlang"
+version = "0.3a12"
---
+++ packages/natlang/natlang-0.3a12-py3-none-any.whl/natlang/format/txtOrTree.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Python version: 2/3
+#
+# Text and Tree loader
+# Simon Fraser University
+# Jetic Gu
+#
+#
+from __future__ import absolute_import
+import os
+import sys
+try:
+    from tree import load as loadTree
+    from txt import load as loadTxt
+except ImportError:
+    from natlang.format.tree import load as loadTree
+    from natlang.format.txt import load as loadTxt
+__version__ = "0.3a"
+
+
+def load(file, linesToLoad=sys.maxsize):
+    try:
+        contents = loadTree(file, linesToLoad)
+        contentsTxt = loadTxt(file, linesToLoad)
+        if len([f for f in contents if f is not None]) <\
+                (len(contentsTxt) / 2):
+            return contentsTxt
+    except AttributeError:
+        return loadTxt(file, linesToLoad)
+    return contents
