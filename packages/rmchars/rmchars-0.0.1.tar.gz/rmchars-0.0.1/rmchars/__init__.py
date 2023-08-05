842fb0390b6a59d65af93a5796e73054f8602545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package rmchars/0.0.1/rmchars-0.0.1.tar.gz

---
+++ packages/rmchars/rmchars-0.0.1.tar.gz/rmchars-0.0.1/rmchars/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2018, Toby Slight. All rights reserved.
+# ISC License (ISCL) - see LICENSE file for details.
+
+name = "rmchars"
+from .__main__ import traverse
+from .rmchars import rename_path
+from .create_testdir import create_rmchars_testdir
