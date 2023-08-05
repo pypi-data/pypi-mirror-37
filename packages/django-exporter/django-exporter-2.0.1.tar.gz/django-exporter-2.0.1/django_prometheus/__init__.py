8f4f005cdb5d7df6ab331ff3d2f06e6162a1756b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:26 +0000

    Add package django-exporter/2.0.1/django-exporter-2.0.1.tar.gz

---
+++ packages/django-exporter/django-exporter-2.0.1.tar.gz/django-exporter-2.0.1/django_prometheus/__init__.py
@@ -0,0 +1,14 @@
+# Import all files that define metrics. This has the effect that
+# `import django_prometheus` will always instanciate all metric
+# objects right away.
+import django_prometheus.middleware
+import django_prometheus.models
+
+# Import pip_prometheus to export the pip metrics automatically.
+try:
+    import pip_prometheus
+except ImportError:
+    # If people don't have pip, don't export anything.
+    pass
+
+default_app_config = 'django_prometheus.apps.DjangoPrometheusConfig'
