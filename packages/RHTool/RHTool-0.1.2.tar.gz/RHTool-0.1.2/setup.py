0456f8526f291afb9047c8174dc17874a8dd85e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:03 +0000

    Add package RHTool/0.1.2/RHTool-0.1.2.tar.gz

---
+++ packages/RHTool/RHTool-0.1.2.tar.gz/RHTool-0.1.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: RH
+# Mail: sculizhenghao@foxmail.com
+# Created Time:  2018-10-14 18:04:28
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "RHTool",
+    version = "0.1.2",
+    keywords = ("pip", "RHTool","rhtool", "RH"),
+    description = "RH packet",
+    long_description = "This is a tool from RH， if you have any problem, please contat sculizhenghao@foxmail.com",
+    license = "MIT Licence",
+
+    url = "https://github.com/fengmm521/pipProject",
+    author = "mage",
+    author_email = "mage@woodcol.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
