f76218034fa99a934853499e0ca6778185be8e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:37 +0000

    Add package clippercard/0.4.1/clippercard-0.4.1.tar.gz

---
+++ packages/clippercard/clippercard-0.4.1.tar.gz/clippercard-0.4.1/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='clippercard',
+    version='0.4.1',
+    author='Unofficial ClipperCard API devs',
+    author_email='goldengate88@systemfu.com',
+    packages=['clippercard'],
+    package_dir = {'clippercard':'clippercard'},
+    entry_points = {
+        'console_scripts': [
+            'clippercard = clippercard.main:main',
+        ]
+    },
+    scripts=[],
+    url='https://github.com/clippercard/clippercard-python',
+    license='MIT',
+    description='Unofficial Python API for Clipper Card (transportation pass used in the San Francisco Bay Area)',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'BeautifulSoup4>=4.3.2',
+        'configparser>=3.5.0,<4.0',
+        'docopt>=0.6.1,<1.0',
+        'PrettyTable>=0.7.2',
+        'requests>=2.2.1,<3.0'
+    ],
+)
