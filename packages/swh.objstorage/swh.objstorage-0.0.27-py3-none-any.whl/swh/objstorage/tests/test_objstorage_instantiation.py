148d061c88b8239cca9feb733db5f6550b1579ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:00 +0000

    Add package swh.objstorage/0.0.27/swh.objstorage-0.0.27-py3-none-any.whl

---
+++ packages/swh.objstorage/swh.objstorage-0.0.27-py3-none-any.whl/swh/objstorage/tests/test_objstorage_instantiation.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2015-2016  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import shutil
+import tempfile
+import unittest
+
+from nose.tools import istest
+
+from swh.objstorage import get_objstorage
+from swh.objstorage.objstorage_pathslicing import PathSlicingObjStorage
+from swh.objstorage.api.client import RemoteObjStorage
+
+
+class TestObjStorageInitialization(unittest.TestCase):
+    """ Test that the methods for ObjStorage initializations with
+    `get_objstorage` works properly.
+    """
+
+    def setUp(self):
+        self.path = tempfile.mkdtemp()
+        self.path2 = tempfile.mkdtemp()
+        # Server is launched at self.url()
+        self.config = {'storage_base': self.path2,
+                       'storage_slicing': '0:1/0:5'}
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+        shutil.rmtree(self.path)
+        shutil.rmtree(self.path2)
+
+    @istest
+    def pathslicing_objstorage(self):
+        conf = {
+            'cls': 'pathslicing',
+            'args': {'root': self.path, 'slicing': '0:2/0:5'}
+        }
+        st = get_objstorage(**conf)
+        self.assertTrue(isinstance(st, PathSlicingObjStorage))
+
+    @istest
+    def remote_objstorage(self):
+        conf = {
+            'cls': 'remote',
+            'args': {
+                'url': 'http://127.0.0.1:4242/'
+            }
+        }
+        st = get_objstorage(**conf)
+        self.assertTrue(isinstance(st, RemoteObjStorage))
