6098df5f0e7911cb62d08599d30e16aec8bc026a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:12 +0000

    Add package avacoin/0.0.0.dev0/avacoin-0.0.0.dev0.tar.gz

---
+++ packages/avacoin/avacoin-0.0.0.dev0.tar.gz/avacoin-0.0.0.dev0/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+
+from setuptools import setup, find_packages
+
+NAME = 'avacoin'
+DESCRIPTION = 'avacoin'
+EMAIL = 'sbellem@gmail.com'
+AUTHOR = 'Sylvain Bellemare'
+REQUIRES_PYTHON = '>=3.7.0'
+VERSION = None
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'License :: Other/Proprietary License',
+    ],
+    packages=find_packages(),
+)
