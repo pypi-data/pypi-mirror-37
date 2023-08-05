0a6289f72e677b7f163147c1b830477da25b0f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:04 +0000

    Add package lytest/0.0.4/lytest-0.0.4-py3-none-any.whl

---
+++ packages/lytest/lytest-0.0.4-py3-none-any.whl/lytest/__init__.py
@@ -0,0 +1,21 @@
+__version__ = '0.0.4'
+
+# quickplotters so you can debug more easily
+try:
+    import lyipc.client as ipc
+    kqp = ipc.generate_display_function(None, 'debugging.gds')
+except ImportError:
+    def kqp(*args, **kwargs):
+        print('klayout quickplot is not available.\nPlease install lyipc.')
+
+try:
+    from phidl import quickplot2 as qp
+except ImportError:
+    def qp(*args, **kwargs):
+        print('phidl does not seem to be installed, so you cannot use qp')
+
+
+from lytest.kdb_xor import GeometryDifference, run_xor
+from lytest.utest_buds import store_reference, difftest_it
+from lytest.nonvisual import contained_geometry
+import lytest.command_line
