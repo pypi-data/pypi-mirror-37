f5eb515789a9b4378734801d6b81c9c5558ca253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:36 +0000

    Add package bscan/0.0.3/bscan-0.0.3.tar.gz

---
+++ packages/bscan/bscan-0.0.3.tar.gz/bscan-0.0.3/setup.py
@@ -0,0 +1,39 @@
+"""Setup configuration for the `bscan` program."""
+
+import codecs
+import os
+
+from setuptools import (
+    find_packages,
+    setup)
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+BSCAN_DIR = os.path.join(HERE, 'bscan')
+VERSION_FILE = os.path.join(BSCAN_DIR, 'version.py')
+
+with codecs.open(VERSION_FILE, encoding='utf-8') as f:
+    exec(f.read())
+    version = __version__  # noqa
+
+setup(
+    name='bscan',
+    version=version,
+    description='An asynchronous target scanner',
+    author='Brian Welch',
+    author_email='welch18@vt.edu',
+    url='https://github.com/welchbj/bscan',
+    license='MIT',
+    install_requires=['colorama', 'toml'],
+    packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': ['bscan = bscan.__main__:main']
+    },
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities'
+    ]
+)
