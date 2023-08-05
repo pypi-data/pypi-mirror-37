7f946e69858f886a0f28f8e8be418c89ba0561ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:40 +0000

    Add package modelicapy/0.2/modelicapy-0.2.tar.gz

---
+++ packages/modelicapy/modelicapy-0.2.tar.gz/modelicapy-0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+	
+setuptools.setup(
+    name="modelicapy",
+    version="0.2",
+    author="Scott Greenwood",
+    author_email="greenwoodms@ornl.gov",
+    description=("A Python library containing a variety of scripts and functions for interaction with Modelica and TRANSFORM"),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="UT-Battelle Open Source License (Permissive)",
+    keywords="modelica dymola openmodelica mat",
+    url="https://github.com/ORNL-Modelica/ModelicaPy",
+    install_requires=['future'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: Other/Proprietary License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities"
+    ],
+)
