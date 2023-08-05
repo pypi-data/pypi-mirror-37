7039fe69f3e58b7d5dac1f59faf1cddd4da7ae58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:21 +0000

    Add package yyggtest/0.6/yyggtest-0.6-cp27-cp27m-macosx_10_6_intel.whl

---
+++ packages/yyggtest/yyggtest-0.6-cp27-cp27m-macosx_10_6_intel.whl/orthodr/__init__.py
@@ -0,0 +1,33 @@
+from setuptools import Extension
+
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
+ext_modules = [
+    Extension(
+        'cpp_exports',
+        ['CppExports.cpp'],
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True)
+        ],
+        language='c++'
+    ),
+]
+
+
+print(ext_modules)
