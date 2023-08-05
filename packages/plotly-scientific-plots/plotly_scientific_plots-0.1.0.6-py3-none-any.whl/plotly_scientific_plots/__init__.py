6b1f88f3a8a53d8863db1a1fbd49e5f0c0f526b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:31 +0000

    Add package plotly-scientific-plots/0.1.0.6/plotly_scientific_plots-0.1.0.6-py3-none-any.whl

---
+++ packages/plotly-scientific-plots/plotly_scientific_plots-0.1.0.6-py3-none-any.whl/plotly_scientific_plots/__init__.py
@@ -0,0 +1,9 @@
+from plotly_scientific_plots.plotly_misc import *
+from plotly_scientific_plots.plotly_plot_tools import *
+from plotly_scientific_plots.plotly_ML import *
+from plotly_scientific_plots.misc_computational_tools import *
+from plotly_scientific_plots.dash_tools import *
+
+# this command enables visualizations in jupyter notebook
+if in_notebook():
+    pyo.init_notebook_mode(connected=True)
