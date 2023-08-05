b42eb8fed98b3e75d340b10ced81e6ec13b142eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:43 +0000

    Add package pytorch2keras/0.1.5/pytorch2keras-0.1.5-py3-none-any.whl

---
+++ packages/pytorch2keras/pytorch2keras-0.1.5-py3-none-any.whl/tests/resnet18_channels_last.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+from torch.autograd import Variable
+from pytorch2keras.converter import pytorch_to_keras
+import torchvision
+
+
+if __name__ == '__main__':
+    max_error = 0
+    for i in range(10):
+        model = torchvision.models.resnet18()
+        for m in model.modules():
+            m.training = False
+
+        input_np = np.random.uniform(0, 1, (1, 3, 224, 224))
+        input_var = Variable(torch.FloatTensor(input_np))
+        output = model(input_var)
+
+        k_model = pytorch_to_keras(model, input_var, (3, 224, 224,), verbose=True, change_ordering=True)
+
+        pytorch_output = output.data.numpy()
+        keras_output = k_model.predict(input_np.transpose(0, 2, 3, 1))
+
+        print(pytorch_output.shape, keras_output.shape)
+
+        error = np.max(pytorch_output - keras_output)
+        print(error)
+        if max_error < error:
+            max_error = error
+
+    print('Max error: {0}'.format(max_error))
