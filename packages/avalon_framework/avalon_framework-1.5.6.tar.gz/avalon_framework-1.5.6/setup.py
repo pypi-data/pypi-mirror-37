2eebbfc3fad8f74e6cabf81774a86f8e95d13483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:32 +0000

    Add package avalon_framework/1.5.6/avalon_framework-1.5.6.tar.gz

---
+++ packages/avalon_framework/avalon_framework-1.5.6.tar.gz/avalon_framework-1.5.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='avalon_framework',
+    packages=['avalon_framework'],
+    version='1.5.6',
+    description='Avalon Digital I/O Framework',
+    author='K4YT3X',
+    author_email='k4t@protonmail.com',
+    url='https://github.com/K4YT3X/AVALON',
+    download_url='https://github.com/K4YT3X/AVALON/archive/1.5.6.tar.gz',
+    keywords=['color', 'framework', 'standard'],  # arbitrary keywords
+    classifiers=[],
+)
