743653222a785063ff223c5d36d9b388eb79ba8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:16 +0000

    Add package pymatex/0.0.5/pymatex-0.0.5.tar.gz

---
+++ packages/pymatex/pymatex-0.0.5.tar.gz/pymatex-0.0.5/pymatex/node/Summation.py
@@ -0,0 +1,15 @@
+from pymatex.listener import MatexASTVisitor
+from pymatex.node.IterativeFunction import IterativeFunction, IterativeType
+
+
+class Summation(IterativeFunction):
+
+    def __init__(self, variable, start_range, end_range, expression):
+        super().__init__(IterativeType.SUM, variable, start_range, end_range, expression)
+
+    def __str__(self):
+        return '\\sum_{{{}={}}}^{{{}}}{{{}}}'.format(self.variable, self.start_range, self.end_range,
+                                                     self.expression)
+
+    def accept(self, visitor: MatexASTVisitor):
+        return visitor.visit_summation(self)
