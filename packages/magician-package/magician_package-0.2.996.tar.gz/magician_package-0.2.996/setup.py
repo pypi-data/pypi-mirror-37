30f786a4fbb16ca9995b097443530afcaa4b8124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:48 +0000

    Add package magician-package/0.2.996/magician_package-0.2.996.tar.gz

---
+++ packages/magician-package/magician_package-0.2.996.tar.gz/magician_package-0.2.996/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+        setuptools.setup(
+                    name="magician_package",
+                    version="0.2.996",
+                    author="RAISE_LAB",
+                    author_email="smajumd3@ncsu.edu",
+                    description="A small example package for text mining application with hyper-parameter optimization on multiple learning to automatically find best model",
+                    long_description=long_description,
+                    long_description_content_type="text/markdown",
+                    url="https://github.com/ai-se/magician_package",
+                    packages=setuptools.find_packages(),
+                    classifiers=[
+                        "Programming Language :: Python :: 2.7",
+                        "License :: OSI Approved :: MIT License",
+                        "Operating System :: OS Independent",
+                    ],
+        )
