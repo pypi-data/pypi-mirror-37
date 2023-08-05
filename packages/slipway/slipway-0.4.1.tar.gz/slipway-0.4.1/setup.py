365c093c7b6d1a91eb1f64a3a1376f7aa36c9766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package slipway/0.4.1/slipway-0.4.1.tar.gz

---
+++ packages/slipway/slipway-0.4.1.tar.gz/slipway-0.4.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools.command.install import install
+from setuptools import setup, find_packages
+
+setup(
+    name = 'slipway',
+    packages = find_packages(),
+    version = '0.4.1',
+    description = 'CLI tool for managing development containers',
+    author = 'Jonathan Boudreau',
+    author_email = 'jonathan.boudreau.92@gmail.com',
+    url = 'https://github.com/AGhost-7/slipway',
+    download_url = '',
+    keywords = ['docker', 'development'],
+    classifiers = [],
+    install_requires = [
+        'docker >= 3.0.0'
+    ],
+    entry_points = { 
+        'console_scripts': [
+            'slipway=slipway:main'
+        ]
+    },
+    scripts=['scripts/entrypoint.py']
+)
