a9d497ccf1fccecd54fcc6c8a4247718364b66ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:55 +0000

    Add package tibiawikisql/1.2.0/tibiawikisql-1.2.0.tar.gz

---
+++ packages/tibiawikisql/tibiawikisql-1.2.0.tar.gz/tibiawikisql-1.2.0/setup.py
@@ -0,0 +1,52 @@
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+
+
+def get_version(package):
+    """Return package version as listed in `__version__` in `init.py`."""
+    init_py = open(os.path.join(package, '__main__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='tibiawikisql',
+    version=get_version("tibiawikisql"),
+    author='Galarzaa90',
+    author_email="allan.galarza@gmail.com",
+    url='https://github.com/Galarzaa90/tibiawiki-sql',
+    license='Apache 2.0',
+    install_requires=requirements,
+    description="Python script that generates a SQLite database from TibiaWiki articles",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: SQL',
+        'Topic :: Database',
+        'Topic :: Games/Entertainment :: Role-Playing',
+        'Topic :: Internet',
+        'Topic :: Utilities'
+    ]
+)
