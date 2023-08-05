75dacb4f1969ee5664d44fa90f7c97b5b1ff228d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:21 +0000

    Add package resolwe/13.0.0/resolwe-13.0.0.tar.gz

---
+++ packages/resolwe/resolwe-13.0.0.tar.gz/resolwe-13.0.0/resolwe/flow/serializers/process.py
@@ -0,0 +1,37 @@
+"""Resolwe process serializer."""
+from resolwe.flow.models import Process
+
+from .base import ResolweBaseSerializer
+
+
+class ProcessSerializer(ResolweBaseSerializer):
+    """Serializer for Process objects."""
+
+    class Meta:
+        """ProcessSerializer Meta options."""
+
+        model = Process
+        read_only_fields = (
+            'created',
+            'id',
+            'modified',
+            'is_active',
+        )
+        update_protected_fields = (
+            'category',
+            'contributor',
+            'data_name',
+            'description',
+            'flow_collection',
+            'input_schema',
+            'name',
+            'output_schema',
+            'persistence',
+            'requirements',
+            'run',
+            'scheduling_class',
+            'slug',
+            'type',
+            'version',
+        )
+        fields = read_only_fields + update_protected_fields
