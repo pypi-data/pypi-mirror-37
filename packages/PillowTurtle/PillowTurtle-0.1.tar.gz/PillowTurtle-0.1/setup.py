d841fb755fb096866cf31d7bedb26a522f87f26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:27 +0000

    Add package PillowTurtle/0.1/PillowTurtle-0.1.tar.gz

---
+++ packages/PillowTurtle/PillowTurtle-0.1.tar.gz/PillowTurtle-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="PillowTurtle",
+    version="0.1",
+    description="",
+    url="",
+    author="Pixelwar",
+    author_email="code@pxlwr.de",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+    ],
+    license="MIT",
+    py_modules=["PillowTurtle"],
+    requires=["PIL"],
+    zip_safe=False
+)
