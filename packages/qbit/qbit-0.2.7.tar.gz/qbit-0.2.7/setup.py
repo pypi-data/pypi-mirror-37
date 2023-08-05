504ee71f6e9bf01d9803baab1f4f3561253a40b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:17 +0000

    Add package qbit/0.2.7/qbit-0.2.7.tar.gz

---
+++ packages/qbit/qbit-0.2.7.tar.gz/qbit-0.2.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='qbit',
+      version='0.2.7',
+      description='Client Library for 1QBit Quantum Cloud',
+      url='http://1qbit.com',
+      author='1 QB Information Technologies',
+      author_email='info@1qbit.com',
+      license='APLv2',
+      packages=['qbit'],
+      install_requires=[
+          'requests',
+          'grpcio-tools',
+          'googleapis-common-protos'
+      ],
+      zip_safe=False)
