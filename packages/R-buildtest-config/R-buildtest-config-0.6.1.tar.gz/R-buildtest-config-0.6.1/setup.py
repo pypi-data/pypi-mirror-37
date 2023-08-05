a2c72d66aa2092562c5d7806498eb32658a20027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package R-buildtest-config/0.6.1/R-buildtest-config-0.6.1.tar.gz

---
+++ packages/R-buildtest-config/R-buildtest-config-0.6.1.tar.gz/R-buildtest-config-0.6.1/setup.py
@@ -0,0 +1,53 @@
+############################################################################
+#
+#  Copyright 2017-2018
+#
+#   https://github.com/HPC-buildtest/buildtest-framework
+#
+#    This file is part of buildtest.
+#
+#    buildtest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    buildtest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with buildtest.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+
+
+"""
+@author: Shahzeb Siddiqui (Pfizer)
+"""
+
+from setuptools import setup, find_packages
+VERSION='0.6.1'
+
+setup(name='R-buildtest-config',
+      version=VERSION,
+      description="""R tests for buildtest-framework""",
+      long_description=open('README.rst').read(),
+      url='https://github.com/HPC-buildtest/R-buildtest-config',
+      author='Shahzeb Siddiqui',
+      author_email='shahzebmsiddiqui@gmail.com',
+      license='GPLv2',
+      platforms = "Linux",
+      classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Console",
+            "Intended Audience :: System Administrators",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python :: 3.6",
+            "Topic :: Software Development :: Build Tools",
+            "Topic :: Software Development :: Testing",
+      ],
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=['buildtest-framework=='+VERSION],
+      )
