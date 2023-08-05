61aff045c87e0fb1aafaf415ea2371a827db1ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package financeager/0.12/financeager-0.12-py2.py3-none-any.whl

---
+++ packages/financeager/financeager-0.12-py2.py3-none-any.whl/financeager/__init__.py
@@ -0,0 +1,14 @@
+import os.path
+
+# versioning information
+__version__ = "0.12"
+
+#
+# Global constants
+#
+
+# fixed date format for database
+PERIOD_DATE_FORMAT = "%m-%d"
+
+# directory for application data
+CONFIG_DIR = os.path.expanduser("~/.config/financeager")
