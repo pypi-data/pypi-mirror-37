64a66c2e90afb4b007d249a34534b07e1fd8124f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:10 +0000

    Add package sentient-shared-libraries/1.0.5/sentient-shared-libraries-1.0.5.tar.gz

---
+++ packages/sentient-shared-libraries/sentient-shared-libraries-1.0.5.tar.gz/sentient-shared-libraries-1.0.5/testing/test_config_loader.py
@@ -0,0 +1,41 @@
+import sys
+import unittest
+
+from config_loader import ConfigLoader
+
+
+class InfluxDBTest(unittest.TestCase):
+
+    @staticmethod
+    def test_influxdb():
+        local = ConfigLoader('test.conf')
+        assert local.config['influxdb']
+
+    @staticmethod
+    def test_influxdb_address():
+        local = ConfigLoader('test.conf')
+        assert local.config['influxdb']['address']
+
+    @staticmethod
+    def test_influxdb_port():
+        local = ConfigLoader('test.conf')
+        assert local.config['influxdb']['port']
+
+    @staticmethod
+    def test_influxdb_password():
+        local = ConfigLoader('test.conf')
+        assert local.config['influxdb']['password']
+
+    @staticmethod
+    def test_influxdb_user():
+        local = ConfigLoader('test.conf')
+        assert local.config['influxdb']['user']
+
+    @staticmethod
+    def test_influxdb_db():
+        local = ConfigLoader('test.conf')
+        assert local.config['influxdb']['db']
+
+
+if __name__ == '__main__':
+    unittest.main()
