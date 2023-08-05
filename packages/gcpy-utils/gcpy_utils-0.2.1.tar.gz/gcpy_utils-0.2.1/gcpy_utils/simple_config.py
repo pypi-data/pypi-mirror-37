ec58bc56f3763933263ae64fbda42cada096ca36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:02 +0000

    Add package gcpy-utils/0.2.1/gcpy_utils-0.2.1.tar.gz

---
+++ packages/gcpy-utils/gcpy_utils-0.2.1.tar.gz/gcpy_utils-0.2.1/gcpy_utils/simple_config.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+# write  by  zhou
+import ConfigParser
+import os
+
+environ = None
+try:
+    with open(os.path.expanduser("~/.environ"), "r") as f:
+        environ = f.read().strip()
+        assert environ
+except:
+    raise Exception("no ~/.environ found! you must create one!")
+
+_ = ConfigParser.ConfigParser()
+_flag = None
+
+
+def parse_config(config_dir):
+    '''
+    :param config_dir: 配置文件的目录
+    :return: 返回一个dict
+    '''
+    global _flag
+    if not _flag:
+        _.readfp(os.path.join(config_dir, "%s.ini" % environ))
+        buff = dict()
+        for s in _.sections():
+            buff[s] = {}
+            for info in _.items(s):
+                buff[s][info[0]] = info[1]
+        _flag = buff
+    return _flag
