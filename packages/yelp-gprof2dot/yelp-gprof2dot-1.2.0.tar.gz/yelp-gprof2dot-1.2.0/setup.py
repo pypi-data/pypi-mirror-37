3ba684c4b651d062b7703cfbc1265ffa3c64a2e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:09 +0000

    Add package yelp-gprof2dot/1.2.0/yelp-gprof2dot-1.2.0.tar.gz

---
+++ packages/yelp-gprof2dot/yelp-gprof2dot-1.2.0.tar.gz/yelp-gprof2dot-1.2.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# The purpose of this script is to enable uploading gprof2dot.py to the Python
+# Package Index, which can be easily done by doing:
+#
+#   python setup.py register
+#   python setup.py sdist upload
+#
+# See also:
+# - https://code.google.com/p/jrfonseca/issues/detail?id=19
+# - http://docs.python.org/2/distutils/packageindex.html
+#
+# Original version was written by Jose Fonseca; see
+# https://github.com/jrfonseca/gprof2dot
+
+import sys
+from setuptools import setup
+
+setup(
+    name='yelp-gprof2dot',
+    version='1.2.0',
+    author='Yelp Performance Team',
+    url='https://github.com/Yelp/gprof2dot',
+    description="Generate a dot graph from the output of python profilers.",
+    long_description="""
+        gprof2dot.py is a Python script to convert the output from python
+        profilers (anything in pstats format) into a dot graph. It is a
+        fork of the original gprof2dot script to extend python features.
+        """,
+    license="LGPL",
+
+    py_modules=['gprof2dot'],
+    entry_points=dict(console_scripts=['gprof2dot=gprof2dot:main']),
+)
