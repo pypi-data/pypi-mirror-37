7e34c5fa0f85972a64c3157a5b9eaa5c03601583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:50 +0000

    Add package qtxdbpackages/1.0/qtxdbpackages-1.0.tar.gz

---
+++ packages/qtxdbpackages/qtxdbpackages-1.0.tar.gz/qtxdbpackages-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qtxdbpackages',
+      version='1.0',
+      description='database connection strings',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['qtxdbpackage'],
+      zip_safe=False)
