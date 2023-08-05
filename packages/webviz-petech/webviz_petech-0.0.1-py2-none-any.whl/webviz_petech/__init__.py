f1747e60680a38c45843e7c22960014e6365e9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package webviz-petech/0.0.1/webviz_petech-0.0.1-py2-none-any.whl

---
+++ packages/webviz-petech/webviz_petech-0.0.1-py2-none-any.whl/webviz_petech/__init__.py
@@ -0,0 +1,4 @@
+"""Actual release will soon come from Travis automatic pypi distribution,
+i.e. from https://travis-ci.com/Statoil/webviz_petech. 
+
+This release is just for setting up the pypi namespace"""
