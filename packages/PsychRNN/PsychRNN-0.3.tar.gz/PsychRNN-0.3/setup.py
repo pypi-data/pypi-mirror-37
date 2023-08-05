a7708f2c0f1142230f61c013438e1acdcec8c65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package PsychRNN/0.3/PsychRNN-0.3.tar.gz

---
+++ packages/PsychRNN/PsychRNN-0.3.tar.gz/PsychRNN-0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='PsychRNN',
+    version='0.3',
+    packages=['psychrnn', 'psychrnn.tasks', 'psychrnn.backend', 'psychrnn.backend.models'],
+    license='MIT',
+    long_description=open('README.md').read(),
+
+    author="Alex Atanasov, David Brandfonbrener, Daniel Ehrlich, Jasmine Stone",
+    author_email="daniel.ehrlich@yale.edu",
+    description="Easy-to-use package for the modeling and analysis of neural network dynamics, directed towards cognitive neuroscientists.",
+    keywords="neuroscience, modeling, analysis, neural networks",
+    url="https://github.com/dbehrlich/PsychRNN",
+
+)
