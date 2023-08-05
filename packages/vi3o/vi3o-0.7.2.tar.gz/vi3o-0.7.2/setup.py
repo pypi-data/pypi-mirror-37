651ee0f5f52f6c8eb7640e85e0bb6fce2cf977ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package vi3o/0.7.2/vi3o-0.7.2.tar.gz

---
+++ packages/vi3o/vi3o-0.7.2.tar.gz/vi3o-0.7.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, Command
+from vi3o import __version__
+import sys
+
+class PyTestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import pytest
+        errno = pytest.main()
+        sys.exit(errno)
+
+setup(
+    name='vi3o',
+    description='VIdeo and Image IO',
+    long_description='''
+Utility for loading/saving/displaying video and images. It gives random
+access to mjpg (in http multipart format) and H264 (in .mkv format) video
+frames. For recordings origination from Axis cameras the camera system
+time at the time of capture is provided as timestamp for each frame.
+    ''',
+    version=__version__,
+    packages=['vi3o'],
+    zip_safe=False,
+    url='http://vi3o.readthedocs.org',
+    author='Hakan Ardo',
+    author_email='hakan@debian.org',
+    license='MIT',
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["build_mjpg.py:ffi", "build_mkv.py:ffi"],
+    install_requires=["cffi>=1.0.0", "numpy>=1.7.1"],
+    cmdclass={'test': PyTestCommand},
+    tests_require=['pytest'],
+)
