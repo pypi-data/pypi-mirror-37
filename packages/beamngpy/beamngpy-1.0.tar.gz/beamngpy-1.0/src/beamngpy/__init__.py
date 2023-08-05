21edc013909e4e7ab01930747f340755f85dd6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:00 +0000

    Add package beamngpy/1.0/beamngpy-1.0.tar.gz

---
+++ packages/beamngpy/beamngpy-1.0.tar.gz/beamngpy-1.0/src/beamngpy/__init__.py
@@ -0,0 +1,11 @@
+"""
+BeamNGPy API module.
+
+.. moduleauthor:: Marc Müller <mmueller@beamng.gmbh>
+"""
+from . import beamng_common, sensors
+
+from .beamng import *
+
+from .scenario import *
+from .vehicle import *
