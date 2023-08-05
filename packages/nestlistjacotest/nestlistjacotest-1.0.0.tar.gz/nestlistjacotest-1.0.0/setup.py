39fe69f059dfe5d3c5eb2c5a94d3f2be1285709e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:05 +0000

    Add package nestlistjacotest/1.0.0/nestlistjacotest-1.0.0.tar.gz

---
+++ packages/nestlistjacotest/nestlistjacotest-1.0.0.tar.gz/nestlistjacotest-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = 'nestlistjacotest',
+        version         = '1.0.0',
+        py_modules      = ['nestlistjaco'],
+        author          = 'Jackos',
+        author_email    = 'jackos@headfirstlabs.com',
+        url             = 'http://www.headfirstlabs.com',
+        description     = 'a simple printer of nested lists',
+      )
