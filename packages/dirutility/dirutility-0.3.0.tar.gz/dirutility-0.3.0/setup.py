1b185f7978e20f27f00d1b69e390da290a39d062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package dirutility/0.3.0/dirutility-0.3.0.tar.gz

---
+++ packages/dirutility/dirutility-0.3.0.tar.gz/dirutility-0.3.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dirutility',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=[
+        'looptools>=1.0.0',
+        'tqdm',
+    ],
+    url='https://github.com/mrstephenneal/dirutility',
+    license='MIT License',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Add compare module and GUI to parse two directories and return lists of unique files.',
+    long_description='Utility functions for quickly reading directory contents and refactoring folder structure'
+)
