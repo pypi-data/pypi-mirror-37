5b8a5f30d3cd2ca4a9b88feda07734d1e086fc33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:51 +0000

    Add package imma/0.4.1/imma-0.4.1.tar.gz

---
+++ packages/imma/imma-0.4.1.tar.gz/imma-0.4.1/imma/measure.py
@@ -0,0 +1,87 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+# import copy
+import numpy as np
+from scipy.sparse import csc_matrix
+
+
+class CooccurrenceMatrix(object):
+    def __init__(self, data, dtype=int):
+        self.update_cooccurrence_matrix(data)
+        self.dtype = dtype
+
+    def update_cooccurrence_matrix(self, data):
+        self.cooccurrence_matrix = cooccurrence_matrix(data)
+
+    def get(self, intensity0, intensity1):
+        return self.cooccurrence_matrix[intensity0][intensity1]
+
+    def to_ndarray(self):
+        keys = self.keys()
+        inv_keys = self.inv_keys()
+        sz = len(self.cooccurrence_matrix)
+        ndnghb = np.zeros([sz, sz], dtype=self.dtype)
+        for keyx in self.cooccurrence_matrix:
+            nghbx = self.cooccurrence_matrix[keyx]
+            for keyy in nghbx:
+                value = nghbx[keyy]
+                ndnghb[inv_keys[keyx], inv_keys[keyy]] = value
+        return ndnghb
+
+    def keys(self):
+        return sorted(list(self.cooccurrence_matrix.keys()))
+
+    def inv_keys(self):
+        keys = list(self.keys())
+        ii = list(range(len(keys)))
+        return dict(zip(keys, ii))
+
+
+def cooccurrence_matrix(data):
+    csc_matrix((3, 4), dtype=np.int8).toarray()
+
+    nbm = {}
+    it = np.nditer(data, flags=['multi_index'])
+    while not it.finished:
+        mindex0 = it.multi_index
+        # print("%d <%s>" % (it[0], mindex0), end=' ')
+        data_value0 = data[mindex0]
+        for axn in range(len(mindex0)):
+            mindex1 = list(mindex0)
+            mindex1[axn] = mindex1[axn] + 1
+            mindex1 = tuple(mindex1)
+            if np.all(np.asarray(mindex1) < np.asarray(data.shape)):
+                data_value1 = data[mindex1]
+                # budeme vyplnovat jen spodni
+                # if data_value0 < data_value1:
+                #     data_value0s = data_value0
+                #     data_value1s = data_value1
+                # else:
+                #     data_value0s = data_value1
+                #     data_value1s = data_value0
+                data_value0s = data_value0
+                data_value1s = data_value1
+
+                if data_value0s not in nbm.keys():
+                    nbm[data_value0s] = {}
+                if data_value1s not in nbm[data_value0s].keys():
+                    nbm[data_value0s][data_value1s] = 0
+                nbm[data_value0s][data_value1s] += 1
+
+                if data_value1s not in nbm.keys():
+                    nbm[data_value1s] = {}
+                if data_value0s not in nbm[data_value1s].keys():
+                    nbm[data_value1s][data_value0s] = 0
+
+                # if data_value0s != data_value1s:
+                #     # we dont want to put the numer into diagonal for twice
+                # on diagonal there will be doubled values
+                nbm[data_value1s][data_value0s] += 1
+
+        it.iternext()
+    return nbm
