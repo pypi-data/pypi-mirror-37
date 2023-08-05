71df7d01d51d2aea7792b1047044fe318a16480f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:32 +0000

    Add package parsl/0.6.2a1/parsl-0.6.2a1-py3-none-any.whl

---
+++ packages/parsl/parsl-0.6.2a1-py3-none-any.whl/parsl/tests/configs/cc_in2p3_local_single_node.py
@@ -0,0 +1,41 @@
+"""
+================== Block
+| ++++++++++++++ | Node
+| |            | |
+| |    Task    | |             . . .
+| |            | |
+| ++++++++++++++ |
+==================
+"""
+from parsl.channels import LocalChannel
+from parsl.providers import GridEngineProvider
+from parsl.config import Config
+from parsl.executors.ipp import IPyParallelExecutor
+from parsl.tests.utils import get_rundir
+
+# If you are a developer running tests, make sure to update parsl/tests/configs/user_opts.py
+# If you are a user copying-and-pasting this as an example, make sure to either
+#       1) create a local `user_opts.py`, or
+#       2) delete the user_opts import below and replace all appearances of `user_opts` with the literal value
+#          (i.e., user_opts['swan']['username'] -> 'your_username')
+from .user_opts import user_opts
+
+config = Config(
+    executors=[
+        IPyParallelExecutor(
+            label='cc_in2p3_local_single_node',
+            provider=GridEngineProvider(
+                channel=LocalChannel(),
+                nodes_per_block=1,
+                tasks_per_node=1,
+                init_blocks=1,
+                max_blocks=1,
+                walltime="00:20:00",
+                overrides=user_opts['cc_in2p3']['overrides'],
+            ),
+            engine_debug_level='DEBUG',
+        )
+
+    ],
+    run_dir=get_rundir()
+)
