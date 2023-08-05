37d1ac6afa56745546ffcd89bd33072103ecdc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package ExceptionHandler/0.1/ExceptionHandler-0.1.tar.gz

---
+++ packages/ExceptionHandler/ExceptionHandler-0.1.tar.gz/ExceptionHandler-0.1/ExceptionHandler/ExceptionHandler.py
@@ -0,0 +1,25 @@
+class ExceptionHandler(Exception):
+    def __init__(self, message, status):
+        super().__init__(message, status)
+        self.message =  message
+        self.status = status
+
+special_character_list = ['@','!', '$']
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+def specialCharacterCheck(str):
+    for char in str:
+        if char.lower() in special_character_list:
+            string = ''.join(special_character_list)
+            raise ExceptionHandler("Special Character "+ string +" Not Accepted",406) # 406 not exceptable Status
+
+def numberExceptionCheck(number):
+    for num in number:
+        if num in ''.join(str(e) for e in number_list):
+            string = ''.join(str(e) for e in num)
+            raise ExceptionHandler("number "+ string +" Not Accepted",406) # 406 not exceptable Status
+
+try:
+    print (numberExceptionCheck("mir!1@@"))
+except Exception as e:
+    print("Exception Reason:",e.message)
---
+++ packages/ExceptionHandler/ExceptionHandler-0.1.tar.gz/ExceptionHandler-0.1/ExceptionHandler/__init__.py
@@ -0,0 +1,2 @@
+from .ExceptionHandler import *
+__version__ = '0.1'
