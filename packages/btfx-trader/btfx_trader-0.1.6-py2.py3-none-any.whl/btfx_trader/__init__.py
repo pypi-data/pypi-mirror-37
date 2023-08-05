8904c1dfa29118138ac1bd70d15143e0a89224ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:53 +0000

    Add package btfx-trader/0.1.6/btfx_trader-0.1.6-py2.py3-none-any.whl

---
+++ packages/btfx-trader/btfx_trader-0.1.6-py2.py3-none-any.whl/btfx_trader/__init__.py
@@ -0,0 +1,11 @@
+from .public import get_symbols, get_symbols_as_updated, PublicData
+from .private import Trader
+
+__author__ = """Djordje Pepic"""
+__email__ = 'djordje.m.pepic@gmail.com'
+__version__ = '0.1.6'
+
+__all__ = [
+    'get_symbols', 'get_symbols_as_updated',
+    'PublicData', 'Trader'
+]
