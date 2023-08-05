d056969f7712427380940a374fcb3e83005cdc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:32 +0000

    Add package yinsolidated/1.2.1/yinsolidated-1.2.1-py2.py3-none-any.whl

---
+++ packages/yinsolidated/yinsolidated-1.2.1-py2.py3-none-any.whl/yinsolidated/__init__.py
@@ -0,0 +1,12 @@
+###############################################################################
+# Copyright (c) 2017 128 Technology, Inc.
+# All rights reserved.
+###############################################################################
+
+"""YINsolidated package"""
+
+# pylint: disable=wildcard-import,unused-wildcard-import
+
+# Forward module definitions
+from yinsolidated._version import __version__
+from yinsolidated.parser import *
