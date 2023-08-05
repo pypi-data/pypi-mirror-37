1087da1b0396273e1e1567a419225775b99878a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:19 +0000

    Add package MODApy/0.0.5.dev26/MoDAPy-0.0.5.dev26.tar.gz

---
+++ packages/MODApy/MoDAPy-0.0.5.dev26.tar.gz/MoDAPy-0.0.5.dev26/MoDAPy/panelmdl.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from MoDAPy import vcfmgr
+
+
+def panelrun(panel, vcffile):
+	pldf = pd.ExcelFile(panel).parse('GeneList')
+	gsymbollist = list(pldf.GeneSymbol.unique())
+	if type(vcffile) == pd.DataFrame:
+		vcfdf = vcffile
+	elif type(vcffile) == str:
+		vcfdf = vcfmgr.vcf_to_df(vcffile)
+
+	df_final = check_panel(gsymbollist,vcfdf)
+	df_final.name = vcfdf.name
+
+	return df_final
+
+
+def check_panel(genelist, vcf: pd.DataFrame):
+	result = pd.DataFrame()
+	for gene in genelist:
+		result = result.append(vcf.loc[vcf['Gene_Name'].str.contains(gene)])
+	result.set_index(['CHROM', 'POS', 'REF', 'ALT'], inplace=True)
+	return result
