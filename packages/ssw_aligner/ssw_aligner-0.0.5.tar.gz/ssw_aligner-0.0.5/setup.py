86df1aa2604ac439fa1a59f237d1298d1e219a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:40 +0000

    Add package ssw_aligner/0.0.5/ssw_aligner-0.0.5.tar.gz

---
+++ packages/ssw_aligner/ssw_aligner-0.0.5.tar.gz/ssw_aligner-0.0.5/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+import numpy as np
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+
+extensions = cythonize([
+    Extension("ssw_aligner.ssw_wrapper",
+              ["ssw_aligner/ssw_wrapper.pyx",
+               "ssw_aligner/_lib/ssw.c"],
+              extra_compile_args=['-Wno-error=declaration-after-statement'])
+])
+
+
+setup(name='ssw_aligner',
+      version='0.0.5',
+      license='MIT',
+      description='Python implementation of Striped Smith-Waterman Algorithm',
+      author="kyu999",
+      author_email="kyukokkyou999@gmail.com",
+      maintainer="kyu999",
+      maintainer_email="kyukokkyou999@gmail.com",
+      url='https://github.com/kyu999/ssw_aligner',
+      packages=find_packages(),
+      ext_modules=extensions,
+      include_dirs=[np.get_include()]
+)
