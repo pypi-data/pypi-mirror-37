16bf0075d994eb3e8b451913cbe88ac0d556cf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package dirutility/0.2.23/dirutility-0.2.23.tar.gz

---
+++ packages/dirutility/dirutility-0.2.23.tar.gz/dirutility-0.2.23/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dirutility',
+    version='0.2.23',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm',
+        'looptools',
+        'databasetools',
+        'PySimpleGUI',
+    ],
+    url='https://github.com/mrstephenneal/dirutility',
+    license='MIT License',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='Add compare module and GUI to parse two directories and return lists of unique files.',
+    long_description='Utility functions for quickly reading directory contents and refactoring folder structure'
+)
