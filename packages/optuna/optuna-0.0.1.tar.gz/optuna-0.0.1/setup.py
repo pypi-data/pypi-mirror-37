1c9bead1937c7fd73ed86d95e0836bb130d3b8e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:32 +0000

    Add package optuna/0.0.1/optuna-0.0.1.tar.gz

---
+++ packages/optuna/optuna-0.0.1.tar.gz/optuna-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='optuna',
+    version='0.0.1',
+    description='',
+    author='Takuya Akiba',
+    author_email='akiba@preferred.jp',
+    packages=find_packages(),
+)
