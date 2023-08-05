b2220f2f0e30d6e73efdf8363a35ee873a3413ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:54 +0000

    Add package qitian-module/1.0.0/qitian-module-1.0.0.tar.gz

---
+++ packages/qitian-module/qitian-module-1.0.0.tar.gz/qitian-module-1.0.0/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+import sys
+
+version = '1.0.0'
+
+long_description = "\n".join([
+    open('README.md', 'r').read(),
+])
+
+if sys.argv[-1] == 'build':
+    os.system('python setup.py sdist bdist_wheel')
+    sys.exit()
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+
+setup(
+    name='qitian-module',
+    description=(
+        '起田(苏州)营销策划有限公司Django项目公用底层框架.'
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='framework qitian common module',
+    version=version,
+    author='Peter Han',
+    author_email='peter@qitian.com',
+    url='https://www.qitian.biz/',
+    license='MIT License',
+    packages=find_packages(include=['autopost', 'system', 'usercenter']),
+    include_package_data=True,
+    install_requires=[
+        'django>=2.0'
+    ],
+    python_requires=">=3.6, !=3.0.*",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
