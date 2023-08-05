ec9b2cbb150b4d99839492b6cdcf7448c9b74be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:52 +0000

    Add package lightsteem/0.1.5/lightsteem-0.1.5.tar.gz

---
+++ packages/lightsteem/lightsteem-0.1.5.tar.gz/lightsteem-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lightsteem',
+    version='0.1.5',
+    packages=find_packages('.'),
+    url='http://github.com/emre/lightsteem',
+    license='MIT',
+    author='emre yilmaz',
+    author_email='mail@emreyilmaz.me',
+    description='A light python client to interact with the STEEM blockchain',
+    install_requires=["requests", "backoff", "ecdsa", "dateutils"],
+    extras_require={
+        'dev': [
+            'requests_mock'
+        ]
+    }
+)
