8d76719d5536e9bd068e68156e0d5a8d8921d201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package velu/0.1/velu-0.1-py3-none-any.whl

---
+++ packages/velu/velu-0.1-py3-none-any.whl/velu/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python#!/usr/bin/python
+
+from velu.printvelu import veladd
+from velu.printvelu import velmul
+from velu.printvelu import velsub
+from velu.printvelu import veldiv
+from velu.printvelu import velprin
+
---
+++ packages/velu/velu-0.1-py3-none-any.whl/velu/printvelu.py
@@ -0,0 +1,15 @@
+
+def veladd():
+    print ("Velmurugan is tes printing add function")
+
+def velmul():
+    print ("Velmurugan is tes printing mul function")
+
+def veldiv():
+    print ("Velmurugan is tes printing div function")
+
+def velsub():
+    print ("Velmurugan is tes printing sub function")
+
+def velprint():
+    print ("Velmurugan is tes printing print function")
