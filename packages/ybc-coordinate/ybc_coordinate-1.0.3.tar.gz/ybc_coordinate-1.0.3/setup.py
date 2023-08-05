c1bd5b29f5a7d7b96b43564114ab98a058928522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:34 +0000

    Add package ybc-coordinate/1.0.3/ybc_coordinate-1.0.3.tar.gz

---
+++ packages/ybc-coordinate/ybc_coordinate-1.0.3.tar.gz/ybc_coordinate-1.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'ybc_coordinate',
+  packages = ['ybc_coordinate'],
+  version = '1.0.3',
+  description = 'get city coordinate',
+  long_description='get city coordinate',
+  author = 'KingHS',
+  author_email = '382771946@qq.com',
+  keywords = ['pip3', 'python3','python','city coordinate'],
+  license='MIT'
+)
