de72927b186fa5828b074518822230e33564f8b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:10 +0000

    Add package adi.trash/0.6/adi.trash-0.6.tar.gz

---
+++ packages/adi.trash/adi.trash-0.6.tar.gz/adi.trash-0.6/docs/CHANGES.rst
@@ -0,0 +1,50 @@
+Changelog for adi.trash
+=======================
+ 
+
+0.6 (2018-10-15)
+----------------
+
+- Fix bug on deletion from folder-contents (thanks to 'laulaz' for reporting).
+[ida]
+
+
+0.5 (2018-09-27)
+----------------
+
+- Set trashed items' workflow-state to 'private' after trashing,
+  so trashed content does not get unintentionally exposed. [ida]
+
+
+- Give temporary Contributor-role to user, for pasting into
+  trashcan and creating trashcans. Fix #7. [ida]
+
+- Include CHANGES in long-description of this egg, so it gets
+  displayed on pypi. [ida]
+
+
+0.4 (2018-03-03)
+----------------
+
+- 'ParseResult' object has no attribute 'endswith', fix error.  [amariscal]
+
+
+0.3 (2017-07-27)
+----------------
+
+- Regard URL can contain parameters, fixes #3. [idgserpro]
+
+- Add plone.api as a dependency. [idgserpro]
+
+
+0.2 (2017-04-12)
+----------------
+
+- Merge PR from idgserpro, correct repo-URL. [ida]
+
+
+0.1 (2015-11-01)
+---------------
+
+- Initial commit.
+    
---
+++ packages/adi.trash/adi.trash-0.6.tar.gz/adi.trash-0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.6'
+
+setup(name='adi.trash',
+      version=version,
+      description="Plone addon, changing deletion-behaviour: " + 
+                  "Moves items into a trash-folder, instead of deleting them.",
+      long_description=open("README.rst").read() + '\n' + 
+                       open('docs/CHANGES.rst').read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ida Ebkes',
+      author_email='contact@ida-ebkes.eu',
+      url='https://github.com/ida/adi.trash',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['adi'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'plone.api',
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
