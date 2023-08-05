3be27836e6b78241e743f113fba1c9b0cf5be20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:24 +0000

    Add package talktracker/0.12/talktracker-0.12.tar.gz

---
+++ packages/talktracker/talktracker-0.12.tar.gz/talktracker-0.12/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+      long_description = fh.read()
+
+setup(
+      name='talktracker',
+      version='0.12',
+      author='Mohammad Bashiri',
+      author_email='mohammadbashiri93@gmail.com',
+      description='Records information about the performance of participants in a group discussion',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/mohammadbashiri/talktracker',
+      license='MIT',
+      packages=find_packages(),
+      
+      # required packages
+      install_requires=['numpy', 'matplotlib', 'click'],
+      )
---
+++ packages/talktracker/talktracker-0.12.tar.gz/talktracker-0.12/talktracker/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from .talktracker import Member, Team, Session
+from .analysis import time_diff, time_add, dissect_time, to_seconds
