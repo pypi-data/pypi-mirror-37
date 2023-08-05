45f4ff3141b0af29ce3b9d52c10b85de188e2f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:51 +0000

    Add package domain/0.1.4/domain-0.1.4.tar.gz

---
+++ packages/domain/domain-0.1.4.tar.gz/domain-0.1.4/setup.py
@@ -0,0 +1,44 @@
+
+import sys
+from setuptools import setup
+from codecs import open
+from os import system
+
+# For convenience.
+if sys.argv[-1] == "publish":
+    system("python setup.py sdist upload")
+    sys.exit()
+
+def read(filename):
+    kwds = {"encoding": "utf-8"} if sys.version_info[0] >= 3 else {}
+    with open(filename, **kwds) as fp:
+        contents = fp.read()
+    return contents
+
+# Get the version information.
+from domain import  __version__
+
+setup(
+    name="domain",
+    version=__version__,
+    author="Andrew R. Casey",
+    author_email="andycasey@gmail.com",
+    description="Python client for the Domain property API",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords="domain property api client",
+    packages=["domain", "domain.authorisation"],
+    install_requires=["requests", "pyyaml"],
+    extras_require={
+        "test": ["coverage"]
+    },
+    package_data={
+        "": ["LICENSE"],
+    },
+    include_package_data=True,
+    data_files=None
+)
