bf2d5c3c966589462678fa1f5323802dc57584ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:30 +0000

    Add package kafka-transport/0.1.0/kafka-transport-0.1.0.tar.gz

---
+++ packages/kafka-transport/kafka-transport-0.1.0.tar.gz/kafka-transport-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='kafka-transport',
+      version='0.1.0',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/storborg/funniest',
+      author='Nozdrin-Plotnitsky Nikolay',
+      author_email='nozdrin.plotnitsky@gmail.com',
+      license='MIT',
+      packages=find_packages())
