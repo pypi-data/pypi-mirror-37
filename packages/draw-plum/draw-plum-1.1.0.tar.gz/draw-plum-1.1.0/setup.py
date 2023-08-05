41bc63076fa3042dc1b7598e62c41e6739f24a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:30 +0000

    Add package draw-plum/1.1.0/draw-plum-1.1.0.tar.gz

---
+++ packages/draw-plum/draw-plum-1.1.0.tar.gz/draw-plum-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(  
+        name         ='draw-plum',
+        version      ='1.1.0',
+        py_modules   =['draw-plum'],
+        author       ='MarChen',
+        author_email ='2210261316@qq.com',
+        url          ='http://www.beteceramics.com',
+        description  ='an app to draw the beautiful flowers',
+      )
