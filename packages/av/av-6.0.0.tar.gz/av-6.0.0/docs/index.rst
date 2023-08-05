c25793a1be1c4442fb07deba88c1a7e7726dbb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package av/6.0.0/av-6.0.0.tar.gz

---
+++ packages/av/av-6.0.0.tar.gz/av-6.0.0/docs/index.rst
@@ -0,0 +1,82 @@
+**PyAV** Documentation
+======================
+
+**PyAV** is a Pythonic binding for FFmpeg_. We aim to provide all of the power and control of the underlying library, but manage the gritty details as much as possible.
+
+Currently we provide the basics of:
+
+- ``libavformat``:
+  :class:`containers <.Container>`,
+  audio/video/subtitle :class:`streams <.Stream>`,
+  :class:`packets <.Packet>`;
+
+- ``libavdevice`` (by specifying a format to containers);
+
+- ``libavcodec``:
+  :class:`.Codec`,
+  :class:`.CodecContext`,
+  audio/video :class:`frames <.Frame>`,
+  :class:`data planes <.Plane>`,
+  :class:`subtitles <.Subtitle>`;
+
+- ``libavfilter``:
+  :class:`.Filter`,
+  :class:`.Graph`;
+
+- ``libswscale``:
+  :class:`.VideoReformatter`;
+
+- ``libswresample``:
+  :class:`.AudioResampler`;
+
+- and a few more utilities.
+
+.. _FFmpeg: http://ffmpeg.org
+
+
+Basic Demo
+----------
+
+.. testsetup::
+
+    path_to_video = common.fate_png() # We don't need a full QT here.
+
+
+.. testcode::
+
+    import av
+
+    container = av.open(path_to_video)
+
+    for frame in container.decode(video=0):
+        frame.to_image().save('frame-%04d.jpg' % frame.index)
+
+
+Contents
+--------
+
+.. toctree::
+    :glob:
+    :maxdepth: 2
+
+    about
+    installation
+    examples/_index
+    api/_index
+
+
+.. toctree::
+    :maxdepth: 1
+
+    hacking
+    includes
+    changelog
+    contributors
+    license
+
+
+Indices and Tables
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/av/av-6.0.0.tar.gz/av-6.0.0/tests/test_file_probing.py
@@ -0,0 +1,102 @@
+from __future__ import division
+
+from fractions import Fraction
+import sys
+
+from .common import fate_suite, av, TestCase
+
+try:
+    long
+except NameError:
+    long = int
+
+
+class TestAudioProbe(TestCase):
+    def setUp(self):
+        self.file = av.open(fate_suite('aac/latm_stereo_to_51.ts'))
+
+    def test_container_probing(self):
+        self.assertEqual(str(self.file.format), "<av.ContainerFormat 'mpegts'>")
+        self.assertEqual(self.file.format.name, 'mpegts')
+        self.assertEqual(self.file.format.long_name, "MPEG-TS (MPEG-2 Transport Stream)")
+        self.assertEqual(self.file.size, 207740)
+
+        # This is a little odd, but on OS X with FFmpeg we get a different value.
+        self.assertIn(self.file.bit_rate, (269558, 270494))
+
+        self.assertEqual(len(self.file.streams), 1)
+        self.assertEqual(self.file.start_time, long(1400000))
+        self.assertEqual(self.file.metadata, {})
+
+    def test_stream_probing(self):
+        stream = self.file.streams[0]
+        self.assertEqual(stream.index, 0)
+        self.assertEqual(stream.type, 'audio')
+        self.assertEqual(stream.name, 'aac_latm')
+        self.assertEqual(stream.long_name, 'AAC LATM (Advanced Audio Coding LATM syntax)')
+        self.assertEqual(stream.bit_rate, None)
+        self.assertEqual(stream.max_bit_rate, None)
+        self.assertEqual(stream.channels, 2)
+        self.assertEqual(stream.layout.name, 'stereo')
+        self.assertEqual(stream.rate, 48000)
+        self.assertEqual(stream.format.name, 'fltp')
+        self.assertEqual(stream.format.bits, 32)
+        self.assertEqual(stream.language, "eng")
+
+
+class TestVideoProbe(TestCase):
+
+    def setUp(self):
+        self.file = av.open(fate_suite('mpeg2/mpeg2_field_encoding.ts'))
+
+    def test_container_probing(self):
+        self.assertEqual(str(self.file.format), "<av.ContainerFormat 'mpegts'>")
+        self.assertEqual(self.file.format.name, 'mpegts')
+        self.assertEqual(self.file.format.long_name, "MPEG-TS (MPEG-2 Transport Stream)")
+        self.assertEqual(self.file.size, 800000)
+
+        # This is a little odd, but on OS X with FFmpeg we get a different value.
+        self.assertIn(self.file.duration, (1620000, 1580000))
+
+        self.assertEqual(self.file.bit_rate, 8 * self.file.size * av.time_base // self.file.duration)
+        self.assertEqual(len(self.file.streams), 1)
+        self.assertEqual(self.file.start_time, long(22953408322))
+        self.assertEqual(self.file.size, 800000)
+        self.assertEqual(self.file.metadata, {})
+
+    def test_stream_probing(self):
+        stream = self.file.streams[0]
+
+        # actual stream properties
+        self.assertEqual(stream.average_rate, Fraction(25, 1))
+        self.assertEqual(stream.duration, 145800)
+        self.assertEqual(stream.frames, 0)
+        self.assertEqual(stream.id, 4131)
+        self.assertEqual(stream.index, 0)
+        self.assertEqual(stream.language, None)
+        self.assertEqual(stream.metadata, {})
+        self.assertEqual(stream.profile, 'Simple')
+        self.assertEqual(stream.start_time, 2065806749)
+        self.assertEqual(stream.time_base, Fraction(1, 90000))
+
+        # codec properties
+        self.assertEqual(stream.long_name, 'MPEG-2 video')
+        self.assertEqual(stream.name, 'mpeg2video')
+        self.assertEqual(stream.type, 'video')
+
+        # codec context properties
+        self.assertEqual(stream.bit_rate, 3364800)
+        self.assertEqual(stream.display_aspect_ratio, Fraction(4, 3))
+        self.assertEqual(stream.format.name, 'yuv420p')
+        self.assertFalse(stream.has_b_frames)
+        self.assertEqual(stream.gop_size, 12)
+        self.assertEqual(stream.height, 576)
+        self.assertEqual(stream.max_bit_rate, None)
+        self.assertEqual(stream.sample_aspect_ratio, Fraction(16, 15))
+        self.assertEqual(stream.width, 720)
+
+        # For some reason, these behave differently on OS X (@mikeboers) and
+        # Ubuntu (Travis). We think it is FFmpeg, but haven't been able to
+        # confirm.
+        self.assertIn(stream.coded_width, (720, 0))
+        self.assertIn(stream.coded_height, (576, 0))
