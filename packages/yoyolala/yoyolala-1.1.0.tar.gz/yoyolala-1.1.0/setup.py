cd9d8879d90c8138631b3d67d47f35719d7359be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:56 +0000

    Add package yoyolala/1.1.0/yoyolala-1.1.0.tar.gz

---
+++ packages/yoyolala/yoyolala-1.1.0.tar.gz/yoyolala-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='yoyolala',
+    version='1.1.0',
+    py_modules=['yoyolala'],
+    author='wangdan',
+    author_email='1449489540@qq.com',
+    url="http://www.headfirstpythonlabs.com",
+    description='A simple printer of nested lists',
+    )
---
+++ packages/yoyolala/yoyolala-1.1.0.tar.gz/yoyolala-1.1.0/yoyolala.py
@@ -0,0 +1,12 @@
+"""This is 'nester.py' module,it provides a function named print_lol() can print
+the list may include nested list"""
+def print_lol(the_list,level):
+    """The function get a positional parameter called the_list can be any list(include
+nested list),every element will be print in screen with every element in a line"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
