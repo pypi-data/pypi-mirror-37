e2c5d2889322b9358b1a93408cf2f1e20a448d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:11 +0000

    Add package jet-django/0.2.4/jet-django-0.2.4.tar.gz

---
+++ packages/jet-django/jet-django-0.2.4.tar.gz/jet-django-0.2.4/jet_django/views/exception_handler.py
@@ -0,0 +1,27 @@
+import logging
+
+from jet_django.deps.rest_framework import status
+from jet_django.deps.rest_framework.compat import set_rollback
+from jet_django.deps.rest_framework.views import exception_handler
+from jet_django.deps.rest_framework.response import Response
+
+logger = logging.getLogger(__name__)
+
+
+def jet_exception_handler(exc, context):
+    response = exception_handler(exc, context)
+
+    if not response:
+        data = {'detail': 'Server Error'}
+        set_rollback()
+        logger.exception('Jet view exception', exc_info=exc)
+        response = Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    if context and 'request' in context and context['request'].method == 'OPTIONS':
+        response.status_code = 204
+
+        response['Access-Control-Max-Age'] = '1728000'
+        response['Content-Type'] = 'text/plain; charset=utf-8'
+        response['Content-Length'] = 0
+
+    return response
