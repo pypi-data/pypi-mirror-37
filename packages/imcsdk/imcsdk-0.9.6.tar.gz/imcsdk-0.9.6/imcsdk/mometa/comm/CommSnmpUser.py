868569810b474b8404c16111df7ad40c5f5b7366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package imcsdk/0.9.6/imcsdk-0.9.6.tar.gz

---
+++ packages/imcsdk/imcsdk-0.9.6.tar.gz/imcsdk-0.9.6/imcsdk/mometa/comm/CommSnmpUser.py
@@ -0,0 +1,117 @@
+"""This module contains the general information for CommSnmpUser ManagedObject."""
+
+from ...imcmo import ManagedObject
+from ...imccoremeta import MoPropertyMeta, MoMeta
+from ...imcmeta import VersionMeta
+
+
+class CommSnmpUserConsts:
+    ADMIN_ACTION_CLEAR = "clear"
+    AUTH_ = ""
+    AUTH_MD5 = "MD5"
+    AUTH_SHA = "SHA"
+    PRIVACY_ = ""
+    PRIVACY_AES = "AES"
+    PRIVACY_DES = "DES"
+    SECURITY_LEVEL_ = ""
+    SECURITY_LEVEL_AUTHNOPRIV = "authnopriv"
+    SECURITY_LEVEL_AUTHPRIV = "authpriv"
+    SECURITY_LEVEL_NOAUTHNOPRIV = "noauthnopriv"
+    PRIVACY_SHA = "SHA"
+
+
+class CommSnmpUser(ManagedObject):
+    """This is CommSnmpUser class."""
+
+    consts = CommSnmpUserConsts()
+    naming_props = set([u'id'])
+
+    mo_meta = {
+        "classic": MoMeta("CommSnmpUser", "commSnmpUser", "snmpv3-user-[id]", VersionMeta.Version151f, "InputOutput", 0xfff, [], ["admin", "read-only", "user"], [u'commSnmp'], [], ["Get", "Set"]),
+        "modular": MoMeta("CommSnmpUser", "commSnmpUser", "snmpv3-user-[id]", VersionMeta.Version2013e, "InputOutput", 0xfff, [], ["admin", "read-only", "user"], [u'commSnmp'], [], ["Get", "Set"])
+    }
+
+
+    prop_meta = {
+
+        "classic": {
+            "admin_action": MoPropertyMeta("admin_action", "adminAction", "string", VersionMeta.Version208d, MoPropertyMeta.READ_WRITE, 0x2, 0, 510, None, ["clear"], []), 
+            "auth": MoPropertyMeta("auth", "auth", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x4, None, None, None, ["", "MD5", "SHA"], []), 
+            "auth_pwd": MoPropertyMeta("auth_pwd", "authPwd", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x8, None, None, r"""(.{8,64})?""", [], []), 
+            "dn": MoPropertyMeta("dn", "dn", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x10, 0, 255, None, [], []), 
+            "id": MoPropertyMeta("id", "id", "uint", VersionMeta.Version151f, MoPropertyMeta.NAMING, 0x20, None, None, None, [], ["1-15"]), 
+            "name": MoPropertyMeta("name", "name", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x40, 0, 31, r"""[a-zA-Z0-9\._\+\-]{0,31}""", [], []), 
+            "privacy": MoPropertyMeta("privacy", "privacy", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x80, None, None, None, ["", "AES", "DES"], []), 
+            "privacy_pwd": MoPropertyMeta("privacy_pwd", "privacyPwd", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x100, None, None, r"""(.{8,64})?""", [], []), 
+            "rn": MoPropertyMeta("rn", "rn", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x200, 0, 255, None, [], []), 
+            "security_level": MoPropertyMeta("security_level", "securityLevel", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x400, None, None, None, ["", "authnopriv", "authpriv", "noauthnopriv"], []), 
+            "status": MoPropertyMeta("status", "status", "string", VersionMeta.Version151f, MoPropertyMeta.READ_WRITE, 0x800, None, None, None, ["", "created", "deleted", "modified", "removed"], []), 
+            "child_action": MoPropertyMeta("child_action", "childAction", "string", VersionMeta.Version151f, MoPropertyMeta.INTERNAL, None, None, None, None, [], []), 
+        },
+
+        "modular": {
+            "admin_action": MoPropertyMeta("admin_action", "adminAction", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x2, 0, 510, None, ["clear"], []), 
+            "auth": MoPropertyMeta("auth", "auth", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x4, None, None, None, ["", "MD5", "SHA"], []), 
+            "auth_pwd": MoPropertyMeta("auth_pwd", "authPwd", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x8, None, None, r"""(.{8,64})?""", [], []), 
+            "dn": MoPropertyMeta("dn", "dn", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x10, 0, 255, None, [], []), 
+            "id": MoPropertyMeta("id", "id", "uint", VersionMeta.Version2013e, MoPropertyMeta.NAMING, 0x20, None, None, None, [], ["1-15"]), 
+            "name": MoPropertyMeta("name", "name", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x40, 0, 31, None, [], []), 
+            "privacy": MoPropertyMeta("privacy", "privacy", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x80, None, None, None, ["", "AES", "DES", "SHA"], []), 
+            "privacy_pwd": MoPropertyMeta("privacy_pwd", "privacyPwd", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x100, None, None, r"""(.{8,64})?""", [], []), 
+            "rn": MoPropertyMeta("rn", "rn", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x200, 0, 255, None, [], []), 
+            "security_level": MoPropertyMeta("security_level", "securityLevel", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x400, None, None, None, ["", "authnopriv", "authpriv", "noauthnopriv"], []), 
+            "status": MoPropertyMeta("status", "status", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x800, None, None, None, ["", "created", "deleted", "modified", "removed"], []), 
+            "child_action": MoPropertyMeta("child_action", "childAction", "string", VersionMeta.Version2013e, MoPropertyMeta.INTERNAL, None, None, None, None, [], []), 
+        },
+
+    }
+
+    prop_map = {
+
+        "classic": {
+            "adminAction": "admin_action", 
+            "auth": "auth", 
+            "authPwd": "auth_pwd", 
+            "dn": "dn", 
+            "id": "id", 
+            "name": "name", 
+            "privacy": "privacy", 
+            "privacyPwd": "privacy_pwd", 
+            "rn": "rn", 
+            "securityLevel": "security_level", 
+            "status": "status", 
+            "childAction": "child_action", 
+        },
+
+        "modular": {
+            "adminAction": "admin_action", 
+            "auth": "auth", 
+            "authPwd": "auth_pwd", 
+            "dn": "dn", 
+            "id": "id", 
+            "name": "name", 
+            "privacy": "privacy", 
+            "privacyPwd": "privacy_pwd", 
+            "rn": "rn", 
+            "securityLevel": "security_level", 
+            "status": "status", 
+            "childAction": "child_action", 
+        },
+
+    }
+
+    def __init__(self, parent_mo_or_dn, id, **kwargs):
+        self._dirty_mask = 0
+        self.id = id
+        self.admin_action = None
+        self.auth = None
+        self.auth_pwd = None
+        self.name = None
+        self.privacy = None
+        self.privacy_pwd = None
+        self.security_level = None
+        self.status = None
+        self.child_action = None
+
+        ManagedObject.__init__(self, "CommSnmpUser", parent_mo_or_dn, **kwargs)
+
---
+++ packages/imcsdk/imcsdk-0.9.6.tar.gz/imcsdk-0.9.6/imcsdk/mometa/lsboot/LsbootSan.py
@@ -0,0 +1,104 @@
+"""This module contains the general information for LsbootSan ManagedObject."""
+
+from ...imcmo import ManagedObject
+from ...imccoremeta import MoPropertyMeta, MoMeta
+from ...imcmeta import VersionMeta
+
+
+class LsbootSanConsts:
+    STATE_DISABLED = "Disabled"
+    STATE_ENABLED = "Enabled"
+    SUBTYPE_SAN = "SAN"
+    TYPE_SAN = "SAN"
+
+
+class LsbootSan(ManagedObject):
+    """This is LsbootSan class."""
+
+    consts = LsbootSanConsts()
+    naming_props = set([u'name'])
+
+    mo_meta = {
+        "classic": MoMeta("LsbootSan", "lsbootSan", "san-[name]", VersionMeta.Version201a, "InputOutput", 0x7ff, [], ["admin", "read-only", "user"], [u'lsbootDevPrecision'], [], ["Add", "Get", "Remove", "Set"]),
+        "modular": MoMeta("LsbootSan", "lsbootSan", "san-[name]", VersionMeta.Version2013e, "InputOutput", 0x7ff, [], ["admin", "read-only", "user"], [u'lsbootDevPrecision'], [], ["Add", "Get", "Remove", "Set"])
+    }
+
+
+    prop_meta = {
+
+        "classic": {
+            "dn": MoPropertyMeta("dn", "dn", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x2, 0, 255, None, [], []), 
+            "lun": MoPropertyMeta("lun", "lun", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x4, None, None, r"""([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]){0,1}""", [], []), 
+            "name": MoPropertyMeta("name", "name", "string", VersionMeta.Version201a, MoPropertyMeta.NAMING, 0x8, None, None, r"""(([a-zA-Z0-9]{1})|([a-zA-Z0-9]{1}[a-zA-Z0-9_\-]{0,28}[a-zA-Z0-9]{1})|([a-zA-Z0-9]{2}))""", [], []), 
+            "order": MoPropertyMeta("order", "order", "uint", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x10, None, None, None, [], ["1-255"]), 
+            "rn": MoPropertyMeta("rn", "rn", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x20, 0, 255, None, [], []), 
+            "slot": MoPropertyMeta("slot", "slot", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x40, None, None, r"""([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]|MLOM){0,1}""", [], []), 
+            "state": MoPropertyMeta("state", "state", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x80, None, None, None, ["Disabled", "Enabled", "disabled", "enabled"], []), 
+            "status": MoPropertyMeta("status", "status", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x100, None, None, None, ["", "created", "deleted", "modified", "removed"], []), 
+            "subtype": MoPropertyMeta("subtype", "subtype", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x200, None, None, None, ["SAN"], []), 
+            "type": MoPropertyMeta("type", "type", "string", VersionMeta.Version201a, MoPropertyMeta.READ_WRITE, 0x400, None, None, None, ["SAN"], []), 
+            "child_action": MoPropertyMeta("child_action", "childAction", "string", VersionMeta.Version201a, MoPropertyMeta.INTERNAL, None, None, None, None, [], []), 
+        },
+
+        "modular": {
+            "dn": MoPropertyMeta("dn", "dn", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x2, 0, 255, None, [], []), 
+            "lun": MoPropertyMeta("lun", "lun", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x4, None, None, r"""([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]){0,1}""", [], []), 
+            "name": MoPropertyMeta("name", "name", "string", VersionMeta.Version2013e, MoPropertyMeta.NAMING, 0x8, None, None, r"""(([a-zA-Z0-9]{1})|([a-zA-Z0-9]{1}[a-zA-Z0-9_\-]{0,28}[a-zA-Z0-9]{1})|([a-zA-Z0-9]{2}))""", [], []), 
+            "order": MoPropertyMeta("order", "order", "uint", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x10, None, None, None, [], ["1-255"]), 
+            "rn": MoPropertyMeta("rn", "rn", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x20, 0, 255, None, [], []), 
+            "slot": MoPropertyMeta("slot", "slot", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x40, None, None, r"""([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]|MLOM|SIOC1|SIOC2|IOESlot1|IOESlot2){0,1}""", [], []), 
+            "state": MoPropertyMeta("state", "state", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x80, None, None, None, ["Disabled", "Enabled", "disabled", "enabled"], []), 
+            "status": MoPropertyMeta("status", "status", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x100, None, None, None, ["", "created", "deleted", "modified", "removed"], []), 
+            "subtype": MoPropertyMeta("subtype", "subtype", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x200, None, None, None, ["SAN"], []), 
+            "type": MoPropertyMeta("type", "type", "string", VersionMeta.Version2013e, MoPropertyMeta.READ_WRITE, 0x400, None, None, None, ["SAN"], []), 
+            "child_action": MoPropertyMeta("child_action", "childAction", "string", VersionMeta.Version2013e, MoPropertyMeta.INTERNAL, None, None, None, None, [], []), 
+        },
+
+    }
+
+    prop_map = {
+
+        "classic": {
+            "dn": "dn", 
+            "lun": "lun", 
+            "name": "name", 
+            "order": "order", 
+            "rn": "rn", 
+            "slot": "slot", 
+            "state": "state", 
+            "status": "status", 
+            "subtype": "subtype", 
+            "type": "type", 
+            "childAction": "child_action", 
+        },
+
+        "modular": {
+            "dn": "dn", 
+            "lun": "lun", 
+            "name": "name", 
+            "order": "order", 
+            "rn": "rn", 
+            "slot": "slot", 
+            "state": "state", 
+            "status": "status", 
+            "subtype": "subtype", 
+            "type": "type", 
+            "childAction": "child_action", 
+        },
+
+    }
+
+    def __init__(self, parent_mo_or_dn, name, **kwargs):
+        self._dirty_mask = 0
+        self.name = name
+        self.lun = None
+        self.order = None
+        self.slot = None
+        self.state = None
+        self.status = None
+        self.subtype = None
+        self.type = None
+        self.child_action = None
+
+        ManagedObject.__init__(self, "LsbootSan", parent_mo_or_dn, **kwargs)
+
