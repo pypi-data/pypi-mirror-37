03c0af5a2ef0b353cf7f355d6156f7fffca56476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:17 +0000

    Add package nesterzl/1.3.0/nesterzl-1.3.0.tar.gz

---
+++ packages/nesterzl/nesterzl-1.3.0.tar.gz/nesterzl-1.3.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name          ='nesterzl',
+    version       ='1.3.0',
+    py_modules    =['nesterzl'],
+    author        ='zl',
+    author_email  ='zl_study@126.com',
+    description   ='A simple printer of nested lists',
+    )
