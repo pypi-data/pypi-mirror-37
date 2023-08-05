59fa98bf22d7d0f170ed28d7302b67f1cc39198e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:55 +0000

    Add package compose-flow/3.1.4/compose-flow-3.1.4.tar.gz

---
+++ packages/compose-flow/compose-flow-3.1.4.tar.gz/compose-flow-3.1.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+SRC_PREFIX = 'src'
+
+packages = find_packages(SRC_PREFIX)
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='compose-flow',
+    url='https://github.com/openslate/compose-flow',
+    author='OpenSlate',
+    author_email='code@openslate.com',
+    version='3.1.4',
+    description='codified workflows for docker compose',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    package_dir={'':'src'},
+    packages=packages,
+    entry_points={
+        'console_scripts': [
+            'compose-flow = compose_flow.entrypoints:compose_flow',
+        ],
+    },
+    install_requires=[
+        'PyYAML',
+        'boltons',
+        'sh',
+        'tag-version',
+        'tabulate',
+        'docker-compose'
+    ]
+)
