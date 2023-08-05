8218008b36960924e809f0361791ada07e8730bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:00 +0000

    Add package responder/0.0.6/responder-0.0.6.tar.gz

---
+++ packages/responder/responder-0.0.6.tar.gz/responder-0.0.6/responder/routes.py
@@ -0,0 +1,57 @@
+from parse import parse, search
+
+
+def memoize(f):
+    def helper(self, s):
+        memoize_key = f"{f.__name__}:{s}"
+        if memoize_key not in self._memo:
+            self._memo[memoize_key] = f(self, s)
+        return self._memo[memoize_key]
+
+    return helper
+
+
+class Route:
+    def __init__(self, route, endpoint):
+        self.route = route
+        self.endpoint = endpoint
+        self._memo = {}
+
+    def __repr__(self):
+        return f"<Route {self.route!r}={self.endpoint!r}>"
+
+    def __eq__(self, other):
+        if hasattr(other, "route"):
+            # Being compared to other routes.
+            return self.route == other.route
+        else:
+            # Strings.
+            return self.does_match(other)
+
+    @property
+    def description(self):
+        return self.endpoint.__doc__
+
+    @property
+    def has_parameters(self):
+        return all([("{" in self.route), ("}" in self.route)])
+
+    @memoize
+    def does_match(self, s):
+        if s == self.route:
+            return True
+
+        named = self.incoming_matches(s)
+        return bool(len(named))
+
+    @memoize
+    def incoming_matches(self, s):
+        results = parse(self.route, s)
+        return results.named if results else {}
+
+    def url(self, testing=False, **params):
+        url = self.route.format(**params)
+        if testing:
+            url = f"http://;{url}"
+
+        return url
