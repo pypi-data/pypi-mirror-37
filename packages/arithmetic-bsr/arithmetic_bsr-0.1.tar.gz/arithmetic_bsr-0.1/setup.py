a8b22eaa7b7a345648ffd671594b99e87bff84dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:52 +0000

    Add package arithmetic-bsr/0.1/arithmetic_bsr-0.1.tar.gz

---
+++ packages/arithmetic-bsr/arithmetic_bsr-0.1.tar.gz/arithmetic_bsr-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='arithmetic_bsr',
+      version='0.1',
+      description='perform simple arithmetics',
+      url='http://github.com/storborg/funniest',
+      author='Barun Singh',
+      author_email='bkumar@quadratyx.com',
+      license='QTX',
+      packages=['arithmetic_bsr'],
+      zip_safe=False)
