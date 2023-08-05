858fb16e0d60f2f390a06419aa6544631468026a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:00 +0000

    Add package aoaomail/0.0.5/aoaomail-0.0.5.tar.gz

---
+++ packages/aoaomail/aoaomail-0.0.5.tar.gz/aoaomail-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+		name="aoaomail",
+		version="0.0.5",
+		description = "easy python email",
+		long_description = "easy python email",
+		license = "MIT license",
+		author = "czaoao",
+		author_email = "717842816@qq.com",
+		packages = find_packages(),
+		include_page_data = True,
+		platforms = "any",
+		url = "http://www.czaoao.me",
+		install_requires = []
+	)
