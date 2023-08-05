c9f9b33c45b4e7a9058cc5ed2a1af52ab1e7f3e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:13 +0000

    Add package fserver/0.0.8/fserver-0.0.8.tar.gz

---
+++ packages/fserver/fserver-0.0.8.tar.gz/fserver-0.0.8/fserver/conf.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+VERSION = '0.0.8'
+BUILD_TIME = '2018/10/13'
+
+DEBUG = True
+
+VIDEO_SUFFIX = ['mp4', 'flv', 'hls', 'dash']
+CDN_JS = {
+    'flv': 'https://cdnjs.cloudflare.com/ajax/libs/flv.js/1.4.2/flv.min.js',
+    'hls': 'https://cdn.jsdelivr.net/npm/hls.js@latest',
+    'dash': 'https://cdnjs.cloudflare.com/ajax/libs/dashjs/2.9.1/dash.all.min.js',
+    'mp4': ''
+}
+
+
