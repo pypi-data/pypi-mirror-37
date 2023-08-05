c2fb72806e63ed5c6b870ca4ccafa61137c8297b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:27 +0000

    Add package doe/0.1.1/doe-0.1.1.tar.gz

---
+++ packages/doe/doe-0.1.1.tar.gz/doe-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+setuptools.setup(
+    name="doe",
+    version="0.1.1",
+    url="https://github.com/pawanaichra/doe",
+    author="Pawan Aichra",
+    author_email="aichrapawan@gmail.com",
+    description="A simple library for design of experiments and analysis",
+    long_description=open('README.md').read(),
+    keywords='design of experiments analysis doe',
+    license='MIT',
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=['numpy', 'pandas', 'scipy', 'PTable'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+)
