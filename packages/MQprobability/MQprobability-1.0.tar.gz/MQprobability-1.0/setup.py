4617928977820d051dde284b36dd52567963db60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:05 +0000

    Add package MQprobability/1.0/MQprobability-1.0.tar.gz

---
+++ packages/MQprobability/MQprobability-1.0.tar.gz/MQprobability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='MQprobability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['MQprobability'],
+      author = 'Max Qiu',
+      author_webpage = 'https://github.com/ft9738962',
+      zip_safe=False)
