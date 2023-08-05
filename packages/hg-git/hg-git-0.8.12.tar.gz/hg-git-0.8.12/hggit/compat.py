986467f792a4099bf6402536486cadf25ce7e15b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:17 +0000

    Add package hg-git/0.8.12/hg-git-0.8.12.tar.gz

---
+++ packages/hg-git/hg-git-0.8.12.tar.gz/hg-git-0.8.12/hggit/compat.py
@@ -0,0 +1,194 @@
+from mercurial import (
+    bookmarks,
+    context,
+    phases,
+    url,
+    util as hgutil,
+)
+
+try:
+    from mercurial import encoding
+    hfsignoreclean = encoding.hfsignoreclean
+except AttributeError:
+    # compat with hg 3.2.1 and earlier, which doesn't have
+    # hfsignoreclean (This was borrowed wholesale from hg 3.2.2.)
+    _ignore = [unichr(int(x, 16)).encode("utf-8") for x in
+               "200c 200d 200e 200f 202a 202b 202c 202d 202e "
+               "206a 206b 206c 206d 206e 206f feff".split()]
+    # verify the next function will work
+    assert set([i[0] for i in _ignore]) == set(["\xe2", "\xef"])
+
+    def hfsignoreclean(s):
+        """Remove codepoints ignored by HFS+ from s.
+
+        >>> hfsignoreclean(u'.h\u200cg'.encode('utf-8'))
+        '.hg'
+        >>> hfsignoreclean(u'.h\ufeffg'.encode('utf-8'))
+        '.hg'
+        """
+        if "\xe2" in s or "\xef" in s:
+            for c in _ignore:
+                s = s.replace(c, '')
+        return s
+
+try:
+    from mercurial import vfs as vfsmod
+    hgvfs = vfsmod.vfs
+except ImportError:
+    # vfsmod was extracted in hg 4.2
+    from mercurial import scmutil
+    hgvfs = scmutil.vfs
+
+try:
+    from mercurial.utils import procutil, stringutil
+    sshargs = procutil.sshargs
+    shellquote = procutil.shellquote
+    quotecommand = procutil.quotecommand
+    binary = stringutil.binary
+except ImportError:
+    # these were moved in 4.6
+    sshargs = hgutil.sshargs
+    shellquote = hgutil.shellquote
+    quotecommand = hgutil.quotecommand
+    binary = hgutil.binary
+
+
+def gitvfs(repo):
+    """return a vfs suitable to read git related data"""
+    # Mercurial >= 3.3:  repo.shared()
+    if repo.sharedpath != repo.path:
+        return hgvfs(repo.sharedpath)
+    else:
+        return repo.vfs
+
+
+def passwordmgr(ui):
+    try:
+        realm = hgutil.urlreq.httppasswordmgrwithdefaultrealm()
+        return url.passwordmgr(ui, realm)
+    except (TypeError, AttributeError):
+        # compat with hg < 3.9
+        return url.passwordmgr(ui)
+
+# bookmarks.setcurrent was renamed to activate in hg 3.5
+if hgutil.safehasattr(bookmarks, 'activate'):
+    activatebookmark = bookmarks.activate
+else:
+    activatebookmark = bookmarks.setcurrent
+
+def advancephaseboundary(repo, tr, targetphase, nodes):
+    # hg 3.2 - advanceboundary uses transaction
+    try:
+        phases.advanceboundary(repo, tr, targetphase, nodes)
+    except TypeError:
+        phases.advanceboundary(repo, targetphase, nodes)
+
+# hg 4.3 - osutil moved, but mercurial.util re-exports listdir
+if hgutil.safehasattr(hgutil, 'listdir'):
+    listdir = hgutil.listdir
+else:
+    from mercurial import osutil
+    listdir = osutil.listdir
+
+
+try:
+    import dulwich.client
+    FetchPackResult = dulwich.client.FetchPackResult
+    read_pkt_refs = dulwich.client.read_pkt_refs
+except (AttributeError, ImportError):
+    # older dulwich doesn't return the symref where remote HEAD points, so we
+    # monkey patch it here
+    from dulwich.errors import GitProtocolError
+    from dulwich.protocol import extract_capabilities
+
+    class FetchPackResult(object):
+        """Result of a fetch-pack operation.
+        :var refs: Dictionary with all remote refs
+        :var symrefs: Dictionary with remote symrefs
+        :var agent: User agent string
+        """
+
+        def __init__(self, refs, symrefs, agent):
+            self.refs = refs
+            self.symrefs = symrefs
+            self.agent = agent
+
+    def read_pkt_refs(proto):
+        server_capabilities = None
+        refs = {}
+        # Receive refs from server
+        for pkt in proto.read_pkt_seq():
+            (sha, ref) = pkt.rstrip('\n').split(None, 1)
+            if sha == 'ERR':
+                raise GitProtocolError(ref)
+            if server_capabilities is None:
+                (ref, server_capabilities) = extract_capabilities(ref)
+                symref = 'symref=HEAD:'
+                for cap in server_capabilities:
+                    if cap.startswith(symref):
+                        sha = cap.replace(symref, '')
+            refs[ref] = sha
+
+        if len(refs) == 0:
+            return None, set([])
+        return refs, set(server_capabilities)
+
+
+def memfilectx(repo, changectx, path, data, islink=False,
+               isexec=False, copied=None):
+    # Different versions of mercurial have different parameters to
+    # memfilectx.  Try them from newest to oldest.
+    args_to_try = (
+        (repo, changectx, path, data),  # hg 4.5+
+        (repo, path, data),             # hg 3.1 - 4.5
+        (path, data),                   # hg < 3.1
+    )
+    for args in args_to_try:
+        try:
+            return context.memfilectx(*args,
+                                      islink=islink,
+                                      isexec=isexec,
+                                      copied=copied)
+        except TypeError as ex:
+            last_ex = ex
+    raise last_ex
+
+
+CONFIG_DEFAULTS = {
+    'git': {
+        'authors': None,
+        'blockdotgit': True,
+        'blockdothg': True,
+        'branch_bookmark_suffix': None,
+        'debugextrainmessage': False,   # test only -- do not document this!
+        'findcopiesharder': False,
+        'intree': None,
+        'mindate': None,
+        'public': list,
+        'renamelimit': 400,
+        'similarity': 0,
+    },
+    'hggit': {
+        'mapsavefrequency': 0,
+        'usephases': False,
+    }
+}
+
+hasconfigitems = False
+
+
+def registerconfigs(configitem):
+    global hasconfigitems
+    hasconfigitems = True
+    for section, items in CONFIG_DEFAULTS.iteritems():
+        for item, default in items.iteritems():
+            configitem(section, item, default=default)
+
+
+def config(ui, subtype, section, item):
+    if subtype == 'string':
+        subtype = ''
+    getconfig = getattr(ui, 'config' + subtype)
+    if hasconfigitems:
+        return getconfig(section, item)
+    return getconfig(section, item, CONFIG_DEFAULTS[section][item])
---
+++ packages/hg-git/hg-git-0.8.12.tar.gz/hg-git-0.8.12/hggit/gitrepo.py
@@ -0,0 +1,111 @@
+from util import isgitsshuri
+from mercurial import (
+    error,
+    util
+)
+
+peerapi = False
+try:
+    from mercurial.repository import peer as peerrepository
+    peerapi = True
+except ImportError:
+    from mercurial.peer import peerrepository
+
+
+class gitrepo(peerrepository):
+    def __init__(self, ui, path, create, intents=None):
+        if create:  # pragma: no cover
+            raise error.Abort('Cannot create a git repository.')
+        self._ui = ui
+        self.path = path
+        self.localrepo = None
+
+    _peercapabilities = ['lookup']
+
+    def _capabilities(self):
+        return self._peercapabilities
+
+    def capabilities(self):
+        return self._peercapabilities
+
+    @property
+    def ui(self):
+        return self._ui
+
+    def url(self):
+        return self.path
+
+    def lookup(self, key):
+        if isinstance(key, str):
+            return key
+
+    def local(self):
+        if not self.path:
+            raise error.RepoError
+
+    def heads(self):
+        return []
+
+    def listkeys(self, namespace):
+        if namespace == 'namespaces':
+            return {'bookmarks': ''}
+        elif namespace == 'bookmarks':
+            if self.localrepo is not None:
+                handler = self.localrepo.githandler
+                result = handler.fetch_pack(self.path, heads=[])
+                # map any git shas that exist in hg to hg shas
+                stripped_refs = {
+                    ref[11:]: handler.map_hg_get(val) or val
+                    for ref, val in result.refs.iteritems()
+                    if ref.startswith('refs/heads/')
+                }
+                return stripped_refs
+        return {}
+
+    def pushkey(self, namespace, key, old, new):
+        return False
+
+    if peerapi:
+        def branchmap(self):
+            raise NotImplementedError
+
+        def canpush(self):
+            return True
+
+        def close(self):
+            pass
+
+        def debugwireargs(self):
+            raise NotImplementedError
+
+        def getbundle(self):
+            raise NotImplementedError
+
+        def iterbatch(self):
+            raise NotImplementedError
+
+        def known(self):
+            raise NotImplementedError
+
+        def peer(self):
+            return self
+
+        def stream_out(self):
+            raise NotImplementedError
+
+        def unbundle(self):
+            raise NotImplementedError
+
+    def commandexecutor(self):
+        from mercurial.wireprotov1peer import peerexecutor
+        return peerexecutor(self)
+
+instance = gitrepo
+
+
+def islocal(path):
+    if isgitsshuri(path):
+        return True
+
+    u = util.url(path)
+    return not u.scheme or u.scheme == 'file'
