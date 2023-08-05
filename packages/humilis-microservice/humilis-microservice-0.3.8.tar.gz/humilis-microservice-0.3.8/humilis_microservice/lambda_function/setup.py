b85145d91f6b84e9363df01974685b3ae0d6112b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:03 +0000

    Add package humilis-microservice/0.3.8/humilis-microservice-0.3.8.tar.gz

---
+++ packages/humilis-microservice/humilis-microservice-0.3.8.tar.gz/humilis-microservice-0.3.8/humilis_microservice/lambda_function/setup.py
@@ -0,0 +1,21 @@
+"""Lambda function setuptools entry point."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="humilis-kinesis-processor-lambda",
+    version="0.2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    # We often need the latest version of boto3 so we include it as a req
+    install_requires=[
+        "boto3",
+        "raven",
+        "contextlib2",
+        "lambdautils>=1.6.2",
+        "werkzeug",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6"],
+    zip_safe=False
+)
