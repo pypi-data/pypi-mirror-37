12cfe7d29cfc3ab8a9490a876f5e9c5e46750e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:36 +0000

    Add package falcon-telegraf-middleware/2018.10.12.1/falcon-telegraf-middleware-2018.10.12.1.tar.gz

---
+++ packages/falcon-telegraf-middleware/falcon-telegraf-middleware-2018.10.12.1.tar.gz/falcon-telegraf-middleware-2018.10.12.1/falcon_telegraf/log_hits.py
@@ -0,0 +1,44 @@
+import warnings
+from typing import Dict, Optional
+
+import falcon
+from telegraf import TelegrafClient
+
+from .base import Middleware
+from .utils import merge_and_normalize_tags
+
+
+class LogHits(Middleware):
+
+    def __init__(
+            self,
+            telegraf_client: Optional[TelegrafClient] = None,
+            tags: Optional[Dict[str, str]] = None,
+            metric_name_prefix: Optional[str] = None,
+            metric_name: Optional[str] = None
+    ) -> None:
+        super().__init__(telegraf_client, tags, metric_name_prefix, metric_name)
+        self._metric_name_prefix = self._metric_name_prefix or 'hits-'
+
+    def process_response(self, req: falcon.Request, resp: falcon.Response, resource, req_succeeded: bool):
+        tags = merge_and_normalize_tags(self.get_tags(req, resp), req.context, resp.context)
+        self._telegraf.metric(
+            self.get_metric_name(req),
+            values={
+                'hits': 1,
+            },
+            tags=tags,
+        )
+
+
+class LogHitsContextAware(LogHits):
+
+    def __init__(
+            self,
+            telegraf_client: Optional[TelegrafClient] = None,
+            tags: Optional[Dict[str, str]] = None,
+            metric_name_prefix: Optional[str] = None,
+            metric_name: Optional[str] = None
+    ) -> None:
+        super().__init__(telegraf_client, tags, metric_name_prefix, metric_name)
+        warnings.warn('LogHitsContextAware middleware is deprecated.', DeprecationWarning)
