e7125963f1b3a1a9defcf4df02b5433695411af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:24 +0000

    Add package natlang/0.3a6/natlang-0.3a6.tar.gz

---
+++ packages/natlang/natlang-0.3a6.tar.gz/natlang-0.3a6/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Python version: 2/3
+#
+# Setup
+# Simon Fraser University
+# Jetic Gu
+#
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="natlang",
+    version="0.3.a6",
+    author="Jetic Gū, Rory Wang",
+    author_email="jeticg@sfu.ca",
+    description="Natural language data loading tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jeticg/datatool",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'jieba',
+    ]
+)
