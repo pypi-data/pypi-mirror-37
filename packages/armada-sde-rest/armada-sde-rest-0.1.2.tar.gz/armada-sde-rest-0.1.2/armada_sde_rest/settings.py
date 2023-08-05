5725866d49128e85738ba2fab8c41bd293ce911b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:51 +0000

    Add package armada-sde-rest/0.1.2/armada-sde-rest-0.1.2.tar.gz

---
+++ packages/armada-sde-rest/armada-sde-rest-0.1.2.tar.gz/armada-sde-rest-0.1.2/armada_sde_rest/settings.py
@@ -0,0 +1,7 @@
+# # -*- coding: utf-8 -*-
+# import os
+# from django.conf import settings
+#
+#
+# BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+# ARMADA = getattr(settings, 'ARMADA', {})
