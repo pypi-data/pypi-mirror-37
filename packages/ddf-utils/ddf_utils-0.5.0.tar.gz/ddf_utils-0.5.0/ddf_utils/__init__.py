66bf740b385217ce88e9099c8550e6b9152a6ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:13:02 +0000

    Add package ddf-utils/0.5.0/ddf_utils-0.5.0.tar.gz

---
+++ packages/ddf-utils/ddf_utils-0.5.0.tar.gz/ddf_utils-0.5.0/ddf_utils/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.5.0'
+
+from . import (str, cli, datapackage, i18n, io,
+               patch, qa, transformer, factory)
+from .datapackage import get_datapackage
