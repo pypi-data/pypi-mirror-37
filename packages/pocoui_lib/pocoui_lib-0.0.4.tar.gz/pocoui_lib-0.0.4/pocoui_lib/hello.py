21b116339ebdc7905231291bcf7c6cab4cb194ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:29 +0000

    Add package pocoui_lib/0.0.4/pocoui_lib-0.0.4.tar.gz

---
+++ packages/pocoui_lib/pocoui_lib-0.0.4.tar.gz/pocoui_lib-0.0.4/pocoui_lib/hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def get_phone_number():
+    pre_lst = ["130", "131", "132", "133", "134", "135", "136", "137", "138", "139", "147", "150", "151", "152", "153", "155", "156", "157", "158", "159", "186", "187",
+"188"]
+    return random.choice(pre_lst) + ''.join(random.sample(string.digits, 8))
+
+if __name__ == "__main__":
+    get_phone_number()
