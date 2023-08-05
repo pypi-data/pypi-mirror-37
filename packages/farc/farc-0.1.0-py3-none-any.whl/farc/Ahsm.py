7f4de1ce2338e7a154729612a28deba26fdcca90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:48 +0000

    Add package farc/0.1.0/farc-0.1.0-py3-none-any.whl

---
+++ packages/farc/farc-0.1.0-py3-none-any.whl/farc/Ahsm.py
@@ -0,0 +1,30 @@
+"""
+Copyright 2017 Dean Hall.  See LICENSE file for details.
+"""
+
+
+from .Hsm import Hsm
+from .Framework import Framework
+
+
+class Ahsm(Hsm):
+    """An Augmented Hierarchical State Machine (AHSM); a.k.a. ActiveObject (AO).
+    Adds a priority, message queue and methods to post to the queue.
+    """
+
+    def start(self, priority, initEvent=None):
+        # must set the priority before Framework.add() which uses the priority
+        self.priority = priority
+        Framework.add(self)
+        self.mq = []
+        self.init(self, initEvent)
+        # Run to completion
+        Framework._event_loop.call_soon_threadsafe(Framework.run)
+
+
+    def postLIFO(self, evt):
+        self.mq.append(evt)
+
+
+    def postFIFO(self, evt):
+        self.mq.insert(0, evt)
