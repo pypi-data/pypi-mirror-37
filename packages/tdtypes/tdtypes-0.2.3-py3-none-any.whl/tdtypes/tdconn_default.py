554753feaa2ee7c256e90ae5ff6c9a3f23280429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:24 +0000

    Add package tdtypes/0.2.3/tdtypes-0.2.3-py3-none-any.whl

---
+++ packages/tdtypes/tdtypes-0.2.3-py3-none-any.whl/tdtypes/tdconn_default.py
@@ -0,0 +1,19 @@
+"Default Teradata connection functions"
+
+__author__ = "Paresh Adhia"
+__copyright__ = "Copyright 2016-2017, Paresh Adhia"
+
+from teradata.api import *
+
+def dbconn_args(parser):
+	"add an optional argument to passed argparse object to enable supplying ODBC connection string"
+	import os
+
+	defdsn = os.environ.get('TDCONN')
+	parser.add_argument("--tdconn", default=defdsn, required=(defdsn is None), help="Connection string")
+
+
+def dbconnect(*args, **kargs):
+	"returns raw database connection"
+	from teradata.tdodbc import connect
+	return connect(driver='Teradata', **{k:v for k, v in [a.split('=') for a in args[0].tdconn.split(';')]})
