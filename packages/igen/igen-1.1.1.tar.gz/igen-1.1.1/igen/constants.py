036de8222bc9fe032e54d3339ac921eed7d88904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package igen/1.1.1/igen-1.1.1.tar.gz

---
+++ packages/igen/igen-1.1.1.tar.gz/igen-1.1.1/igen/constants.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+SWIFT_TYPES_DEFAULT_VALUES = {
+	"Int": "0",
+	"Bool": "false",
+	"String": '""',
+	"Double": "0.0",
+	"Float": "0.0",
+	"Date": "Date()"
+}
+
+SWIFT_TYPES = { 
+	"Int", 
+	"Bool", 
+	"String", 
+	"Double", 
+	"Float"
+	"Date"
+}
