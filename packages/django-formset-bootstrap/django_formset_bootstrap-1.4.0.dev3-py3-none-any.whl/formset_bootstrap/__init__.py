749ba5833a7a473dc70377fbf4416c2acba65541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:03 +0000

    Add package django-formset-bootstrap/1.4.0.dev3/django_formset_bootstrap-1.4.0.dev3-py3-none-any.whl

---
+++ packages/django-formset-bootstrap/django_formset_bootstrap-1.4.0.dev3-py3-none-any.whl/formset_bootstrap/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__license__ = 'BSD License <https://opensource.org/licenses/BSD-3-Clause>'
+__authors__ = ['Stanislaus Madueke <stan DOT madueke AT gmail DOT com>', 'Marc Bourqui <pypi.kemar@bourqui.org>']
+__docformat__ = 'numpy'
+__version__ = '1.4.0.dev3'
+__version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
+__status__ = 'Beta'
---
+++ packages/django-formset-bootstrap/django_formset_bootstrap-1.4.0.dev3-py3-none-any.whl/formset_bootstrap/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FormsetBootstrapConfig(AppConfig):
+    name = 'formset_bootstrap'
+    verbose_name = "Formset Bootstrap"
