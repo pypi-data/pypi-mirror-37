504c55dc7009631790e575d6b5487a25d22bda1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package jet-django/0.2.2/jet-django-0.2.2.tar.gz

---
+++ packages/jet-django/jet-django-0.2.2.tar.gz/jet-django-0.2.2/jet_django/serializers/sql.py
@@ -0,0 +1,18 @@
+from jet_django.deps.rest_framework import serializers
+from jet_django.deps.rest_framework.exceptions import ValidationError
+
+
+class SqlSerializer(serializers.Serializer):
+    query = serializers.CharField()
+    params = serializers.CharField(required=False)
+
+    def validate_query(self, value):
+        forbidden = ['insert', 'update', 'delete', 'grant', 'show']
+        for i in range(len(forbidden)):
+            forbidden.append('({}'.format(forbidden[i]))
+        if any(map(lambda x: ' {} '.format(value.lower()).find(' {} '.format(x)) != -1, forbidden)):
+            raise ValidationError('forbidden query')
+        return value
+
+    def validate_params(self, value):
+        return list(filter(lambda x: x != '', value.split(',')))
