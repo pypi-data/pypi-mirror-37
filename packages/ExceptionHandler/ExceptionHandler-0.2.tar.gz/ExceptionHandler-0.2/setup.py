c15648ca08ca1d58492969aadeffbeb167374f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package ExceptionHandler/0.2/ExceptionHandler-0.2.tar.gz

---
+++ packages/ExceptionHandler/ExceptionHandler-0.2.tar.gz/ExceptionHandler-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='ExceptionHandler',
+      version='0.2',
+      description='Exception Handler',
+      url='https://github.com/mirbahar/pyException-handler',
+      author='Mirbahar Nurul Amin',
+      author_email='rahat.bubt@gmail.com',
+      license='None',
+      packages=['ExceptionHandler'])
