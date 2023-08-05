4b5b67e62ecf86f85c731d9779e7b3779ef614f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:09 +0000

    Add package zsharp/0.58/zsharp-0.58.tar.gz

---
+++ packages/zsharp/zsharp-0.58.tar.gz/zsharp-0.58/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='zsharp',
+      version='0.58',
+      description='Crapy code from Zhenjie',
+      author='Zhenjie Yan',
+      author_email='zhenjie@mit.edu',
+      packages=['zsharp',],
+)
