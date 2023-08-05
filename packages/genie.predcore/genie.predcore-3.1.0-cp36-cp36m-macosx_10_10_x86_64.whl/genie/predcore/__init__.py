e8df8d0f029a787f7d8a7a237da39b72cc201717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:29 +0000

    Add package genie.predcore/3.1.0/genie.predcore-3.1.0-cp36-cp36m-macosx_10_10_x86_64.whl

---
+++ packages/genie.predcore/genie.predcore-3.1.0-cp36-cp36m-macosx_10_10_x86_64.whl/genie/predcore/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '3.1.0'
+__author__  = "pyats-support@cisco.com"
+__contact__ = 'asg-genie-support@cisco.com'
+__copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
+
+from ._predcore import *
