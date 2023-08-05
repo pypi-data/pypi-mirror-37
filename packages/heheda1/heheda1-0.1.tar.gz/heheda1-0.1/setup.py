9342278e8339359deed6ca870d3c9110903ca503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:46 +0000

    Add package heheda1/0.1/heheda1-0.1.tar.gz

---
+++ packages/heheda1/heheda1-0.1.tar.gz/heheda1-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='heheda1',
+      version='0.1',
+      description='clinical trial information retriver',
+      url='http://github.com/tongling/clinicaltrial',
+      author='Ling',
+      author_email='tonglingacademic@gmail.com',
+      license='MIT',
+      packages=['heheda1'],
+      zip_safe=False)
