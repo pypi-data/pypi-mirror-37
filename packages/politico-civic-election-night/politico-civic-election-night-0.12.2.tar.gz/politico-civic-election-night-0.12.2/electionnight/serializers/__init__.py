c7511a067598e2727b6ec617fe5c67ab7f4c3fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:18 +0000

    Add package politico-civic-election-night/0.12.2/politico-civic-election-night-0.12.2.tar.gz

---
+++ packages/politico-civic-election-night/politico-civic-election-night-0.12.2.tar.gz/politico-civic-election-night-0.12.2/electionnight/serializers/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+from .body import BodySerializer, BodyListSerializer
+from .division import ChildDivisionSerializer, DivisionSerializer
+from .election_day import ElectionDaySerializer
+from .election import ElectionSerializer, ElectionViewSerializer
+from .office import OfficeSerializer, OfficeListSerializer
+from .party import PartySerializer
+from .special_election import SpecialElectionSerializer, SpecialElectionListSerializer
+from .state import StateSerializer, StateListSerializer
+from .votes import VotesSerializer
