bb966b8c63edf31108660c70d8be891687f1330f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:25 +0000

    Add package pcte-dynamic-provisioning/0.1.0/pcte_dynamic_provisioning-0.1.0-py3-none-any.whl

---
+++ packages/pcte-dynamic-provisioning/pcte_dynamic_provisioning-0.1.0-py3-none-any.whl/pcte_dynaprov/validators.py
@@ -0,0 +1,23 @@
+class ValidatorBase:
+    def __init__(self):
+        pass
+
+
+class ValidateCSV(ValidatorBase):
+    def __init__(self):
+        super().__init__()
+
+
+class ValidateJSON(ValidatorBase):
+    def __init__(self):
+        super().__init__()
+
+
+class ValidateINI(ValidatorBase):
+    def __init__(self):
+        super().__init__()
+
+
+class ValidateYAML(ValidatorBase):
+    def __init__(self):
+        super().__init__()
