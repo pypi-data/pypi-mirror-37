32eccbd6b0efd5fe722909630bc9b90750f16ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:34 +0000

    Add package webdriver-assistant/0.0.3/webdriver-assistant-0.0.3.tar.gz

---
+++ packages/webdriver-assistant/webdriver-assistant-0.0.3.tar.gz/webdriver-assistant-0.0.3/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+setup(
+    name='webdriver-assistant',
+    version='0.0.3',
+    description='webdriver assistant',
+    license='',
+    author='ytyng',
+    author_email='ytyng@live.jp',
+    url='https://github.com/ytyng/python-webdriver-assistant',
+    keywords='',
+    packages=['webdriver_assistant'],
+)
