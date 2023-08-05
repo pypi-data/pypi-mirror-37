deaa04b746637193201e96f5cc1a9466f9c9bed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:55 +0000

    Add package django-asterisk-commands/1.0/django-asterisk-commands-1.0.tar.gz

---
+++ packages/django-asterisk-commands/django-asterisk-commands-1.0.tar.gz/django-asterisk-commands-1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django-asterisk-commands',
+    version='1.0',
+    url='https://bitbucket.org/code-orange/django-asterisk-commands/',
+    download_url='https://bitbucket.org/code-orange/django-asterisk-commands/downloads/',
+    license='LGPL version 3 or later',
+    description='Asterisk commands for Django',
+    long_description=open('README.rst').read(),
+    author='Kevin Olbrich',
+    author_email='ko@sv01.de',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=[
+        'Django>=1.2',
+    ],
+)
