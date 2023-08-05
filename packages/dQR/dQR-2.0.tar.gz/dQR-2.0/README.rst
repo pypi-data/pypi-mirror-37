f9c93af728e2e8a496960e4fef4ef5f0e0cfe764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:34 +0000

    Add package dQR/2.0/dQR-2.0.tar.gz

---
+++ packages/dQR/dQR-2.0.tar.gz/dQR-2.0/README.rst
@@ -0,0 +1,26 @@
+
+# dQR
+
+*Convert string to qrcode on your terminal.*
+
+
+## Requirements
+
+* python 2/3
+* pip
+
+### Installation
+pip::
+        pip install dQR
+
+### Usage
+
+Terminal::
+
+        dQR  http://www.sina.com
+
+In python project::
+
+        from dQR.dqrcode import QR
+        img = QR('http://www.sina.com')
+        print(img.showqr())
---
+++ packages/dQR/dQR-2.0.tar.gz/dQR-2.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#Auther: dshowing
+#Email: watt109629@outlook.com
+
+from setuptools import setup
+
+__author__ = 'dshowing'
+__time__ = '2018/10/17'
+
+setup(
+    name='dQR',
+    version='2.0',
+    url='https://github.com/dshowing/dQR',
+    author='dshowing',
+    author_email='watt109629@outlook.com',
+    description='Translate string in QRcode on terminal.',
+    license='Apache-2.0',
+    packages=['dQR'],
+    entry_points={
+        'console_scripts': [
+            'dQR=dQR.main:show'
+        ],
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'qrcode==5.3',
+        'Pillow==4.2.1'
+    ],
+)
