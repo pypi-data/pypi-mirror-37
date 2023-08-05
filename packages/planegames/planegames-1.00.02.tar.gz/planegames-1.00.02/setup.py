90842af145a87ee714650f736260cde35661250a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:10 +0000

    Add package planegames/1.00.02/planegames-1.00.02.tar.gz

---
+++ packages/planegames/planegames-1.00.02.tar.gz/planegames-1.00.02/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='planegames', # 发布的包文件名称
+    version='1.00.02', # 发布的包的版本序号
+    description='我的飞机大战', # 发布包的描述信息
+    author='wqx', # 发布包的作者信息
+    author_email='damu@163.com', # 作者联系邮箱信息
+    py_modules=['setup','planegame']# 发布的包中的模块文件列表
+)
