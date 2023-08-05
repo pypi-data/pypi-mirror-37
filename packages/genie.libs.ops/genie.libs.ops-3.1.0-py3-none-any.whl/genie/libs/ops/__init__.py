24110a2a44d5ff3d4f20dbdfcdb1dd6bbd14a981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:39 +0000

    Add package genie.libs.ops/3.1.0/genie.libs.ops-3.1.0-py3-none-any.whl

---
+++ packages/genie.libs.ops/genie.libs.ops-3.1.0-py3-none-any.whl/genie/libs/ops/__init__.py
@@ -0,0 +1,26 @@
+'''
+    Module:
+        libs.ops
+
+    Description:
+        This is the library sub-component of Genie for `genie.ops`.
+'''
+
+# metadata
+__version__ = '3.1.0'
+__author__ = 'Cisco Systems Inc.'
+__contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
+__copyright__ = 'Copyright (c) 2018, Cisco Systems Inc.'
+
+try:
+    from ats.cisco.stats import CesMonitor
+    CesMonitor(action = 'genielibsOps', application='Genie').post()
+except Exception:
+    try:
+        from ats.utils.stats import CesMonitor
+        CesMonitor(action = 'genielibsOps', application='Genie').post()
+    except Exception:
+        pass
+
+from genie import abstract
+abstract.declare_package(__name__)
