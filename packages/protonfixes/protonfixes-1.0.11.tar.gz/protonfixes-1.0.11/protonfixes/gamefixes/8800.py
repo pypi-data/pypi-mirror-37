06a25ac175bb33f160a815cafdfc9c61742abc1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:08 +0000

    Add package protonfixes/1.0.11/protonfixes-1.0.11.tar.gz

---
+++ packages/protonfixes/protonfixes-1.0.11.tar.gz/protonfixes-1.0.11/protonfixes/gamefixes/8800.py
@@ -0,0 +1,16 @@
+""" Game fix for Civilization 4 (Beyond the Sword)
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+from protonfixes.logger import log
+
+def main():
+    """ Install msxml3
+    """
+
+    log('Applying fixes for Civilization 4 (Beyond the Sword)')
+
+    # https://github.com/ValveSoftware/Proton/issues/179#issuecomment-415593087
+    util.protontricks('msxml3')
+    util.protontricks('msxml4')
