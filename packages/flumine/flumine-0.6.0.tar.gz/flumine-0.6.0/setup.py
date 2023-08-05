e8fab4bf6b83315dfc9e5efee56f6e7900fbd53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:04 +0000

    Add package flumine/0.6.0/flumine-0.6.0.tar.gz

---
+++ packages/flumine/flumine-0.6.0.tar.gz/flumine-0.6.0/setup.py
@@ -0,0 +1,47 @@
+import re
+from setuptools import setup
+
+
+INSTALL_REQUIRES = [
+    'betfairlightweight==0.0.0b3',
+    'boto3',
+    'botocore',
+    'retrying',
+]
+
+TEST_REQUIRES = [
+]
+
+with open('flumine/__init__.py', 'r') as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        f.read(),
+        re.MULTILINE
+    ).group(1)
+
+setup(
+    name='flumine',
+    version=version,
+    packages=[
+        'flumine',
+        'flumine.resources',
+        'flumine.storage'
+    ],
+    package_dir={
+        'flumine': 'flumine'
+    },
+    install_requires=INSTALL_REQUIRES,
+    url='https://github.com/liampauling/flumine',
+    license='MIT',
+    author='liampauling',
+    author_email='',
+    description='Betfair data record framework utilising betfair streaming',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests'
+)
