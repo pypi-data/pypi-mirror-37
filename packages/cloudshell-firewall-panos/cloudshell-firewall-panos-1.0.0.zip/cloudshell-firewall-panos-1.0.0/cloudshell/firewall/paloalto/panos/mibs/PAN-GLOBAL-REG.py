8681923d4a55803428344a5cd095a1c0af151a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:06 +0000

    Add package cloudshell-firewall-panos/1.0.0/cloudshell-firewall-panos-1.0.0.zip

---
+++ packages/cloudshell-firewall-panos/cloudshell-firewall-panos-1.0.0.zip/cloudshell-firewall-panos-1.0.0/cloudshell/firewall/paloalto/panos/mibs/PAN-GLOBAL-REG.py
@@ -0,0 +1,32 @@
+#
+# PySNMP MIB module PAN-GLOBAL-REG (http://pysnmp.sf.net)
+# ASN.1 source http://mibs.snmplabs.com:80/asn1/PAN-GLOBAL-REG
+# Produced by pysmi-0.2.2 at Fri Sep 28 13:31:58 2018
+# On host ? platform ? version ? by user ?
+# Using Python version 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)]
+#
+Integer, ObjectIdentifier, OctetString = mibBuilder.importSymbols("ASN1", "Integer", "ObjectIdentifier", "OctetString")
+NamedValues, = mibBuilder.importSymbols("ASN1-ENUMERATION", "NamedValues")
+ConstraintsUnion, SingleValueConstraint, ConstraintsIntersection, ValueSizeConstraint, ValueRangeConstraint = mibBuilder.importSymbols("ASN1-REFINEMENT", "ConstraintsUnion", "SingleValueConstraint", "ConstraintsIntersection", "ValueSizeConstraint", "ValueRangeConstraint")
+NotificationGroup, ModuleCompliance = mibBuilder.importSymbols("SNMPv2-CONF", "NotificationGroup", "ModuleCompliance")
+Integer32, MibScalar, MibTable, MibTableRow, MibTableColumn, NotificationType, MibIdentifier, IpAddress, TimeTicks, Counter64, Unsigned32, enterprises, iso, Gauge32, ModuleIdentity, ObjectIdentity, Bits, Counter32 = mibBuilder.importSymbols("SNMPv2-SMI", "Integer32", "MibScalar", "MibTable", "MibTableRow", "MibTableColumn", "NotificationType", "MibIdentifier", "IpAddress", "TimeTicks", "Counter64", "Unsigned32", "enterprises", "iso", "Gauge32", "ModuleIdentity", "ObjectIdentity", "Bits", "Counter32")
+DisplayString, TextualConvention = mibBuilder.importSymbols("SNMPv2-TC", "DisplayString", "TextualConvention")
+panGlobalRegModule = ModuleIdentity((1, 3, 6, 1, 4, 1, 25461, 1, 1, 1))
+panGlobalRegModule.setRevisions(('2011-02-09 16:10',))
+if mibBuilder.loadTexts: panGlobalRegModule.setLastUpdated('201106271040Z')
+if mibBuilder.loadTexts: panGlobalRegModule.setOrganization('Palo Alto Networks')
+panRoot = ObjectIdentity((1, 3, 6, 1, 4, 1, 25461))
+if mibBuilder.loadTexts: panRoot.setStatus('current')
+panReg = ObjectIdentity((1, 3, 6, 1, 4, 1, 25461, 1))
+if mibBuilder.loadTexts: panReg.setStatus('current')
+panModules = ObjectIdentity((1, 3, 6, 1, 4, 1, 25461, 1, 1))
+if mibBuilder.loadTexts: panModules.setStatus('current')
+panMibs = ObjectIdentity((1, 3, 6, 1, 4, 1, 25461, 2))
+if mibBuilder.loadTexts: panMibs.setStatus('current')
+panCommonMib = ObjectIdentity((1, 3, 6, 1, 4, 1, 25461, 2, 1))
+if mibBuilder.loadTexts: panCommonMib.setStatus('current')
+panSpecificMib = ObjectIdentity((1, 3, 6, 1, 4, 1, 25461, 2, 2))
+if mibBuilder.loadTexts: panSpecificMib.setStatus('current')
+panProductsMibs = ObjectIdentity((1, 3, 6, 1, 4, 1, 25461, 2, 3))
+if mibBuilder.loadTexts: panProductsMibs.setStatus('current')
+mibBuilder.exportSymbols("PAN-GLOBAL-REG", panMibs=panMibs, panCommonMib=panCommonMib, panModules=panModules, panGlobalRegModule=panGlobalRegModule, panSpecificMib=panSpecificMib, PYSNMP_MODULE_ID=panGlobalRegModule, panProductsMibs=panProductsMibs, panRoot=panRoot, panReg=panReg)
---
+++ packages/cloudshell-firewall-panos/cloudshell-firewall-panos-1.0.0.zip/cloudshell-firewall-panos-1.0.0/cloudshell/firewall/paloalto/panos/mibs/PAN-GLOBAL-TC.py
@@ -0,0 +1,28 @@
+#
+# PySNMP MIB module PAN-GLOBAL-TC (http://pysnmp.sf.net)
+# ASN.1 source http://mibs.snmplabs.com:80/asn1/PAN-GLOBAL-TC
+# Produced by pysmi-0.2.2 at Fri Sep 28 13:31:58 2018
+# On host ? platform ? version ? by user ?
+# Using Python version 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)]
+#
+Integer, ObjectIdentifier, OctetString = mibBuilder.importSymbols("ASN1", "Integer", "ObjectIdentifier", "OctetString")
+NamedValues, = mibBuilder.importSymbols("ASN1-ENUMERATION", "NamedValues")
+ConstraintsUnion, SingleValueConstraint, ConstraintsIntersection, ValueSizeConstraint, ValueRangeConstraint = mibBuilder.importSymbols("ASN1-REFINEMENT", "ConstraintsUnion", "SingleValueConstraint", "ConstraintsIntersection", "ValueSizeConstraint", "ValueRangeConstraint")
+panModules, = mibBuilder.importSymbols("PAN-GLOBAL-REG", "panModules")
+NotificationGroup, ModuleCompliance = mibBuilder.importSymbols("SNMPv2-CONF", "NotificationGroup", "ModuleCompliance")
+Integer32, MibScalar, MibTable, MibTableRow, MibTableColumn, NotificationType, MibIdentifier, IpAddress, TimeTicks, Counter64, Unsigned32, iso, Gauge32, ModuleIdentity, ObjectIdentity, Bits, Counter32 = mibBuilder.importSymbols("SNMPv2-SMI", "Integer32", "MibScalar", "MibTable", "MibTableRow", "MibTableColumn", "NotificationType", "MibIdentifier", "IpAddress", "TimeTicks", "Counter64", "Unsigned32", "iso", "Gauge32", "ModuleIdentity", "ObjectIdentity", "Bits", "Counter32")
+DisplayString, TextualConvention = mibBuilder.importSymbols("SNMPv2-TC", "DisplayString", "TextualConvention")
+panGlobalTcModule = ModuleIdentity((1, 3, 6, 1, 4, 1, 25461, 1, 1, 2))
+panGlobalTcModule.setRevisions(('2011-02-09 16:10',))
+if mibBuilder.loadTexts: panGlobalTcModule.setLastUpdated('201106271040Z')
+if mibBuilder.loadTexts: panGlobalTcModule.setOrganization('Palo Alto Networks')
+class TcAppaname(TextualConvention, OctetString):
+    status = 'current'
+    displayHint = '64a'
+    subtypeSpec = OctetString.subtypeSpec + ValueSizeConstraint(0, 64)
+
+class TcChassisType(TextualConvention, OctetString):
+    status = 'current'
+    subtypeSpec = OctetString.subtypeSpec + ValueSizeConstraint(0, 64)
+
+mibBuilder.exportSymbols("PAN-GLOBAL-TC", TcAppaname=TcAppaname, TcChassisType=TcChassisType, panGlobalTcModule=panGlobalTcModule, PYSNMP_MODULE_ID=panGlobalTcModule)
