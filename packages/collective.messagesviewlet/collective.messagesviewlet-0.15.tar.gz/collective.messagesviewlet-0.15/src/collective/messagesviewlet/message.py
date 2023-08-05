eed5e37cc68331a994cc096dad171c63b3a9b613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:27 +0000

    Add package collective.messagesviewlet/0.15/collective.messagesviewlet-0.15.tar.gz

---
+++ packages/collective.messagesviewlet/collective.messagesviewlet-0.15.tar.gz/collective.messagesviewlet-0.15/src/collective/messagesviewlet/message.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+from DateTime import DateTime
+from datetime import datetime
+from zope import schema
+from zope.interface import invariant, Invalid, alsoProvides
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from z3c.form.browser.checkbox import CheckBoxFieldWidget
+from z3c.form.browser.radio import RadioFieldWidget
+
+from plone.app.textfield import RichText
+from plone.autoform import directives as form
+from plone.formwidget.datetime.z3cform.widget import DatetimeFieldWidget
+from plone.indexer import indexer
+from plone.supermodel import model
+
+from collective.messagesviewlet import _
+
+
+def msg_types(context):
+    terms = []
+    terms.append(SimpleTerm("info", title=_("info")))
+    terms.append(SimpleTerm("significant", title=_("significant")))
+    terms.append(SimpleTerm("warning", title=_("warning")))
+    return SimpleVocabulary(terms)
+
+
+alsoProvides(msg_types, schema.interfaces.IContextSourceBinder)
+
+
+def location(context):
+    terms = []
+    terms.append(SimpleTerm("fullsite", title=_("Full site")))
+    terms.append(SimpleTerm("homepage", title=_("Homepage")))
+    return SimpleVocabulary(terms)
+
+
+alsoProvides(location, schema.interfaces.IContextSourceBinder)
+
+
+def generate_uid():
+    return unicode(DateTime().millis())
+
+
+def default_start():
+    return datetime.now()
+
+
+class IMessage(model.Schema):
+
+    title = schema.TextLine(
+        title=_(u"Title"),
+        required=True,
+    )
+
+    text = RichText(
+        title=_(u"Text"),
+        required=True,
+        description=_(u"Message text"),
+    )
+
+    msg_type = schema.Choice(
+        title=_(u"Message type"),
+        required=True,
+        source=msg_types,
+        description=_(u"Following the type, the color will be different"),
+    )
+
+    form.widget('can_hide', RadioFieldWidget)
+    can_hide = schema.Bool(
+        title=_(u"Can be marked as read"),
+        description=_(u"If checked, the user can hide the message"),
+    )
+
+    start = schema.Datetime(
+        title=_(u"Start date"),
+        required=False,
+        description=_(u"Specify start date message appearance"),
+        defaultFactory=default_start,
+    )
+    form.widget('start', DatetimeFieldWidget)
+
+    end = schema.Datetime(
+        title=_(u"End date"),
+        required=False,
+        description=_(u"Specify end date message appearance. If nothing specified, this is infinite. "
+                      "If you pick a date, <span class=warning-formHelp>dont't forget hours !</span>"),
+    )
+    form.widget('end', DatetimeFieldWidget)
+
+    required_roles = schema.Set(
+        title=_(u'Required roles'),
+        description=_(u'Choose the roles for which the message will be displayed'),
+        required=False,
+        value_type=schema.Choice(vocabulary='plone.app.vocabularies.Roles'),
+    )
+    form.widget('required_roles', CheckBoxFieldWidget, multiple='multiple', size=15)
+
+    form.widget('use_local_roles', RadioFieldWidget)
+    use_local_roles = schema.Bool(
+        title=_(u"Use Reader local role"),
+        description=_(u"If checked, the message will be shown only to users having message local role 'Reader'"),
+        default=False,
+    )
+
+    location = schema.Choice(
+        title=_(u"Location"),
+        required=True,
+        source=location,
+    )
+
+    hidden_uid = schema.TextLine(
+        title=u"Generated uid",
+        defaultFactory=generate_uid,
+    )
+    form.mode(hidden_uid='hidden')
+
+    @invariant
+    def validateStartEnd(data):
+        if data.start is not None and data.end is not None:
+            if data.start > data.end:
+                raise Invalid(_(u"The start date must precede the end date."))
+
+
+@indexer(IMessage)
+def start_index(obj):
+    if obj.start is None:
+        return obj.created()
+    else:
+        return obj.start
+
+
+@indexer(IMessage)
+def end_index(obj):
+    if obj.end is None:
+        return DateTime(2099, 01, 01)
+    else:
+        return obj.end
---
+++ packages/collective.messagesviewlet/collective.messagesviewlet-0.15.tar.gz/collective.messagesviewlet-0.15/src/collective/messagesviewlet/setuphandlers.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from Products.CMFCore.utils import getToolByName
+from plone.app.dexterity.behaviors.exclfromnav import IExcludeFromNavigation
+from utils import _, add_message
+
+FOLDER = "messages-config"
+
+
+def post_install(context):
+    """Post install script."""
+    if context.readDataFile('collectivemessagesviewlet_default.txt') is None:
+        return
+    site = context.getSite()
+
+    if not site.get(FOLDER):
+        types = getToolByName(site, 'portal_types')
+        types.getTypeInfo('MessagesConfig').global_allow = True
+        container = api.content.create(site,
+                                       "MessagesConfig",
+                                       id=FOLDER,
+                                       title=_('Messages viewlet settings', context=site)
+                                       )
+        excl = IExcludeFromNavigation(container)
+        excl.exclude_from_nav = True
+        types.getTypeInfo('MessagesConfig').global_allow = False
+
+
+def add_default_messages(context):
+    """ Add maintenance messages that can be activated when necessary """
+    if context.readDataFile('collectivemessagesviewlet_messages.txt') is None:
+        return
+    site = context.getSite()
+    add_message('maintenance-soon', _('maintenance_soon_tit', context=site), _('maintenance_soon_txt', context=site),
+                msg_type='significant', can_hide=True, req_roles=['Authenticated'])
+    add_message('maintenance-now', _('maintenance_now_tit', context=site), _('maintenance_now_txt', context=site),
+                msg_type='warning', can_hide=False, req_roles=['Anonymous'])
+    add_message('test-site', _('test_site_tit', context=site), _('test_site_txt', context=site),
+                msg_type='warning', can_hide=False)
+    add_message('browser-warning', _('bad_browser_tit', context=site),
+                _('bad_browser_txt', context=site),
+                msg_type='warning', can_hide=False,
+                tal_condition="python:'Firefox' not in context.REQUEST.get('HTTP_USER_AGENT')")
+    add_message('browser-warning-ff-chrome', _('bad_browser_ff_chrome_tit', context=site),
+                _('bad_browser_ff_chrome_txt', context=site),
+                msg_type='warning', can_hide=False,
+                tal_condition="python: 'Firefox' not in context.REQUEST.get('HTTP_USER_AGENT') and "
+                "'Chrome' not in context.REQUEST.get('HTTP_USER_AGENT')")
