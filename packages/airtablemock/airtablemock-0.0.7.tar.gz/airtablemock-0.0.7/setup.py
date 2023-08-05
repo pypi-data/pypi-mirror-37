87e27d253ddbebe05f5f3234fdafa6f2484563c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:30 +0000

    Add package airtablemock/0.0.7/airtablemock-0.0.7.tar.gz

---
+++ packages/airtablemock/airtablemock-0.0.7.tar.gz/airtablemock-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='airtablemock',
+    version='0.0.7',
+    packages=['airtablemock'],
+    install_requires=['mock>=2.0.0', 'parsimonious>=0.8.1', 'requests'],
+    tests_require=['airtable', 'pytest'],
+    setup_requires=['pytest-runner'],
+    description='Mock library for the airtable client',
+    author='Pascal Corpet',
+    url='https://github.com/bayesimpact/airtablemock',
+    keywords=['airtable', 'api', 'mock'],
+    license='The MIT License (MIT)',
+)
