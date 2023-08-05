1926309004aa9d01aaf542fdc416311100b0d0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:57 +0000

    Add package onegov.org/0.38.1/onegov.org-0.38.1.tar.gz

---
+++ packages/onegov.org/onegov.org-0.38.1.tar.gz/onegov.org-0.38.1/onegov/org/views/homepage.py
@@ -0,0 +1,22 @@
+""" The onegov organisation homepage. """
+
+from onegov.core.security import Public
+from onegov.org import OrgApp
+from onegov.org.layout import DefaultLayout
+from onegov.org.models import Organisation
+from onegov.org.homepage_widgets import inject_widget_variables
+
+
+@OrgApp.html(model=Organisation, template='homepage.pt', permission=Public)
+def view_org(self, request):
+    """ Renders the org's homepage. """
+
+    layout = DefaultLayout(self, request)
+
+    default = {
+        'layout': layout,
+        'title': self.title
+    }
+
+    structure = self.meta.get('homepage_structure')
+    return inject_widget_variables(layout, structure, default)
