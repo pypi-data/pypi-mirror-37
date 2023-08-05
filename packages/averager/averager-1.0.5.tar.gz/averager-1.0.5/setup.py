a2b5eea08470a700e5b0321d68a6b1b2aec830cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:27 +0000

    Add package averager/1.0.5/averager-1.0.5.tar.gz

---
+++ packages/averager/averager-1.0.5.tar.gz/averager-1.0.5/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='averager',
+      version='1.0.5',
+      description='A simple averager',
+      url='http://github.com/bsoyka/averager',
+      author='Benjamin Soyka',
+      author_email='bensoyka@icloud.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=setuptools.find_packages(),
+      license='GPL-3.0',
+      zip_safe=False)
