eeb6bbec2613a1ec705822b86295ff9b3883697c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package zope.ramcache/2.3/zope.ramcache-2.3.tar.gz

---
+++ packages/zope.ramcache/zope.ramcache-2.3.tar.gz/zope.ramcache-2.3/CHANGES.rst
@@ -0,0 +1,55 @@
+=========
+ Changes
+=========
+
+2.3 (2018-10-10)
+================
+
+- Add support for Python 3.7.
+
+
+2.2.0 (2017-09-05)
+==================
+
+- Add support for Python 3.5 and 3.6.
+
+- Drop support for Python 2.6 and 3.3.
+
+- Drop support for ``python setup.py test``.
+
+- Test PyPy3 on Travis CI.
+
+- Stop requiring all values to support pickling in order to get
+  statistics. Instead, return ``False`` for the size if such a value
+  is found. Fixes `issue 1 <https://github.com/zopefoundation/zope.ramcache/issues/1>`_.
+
+- Change the internal storage format of the RAM cache to require less
+  memory and be easier to maintain.
+
+2.1.0 (2014-12-29)
+==================
+
+- Added support for PyPy.  (PyPy3 is pending release of a fix for:
+  https://bitbucket.org/pypy/pypy/issue/1946)
+
+- Add support for Python 3.4.
+
+- Add support for testing on Travis.
+
+
+2.0.0 (2013-02-28)
+==================
+
+- Add support for Python 3.3.
+
+- Replace deprecated ``zope.interface.implements`` usage with equivalent
+  ``zope.interface.implementer`` decorator.
+
+- Drop support for Python 2.4 and 2.5.
+
+- Remove outdated classifier / keywords.
+
+1.0 (2009-07-23)
+================
+
+- Broke out the ram cache functionality from ``zope.app.cache``.
---
+++ packages/zope.ramcache/zope.ramcache-2.3.tar.gz/zope.ramcache-2.3/setup.py
@@ -0,0 +1,75 @@
+##############################################################################
+#
+# Copyright (c) 2006-2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.ramcache package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+TESTS_REQUIRE = [
+    'zope.testing',
+    'zope.testrunner',
+]
+
+setup(name='zope.ramcache',
+      version='2.3',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Zope RAM Cache',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      keywords="zope cache",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+      ],
+      url='https://github.com/zopefoundation/zope.ramcache',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=[
+          'persistent',
+          'setuptools',
+          'zope.interface',
+          'zope.location',
+      ],
+      tests_require=TESTS_REQUIRE,
+      extras_require={
+          'test': TESTS_REQUIRE,
+      },
+      include_package_data=True,
+      zip_safe=False,
+)
