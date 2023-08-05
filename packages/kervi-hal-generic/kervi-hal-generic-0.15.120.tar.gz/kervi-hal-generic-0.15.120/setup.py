658f46fbb0dcd784a560b679a805f4bca406c2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:03 +0000

    Add package kervi-hal-generic/0.15.120/kervi-hal-generic-0.15.120.tar.gz

---
+++ packages/kervi-hal-generic/kervi-hal-generic-0.15.120.tar.gz/kervi-hal-generic-0.15.120/setup.py
@@ -0,0 +1,27 @@
+import distutils
+from setuptools import setup
+
+import sys
+
+from kervi.platforms.generic.version import VERSION
+
+try:
+    distutils.dir_util.remove_tree("dist")
+except:
+    pass
+
+setup(
+    name='kervi-hal-generic',
+    version=VERSION,
+    description="""
+    Generic platform driver for the Kervi automation framework.
+    """,
+    packages=[
+        "kervi/platforms/generic",
+    ],
+    install_requires=[
+        'psutil',
+        'inputs'
+    ],
+
+)
