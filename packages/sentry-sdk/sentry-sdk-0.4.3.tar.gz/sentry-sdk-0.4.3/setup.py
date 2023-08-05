905f5d18522993f9ef9ea7c9c414f9bf080f5a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:38 +0000

    Add package sentry-sdk/0.4.3/sentry-sdk-0.4.3.tar.gz

---
+++ packages/sentry-sdk/sentry-sdk-0.4.3.tar.gz/sentry-sdk-0.4.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Sentry-Python - Sentry SDK for Python
+=====================================
+
+**Sentry-Python is an experimental SDK for Sentry.** Check out `GitHub
+<https://github.com/getsentry/sentry-python>`_ to find out more.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sentry-sdk",
+    version="0.4.3",
+    author="Sentry Team and Contributors",
+    author_email="hello@getsentry.com",
+    url="https://github.com/getsentry/sentry-python",
+    description="Python client for Sentry (https://getsentry.com)",
+    long_description=__doc__,
+    packages=find_packages(exclude=("tests", "tests.*")),
+    zip_safe=False,
+    license="BSD",
+    install_requires=["urllib3", "certifi"],
+    extras_require={"flask": ["flask>=0.8", "blinker>=1.1"]},
+)
