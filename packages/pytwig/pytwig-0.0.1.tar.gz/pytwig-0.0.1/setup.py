554491689e7c57b82748157513f3c9b734ddc872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:02 +0000

    Add package pytwig/0.0.1/pytwig-0.0.1.tar.gz

---
+++ packages/pytwig/pytwig-0.0.1.tar.gz/pytwig-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pytwig',
+      version='0.0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='jaxter184',
+      author_email='jaxter184@gmail.com',
+      license='MIT',
+      packages=['pytwig'],
+      zip_safe=False)
