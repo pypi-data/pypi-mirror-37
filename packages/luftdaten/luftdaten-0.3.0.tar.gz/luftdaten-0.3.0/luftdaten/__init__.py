660f4a6a4c82d340074553a41d60e1808255c90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:52 +0000

    Add package luftdaten/0.3.0/luftdaten-0.3.0.tar.gz

---
+++ packages/luftdaten/luftdaten-0.3.0.tar.gz/luftdaten-0.3.0/luftdaten/__init__.py
@@ -0,0 +1,67 @@
+"""
+Copyright (c) 2017-2018 Fabian Affolter <fabian@affolter-engineering.ch>
+
+Licensed under MIT. All rights reserved.
+"""
+import asyncio
+import logging
+
+import aiohttp
+import async_timeout
+
+from . import exceptions
+
+_LOGGER = logging.getLogger(__name__)
+_RESOURCE = 'https://api.luftdaten.info/v1'
+
+
+class Luftdaten(object):
+    """A class for handling the data retrieval."""
+
+    def __init__(self, sensor_id, loop, session):
+        """Initialize the connection."""
+        self._loop = loop
+        self._session = session
+        self.sensor_id = sensor_id
+        self.data = None
+        self.values = {
+                'humidity': None,
+                'P1': None,
+                'P2': None,
+                'pressure': None,
+                'temperature': None,
+            }
+        self.meta = {}
+
+    async def get_data(self):
+        """Retrieve the data."""
+        url = '{}/{}/{}/'.format(_RESOURCE, 'sensor', self.sensor_id)
+
+        try:
+            with async_timeout.timeout(5, loop=self._loop):
+                response = await self._session.get(url)
+
+            _LOGGER.debug(
+                "Response from luftdaten.info: %s", response.status)
+            data = await response.json()
+            _LOGGER.debug(data)
+        except (asyncio.TimeoutError, aiohttp.ClientError):
+            _LOGGER.error("Can not load data from luftdaten.info")
+            raise exceptions.LuftdatenConnectionError()
+
+        try:
+            sensor_data = sorted(
+                data, key=lambda timestamp: timestamp['timestamp'],
+                reverse=True)[0]
+
+            for entry in sensor_data['sensordatavalues']:
+                for measurement in self.values.keys():
+                    if measurement == entry['value_type']:
+                        self.values[measurement] = float(entry['value'])
+
+            self.meta['sensor_id'] = self.sensor_id
+            self.meta['longitude'] = float(
+                sensor_data['location']['longitude'])
+            self.meta['latitude'] = float(sensor_data['location']['latitude'])
+        except (TypeError, IndexError):
+            raise exceptions.LuftdatenError()
