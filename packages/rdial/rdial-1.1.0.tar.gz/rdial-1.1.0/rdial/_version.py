aca0f6e87139c7d74ce12afff3a2652afecd85d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:55 +0000

    Add package rdial/1.1.0/rdial-1.1.0.tar.gz

---
+++ packages/rdial/rdial-1.1.0.tar.gz/rdial-1.1.0/rdial/_version.py
@@ -0,0 +1,10 @@
+# This is rdial version 1.1.0 (2018-10-14)
+# pylint: skip-file
+
+dotted = '1.1.0'
+libtool = '11:20'
+hex = 0x010100
+date = '2018-10-14'
+tuple = (1, 1, 0)
+dict = {'major': 1, 'minor': 1, 'micro': 0}
+web = 'rdial/1.1.0'
