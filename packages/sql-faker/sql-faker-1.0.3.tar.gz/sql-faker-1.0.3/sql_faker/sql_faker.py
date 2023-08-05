9d4498cec1d32c196897c2fa3f4c499ff924795d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:29 +0000

    Add package sql-faker/1.0.3/sql-faker-1.0.3.tar.gz

---
+++ packages/sql-faker/sql-faker-1.0.3.tar.gz/sql-faker-1.0.3/sql_faker/sql_faker.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from utils.datatype import DataType
+from utils.values import Values
+from utils.times import Times
+from utils.db_utils import DBHelper
+from random_data.random_abstract import RandomData
+from faker import Faker
+
+# 数据类型
+DataType = DataType
+
+# 取值范围设置器
+Values = Values
+
+# 时间范围设置器
+Times = Times
+
+# 数据库连接池助手
+DBHelper = DBHelper
+
+# 随机值抽象类
+RandomData = RandomData
+
+# 数据伪造器
+Faker = Faker
