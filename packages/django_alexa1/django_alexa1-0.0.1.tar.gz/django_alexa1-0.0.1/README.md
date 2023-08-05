4a6b10abae0266c11ee0f1644fea12ab5d1aae36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:37 +0000

    Add package django_alexa1/0.0.1/django_alexa1-0.0.1.tar.gz

---
+++ packages/django_alexa1/django_alexa1-0.0.1.tar.gz/django_alexa1-0.0.1/README.md
@@ -0,0 +1,2 @@
+# django-alexa2 #
+
---
+++ packages/django_alexa1/django_alexa1-0.0.1.tar.gz/django_alexa1-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+def read_long_description():
+    try:
+        import pypandoc
+        return pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError, RuntimeError):
+        return ''
+
+
+packages = find_packages(exclude=['test_*.py'])
+
+setup(
+    name='django_alexa1',
+    packages=packages,
+    version='0.0.1',
+    author='Anton Tuchak',
+    author_email='anton.tuchak@gmail.com',
+    description='A library ',
+    keywords=['amazon alexa'],
+    url='https://github.com/atuchak/django-alexa',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+    install_requires=['requests'],
+)
