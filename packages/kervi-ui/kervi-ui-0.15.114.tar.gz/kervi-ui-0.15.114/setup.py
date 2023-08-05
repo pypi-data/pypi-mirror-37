4349634ea9aa4cfb9243d0eb8e842fcd4c08e324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:52 +0000

    Add package kervi-ui/0.15.114/kervi-ui-0.15.114.tar.gz

---
+++ packages/kervi-ui/kervi-ui-0.15.114.tar.gz/kervi-ui-0.15.114/setup.py
@@ -0,0 +1,45 @@
+from distutils.core import setup
+import distutils
+
+import sys
+
+VERSION = "0.0.0"
+if '--version' in sys.argv:
+    index = sys.argv.index('--version')
+    sys.argv.pop(index)
+    VERSION = sys.argv.pop(index)
+
+
+try:
+    distutils.dir_util.remove_tree("dist")
+except:
+    pass
+
+setup(
+    name='kervi-ui',
+    packages=['kervi.ui'],
+    version=VERSION,
+    description='UI module for the kervi framework. It is included as dependency when kervi in installed.',
+    author='Tim Wentzlau',
+    author_email='tim.wentzlau@gmail.com',
+    url='https://github.com/kervi/kervi-ui',
+    download_url='https://github.com/wentzlau/kervi-ui/archive/v1.0-alpha.tar.gz',
+    keywords=['ui', 'robotic', 'automation'],
+    classifiers=[],
+    include_package_data=True,
+    package_data={
+        'kervi.ui': [
+            'web/dist/*.html',
+            'web/dist/*.js',
+            'web/dist/*.css',
+            'web/dist/*.map',
+            'web/dist/*.ico',
+            'web/dist/*.png',
+            'web/dist/*.eot',
+            'web/dist/*.svg',
+            'web/dist/*.woff',
+            'web/dist/*.woff2',
+            'web/dist/*.ttf'
+        ],
+    },
+)
