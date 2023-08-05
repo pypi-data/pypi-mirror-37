9e4216c3a3dc2c3597eef8d8e6319d085cd2eeab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package text-fabric/6.0.8/text-fabric-6.0.8.tar.gz

---
+++ packages/text-fabric/text-fabric-6.0.8.tar.gz/text-fabric-6.0.8/tf/notebook.py
@@ -0,0 +1,55 @@
+import json
+import os.path
+import re
+import ipykernel
+import requests
+
+from requests.compat import urljoin
+
+from notebook.notebookapp import list_running_servers
+
+from tf.apphelpers import URL_GH, URL_NB
+
+
+def repoLocation(cwd):
+  cwdPat = re.compile(f'^.*/github/([^/]+)/([^/]+)((?:/.+)?)$', re.I)
+  cwdRel = cwdPat.findall(cwd)
+  if not cwdRel:
+    return None
+  # org, repo, path
+  (org, repo, path) = cwdRel[0]
+  onlineTail = (f'{org}/{repo}' f'/blob/master{path}')
+  nbUrl = f'{URL_NB}/{onlineTail}'
+  ghUrl = f'{URL_GH}/{onlineTail}'
+  return (org, repo, path, nbUrl, ghUrl)
+
+
+def location(cwd, name):
+  repoLoc = repoLocation(cwd)
+
+  hasKernel = False
+  try:
+    kernelId = re.search('kernel-(.*).json', ipykernel.connect.get_connection_file()).group(1)
+    hasKernel = True
+  except Exception:
+    pass
+  servers = list_running_servers()
+
+  found = None
+  if hasKernel:
+    for ss in servers:
+      response = requests.get(
+          urljoin(ss['url'], 'api/sessions'), params={'token': ss.get('token', '')}
+      )
+      for nn in json.loads(response.text):
+        if nn['kernel']['id'] == kernelId:
+          relPath = nn['notebook']['path']
+          absPath = os.path.join(ss['notebook_dir'], relPath)
+          (dirName, filePart) = os.path.split(absPath)
+          (fileName, extension) = os.path.splitext(filePart)
+          if name is not None:
+            fileName = name
+          found = (dirName, fileName, extension)
+          break
+
+  return (found, repoLoc)
