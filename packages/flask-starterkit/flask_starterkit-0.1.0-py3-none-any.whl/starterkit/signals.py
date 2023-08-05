68bf0d981dd1529021150f579554e16829c2fbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:33 +0000

    Add package flask-starterkit/0.1.0/flask_starterkit-0.1.0-py3-none-any.whl

---
+++ packages/flask-starterkit/flask_starterkit-0.1.0-py3-none-any.whl/starterkit/signals.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from blinker import Namespace
+
+_namespace = Namespace()
+
+add_blueprints = _namespace.signal("add-blueprints")
+add_extensions = _namespace.signal("add-extensions")
+amend_settings = _namespace.signal("amend-settings")
