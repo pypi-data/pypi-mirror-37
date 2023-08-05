9eb7488445329a957c9aa92e2cef106a2db0482e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:29 +0000

    Add package genie.telemetry/3.1.0/genie.telemetry-3.1.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/genie.telemetry/genie.telemetry-3.1.0-cp35-cp35m-manylinux1_x86_64.whl/genie/telemetry/__init__.py
@@ -0,0 +1,19 @@
+# metadata
+__version__ = '3.1.0'
+__author__ = 'ASG/ATS Team'
+__contact__ = 'pyats-support@cisco.com'
+__copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
+
+from .main import main
+from .plugin import BasePlugin
+from .manager import Manager, TimedManager
+
+try:
+    from ats.cisco.stats import CesMonitor
+    CesMonitor(action = 'genietelemetry', application='Genie').post()
+except Exception:
+    try:
+        from ats.utils.stats import CesMonitor
+        CesMonitor(action = 'genietelemetry', application='Genie').post()
+    except Exception:
+        pass
