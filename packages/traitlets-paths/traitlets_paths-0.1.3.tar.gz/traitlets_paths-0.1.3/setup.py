5490231dab20291ac8ed19324d602fcce7414fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:21 +0000

    Add package traitlets-paths/0.1.3/traitlets_paths-0.1.3.tar.gz

---
+++ packages/traitlets-paths/traitlets_paths-0.1.3.tar.gz/traitlets_paths-0.1.3/setup.py
@@ -0,0 +1,24 @@
+import os
+import io
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+
+setup(
+    name='traitlets_paths',
+    version='0.1.3',
+    author='Zach Sailer',
+    author_email='zachsailer@gmail.com',
+    description='Traitlets module for pathlib.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Zsailer/traitlets_paths',
+    packages=find_packages(exclude=('tests',)),
+    install_requires=['traitlets'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
