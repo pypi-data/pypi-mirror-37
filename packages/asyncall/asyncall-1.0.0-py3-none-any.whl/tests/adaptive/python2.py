67c35637c73a9029538add40601fa309163b2b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:58 +0000

    Add package asyncall/1.0.0/asyncall-1.0.0-py3-none-any.whl

---
+++ packages/asyncall/asyncall-1.0.0-py3-none-any.whl/tests/adaptive/python2.py
@@ -0,0 +1,18 @@
+# /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from tests.context import Result
+import random
+
+try:
+    import asyncio
+except ImportError:
+    import trollius as asyncio
+
+@asyncio.coroutine
+def demo(name):
+    asleep = random.randint(0, 1)
+    print('---start, %s  %s' % (name, asleep))
+    yield asyncio.sleep(asleep)
+    print('---stop, %s' % name)
+    raise Result(name)
