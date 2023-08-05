e259975123a98ab1343faecbced3a44514ec87b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:51 +0000

    Add package python-dubbo/0.0.4/python-dubbo-0.0.4.tar.gz

---
+++ packages/python-dubbo/python-dubbo-0.0.4.tar.gz/python-dubbo-0.0.4/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='python-dubbo',
+    version='0.0.4',
+    url='https://github.com/RitterHou/python-dubbo',
+    author='hourui',
+    author_email='hourui@qianmi.com',
+    description='Python Dubbo Client.',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=['tests', 'tools']),
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: Chinese (Simplified)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[
+        'kazoo==2.4.0'
+    ],
+)
