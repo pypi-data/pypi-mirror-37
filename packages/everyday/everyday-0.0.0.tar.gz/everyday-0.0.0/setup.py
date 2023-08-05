1f7d49eaf4e1eb1658bb345267af5d4d37bbe26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:45 +0000

    Add package everyday/0.0.0/everyday-0.0.0.tar.gz

---
+++ packages/everyday/everyday-0.0.0.tar.gz/everyday-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup                                                     
+setup(                                                                           
+  name = 'everyday',                                                                 
+  version = '0.0.0',                                                             
+  description = 'Python client library for the Everyday API.',                       
+  author = 'Fabian Popa',                                                        
+  author_email = 'public@fabianpopa.com',                                        
+  url = 'https://github.com/everyday',                                               
+)
