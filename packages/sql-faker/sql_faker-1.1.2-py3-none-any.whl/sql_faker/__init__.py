dafd240cd9f51e3aa38439b51606a34dd949d65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:05 +0000

    Add package sql-faker/1.1.2/sql_faker-1.1.2-py3-none-any.whl

---
+++ packages/sql-faker/sql_faker-1.1.2-py3-none-any.whl/sql_faker/__init__.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+__all__ = [
+    'cache_dict',
+    'datatype',
+    'db_utils',
+    'examples',
+    'faker',
+    'mapping',
+    'sql_faker',
+    'times',
+    'values',
+    'random_data',
+    'asserts',
+]
---
+++ packages/sql-faker/sql_faker-1.1.2-py3-none-any.whl/sql_faker/random_data/__init__.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+"""
+    该模块用于存放随机值生成器
+"""
+__all__ = [
+    'random_abstract',
+    'address_random',
+    'age_random',
+    'email_random',
+    'id_random',
+    'phone_random',
+    'sex_random',
+    'time_random',
+    'username_random',
+]
+
+from .random_abstract import RandomData
