e55b53708c97f26ccae873f5e58d398da827ff05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:29 +0000

    Add package hippo2client/1.3/hippo2client-1.3-py2.py3-none-any.whl

---
+++ packages/hippo2client/hippo2client-1.3-py2.py3-none-any.whl/hippo2client/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import sys
+
+if sys.version_info < (3, 0):
+    from hippo2client import Agent
+    from hippo2client import MajorEntity
+    from hippo2client import Meta
+    from hippo2client import MetaTest
+else:
+    from hippo2client.hippo2client import Agent
+    from hippo2client.hippo2client import MajorEntity
+    from hippo2client.hippo2client import Meta
+    from hippo2client.hippo2client import MetaTest
+
+
