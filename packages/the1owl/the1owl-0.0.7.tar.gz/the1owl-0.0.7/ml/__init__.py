e82e741f5126e95db42f59d9cdb9e9633e3b0ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package the1owl/0.0.7/the1owl-0.0.7.tar.gz

---
+++ packages/the1owl/the1owl-0.0.7.tar.gz/the1owl-0.0.7/ml/__init__.py
@@ -0,0 +1,12 @@
+"""
+Common machine learning operations module for Python
+=============================================================================
+
+the1owl is a Python module simplifying getting started with machine learning 
+with shortcuts for common Python packages 
+(numpy, scipy, pandas, matplotlib, sklearn, nltk, xgboost, lightgbm, catboost).
+
+See http://the1owl.com for documentation
+"""
+__all__ = ['simple']
+
