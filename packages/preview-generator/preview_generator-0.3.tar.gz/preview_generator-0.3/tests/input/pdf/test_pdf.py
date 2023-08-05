96f1e1c2637b8f3b5087fae1d4a5026cc078b9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:16 +0000

    Add package preview-generator/0.3/preview_generator-0.3.tar.gz

---
+++ packages/preview-generator/preview_generator-0.3.tar.gz/preview_generator-0.3/tests/input/pdf/test_pdf.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import os
+from PIL import Image
+import shutil
+from tests import test_utils
+import re
+
+from preview_generator.manager import PreviewManager
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+CACHE_DIR = '/tmp/preview-generator-tests/cache'
+
+
+def setup_function(function):
+    shutil.rmtree(CACHE_DIR, ignore_errors=True)
+
+
+def test_to_jpeg():
+    manager = PreviewManager(
+        cache_folder_path=CACHE_DIR,
+        create_folder=True
+    )
+    path_to_file = manager.get_jpeg_preview(
+        file_path=os.path.join(CURRENT_DIR, 'the_pdf.pdf'),
+        height=512,
+        width=321,
+        force=True
+    )
+    assert os.path.exists(path_to_file) == True
+    assert os.path.getsize(path_to_file) > 0
+    assert re.match(test_utils.CACHE_FILE_PATH_PATTERN__JPEG, path_to_file)
+
+    with Image.open(path_to_file) as jpeg:
+        assert jpeg.height in range(453, 455)
+        assert jpeg.width == 321
+
+
+def test_to_jpeg_no_size():
+    manager = PreviewManager(
+        cache_folder_path=CACHE_DIR,
+        create_folder=True
+    )
+    path_to_file = manager.get_jpeg_preview(
+        file_path=os.path.join(CURRENT_DIR, 'the_pdf.pdf'),
+        force=True
+    )
+    assert os.path.exists(path_to_file) == True
+    assert os.path.getsize(path_to_file) > 0
+    assert re.match(test_utils.CACHE_FILE_PATH_PATTERN__JPEG, path_to_file)
+
+    with Image.open(path_to_file) as jpeg:
+        assert jpeg.height == 256
+        assert jpeg.width in range(180, 182)
