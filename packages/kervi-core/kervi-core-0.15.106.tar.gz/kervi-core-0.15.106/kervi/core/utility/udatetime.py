a58db13c211d30f95ae81df201e54f2e9b4875ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:39 +0000

    Add package kervi-core/0.15.106/kervi-core-0.15.106.tar.gz

---
+++ packages/kervi-core/kervi-core-0.15.106.tar.gz/kervi-core-0.15.106/kervi/core/utility/udatetime.py
@@ -0,0 +1,10 @@
+class datetime():
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def utcnow():
+        return datetime()
+
+    def strftime(self, format):
+        return "2018-10-14 12:12:12"
