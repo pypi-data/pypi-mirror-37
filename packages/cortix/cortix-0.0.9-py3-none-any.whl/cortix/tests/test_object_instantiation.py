368a8094f8634b9347b1347a33de08f96468f914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:52 +0000

    Add package cortix/0.0.9/cortix-0.0.9-py3-none-any.whl

---
+++ packages/cortix/cortix-0.0.9-py3-none-any.whl/cortix/tests/test_object_instantiation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the Cortix toolkit evironment
+# https://github.com/dpploy/cortix
+#
+# All rights reserved, see COPYRIGHT for full restrictions.
+# https://github.com/dpploy/cortix/blob/master/COPYRIGHT.txt
+#
+# Licensed under the GNU General Public License v. 3, please see LICENSE file.
+# https://www.gnu.org/licenses/gpl-3.0.txt
+
+import os
+from cortix import Cortix
+
+def test_object_instantiation():
+    '''
+    Test for successful object instantiation
+    '''
+    pwd = os.path.dirname(__file__)
+    full_path_config_file = os.path.join(
+        pwd, 'input/cortix-config-pyplot.xml')
+    cortix1 = Cortix('cortix-dev1', full_path_config_file)
+    assert cortix1 is not None
+
+if __name__ == "__main__":
+    test_object_instantiation()
