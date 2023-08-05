d955350ef0592bbe1693874c421fbc4d7aa6baf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:39 +0000

    Add package genie.libs.robot/3.1.0/genie.libs.robot-3.1.0-py3-none-any.whl

---
+++ packages/genie.libs.robot/genie.libs.robot-3.1.0-py3-none-any.whl/genie/libs/robot/__init__.py
@@ -0,0 +1,27 @@
+'''
+    Module:
+        libs.robot
+
+    Description:
+        This is the library sub-component of Genie for `genie.robot`.
+
+'''
+
+# metadata
+__version__ = '3.1.0'
+__author__ = 'Cisco Systems Inc.'
+__contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
+__copyright__ = 'Copyright (c) 2018, Cisco Systems Inc.'
+
+try:
+    from ats.cisco.stats import CesMonitor
+    CesMonitor(action = 'genielibsRobot', application='Genie').post()
+except Exception:
+    try:
+        from ats.utils.stats import CesMonitor
+        CesMonitor(action = 'genielibsRobot', application='Genie').post()
+    except Exception:
+        pass
+
+from genie import abstract
+abstract.declare_package(__name__)
