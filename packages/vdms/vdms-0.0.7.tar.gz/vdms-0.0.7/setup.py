64e2cd347a8c2a01706b979ba4d8bc9fddff083d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:20 +0000

    Add package vdms/0.0.7/vdms-0.0.7.tar.gz

---
+++ packages/vdms/vdms-0.0.7.tar.gz/vdms-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vdms",
+    version="0.0.7",
+    author="Luis Remis",
+    author_email="luis.remis@intel.com",
+    description="VDMS Client Module",
+    install_requires=['protobuf'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IntelLabs/vdms",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
