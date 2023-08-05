44779ef797518cd836798f54024c31750fb82056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:08 +0000

    Add package cytopast/1.0.8/cytopast-1.0.8.tar.gz

---
+++ packages/cytopast/cytopast-1.0.8.tar.gz/cytopast-1.0.8/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+setup(
+    name='cytopast',
+    packages=['cytopast'],
+    include_package_data=True,
+    package_data={'cytopast': [os.path.join('templates', '*.html'), os.path.join('templates', '*.js'),
+                               os.path.join('..', 'README.md')]},
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    version='1.0.8',
+    description='Visualisation of PastML trees.',
+    author='Anna Zhukova',
+    author_email='anna.zhukova@pasteur.fr',
+    url='https://github.com/evolbioinfo/cytopast',
+    keywords=['PASTML', 'visualisation', 'phylogeny'],
+    install_requires=['ete3', 'pandas', 'numpy', 'jinja2', 'pastml>=1.0.8'],
+    entry_points={
+            'console_scripts': [
+                'cytopast = cytopast.pastml_analyser:main',
+            ]
+    },
+)
