b57924a6ceab0e856a251ebeb239bfa966b48257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:12 +0000

    Add package easy-getch/1.0.2/easy_getch-1.0.2.tar.gz

---
+++ packages/easy-getch/easy_getch-1.0.2.tar.gz/easy_getch-1.0.2/README.md
@@ -0,0 +1,20 @@
+[![Build Status](https://travis-ci.com/cmccandless/easy_getch.svg?branch=master)](https://travis-ci.com/cmccandless/easy_getch)[![PyPI](https://img.shields.io/pypi/v/nine.svg)](https://pypi.org/project/easy_getch/)
+
+# easy-getch
+Easily get a single character.
+
+Inspired by [this article](https://code.activestate.com/recipes/134892).
+
+## Installation
+```bash
+pip install easy_getch
+```
+
+## Usage
+
+```Python
+from easy_getch import getch
+
+ch = getch()
+print(ch)
+```
---
+++ packages/easy-getch/easy_getch-1.0.2.tar.gz/easy_getch-1.0.2/setup.py
@@ -0,0 +1,33 @@
+import subprocess
+import sys
+import setuptools
+from changelog import changelog
+from easy_getch.__version__ import VERSION
+
+
+if __name__ == '__main__':
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+    long_description += '\n'.join(changelog())
+
+    setuptools.setup(
+        name="easy_getch",
+        version=VERSION,
+        author="Corey McCandless",
+        author_email="crm1994@gmail.com",
+        description=(
+            "Easily get a single character"
+        ),
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/cmccandless/easy_getch",
+        packages=setuptools.find_packages(),
+        classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        entry_points={},
+        install_requires=[],
+        include_package_data=True
+    )
