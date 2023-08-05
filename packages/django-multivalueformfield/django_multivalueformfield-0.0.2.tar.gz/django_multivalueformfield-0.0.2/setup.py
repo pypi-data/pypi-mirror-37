35ee60cbbddd7c72e9b61c1fac8f26bb42012735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:51 +0000

    Add package django-multivalueformfield/0.0.2/django_multivalueformfield-0.0.2.tar.gz

---
+++ packages/django-multivalueformfield/django_multivalueformfield-0.0.2.tar.gz/django_multivalueformfield-0.0.2/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_multivalueformfield",
+    version="0.0.2",
+    author="Joel Cross",
+    author_email="joel@kazbak.co.uk",
+    description="Multiple-value field for Django forms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ukch/django_multivalueformfield",
+    py_modules=["multivaluefield"],
+    license="MIT",
+    classifiers=(
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django :: 2.0",
+    ),
+    install_requires=(
+        "Django>=2.0",
+    ),
+)
