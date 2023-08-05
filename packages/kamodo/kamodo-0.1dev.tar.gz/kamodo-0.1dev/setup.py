9957a46c5925be8d99e9bab474db02885569bdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:42 +0000

    Add package kamodo/0.1dev/kamodo-0.1dev.tar.gz

---
+++ packages/kamodo/kamodo-0.1dev.tar.gz/kamodo-0.1dev/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='kamodo',
+    version='0.1dev',
+    author='Asher Pembroke',
+    url='https://github.com/asher-pembroke/kamodo',
+    author_email='apembroke@gmail.com',
+    packages=['kamodo',],
+    license='Apache2.0',
+    long_description=open('README').read(),
+)
+
