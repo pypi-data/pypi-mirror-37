32ac16f329be91e79b11fdff4f035223bc90d876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:14 +0000

    Add package palombe/0.3.1/palombe-0.3.1-py3-none-any.whl

---
+++ packages/palombe/palombe-0.3.1-py3-none-any.whl/palombe/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+def mkfifo(name):
+    prefix = "/tmp/palombe/"
+    path = "%s%s" % (prefix, name)
+    if not os.path.exists(prefix):
+        os.makedirs(prefix)
+    if not os.path.exists(path):
+        os.mkfifo(path, 0o600)
+    return path
+
+def send(name, value):
+    file = open(mkfifo(name), "w")
+    file.write(value)
+
+def receive(name):
+    path = mkfifo(name)
+    file = open(path, "r")
+    os.remove(path)
+    return file.read()
