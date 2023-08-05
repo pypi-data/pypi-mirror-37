c1ba722c1215a4c5ee62993b31e69a8de8ef240b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:51 +0000

    Add package insta-api/0.1.5/insta_api-0.1.5.tar.gz

---
+++ packages/insta-api/insta_api-0.1.5.tar.gz/insta_api-0.1.5/insta_api/exceptions.py
@@ -0,0 +1,16 @@
+class LoginAuthenticationError(Exception):
+    """Raised when the server fails to authenticate login credentials"""
+
+
+class InvalidHashtag(Exception):
+    """Raised when user entered an invalid hashtag"""
+
+
+class CheckpointRequired(Exception):
+    """ Raised when checkpoint is required"""
+
+
+class MissingMedia(Exception):
+    """ Raised when the post one is trying to like or follow does not exist (maybe deleted) """
+
+
