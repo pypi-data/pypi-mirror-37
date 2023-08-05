1dc6f0875f0cd58acdcae43439eb7b848392b30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:43 +0000

    Add package rotest/4.2.1/rotest-4.2.1-py2.py3-none-any.whl

---
+++ packages/rotest/rotest-4.2.1-py2.py3-none-any.whl/rotest/management/models/__init__.py
@@ -0,0 +1,10 @@
+"""Define Rotest's common models.
+
+The Django infrastructure expects a models.py file containing all the models
+definitions for each application. This folder is a workaround used in order
+to separate the different common application models into different files.
+"""
+# pylint: disable=unused-import
+from rotest.management.models.resource_data import ResourceData
+from rotest.management.models.ut_models import \
+                                DemoResourceData, DemoComplexResourceData
