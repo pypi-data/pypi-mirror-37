399a5afca646f078a98d0292a35ca9dceb641e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package vetrr/0.1.5/vetrr-0.1.5.tar.gz

---
+++ packages/vetrr/vetrr-0.1.5.tar.gz/vetrr-0.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+import os
+import glob
+
+def get_scripts():
+    """ Grab all the scripts in the bin directory.  """
+    scripts = []
+    if os.path.isdir('bin'):
+        scripts = [ fname for fname in glob.glob(os.path.join('bin', '*'))
+                                if not os.path.basename(fname).endswith('.rst') ]
+    return scripts
+
+scripts = get_scripts()
+
+setup(name='vetrr',
+      version='0.1.5',
+      description='vet redrock outputs',
+      url='http://github.com/mattcwilde/vetrr',
+      author='Matthew Wilde',
+      author_email='mattcwilde@gmail.com',
+      license='MIT',
+      packages=['vetrr'],
+      scripts=scripts,
+      dependency_links=['https://github.com/linetools/linetools'],
+      install_requires=[
+        'matplotlib',
+        'pyYAML',
+        'astropy',
+        'future',
+        'numpy'
+        ],
+      zip_safe=True)
