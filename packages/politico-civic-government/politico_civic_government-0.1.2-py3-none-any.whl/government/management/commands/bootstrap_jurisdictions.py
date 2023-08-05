b8fb408871543d9f212bbbce7a70e7ed6b581616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:16 +0000

    Add package politico-civic-government/0.1.2/politico_civic_government-0.1.2-py3-none-any.whl

---
+++ packages/politico-civic-government/politico_civic_government-0.1.2-py3-none-any.whl/government/management/commands/bootstrap_jurisdictions.py
@@ -0,0 +1,29 @@
+import us
+from django.core.management.base import BaseCommand
+from geography.models import Division, DivisionLevel
+from government.models import Jurisdiction
+from tqdm import tqdm
+
+
+class Command(BaseCommand):
+    help = (
+        "Loads federal and state jurisdictions. Must be run AFTER the"
+        "bootstrap_geography command."
+    )
+
+    def handle(self, *args, **options):
+        print("Loading jurisdictions")
+        USA = Division.objects.get(code="00", level__name="country")
+        FED, created = Jurisdiction.objects.get_or_create(
+            name="U.S. Federal Government", division=USA
+        )
+        state_level = DivisionLevel.objects.get(name="state")
+        for state in tqdm(us.states.STATES):
+            division = Division.objects.get(code=state.fips, level=state_level)
+            name = "{} State Government".format(state.name)
+            if str(state.fips) == "11":
+                continue
+            Jurisdiction.objects.get_or_create(
+                name=name, division=division, parent=FED
+            )
+        self.stdout.write(self.style.SUCCESS("Done."))
