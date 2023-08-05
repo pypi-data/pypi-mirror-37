23032a4fe27646c7636b3d0a151b8c34c9f56003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:49 +0000

    Add package orgbabelhelper/1.0.7/orgbabelhelper-1.0.7.tar.gz

---
+++ packages/orgbabelhelper/orgbabelhelper-1.0.7.tar.gz/orgbabelhelper-1.0.7/setup.py
@@ -0,0 +1,23 @@
+# we use the distribute framework that has a backward compatible invocation
+# to the setuptools
+from setuptools import setup
+
+setup(
+    name = "orgbabelhelper",
+    version = "1.0.7",
+    description = "python helper module for Emacs org mode babel",
+    long_description = "python helper module for working with Emacs"
+    + " org babel source blocks",
+    author = "Derek Feichtinger",
+    author_email = "dfeich@gmail.com",
+    license = "GPL",
+    url = "https://github.com/dfeich/org-babel-examples/tree/master/python",
+    packages = ['orgbabelhelper'],
+
+    install_requires = ['pandas'],
+    classifiers = [
+	"Development Status :: 4 - Beta",
+	"Intended Audience :: Developers",
+	"Programming Language :: Python :: 3"
+    ]
+)
