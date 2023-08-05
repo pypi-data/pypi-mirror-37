c792b5c5095e02e9ab16e8a97fd1e811e10dc100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:46 +0000

    Add package space-bandits/0.0.91/space_bandits-0.0.91.tar.gz

---
+++ packages/space-bandits/space_bandits-0.0.91.tar.gz/space_bandits-0.0.91/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='space_bandits',
+    version='0.0.91',
+    description='Deep Bayesian Contextual Bandits Library',
+    author='Michael Klear',
+    author_email='michael@launchpad.ai',
+    url='https://github.com/AlliedToasters/space_bandits/archive/v0.0.91.tar.gz',
+    install_requires=[
+      'tensorflow>=1.5.0',
+      'numpy>=1.14.3',
+      'scipy>=0.19.1',
+      'pandas>=0.21.0',
+      'cython'
+    ],
+    packages=['space_bandits']
+)
