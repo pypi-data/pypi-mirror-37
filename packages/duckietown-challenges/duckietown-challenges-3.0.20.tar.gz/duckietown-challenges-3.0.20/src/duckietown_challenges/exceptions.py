be02083c41e620413f750d077a729878aff777de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:29 +0000

    Add package duckietown-challenges/3.0.20/duckietown-challenges-3.0.20.tar.gz

---
+++ packages/duckietown-challenges/duckietown-challenges-3.0.20.tar.gz/duckietown-challenges-3.0.20/src/duckietown_challenges/exceptions.py
@@ -0,0 +1,24 @@
+
+class ChallengeException(Exception):
+    pass
+
+
+class NotAvailable(ChallengeException):
+    pass
+
+
+class InvalidConfiguration(ChallengeException):
+    pass
+
+
+class InvalidSubmission(Exception):
+    """ Can be raised by evaluator """
+    pass
+
+
+class InvalidEvaluator(Exception):
+    pass
+
+class InvalidEnvironment(Exception):
+    pass
+
