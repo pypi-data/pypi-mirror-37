53d683c590c6ccd0dd3c2e682e1fc69ec331feba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:19 +0000

    Add package easyxlsx/0.3.6/easyxlsx-0.3.6.tar.gz

---
+++ packages/easyxlsx/easyxlsx-0.3.6.tar.gz/easyxlsx-0.3.6/easyxlsx/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    easyxlsx
+    ~~~~~
+
+    An easy way to export excel based on XlsxWriter.
+
+    :copyright: (c) 2016 by tommao.
+    :license: MIT, see LICENSE for more details.
+"""
+
+from easyxlsx.base import BaseWriter, ModelWriter, SimpleWriter, FormatMixin
+
+
+__all__ = [
+    'BaseWriter',
+    'SimpleWriter',
+    'ModelWriter',
+    'FormatMixin'
+]
