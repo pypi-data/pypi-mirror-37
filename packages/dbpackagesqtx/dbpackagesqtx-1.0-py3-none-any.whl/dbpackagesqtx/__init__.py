1c4b7f410c20ca0f65eee98f22cfdb275a141dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:09 +0000

    Add package dbpackagesqtx/1.0/dbpackagesqtx-1.0-py3-none-any.whl

---
+++ packages/dbpackagesqtx/dbpackagesqtx-1.0-py3-none-any.whl/dbpackagesqtx/__init__.py
@@ -0,0 +1,13 @@
+import phoenixdb
+import phoenixdb.cursor
+
+def qtx_phoenix_con(host, port, commit=True):
+    db_url = 'http://'+host+':'+port+'/'
+    conn = phoenixdb.connect(db_url, autocommit=commit)
+    return conn.cursor()
+
+def qtx_mssql_con():
+    pass
+
+def qtx_mysql_con():
+    pass
