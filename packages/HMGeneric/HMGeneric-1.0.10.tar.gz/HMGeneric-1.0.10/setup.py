f96696cd929a914479835b28d82ae76d5d9f4d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:37 +0000

    Add package HMGeneric/1.0.10/HMGeneric-1.0.10.tar.gz

---
+++ packages/HMGeneric/HMGeneric-1.0.10.tar.gz/HMGeneric-1.0.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-16') as fh:
+    long_description = fh.read()
+
+setup(
+    name="HMGeneric",
+    version="1.0.10",
+    author="HM",
+    description="A package contains a generic functions and classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+    ],
+)
