be502704635ed8e5f74794966970f13b2269f6e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:43 +0000

    Add package responder/0.0.1/responder-0.0.1.tar.gz

---
+++ packages/responder/responder-0.0.1.tar.gz/responder-0.0.1/responder/formats.py
@@ -0,0 +1,27 @@
+import yaml
+import json
+
+
+def format_form(r, encode=False):
+    if not encode:
+        return r._wz.form
+
+
+def format_yaml(r, encode=False):
+    if encode:
+        r.headers.update({"Content-Type": "application/x-yaml"})
+        return yaml.dump(r.media)
+    else:
+        return yaml.load(r.content)
+
+
+def format_json(r, encode=False):
+    if encode:
+        r.headers.update({"Content-Type": "application/json"})
+        return json.dumps(r.media)
+    else:
+        return json.loads(r.content)
+
+
+def get_formats():
+    return {"json": format_json, "yaml": format_yaml, "form": format_form}
