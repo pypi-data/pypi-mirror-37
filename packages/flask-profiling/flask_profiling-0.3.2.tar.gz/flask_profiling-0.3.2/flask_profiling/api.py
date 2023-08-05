5ecb14a700ca7cb386c7667fc3de3a815d114895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package flask-profiling/0.3.2/flask_profiling-0.3.2.tar.gz

---
+++ packages/flask-profiling/flask_profiling-0.3.2.tar.gz/flask_profiling-0.3.2/flask_profiling/api.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, request, jsonify
+from .ext import backend
+
+
+api = Blueprint(
+    'flask-profiling', __name__,
+    url_prefix="/flask-profiling",
+    static_folder="static/dist/", static_url_path='/static/dist')
+
+
+@api.route("/")
+def index():
+    return api.send_static_file("index.html")
+
+
+@api.route("/api/measurements/")
+def filter_measurements():
+    args = dict(request.args.items())
+    measurements = backend.filter(args)
+    return jsonify({"measurements": measurements})
+
+
+@api.route("/api/measurements/grouped")
+def get_measurements_summary():
+    args = dict(request.args.items())
+    measurements = backend.get_summary(args)
+    return jsonify({"measurements": measurements})
+
+
+@api.route("/api/measurements/<measurement_id>")
+def getContext(measurement_id):
+    return jsonify(backend.get(measurement_id))
+
+
+@api.route("/api/measurements/timeseries/")
+def get_requests_timeseries():
+    args = dict(request.args.items())
+    return jsonify({"series": backend.get_timeseries(args)})
+
+
+@api.route("/api/measurements/method_distribution/")
+def get_method_distribution():
+    args = dict(request.args.items())
+    return jsonify({"distribution": backend.get_method_distribution(args)})
+
+
+@api.route("/db/dump")
+def dumpDatabase():
+    response = jsonify({
+        "summary": backend.get_summary()})
+    response.headers["Content-Disposition"] = "attachment; filename=dump.json"
+    return response
+
+
+@api.route("/db/delete")
+def deleteDatabase():
+    return jsonify({"status": backend.truncate()})
+
+
+@api.after_request
+def x_robots_tag_header(response):
+    response.headers['X-Robots-Tag'] = 'noindex, nofollow'
+    return response
---
+++ packages/flask-profiling/flask_profiling-0.3.2.tar.gz/flask_profiling-0.3.2/setup.py
@@ -0,0 +1,72 @@
+"""
+Flask-Profiling
+-------------
+
+Flask Profiling
+
+Links
+`````
+
+* `development version <http://github.com/huangxiaohen2738/flask-profiling/>`
+
+"""
+from setuptools import setup
+
+
+tests_require = [
+    "Flask-Testing",
+]
+
+install_requires = [
+    'flask',
+    'flask-admin',
+    'flask-httpauth',
+    'itsdangerous',
+    'jinja2',
+    'ldap',
+    'markupSafe',
+    'pymysql',
+    'sqlalchemy',
+    'werkzeug'
+]
+
+setup(
+    name='flask_profiling',
+    version='0.3.2',
+    url='https://github.com/huangxiaohen2738/flask-profiling',
+    license=open('LICENSE').read(),
+    author='huangxiaohen2738',
+    author_email='huangxiaohen2738@gmail.com',
+    description='A simple web UI for flask to profile the apis',
+    keywords=[
+        'profiler', 'flask', 'performance'
+    ],
+    packages=['flask_profiling'],
+    package_data={
+        'flask_profiling': [
+            'storage/*',
+            'static/dist/fonts/*',
+            'static/dist/css/*',
+            'static/dist/js/*',
+            'static/dist/images/*',
+            'static/dist/js/*'
+            'static/dist/*',
+            'static/dist/index.html',
+            ]
+        },
+    test_suite="tests.suite",
+    zip_safe=False,
+    platforms='any',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
