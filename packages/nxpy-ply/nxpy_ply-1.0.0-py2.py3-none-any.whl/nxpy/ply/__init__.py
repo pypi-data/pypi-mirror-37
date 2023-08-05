6ea0676024df724aaa3162a73bfd8da4f08603d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package nxpy-ply/1.0.0/nxpy_ply-1.0.0-py2.py3-none-any.whl

---
+++ packages/nxpy-ply/nxpy_ply-1.0.0-py2.py3-none-any.whl/nxpy/ply/__init__.py
@@ -0,0 +1,13 @@
+# nxpy_ply --------------------------------------------------------------------
+
+# Copyright Nicola Musatti 2011 - 2018
+# Use, modification, and distribution are subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+# See https://github.com/nmusatti/nxpy/tree/master/libs/ply. ------------------
+
+r"""
+Wrapper classes for the PLY parser generator.
+
+"""
