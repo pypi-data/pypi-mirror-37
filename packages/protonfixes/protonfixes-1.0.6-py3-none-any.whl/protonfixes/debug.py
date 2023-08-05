e8a5ec73049a0fbed9b9c86af929b8b5d9317dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:00 +0000

    Add package protonfixes/1.0.6/protonfixes-1.0.6-py3-none-any.whl

---
+++ packages/protonfixes/protonfixes-1.0.6-py3-none-any.whl/protonfixes/debug.py
@@ -0,0 +1,44 @@
+""" Prints debug info if the environment variable DEBUG is 1
+"""
+
+import os
+import sys
+import shutil
+from __main__ import CURRENT_PREFIX_VERSION, basedir, env
+
+def log(msg=None):
+    pfx = 'ProtonFixes[' + str(os.getpid()) + '] DEBUG: '
+    sys.stderr.write(pfx + str(msg) +  os.linesep)
+    sys.stderr.flush()
+
+line = '---------------------------------------'
+log('---- begin protontricks debug info ----')
+log('Proton Python Version:')
+log(sys.executable)
+log(sys.version)
+log(line)
+log('System Python Version:')
+try:
+    log(os.readlink(shutil.which('python')))
+except:
+    log(which('python'))
+log(line)
+
+log('Proton Version:')
+log(CURRENT_PREFIX_VERSION)
+log(line)
+
+log('Proton Directory:')
+log(basedir)
+log(line)
+
+ignorevars = ['SteamUser', 'OLDPWD', 'SDL_GAMECONTROLLERCONFIG', 'SteamAppUser', 'SDL_GAMECONTROLLER_IGNORE_DEVICES']
+log('Environment Variables:')
+for key, value in os.environ.items():
+    if key not in ignorevars:
+        log(key + '=' + value)
+log(line)
+
+log('Command Line:')
+log(sys.argv)
+log('----- end protontricks debug info -----')
