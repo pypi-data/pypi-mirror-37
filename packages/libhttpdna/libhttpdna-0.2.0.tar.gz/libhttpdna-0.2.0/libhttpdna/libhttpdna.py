c0be74bb1944dba9e10a36bd929a6cabfb2fc592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:34 +0000

    Add package libhttpdna/0.2.0/libhttpdna-0.2.0.tar.gz

---
+++ packages/libhttpdna/libhttpdna-0.2.0.tar.gz/libhttpdna-0.2.0/libhttpdna/libhttpdna.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 16:09:37 2018
+
+@author: antony
+"""
+
+import libdna
+
+def get_loc_from_params(id_map):
+    
+    if 'chr' not in id_map:
+        return None
+        
+    if 's' not in id_map:
+        return None
+        
+    if 'e' not in id_map:
+        return None
+    
+    chr = id_map['chr'][0]
+    start = id_map['s'][0]
+    end = id_map['e'][0]
+    
+    if start > end:
+      start = start ^ end
+      end = start ^ end
+      start = start ^ end
+    
+    return libdna.Loc(chr, start, end)
