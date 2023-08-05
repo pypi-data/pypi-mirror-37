8e397a38b708fc056d5763d67a726e06736c68d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:40 +0000

    Add package smsutil/1.1.0/smsutil-1.1.0.tar.gz

---
+++ packages/smsutil/smsutil-1.1.0.tar.gz/smsutil-1.1.0/setup.py
@@ -0,0 +1,50 @@
+import os
+
+from setuptools import setup
+
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+about = {}
+with open(os.path.join(BASE_DIR, 'smsutil', '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+requirements = [
+    'future==0.16.0'
+]
+
+test_requirements = [
+    'pytest'
+]
+
+setup_requirements = [
+    'pytest-runner'
+]
+
+setup(
+    name='smsutil',
+    version=about['__version__'],
+    description='encode, decode and split SMS.',
+    long_description=readme,
+    author='Jezeniel Zapanta',
+    author_email='jezeniel.zapanta@gmail.com',
+    url='https://github.com/jezeniel/smsutil',
+    license='MIT',
+    packages=['smsutil'],
+    keywords='sms utils utilities smsutil short message service',
+    install_requires=requirements,
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+    ]
+)
