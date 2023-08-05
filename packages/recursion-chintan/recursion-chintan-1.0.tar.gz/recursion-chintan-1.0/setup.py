edb326cb4377e027919ef1903ea4efd315657c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:51 +0000

    Add package recursion-chintan/1.0/recursion-chintan-1.0.tar.gz

---
+++ packages/recursion-chintan/recursion-chintan-1.0.tar.gz/recursion-chintan-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name='recursion-chintan',
+		version='1.0',
+		py_modules=['recursion'],
+		author='chintan',
+		author_email='chintan131@gmail.com',
+		url='http://www.headfirstlabs.com',
+		description='test_desc'
+	)
