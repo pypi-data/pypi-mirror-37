e19e75dfe76bd2ab2082a3ecb4bbad3565915359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:48 +0000

    Add package rotest/4.3.0/rotest-4.3.0.tar.gz

---
+++ packages/rotest/rotest-4.3.0.tar.gz/rotest-4.3.0/src/rotest/__init__.py
@@ -0,0 +1,12 @@
+"""Rotest testing framework, based on Python unit-test and Django."""
+# pylint: disable=unused-import
+from unittest import skip, SkipTest, skipIf as skip_if
+
+import colorama
+
+from .common import config
+from .cli.client import main
+
+
+# Enable color printing on screen.
+colorama.init()
