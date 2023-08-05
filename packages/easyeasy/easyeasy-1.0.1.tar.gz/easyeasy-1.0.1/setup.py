f288baad8114bc6e70f909b6fc1f1e182a47e929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:16 +0000

    Add package easyeasy/1.0.1/easyeasy-1.0.1.tar.gz

---
+++ packages/easyeasy/easyeasy-1.0.1.tar.gz/easyeasy-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easyeasy",
+    version="1.0.1",
+    author="EasyEasy.io",
+    author_email="feedback@easyeasy.io",
+    description="Client for EasyEasy.io cloud database",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EasyEasyio/python-client",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: OS Independent",
+    ],
+)
