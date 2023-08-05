99081e35031858e99c40cdf145afe7202afd1de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:49 +0000

    Add package ybc-poetry/1.0.0/ybc_poetry-1.0.0.tar.gz

---
+++ packages/ybc-poetry/ybc_poetry-1.0.0.tar.gz/ybc_poetry-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+  name='ybc_poetry',
+  packages=['ybc_poetry'],
+  version='1.0.0',
+  description='Poetry search',
+  long_description='Poetry search',
+  author='hurs',
+  author_email='hurs@fenbi.com',
+  keywords=['pip3', 'python3', 'python', 'Poetry search'],
+  license='MIT',
+)
