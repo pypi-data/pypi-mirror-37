219c573dcee2c19b875c8d8f3ec91e66e7732103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:16 +0000

    Add package torchbearer/0.2.3/torchbearer-0.2.3-py3-none-any.whl

---
+++ packages/torchbearer/torchbearer-0.2.3-py3-none-any.whl/torchbearer/callbacks/printer.py
@@ -0,0 +1,135 @@
+import torchbearer
+
+from torchbearer.callbacks import Callback
+from tqdm import tqdm
+
+
+class ConsolePrinter(Callback):
+    """The ConsolePrinter callback simply outputs the training metrics to the console.
+
+    :param validation_label_letter: This is the letter displayed after the epoch number indicating the current phase of training
+    :type validation_label_letter: String
+    """
+    def __init__(self, validation_label_letter='v'):
+        super().__init__()
+        self.validation_label = validation_label_letter
+
+    @staticmethod
+    def _step(state, letter, steps):
+        epoch_str = '{:d}/{:d}({:s}): '.format(state[torchbearer.EPOCH], state[torchbearer.MAX_EPOCHS], letter)
+        batch_str = '{:d}/{:d} '.format(state[torchbearer.BATCH], steps)
+        stats_str = ', '.join(['{0}:{1:.03g}'.format(key, value) for (key, value) in state[torchbearer.METRICS].items()])
+        print('\r' + epoch_str + batch_str + stats_str, end='')
+
+    @staticmethod
+    def _end(state, letter):
+        epoch_str = '{:d}/{:d}({:s}): '.format(state[torchbearer.EPOCH], state[torchbearer.MAX_EPOCHS], letter)
+        stats_str = ', '.join(['{0}:{1:.03g}'.format(key, value) for (key, value) in state[torchbearer.METRICS].items()])
+        print('\r' + epoch_str + stats_str)
+
+    def on_step_training(self, state):
+        ConsolePrinter._step(state, 't', state[torchbearer.STEPS])
+
+    def on_end_training(self, state):
+        ConsolePrinter._end(state, 't')
+
+    def on_step_validation(self, state):
+        ConsolePrinter._step(state, self.validation_label, state[torchbearer.STEPS])
+
+    def on_end_validation(self, state):
+        ConsolePrinter._end(state, self.validation_label)
+
+
+class Tqdm(Callback):
+    """The Tqdm callback outputs the progress and metrics for training and validation loops to the console using TQDM. The given key is used to label validation output.
+
+    :param validation_label_letter: The letter to use for validation outputs.
+    :type validation_label_letter: str
+    :param on_epoch: If True, output a single progress bar which tracks epochs
+    :type on_epoch: bool
+    :param tqdm_args: Any extra keyword args provided here will be passed through to the tqdm module constructor. See `github.com/tqdm/tqdm#parameters <https://github.com/tqdm/tqdm#parameters>`_ for more details.
+    """
+    def __init__(self, tqdm_module=tqdm, validation_label_letter='v', on_epoch=False, **tqdm_args):
+        self.tqdm_module = tqdm_module
+        self._loader = None
+        self.validation_label = validation_label_letter
+        self._on_epoch = on_epoch
+        self.tqdm_args = tqdm_args
+
+    def _on_start(self, state, letter):
+        bar_desc = '{:d}/{:d}({:s})'.format(state[torchbearer.EPOCH], state[torchbearer.MAX_EPOCHS], letter)
+        self._loader = self.tqdm_module(total=state[torchbearer.STEPS], desc=bar_desc, **self.tqdm_args)
+
+    def _update(self, state):
+        self._loader.update(1)
+        self._loader.set_postfix(state[torchbearer.METRICS])
+
+    def _close(self, state):
+        self._loader.set_postfix(state[torchbearer.METRICS])
+        self._loader.close()
+
+    def on_start(self, state):
+        if self._on_epoch:
+            self._loader = self.tqdm_module(total=state[torchbearer.MAX_EPOCHS], **self.tqdm_args)
+
+    def on_end_epoch(self, state):
+        if self._on_epoch:
+            self._update(state)
+
+    def on_end(self, state):
+        if self._on_epoch:
+            self._close(state)
+
+    def on_start_training(self, state):
+        """Initialise the TQDM bar for this training phase.
+
+        :param state: The Model state
+        :type state: dict
+        """
+        if not self._on_epoch:
+            self._on_start(state, 't')
+
+    def on_step_training(self, state):
+        """Update the bar with the metrics from this step.
+
+        :param state: The Model state
+        :type state: dict
+        """
+        if not self._on_epoch:
+            self._update(state)
+
+    def on_end_training(self, state):
+        """Update the bar with the terminal training metrics and then close.
+
+        :param state: The Model state
+        :type state: dict
+        """
+        if not self._on_epoch:
+            self._close(state)
+
+    def on_start_validation(self, state):
+        """Initialise the TQDM bar for this validation phase.
+
+        :param state: The Model state
+        :type state: dict
+        """
+        if not self._on_epoch:
+            self._on_start(state, self.validation_label)
+
+    def on_step_validation(self, state):
+        """Update the bar with the metrics from this step.
+
+        :param state: The Model state
+        :type state: dict
+        """
+        if not self._on_epoch:
+            self._update(state)
+
+    def on_end_validation(self, state):
+        """Update the bar with the terminal validation metrics and then close.
+
+        :param state: The Model state
+        :type state: dict
+        """
+        if not self._on_epoch:
+            self._close(state)
---
+++ packages/torchbearer/torchbearer-0.2.3-py3-none-any.whl/torchbearer/state.py
@@ -0,0 +1,129 @@
+import warnings
+
+__keys__ = []
+
+
+def state_key(key):
+    """Computes and returns a non-conflicting key for the state dictionary when given a seed key
+
+    :param key: The seed key - basis for new state key
+    :type key: String
+    :return: New state key
+    :rtype: String
+    """
+    return StateKey(key)
+
+
+class StateKey:
+    """ StateKey class that is a unique state key based of input string key
+
+    :param key: Base key
+    """
+    def __init__(self, key):
+        super().__init__()
+        self.key = self._gen_key_(key)
+
+    def _gen_key_(self, key):
+        if key in __keys__:
+            count = 1
+            my_key = key + '_' + str(count)
+
+            while my_key in __keys__:
+                count += 1
+                my_key = key + '_' + str(count)
+
+            key = my_key
+
+        __keys__.append(key)
+        return key
+
+    def __repr__(self):
+        return self.key
+
+    def __str__(self):
+        return self.key
+
+    def __eq__(self, other):
+        return self.key == str(other)
+        
+    def __hash__(self):
+        return self.key.__hash__()
+
+
+class State(dict):
+    """
+    State dictionary that behaves like a python dict but accepts StateKeys
+    """
+    def __init__(self):
+        super().__init__()
+
+    def get_key(self, statekey):
+        if isinstance(statekey, str):
+            warnings.warn("State was accessed with a string: {}, generate keys with StateKey(str).".format(statekey), stacklevel=2)
+        return statekey
+
+    def __getitem__(self, key):
+        return super().__getitem__(self.get_key(key))
+
+    def __setitem__(self, key, val):
+        super().__setitem__(self.get_key(key), val)
+
+    def __delitem__(self, val):
+        super().__delitem__(val)
+
+    def __contains__(self, o: object) -> bool:
+        return super().__contains__(self.get_key(o))
+
+    def update(self, d):
+        new_dict = {}
+        for key in d:
+            new_dict[self.get_key(key)] = d[key]
+        super().update(new_dict)
+
+
+VERSION = state_key('torchbearer_version')
+
+MODEL = state_key('model')
+CRITERION = state_key('criterion')
+OPTIMIZER = state_key('optimizer')
+DEVICE = state_key('device')
+DATA_TYPE = state_key('dtype')
+METRIC_LIST = state_key('metric_list')
+METRICS = state_key('metrics')
+SELF = state_key('self')
+EPOCH = state_key('epoch')
+MAX_EPOCHS = state_key('max_epochs')
+
+DATA = state_key('data')
+GENERATOR = state_key('generator')
+ITERATOR = state_key('iterator')
+STEPS = state_key('steps')
+
+TRAIN_GENERATOR = state_key('train_generator')
+TRAIN_STEPS = state_key('train_steps')
+TRAIN_DATA = state_key('train_data')
+
+VALIDATION_GENERATOR = state_key('validation_generator')
+VALIDATION_STEPS = state_key('validation_steps')
+VALIDATION_DATA = state_key('validation_data')
+
+TEST_GENERATOR = state_key('test_generator')
+TEST_STEPS = state_key('test_steps')
+TEST_DATA = state_key('test_data')
+
+STOP_TRAINING = state_key('stop_training')
+Y_TRUE = state_key('y_true')
+Y_PRED = state_key('y_pred')
+X = state_key('x')
+SAMPLER = state_key('sampler')
+LOSS = state_key('loss')
+FINAL_PREDICTIONS = state_key('final_predictions')
+BATCH = state_key('t')
+TIMINGS = state_key('timings')
+CALLBACK_LIST = state_key('callback_list')
+HISTORY = state_key('history')
+BACKWARD_ARGS = state_key('backward_args')
+
+# Legacy
+VALIDATION_ITERATOR = 'validation_iterator'
+TRAIN_ITERATOR = 'train_iterator'
