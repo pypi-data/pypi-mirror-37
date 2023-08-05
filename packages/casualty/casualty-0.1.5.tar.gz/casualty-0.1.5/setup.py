775a69a1aec319c12e31de96c505455fc56ba90d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:59 +0000

    Add package casualty/0.1.5/casualty-0.1.5.tar.gz

---
+++ packages/casualty/casualty-0.1.5.tar.gz/casualty-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="casualty",
+    version="0.1.5",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        "structlog==18.2.0",
+        "wrapt==1.10.11",
+        "pre-commit-hooks==1.4.0",
+        "mock==2.0.0",
+        "pytest==3.8.2",
+        "pytest-mock==1.10.0",
+        "pytest-cov"
+    ],
+    url="",
+    license="MIT",
+    author="Sohit Kumar",
+    author_email="sumitk002@gmail.com",
+    test_suite="tests",
+    description="A python library to generate co-relation id and bind it to headers in outgoing request",
+)
