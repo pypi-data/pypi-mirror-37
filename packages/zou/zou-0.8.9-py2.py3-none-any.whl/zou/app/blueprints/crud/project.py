880d7c876d46b74c31c26cad028722efd6333617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:24 +0000

    Add package zou/0.8.9/zou-0.8.9-py2.py3-none-any.whl

---
+++ packages/zou/zou-0.8.9-py2.py3-none-any.whl/zou/app/blueprints/crud/project.py
@@ -0,0 +1,41 @@
+from zou.app.models.project import Project
+from zou.app.services import user_service, projects_service, shots_service
+from zou.app.utils import permissions
+
+from .base import BaseModelResource, BaseModelsResource
+
+
+class ProjectsResource(BaseModelsResource):
+
+    def __init__(self):
+        BaseModelsResource.__init__(self, Project)
+
+    def add_project_permission_filter(self, query):
+        if permissions.has_admin_permissions():
+            return query
+        else:
+            return query.filter(user_service.build_related_projects_filter())
+
+    def check_read_permissions(self):
+        return True
+
+    def update_data(self, data):
+        open_status = projects_service.get_or_create_open_status()
+        if "project_status_id" not in data:
+            data["project_status_id"] = open_status["id"]
+
+        return data
+
+    def post_creation(self, instance):
+        if instance.production_type == "tvshow":
+            shots_service.create_episode(instance.id, "E01")
+        return instance
+
+
+class ProjectResource(BaseModelResource):
+
+    def __init__(self):
+        BaseModelResource.__init__(self, Project)
+
+    def check_read_permissions(self, project):
+        user_service.check_project_access(project["id"])
