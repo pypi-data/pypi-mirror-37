939b0749d743976ef6ab5811da4ad7728eefc911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:14 +0000

    Add package pyairmore/0.1.0a10/pyairmore-0.1.0a10.tar.gz

---
+++ packages/pyairmore/pyairmore-0.1.0a10.tar.gz/pyairmore-0.1.0a10/pyairmore/__init__.py
@@ -0,0 +1,6 @@
+"""PyAirmore (the package name of which is ``pyairmore``) is a Python client library for Android Airmore server,
+which helps the developers to take programmatic actions on Android.
+"""
+
+__version__ = "0.1.0a10"
+__author__ = "Eray Erdin"
