681e12b76a81bb52f0205d4b2c93f6b15f1f80f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:33 +0000

    Add package vm/2.0.18/vm-2.0.18.tar.gz

---
+++ packages/vm/vm-2.0.18.tar.gz/vm-2.0.18/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+packages = find_packages()
+
+setup(
+    name='vm',
+    version='2.0.18',
+    description='version_manager',
+    long_description=readme,
+    author='Bogdan Mustiata',
+    author_email='bogdan.mustiata@gmail.com',
+    license='BSD',
+    entry_points={
+        "console_scripts": [
+            "version-manager=version_manager.mainapp:main",
+            "vm=version_manager.mainapp:main"
+        ]
+    },
+    install_requires=[
+        "colorama==0.3.7",
+        "termcolor==1.1.0",
+        "termcolor_util==1.0.1",
+        "PyYAML==3.12"],
+    packages=packages,
+    package_data={
+        '': ['*.txt', '*.rst']
+    })
---
+++ packages/vm/vm-2.0.18.tar.gz/vm-2.0.18/version_manager/command_current_version.py
@@ -0,0 +1,30 @@
+import subprocess
+import re
+from termcolor_util import eprint, red
+
+DIVERGED_FROM_RELEASE = re.compile(r'.+?-\d+-\S+$')
+
+
+def print_current_tag_version() -> None:
+    current_release_version: str
+    try:
+        current_release_version = subprocess.check_output([
+            "git", "describe"
+        ]).decode('utf-8').strip()
+
+        if not DIVERGED_FROM_RELEASE.match(current_release_version):
+            print(current_release_version)
+            return  # => we're on a tagged release
+    except Exception as e:
+        eprint(red(str(e)))
+
+    current_branch_name: str = subprocess.check_output([
+        "git", "rev-parse", "--abbrev-ref", "HEAD"
+    ]).decode('utf-8').strip()
+
+    tag_name = f"{current_branch_name}"
+    print(escape_tag_name(tag_name))
+
+
+def escape_tag_name(name: str) -> str:
+    return re.sub(r'[^A-Za-z-0-9]', '_', name)
