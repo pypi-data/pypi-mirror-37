5f0373d7c7df3e9fb8377162412bc7ab6579a7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:41 +0000

    Add package pyleap/0.1.6/pyleap-0.1.6.tar.gz

---
+++ packages/pyleap/pyleap-0.1.6.tar.gz/pyleap-0.1.6/test/test_key_event.py
@@ -0,0 +1,18 @@
+from pyleap import *
+
+
+def press():
+    print("a press")
+
+def release():
+    print("a release")
+
+key.a.on_press(press)
+key.A.on_release(release)
+
+def a_value(dt):
+    print(key.a.pressed)
+
+repeat(a_value, 1)
+
+run();
