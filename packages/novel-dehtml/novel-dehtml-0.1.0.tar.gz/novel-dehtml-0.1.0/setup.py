dae7db77da8bb725d412d1ac32747e10f1f25fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:52 +0000

    Add package novel-dehtml/0.1.0/novel-dehtml-0.1.0.tar.gz

---
+++ packages/novel-dehtml/novel-dehtml-0.1.0.tar.gz/novel-dehtml-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="novel-dehtml",
+    version="0.1.0",
+    author="Willian Z.",
+    author_email="willian@willian-zhang.com",
+    description="Decode HTML in novel text",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/willian-zhang/novel-dehtml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # scripts=['bin/novel-dehtml'],
+    entry_points = {
+        'console_scripts': ['novel-dehtml=dehtml.main:main'],
+    }
+)
