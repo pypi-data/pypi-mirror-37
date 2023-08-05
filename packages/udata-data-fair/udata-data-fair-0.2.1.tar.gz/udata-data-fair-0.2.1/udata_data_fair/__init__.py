1e83cf652ef1782f3fba093ec73489e359435b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:09 +0000

    Add package udata-data-fair/0.2.1/udata-data-fair-0.2.1.tar.gz

---
+++ packages/udata-data-fair/udata-data-fair-0.2.1.tar.gz/udata-data-fair-0.2.1/udata_data_fair/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+data-fair
+
+Use DataFair embeds to preview data
+'''
+from __future__ import unicode_literals
+
+__version__ = '0.2.1'
+__description__ = 'Use DataFair embeds to preview data'
