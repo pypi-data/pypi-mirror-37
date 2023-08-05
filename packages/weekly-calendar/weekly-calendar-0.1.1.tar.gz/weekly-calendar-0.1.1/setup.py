876a4605a27ca0c464cb5d1b4b6da0d9469f1462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:09 +0000

    Add package weekly-calendar/0.1.1/weekly-calendar-0.1.1.tar.gz

---
+++ packages/weekly-calendar/weekly-calendar-0.1.1.tar.gz/weekly-calendar-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='weekly-calendar',
+    version='0.1.1',
+    author='kardaj',
+    author_email='bensalemhsn@gmail.com',
+    description='A simple library to deal with weekly opening hours and other recurrent weekly events',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kardaj/weekly-calendar',
+    install_requires=['pytz', 'tzlocal'],
+    packages=['weekly_calendar'],
+    classifiers=(
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+
+)
