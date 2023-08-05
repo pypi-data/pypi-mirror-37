aabbe6e4b47a043b69fc4bdcb366d8334e6ae97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package Grank/0.0.21/Grank-0.0.21.tar.gz

---
+++ packages/Grank/Grank-0.0.21.tar.gz/Grank-0.0.21/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+setuptools.setup(
+    name="Grank",
+    version="0.0.21",
+    author="Bestony@LCTT",
+    author_email="xiqingongzi@gmail.com",
+    description="A Github Project Rank Command Line Tool",
+    long_description=open('README.rst').read(),
+    url="https://github.com/LCTT/Grank",
+    project_urls={
+        "Bug Tracker": "https://github.com/LCTT/Grank/issues",
+        "Documentation": "https://github.com/LCTT/Grank",
+        "Source Code": "https://github.com/LCTT/Grank",
+    },
+    license="GPLv3",
+    py_modules=['grank'],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'click',
+        'requests',
+        'pandas',
+        'numpy',
+        'matplotlib'
+    ],
+    entry_points="""
+        [console_scripts]
+        grank=grank.core:main
+    """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
