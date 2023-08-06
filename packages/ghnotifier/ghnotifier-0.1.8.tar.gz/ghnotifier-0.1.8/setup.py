c64291d1872c18c57e30cf83c728031152e1c05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:55 +0000

    Add package ghnotifier/0.1.8/ghnotifier-0.1.8.tar.gz

---
+++ packages/ghnotifier/ghnotifier-0.1.8.tar.gz/ghnotifier-0.1.8/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='ghnotifier',
+    version='0.1.8',
+    description='Github notifications.',
+    url='http://github.com/kunicmarko20/ghnotifier',
+    author='Marko Kunic',
+    author_email='kunicmarko20@gmail.com',
+    license='MIT',
+    packages=['ghnotifier'],
+    scripts=['bin/ghnotifier'],
+    install_requires=[
+        'configparser',
+        'requests'
+    ],
+    zip_safe=False,
+    include_package_data=True
+)
