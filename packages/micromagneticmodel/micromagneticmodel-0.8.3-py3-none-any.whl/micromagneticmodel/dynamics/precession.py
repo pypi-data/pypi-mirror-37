1d92150426d253d22e7ed4c44aaf4d327065108c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:45 +0000

    Add package micromagneticmodel/0.8.3/micromagneticmodel-0.8.3-py3-none-any.whl

---
+++ packages/micromagneticmodel/micromagneticmodel-0.8.3-py3-none-any.whl/micromagneticmodel/dynamics/precession.py
@@ -0,0 +1,29 @@
+import joommfutil.typesystem as ts
+from .dynamicsterm import DynamicsTerm
+
+
+@ts.typesystem(gamma=ts.Scalar(unsigned=True),
+               name=ts.Name(const=True))
+class Precession(DynamicsTerm):
+    _latex = ("$-\gamma_{0}^{*} \mathbf{m} \\times "
+              "\mathbf{H}_\\text{eff}$")
+
+    def __init__(self, gamma, name="precession"):
+        """A precession dynamics term class.
+
+        Args:
+            gamma (Real): gyrotropic ratio (m/As)
+
+        """
+        self.gamma = gamma
+        self.name = name
+
+    @property
+    def _repr(self):
+        """A representation string property.
+
+        Returns:
+           A representation string.
+
+        """
+        return "Precession(gamma={}, name=\"{}\")".format(self.gamma, self.name)
---
+++ packages/micromagneticmodel/micromagneticmodel-0.8.3-py3-none-any.whl/micromagneticmodel/hamiltonian/dmi.py
@@ -0,0 +1,48 @@
+import joommfutil.typesystem as ts
+from .energyterm import EnergyTerm
+
+
+@ts.typesystem(D=ts.Scalar,
+               crystalclass=ts.InSet(allowed_values={"cnv", "t",
+                                                     "o", "d2d",
+                                                     "interfacial"}),
+               name=ts.Name(constant=True))
+class DMI(EnergyTerm):
+    def __init__(self, D, crystalclass="t", name="dmi"):
+        """A DMI energy class.
+
+        Args:
+            D (Real): DMI energy constant (J/m**2)
+            
+            crystalclass (string): Type of crystal class.
+            Possible values are:
+                "cnv", "t", "o", "d2d", "interfacial"
+
+        """
+        self.D = D
+        self.crystalclass = crystalclass.lower()
+        self.name = name
+
+    @property
+    def _latex(self):
+        if self.crystalclass in ["t", "o"]:
+            return ("$D \mathbf{m} \\cdot (\\nabla \\times \mathbf{m})$")
+        elif self.crystalclass in ["cnv", "interfacial"]:
+            return ("$D ( \mathbf{m} \\cdot \\nabla m_{z} "
+                    "- m_{z} \\nabla \\cdot \mathbf{m} )$")
+        else:
+            return ("$D\mathbf{m} \\cdot \\left( \\frac{\\partial "
+                    "\mathbf{m}}{\\partial x} \\times \hat{x} - "
+                    "\\frac{\\partial \mathbf{m}}{\\partial y} "
+                    "\\times \hat{y} \\right)$")
+
+    @property
+    def _repr(self):
+        """A representation string property.
+
+        Returns:
+           A representation string.
+
+        """
+        return ("DMI(D={}, crystalclass=\"{}\", "
+                "name=\"{}\")").format(self.D, self.crystalclass, self.name)
