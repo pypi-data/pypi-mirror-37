c07c7ce01cad6723d24f63d0fe0984d104f3848c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:07 +0000

    Add package cloudwise/0.1/cloudwise-0.1.tar.gz

---
+++ packages/cloudwise/cloudwise-0.1.tar.gz/cloudwise-0.1/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read().strip()
+
+
+setup(
+    name='cloudwise',
+    version='0.1',
+    author='Surreal AI',
+    url='http://github.com/SurrealAI/cloudwise',
+    description='Set up a Kubernetes cluster for distributed AI research',
+    long_description=read('README.rst'),
+    keywords=['machine learning',
+              'cloud computing',
+              'distributed computing',
+              'terraform',
+              'kubernetes'],
+    license='GPLv3',
+    packages=['cloudwise'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Programming Language :: Python :: 3"
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points={
+        'console_scripts': ['cloudwise-gke=cloudwise.gke.gke_commandline:main'],
+    },
+    python_requires='>=3.5',
+)
