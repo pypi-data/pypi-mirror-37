39307d3ee820b721be55f4df66d676a13c323ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:35 +0000

    Add package yatiml/0.3.0/yatiml-0.3.0.tar.gz

---
+++ packages/yatiml/yatiml-0.3.0.tar.gz/yatiml-0.3.0/yatiml/util.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+scalar_type_to_tag = {
+    str: 'tag:yaml.org,2002:str',
+    int: 'tag:yaml.org,2002:int',
+    float: 'tag:yaml.org,2002:float',
+    bool: 'tag:yaml.org,2002:bool',
+    None: 'tag:yaml.org,2002:null',
+    type(None): 'tag:yaml.org,2002:null'
+}
+
+ScalarType = Union[str, int, float, bool, None]
