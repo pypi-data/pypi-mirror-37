8bf07c0c421dc24074257d6179ea174159e08340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:36 +0000

    Add package devdocs-cli/0.0.2/devdocs-cli-0.0.2.tar.gz

---
+++ packages/devdocs-cli/devdocs-cli-0.0.2.tar.gz/devdocs-cli-0.0.2/README.rst
@@ -0,0 +1,32 @@
+devdocs-cli
+==============
+
+.. image:: https://img.shields.io/pypi/v/devdocs-cli.svg
+    :target: https://pypi.python.org/pypi/devdocs-cli
+    :alt: Latest PyPI version
+
+.. image:: https://travis-ci.org/kdeal/devdocs-cli.png
+   :target: https://travis-ci.org/kdeal/devdocs-cli
+   :alt: Latest Travis CI build status
+
+CLI for searching and viewing devdocs.io
+
+Usage
+-----
+
+Install
+------------
+
+Install via pip
+^^^^^^^^^^^^^^^
+
+::
+
+    pip install devdocs-cli
+
+Install via source
+^^^^^^^^^^^^^^^^^^
+
+extract the tarball and run::
+
+    python setup.py install
---
+++ packages/devdocs-cli/devdocs-cli-0.0.2.tar.gz/devdocs-cli-0.0.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+import devdocs_cli
+
+setuptools.setup(
+    name='devdocs-cli',
+    version=devdocs_cli.__version__,
+    url='https://github.com/kdeal/devdocs-cli',
+
+    author='Kyle Deal',
+    author_email='kdeal@kyledeal.com',
+
+    description='Devdocs integration for alfred',
+    long_description=open('README.rst').read(),
+
+    packages=setuptools.find_packages('.', exclude=('tests*',)),
+
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+            'devdocs = devdocs_cli.cli:main',
+            'devdocs-alfred = devdocs_cli.alfred:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
