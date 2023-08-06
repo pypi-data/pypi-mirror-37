84d3a3545533850f18ed0bc4c35f242b4035f24e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:34 +0000

    Add package sophy/0.6.2/sophy-0.6.2.tar.gz

---
+++ packages/sophy/sophy-0.6.2.tar.gz/sophy-0.6.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools.extension import Extension
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    import warnings
+    cython_installed = False
+    warnings.warn('Cython not installed, using pre-generated C source file.')
+else:
+    cython_installed = True
+
+
+if cython_installed:
+    python_source = 'sophy.pyx'
+else:
+    python_source = 'sophy.c'
+    cythonize = lambda obj: obj
+
+library_source = 'src/sophia.c'
+
+sophy = Extension(
+    'sophy',
+    #extra_compile_args=['-g', '-O0'],
+    #extra_link_args=['-g'],
+    sources=[python_source, library_source])
+
+setup(
+    name='sophy',
+    version='0.6.2',
+    description='Python bindings for the sophia database.',
+    author='Charles Leifer',
+    author_email='',
+    ext_modules=cythonize([sophy]),
+)
