0bd76ee33960461408c261844c3e0f9b6f55a5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package dsnd-dist-AG/0.2/dsnd_dist_AG-0.2.tar.gz

---
+++ packages/dsnd-dist-AG/dsnd_dist_AG-0.2.tar.gz/dsnd_dist_AG-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_dist_AG',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['dsnd_dist_AG'],
+      zip_safe=False)
