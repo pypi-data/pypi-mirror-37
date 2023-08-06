0daa2b605327f195d8cea0606a93dd0d1b7e55e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:21 +0000

    Add package prometheus-flask-exporter/0.3.1/prometheus_flask_exporter-0.3.1.tar.gz

---
+++ packages/prometheus-flask-exporter/prometheus_flask_exporter-0.3.1.tar.gz/prometheus_flask_exporter-0.3.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+try:
+    long_description = open('README.rst').read()
+except IOError:
+    long_description = open('README.md').read()
+
+setup(
+    name='prometheus_flask_exporter',
+    packages=['prometheus_flask_exporter'],
+    version='0.3.1',
+    description='Prometheus metrics exporter for Flask',
+    long_description=long_description,
+    license='MIT',
+    author='Viktor Adam',
+    author_email='rycus86@gmail.com',
+    url='https://github.com/rycus86/prometheus_flask_exporter',
+    download_url='https://github.com/rycus86/prometheus_flask_exporter/archive/0.3.1.tar.gz',
+    keywords=['prometheus', 'flask', 'monitoring', 'exporter'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Monitoring',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    install_requires=['prometheus_client', 'flask'],
+)
