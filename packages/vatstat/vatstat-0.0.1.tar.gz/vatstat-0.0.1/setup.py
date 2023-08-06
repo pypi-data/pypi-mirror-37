ab25a9683a481205ddca374c708abb6e49530909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:08 +0000

    Add package vatstat/0.0.1/vatstat-0.0.1.tar.gz

---
+++ packages/vatstat/vatstat-0.0.1.tar.gz/vatstat-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vatstat",
+    version="0.0.1",
+    author="Ryan Null",
+    author_email="ryan.null@gmail.com",
+    description="python api that makes interacting with the VATSIM data less painful and more pragmatic.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BIGjuevos/vatstat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
