4b3495bb9389faa418586d4030f6104931ec2a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:28 +0000

    Add package macawspeak/0.0.1/macawspeak-0.0.1-py3-none-any.whl

---
+++ packages/macawspeak/macawspeak-0.0.1-py3-none-any.whl/macaw/macaw.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Macaw
+#
+# Testing file open and string concatenation.
+
+def main():
+    #password = ''
+    #f = open('dictionary.txt')
+    #for word in f.read().split():
+    #    password = password + word
+    #print(password)
+    print("Macaw!")
