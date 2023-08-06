dd95245fe854dc3ff62c30a9e2aa12a7c6a980dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:16 +0000

    Add package hestia/0.0.1/hestia-0.0.1-py2.py3-none-any.whl

---
+++ packages/hestia/hestia-0.0.1-py2.py3-none-any.whl/hestia/bool_utils.py
@@ -0,0 +1,17 @@
+from distutils.util import strtobool  # pylint:disable=import-error
+
+
+def to_bool(value, handle_none=False, exception=TypeError):
+    if isinstance(value, str):
+        value = strtobool(value)
+
+    if value in (False, 0):
+        return False
+
+    if value in (True, 1):
+        return True
+
+    if handle_none and value is None:
+        return False
+
+    raise exception('The value `{}` cannot be interpreted as boolean'.format(value))
