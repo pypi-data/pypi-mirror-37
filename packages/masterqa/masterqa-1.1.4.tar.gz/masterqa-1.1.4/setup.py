63e27a663cb9f36ce8e3b572b5a9d372034578b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:00 +0000

    Add package masterqa/1.1.4/masterqa-1.1.4.tar.gz

---
+++ packages/masterqa/masterqa-1.1.4.tar.gz/masterqa-1.1.4/setup.py
@@ -0,0 +1,26 @@
+"""
+The setup package to install MasterQA requirements
+"""
+
+from setuptools import setup, find_packages  # noqa
+
+setup(
+    name='masterqa',
+    version='1.1.4',
+    description='Automation-Assisted Manual Testing - http://masterqa.com',
+    long_description='Automation-Assisted Manual Testing. http://masterqa.com',
+    platforms='Mac * Windows * Linux',
+    url='http://masterqa.com',
+    author='Michael Mintz',
+    author_email='mdmintz@gmail.com',
+    maintainer='Michael Mintz',
+    license='The MIT License',
+    install_requires=[
+        'seleniumbase>=1.16.14',
+        'flake8==3.5.0',
+        ],
+    packages=['masterqa'],
+    entry_points={
+        'nose.plugins': []
+        }
+    )
