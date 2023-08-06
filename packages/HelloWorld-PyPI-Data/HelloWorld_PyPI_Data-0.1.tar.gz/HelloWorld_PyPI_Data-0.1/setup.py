3ce56bf154cc42d5b51e2f1747921f6967e94f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:58 +0000

    Add package HelloWorld-PyPI-Data/0.1/HelloWorld_PyPI_Data-0.1.tar.gz

---
+++ packages/HelloWorld-PyPI-Data/HelloWorld_PyPI_Data-0.1.tar.gz/HelloWorld_PyPI_Data-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name = "HelloWorld_PyPI_Data",
+    version = "0.1",
+    decription = 'Hello world pypi distribution',
+    packages = find_packages())
