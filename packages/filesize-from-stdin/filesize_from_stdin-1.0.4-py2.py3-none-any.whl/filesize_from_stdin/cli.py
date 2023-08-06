1b61793d55a90a7ef5e842b3f62794520ba5bec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:13 +0000

    Add package filesize-from-stdin/1.0.4/filesize_from_stdin-1.0.4-py2.py3-none-any.whl

---
+++ packages/filesize-from-stdin/filesize_from_stdin-1.0.4-py2.py3-none-any.whl/filesize_from_stdin/cli.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for filesize_from_stdin."""
+import sys
+import click
+from .filesize_from_stdin import display_friendly, get_file_list_from_stdin
+
+
+@click.command()
+def main(args=None):
+    """Console script for filesize_from_stdin."""
+    display_friendly(get_file_list_from_stdin(sys.stdin))
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
