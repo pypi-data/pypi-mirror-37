6e831a321e018bea146d4b03cbcaf00407611635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:21 +0000

    Add package py-ryobi-gdo/0.0.25/py_ryobi_gdo-0.0.25.tar.gz

---
+++ packages/py-ryobi-gdo/py_ryobi_gdo-0.0.25.tar.gz/py_ryobi_gdo-0.0.25/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='py_ryobi_gdo',
+    version='0.0.25',
+    description='Python package for controlling Ryobi Garage Door',
+    author='Guillaume1410',
+    author_email='super.guillaume@gmail.com',
+    url='https://github.com/guillaume1410/py_ryobi_gdo',
+    license='MIT',
+    packages=['py_ryobi_gdo'],
+    install_requires = ['websocket-client==0.37.0'],
+    package_dir={'py_ryobi_gdo': 'py_ryobi_gdo'}
+)
