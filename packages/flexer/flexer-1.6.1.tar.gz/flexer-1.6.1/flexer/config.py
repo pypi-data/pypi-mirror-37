ad185cb2401accd29156baa1d5e438f8d10be9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:23 +0000

    Add package flexer/1.6.1/flexer-1.6.1.tar.gz

---
+++ packages/flexer/flexer-1.6.1.tar.gz/flexer-1.6.1/flexer/config.py
@@ -0,0 +1,22 @@
+import os
+
+CONFIG_FILE = os.path.expanduser('~/.flexer.yaml')
+DEFAULT_CMP_URL = 'https://sandbox.cmp.nflex.io/cmp/basic/api'
+DEFAULT_CONFIG_YAML = os.path.join(os.getcwd(), "config.yaml")
+
+
+class Config(object):
+    CMP_URL = os.getenv('CMP_URL', 'http://localhost/cmp/api')
+    CMP_REGION = os.getenv('CMP_REGION', '')
+    CMP_PLATFORM = os.getenv('CMP_PLATFORM', '')
+    CMP_USERNAME = os.getenv('CMP_USERNAME', '')
+    CMP_PASSWORD = os.getenv('CMP_PASSWORD', '')
+    CMP_ACCESS_TOKEN = os.getenv('CMP_ACCESS_TOKEN', '')
+    FLEXER_VERSION = os.getenv('FLEXER_VERSION', '')
+    NFLEX_CODEDIR = os.getenv('NFLEX_CODEDIR', '/sandbox')
+    MODULE_ID = os.getenv('NFLEX_MODULE_ID')
+    DB_KEY_PREFIX = '_nflexdb_'
+
+
+class TestingConfig(Config):
+    NFLEX_CODEDIR = '/tmp'
