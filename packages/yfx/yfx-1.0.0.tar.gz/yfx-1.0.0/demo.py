1fd5e59123e635f2c125c6cf9be57851aad5960b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:28 +0000

    Add package yfx/1.0.0/yfx-1.0.0.tar.gz

---
+++ packages/yfx/yfx-1.0.0.tar.gz/yfx-1.0.0/demo.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+# author:eric(2384363842@qq.com)
+# 创建于: 2018/10/24 18:47
+print("hello")
---
+++ packages/yfx/yfx-1.0.0.tar.gz/yfx-1.0.0/setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+# author:eric(2384363842@qq.com)
+# 创建于: 2018/10/24 18:46
+
+from distutils.core import setup
+setup (
+name = 'yfx',
+version='1.0.0',
+author = 'yfx',
+author_email='2573179560@qq.com',
+# py_modules = 'my_module',
+url = 'https://pypi.org/',
+description='测试打包的过程',
+py_modules = ['demo'],
+)
