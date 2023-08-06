86c537a81b1c3a9e7e6fb428fc15dc609c0ea218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:28 +0000

    Add package UncleCTowelStuff/0.3dev/UncleCTowelStuff-0.3dev.tar.gz

---
+++ packages/UncleCTowelStuff/UncleCTowelStuff-0.3dev.tar.gz/UncleCTowelStuff-0.3dev/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+from distutils.core import setup
+
+setup(
+    name='UncleCTowelStuff',
+    version='0.3dev',
+    packages=['towelstuff',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
