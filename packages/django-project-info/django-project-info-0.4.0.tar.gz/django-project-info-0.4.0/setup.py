49b03efcaf41363abca5d823750efd3c664f8c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:06 +0000

    Add package django-project-info/0.4.0/django-project-info-0.4.0.tar.gz

---
+++ packages/django-project-info/django-project-info-0.4.0.tar.gz/django-project-info-0.4.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-project-info',
+    version='0.4.0',
+    description='A small plugin which takes a configuration (like Bower or npm) and it provides data via context'
+    'processors.',
+    keywords='django, project-info',
+    author='Lukas Rychtecky',
+    author_email='lukas.rychtecky@gmail.com',
+    url='https://github.com/druids/django-project-info',
+    license='MIT',
+    package_dir={'project_info': 'project_info'},
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'django>=1.6',
+        'gitpython>=0.3.2.RC1',
+    ],
+    zip_safe=False
+)
