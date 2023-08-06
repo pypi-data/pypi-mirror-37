d2a0901e4c0fd6c3b0eac56b8c85cdb51a935654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:57 +0000

    Add package followthemoney/1.3.10/followthemoney-1.3.10.tar.gz

---
+++ packages/followthemoney/followthemoney-1.3.10.tar.gz/followthemoney-1.3.10/tests/types/test_entity.py
@@ -0,0 +1,19 @@
+import unittest
+
+from followthemoney.types import entities
+
+
+class EntityTest(unittest.TestCase):
+
+    def test_parse(self):
+        self.assertEqual(entities.clean('88'), '88')
+        self.assertEqual(entities.clean(88), '88')
+        self.assertEqual(entities.clean({'id': 88}), '88')
+        self.assertEqual(entities.clean(None), None)
+
+    def test_normalize(self):
+        self.assertEqual(entities.normalize('FOO'), ['FOO'])
+        self.assertEqual(entities.normalize(None), [])
+
+    def test_funcs(self):
+        self.assertEqual(entities.specificity('bla'), 1)
