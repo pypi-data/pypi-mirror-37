85d8f7cbf126338cf8dcb887ddc944e153c271e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:14 +0000

    Add package eagle-cli/0.1.1/eagle-cli-0.1.1.tar.gz

---
+++ packages/eagle-cli/eagle-cli-0.1.1.tar.gz/eagle-cli-0.1.1/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("description.rst", "r") as f:
+    description = f.read()
+
+setuptools.setup(
+    name="eagle-cli",
+    # version="0.1.1",
+    version="0.1.1",
+    description="Simple TODO tool for CLI.",
+    long_description=description,
+    author="n1",
+    url="https://gitlab.com/n1_/eagle",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ["eagle = eagle.eagle:eagle"]
+    },
+    python_requires=">=3.6",
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Office/Business :: Scheduling ",
+    ]
+)
