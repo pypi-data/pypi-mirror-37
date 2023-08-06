c0a5d25ea5fbd15ed7fad3cb7baca4fcec48d4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:00 +0000

    Add package FoxDot/0.7.14/FoxDot-0.7.14.tar.gz

---
+++ packages/FoxDot/FoxDot-0.7.14.tar.gz/FoxDot-0.7.14/FoxDot/__main__.py
@@ -0,0 +1,28 @@
+"""
+    FoxDot __main__.py
+    ------------------
+
+    Use FoxDot's interface by running this as a Python script, e.g.
+    python __main__.py or python -m FoxDot if you have FoxDot correctly
+    installed and Python on your path.
+
+"""
+
+from __future__ import absolute_import, division, print_function
+from .lib import FoxDotCode, handle_stdin
+from .lib.Workspace import workspace
+import sys
+
+# If we are getting command line input
+
+if sys.argv[-1] == "--pipe":
+
+    # Set up pipe
+
+    handle_stdin()
+
+else:
+
+    # Start the gui
+
+    FoxDot = workspace(FoxDotCode).run()
