d00d45a2dcf5d1c0303b7277b74c81e7a5bd4453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:38 +0000

    Add package ReExpose/0.1.1/ReExpose-0.1.1.tar.gz

---
+++ packages/ReExpose/ReExpose-0.1.1.tar.gz/ReExpose-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+setup(
+    name='ReExpose',
+    version='0.1.1',
+    author='Jeff Casavant',
+    author_email='jeff.casavant@gmail.com',
+    description='ReExpose HTTP basic auth endpoints on localhost as unauthenticated endpoints',
+    url='https://github.com/jeffcasavant/reexpose',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'cachetools',
+        'flask',
+        'gevent',
+        'requests',
+        'pyyaml'
+    ],
+    extras_require={
+        'dev': [
+            'pylint',
+            'twine'
+        ]
+    },
+    packages=['reexpose',],
+    scripts=['bin/reexpose'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP'
+    ]
+)
