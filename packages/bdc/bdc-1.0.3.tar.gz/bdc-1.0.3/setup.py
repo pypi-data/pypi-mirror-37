98ec1f8e2ab12bfbc84483c8ede2ffb396ea38ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:32 +0000

    Add package bdc/1.0.3/bdc-1.0.3.tar.gz

---
+++ packages/bdc/bdc-1.0.3.tar.gz/bdc-1.0.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open('README.md', 'r') as f:
+    longDescription = f.read()
+
+setuptools.setup(
+     name='bdc',  
+     version='1.0.3',
+     scripts=['src/bdc/bdc', 'src/bdc/bdc-gui'] ,
+     author='Adam Jenkins',
+     author_email='adamjenkins1701@gmail.com',
+     description='bootable install media creator',
+     long_description=longDescription,
+     long_description_content_type='text/markdown',
+     url='https://github.com/adamjenkins1/BootableDiskCreator',
+     packages=['bdc'],
+     include_package_data=True,
+     package_dir={'': 'src'},
+     platforms="Linux",
+     install_requires=['PyQt5==5.11.3'],
+     python_requires='~=3.5',
+     classifiers=[
+         'Programming Language :: Python :: 3.5',
+         'Programming Language :: Python :: 3.6',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: POSIX :: Linux',
+     ],
+ )
