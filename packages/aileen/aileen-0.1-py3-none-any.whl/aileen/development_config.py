2925e976d30290a5067d9d705a45e9ddf2aa1c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:13 +0000

    Add package aileen/0.1/aileen-0.1-py3-none-any.whl

---
+++ packages/aileen/aileen-0.1-py3-none-any.whl/aileen/development_config.py
@@ -0,0 +1,10 @@
+SQLALCHEMY_DATABASE_URI = "postgresql://aileen_dev:aileen_dev@localhost/aileen_dev"
+
+LOGGING_LEVEL = "INFO"
+
+AILEEN_WIFI_INTERFACE = "wlan0"  # "wlx00c0ca979d1c"
+AILEEN_FULL_PATH_TO_AIRMON_NG = "/usr/local/sbin/airmon-ng"
+AILEEN_FULL_PATH_TO_AIRODUMP = "/usr/local/sbin/airodump-ng"
+
+AILEEN_AIRODUMP_LOG_INTERVAL_IN_SECONDS = 5
+AILEEN_COUNTING_INTERVAL_IN_MINUTES = 1
