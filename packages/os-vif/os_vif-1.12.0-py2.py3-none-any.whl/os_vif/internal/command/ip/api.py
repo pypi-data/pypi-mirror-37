ff9605dd6417d9c679954a70daf5d76070360338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:49 +0000

    Add package os-vif/1.12.0/os_vif-1.12.0-py2.py3-none-any.whl

---
+++ packages/os-vif/os_vif-1.12.0-py2.py3-none-any.whl/os_vif/internal/command/ip/api.py
@@ -0,0 +1,28 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+from oslo_log import log as logging
+
+from os_vif.internal.command.ip.linux import impl_pyroute2 as linux_ip_lib
+from os_vif.internal.command.ip.windows import impl_netifaces as win_ip_lib
+
+
+LOG = logging.getLogger(__name__)
+
+
+def _get_impl():
+    if os.name == 'nt':
+        return win_ip_lib.Netifaces()
+    else:
+        return linux_ip_lib.PyRoute2()
