0f9c816fe4f526f5b884d6a641512698a34c14d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:53 +0000

    Add package track_error/1.1.0/track_error-1.1.0.tar.gz

---
+++ packages/track_error/track_error-1.1.0.tar.gz/track_error-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'track_error',
+    version = '1.1.0',
+    py_modules = 'track_error',
+    author = 'legiaquy',
+    author_email = 'legiaquy@gmail.com',
+    url = 'https://github.com/legiaquy/hfpython',
+    description = 'A simple track error instruction',
+    )
