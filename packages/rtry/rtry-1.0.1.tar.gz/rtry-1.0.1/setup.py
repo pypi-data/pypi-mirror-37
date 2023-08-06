70ddd7ee41f86a0a69eb35a58a056e6d6567e36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:23 +0000

    Add package rtry/1.0.1/rtry-1.0.1.tar.gz

---
+++ packages/rtry/rtry-1.0.1.tar.gz/rtry-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="rtry",
+    description="",
+    version="1.0.1",
+    url="https://github.com/nikitanovosibirsk/rtry",
+    author="Nikita Tsvetkov",
+    author_email="nikitanovosibirsk@yandex.com",
+    license="MIT",
+    packages=find_packages(),
+)
