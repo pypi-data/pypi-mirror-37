753cb8efae4f6ab62c14947b2dccbbd7a9743eee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:09 +0000

    Add package hyperpoints/0.0.1/hyperpoints-0.0.1.tar.gz

---
+++ packages/hyperpoints/hyperpoints-0.0.1.tar.gz/hyperpoints-0.0.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+doclink = """
+Documentation
+-------------
+
+The full documentation is at http://hyperpoints.rtfd.org."""
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='hyperpoints',
+    version='0.0.1',
+    description='Distributed hyperparameter optimization and reinforcement learning algorithms.',
+    long_description=readme + '\n\n' + doclink + '\n\n' + history,
+    author='Todd Young',
+    author_email='youngmt1@ornl.gov',
+    url='https://github.com/yngtodd/hyperpoints',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords='hyperpoints',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
