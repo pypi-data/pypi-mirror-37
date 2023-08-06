2ce35f31937531d55ddc293ce2167552a55d19cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:18 +0000

    Add package scheduled-poller/1.1/scheduled_poller-1.1.tar.gz

---
+++ packages/scheduled-poller/scheduled_poller-1.1.tar.gz/scheduled_poller-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='scheduled_poller',
+      version='1.1',
+      description='Base class for classes that poll a task regularly, with a constant minimum time interval between each poll. Each polling interval is the maximum of a) polling_interval_secs and b) the time taken to do the task.',
+      url='https://github.com/AnnAnnFryingPan/scheduled_poller',
+      author='Ann Gledson',
+      author_email='anngledson@gmail.com',
+      license='MIT',
+      packages=['scheduled_poller'],
+      zip_safe=False)
