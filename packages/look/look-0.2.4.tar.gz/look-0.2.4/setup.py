174265ae86f21783d65bd7037c7b60a4f16c8d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:24 +0000

    Add package look/0.2.4/look-0.2.4.tar.gz

---
+++ packages/look/look-0.2.4.tar.gz/look-0.2.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="look",
+    version="0.2.4",
+    author="Leo Tong",
+    author_email="tonglei@qq.com",
+    description="Look",
+    long_description=open("README.rst").read(),
+    license="Apache License, Version 2.0",
+    url="https://github.com/tonglei100/look",
+    packages=['look'],
+    package_data={'look': ['*.py', '*.zip', '*.pkl']},
+    install_requires=[
+        'visdom',
+        'numpy',
+        'Pillow'
+        ],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3"
+    ],
+    entry_points={
+        'console_scripts': [
+            'look=look:example'
+        ]    
+
+    }
+)
