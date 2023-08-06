387205afada9439d46f3b84f009ad41f754db6fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:58 +0000

    Add package protonfixes/1.0.16/protonfixes-1.0.16.tar.gz

---
+++ packages/protonfixes/protonfixes-1.0.16.tar.gz/protonfixes-1.0.16/protonfixes/gamefixes/253230.py
@@ -0,0 +1,14 @@
+""" Game fix for A Hat in Time
+Source: https://github.com/ValveSoftware/Proton/issues/173
+"""
+
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Installs xact
+    """
+
+    util.protontricks('xact')
+    util.winedll_override('xaudio2_7', 'n')
