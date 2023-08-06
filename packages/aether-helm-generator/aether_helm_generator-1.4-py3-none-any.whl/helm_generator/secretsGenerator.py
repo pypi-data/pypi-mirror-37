1ab992c342a57650c7d8edc001684cf511a79f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:08 +0000

    Add package aether-helm-generator/1.4/aether_helm_generator-1.4-py3-none-any.whl

---
+++ packages/aether-helm-generator/aether_helm_generator-1.4-py3-none-any.whl/helm_generator/secretsGenerator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import secrets
+import base64
+
+
+def generate_encoded_secret():
+    """Generate secure token."""
+    generated_secret = secrets.token_hex(36).encode()
+    encoded_secret = base64.b64encode(generated_secret)
+    secret = encoded_secret.decode("utf-8")
+    return(secret)
+
+
+def generate_secrets(application):
+    """Generate secrets."""
+    secrets_dict = {}
+    secrets_list = [
+        'admin_token',
+        'secret_key',
+        'admin_password',
+        'database_password',
+        'readonly_db_password'
+    ]
+    for secret in secrets_list:
+        secrets_dict[secret] = generate_encoded_secret()
+    secrets_dict['application'] = application
+    return secrets_dict
