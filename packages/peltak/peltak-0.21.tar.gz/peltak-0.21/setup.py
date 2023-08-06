1343dbb5bcc61427ca79b87a7e528334c0c3cc05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:27 +0000

    Add package peltak/0.21/peltak-0.21.tar.gz

---
+++ packages/peltak/peltak-0.21.tar.gz/peltak-0.21/setup.py
@@ -0,0 +1,55 @@
+import os
+import re
+from setuptools import setup, find_packages
+
+
+RE_PY_VERSION = re.compile(
+    r'__version__\s*=\s*["\']'
+    r'(?P<version>\d+(\.\d+(\.\d+)?)?)'
+    r'["\']'
+)
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def read_version():
+    content = read('src/peltak/__init__.py')
+    m = RE_PY_VERSION.search(content)
+    if not m:
+        return '0.0'
+    else:
+        return m.group('version')
+
+
+setup(
+    name="peltak",
+    version=read_version(),
+    author="Mateusz 'novo' Klos",
+    author_email="novopl@gmail.com",
+    license="MIT",
+    keywords="fabric ops devops",
+    url="http://github.com/novopl/peltak",
+    description="Set of CLI commands to help manage a project",
+    long_description=read('README.rst'),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'click~=7.0',
+        'PyYAML~=3.13',
+        'six~=1.11'
+    ],
+    entry_points={
+        'console_scripts': [
+            'peltak = peltak.main:root_cli',
+        ]
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
