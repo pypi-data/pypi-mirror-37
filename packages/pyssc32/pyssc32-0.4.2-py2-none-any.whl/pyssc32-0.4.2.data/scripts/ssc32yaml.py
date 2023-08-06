f183649f67b9f14bfcafbae4676b88e4cd7d4696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:24 +0000

    Add package pyssc32/0.4.2/pyssc32-0.4.2-py2-none-any.whl

---
+++ packages/pyssc32/pyssc32-0.4.2-py2-none-any.whl/pyssc32-0.4.2.data/scripts/ssc32yaml.py
@@ -0,0 +1,78 @@
+#!python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import yaml
+import ssc32
+from optparse import OptionParser
+
+
+DEFAULT_CFG = '$HOME/.ssc32.rc'
+
+def load_yaml(filename):
+    with open(filename, 'r') as fd:
+        return yaml.load(fd)
+
+def save_yaml(filename, data):
+    d = yaml.dump(data)
+    with open(filename, 'w') as fd:
+        fd.write(d.encode('utf-8'))
+
+def abspath(path):
+    return os.path.abspath(
+        os.path.expanduser(
+            os.path.expandvars(
+                path
+            )
+        )
+    )
+
+def load_config(cfg_fname=DEFAULT_CFG):
+    cfg_fname = abspath(cfg_fname)
+    try:
+        cfg = load_yaml(cfg_fname)
+    except:
+        cfg = {
+            'port': '/dev/ttyUSB0',
+            'baud': 115200,
+            'config': abspath('$HOME/.ssc32.servo')
+        }
+    return cfg
+
+def main():
+    parser = OptionParser()
+    parser.add_option('-c', '--cofig', dest='cfg_fname', default=DEFAULT_CFG, help='config file')
+    parser.add_option('-p', '--port', dest='port', help='serial port', default=None)
+    parser.add_option('-b', '--baudrate', dest='baud', help='baudrate', default=None)
+    parser.add_option('-s', '--servo-config', dest='servoconfig', help='servo config', default=None)
+    parser.add_option('-u', '--update-config', dest='upconf', action='store_true', default=False, help='Update config file')
+
+    options, args = parser.parse_args()
+
+    if len(args) != 1:
+        print 'Error args: ', args
+        return 1
+
+    conf = load_config(options.cfg_fname)
+    if options.port is not None:
+        conf['port'] = options.port
+    if options.baud is not None:
+        conf['baud'] = int(options.baud)
+    if options.servoconfig is not None:
+        conf['config'] = abspath(options.servoconfig)
+
+    ssc = ssc32.SSC32(conf['port'], conf['baud'], config=conf['config'])
+
+    if options.upconf:
+        save_yaml(abspath(options.cfg_fname), conf)
+
+    filename = os.path.abspath(args[0])
+
+    script = load_yaml(filename)
+    script(ssc)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ packages/pyssc32/pyssc32-0.4.2-py2-none-any.whl/pyssc32-0.4.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,72 @@
+.. -*- mode: rst -*-
+
+=======
+PySSC32
+=======
+
+This is simple interface to control RC servos with an SSC32 board.
+
+Features:
+
+- Direct and grouped queries (`#<N>P<US>` and `<SERVO_POS>...T<MS>`)
+- Angle (degrees or radians) to microsecond position calculation
+- Simple configuration file that help to map board output pin to servo name and provide limits.
+- Sequences scripts (in YAML format)
+
+
+Example
+=======
+
+::
+
+    >>> import ssc32
+    >>> import math
+    >>> ssc = ssc32.SSC32('/dev/ttyUSB0', 115200, count=32)
+    >>> ssc[2].position = 2000
+    >>> ssc[3].name = 'pan'
+    >>> ssc[4].name = 'tilt'
+    >>> pan_servo = ssc['pan']
+    >>> tilt_servo = ssc['tilt']
+    >>> pan_servo.degrees = 0
+    >>> tilt_servo.radians = math.pi/4
+    >>> ssc.commit(time=1000)
+    >>> ssc.is_done()
+    False
+    >>> ssc.is_done()
+    True
+    >>> ssc.description = 'My camera's pan/tilt'
+    >>> ssc.save_config('my_pantilt.cfg')
+
+
+.. -*- mode: rst -*-
+
+0.4.2
+~~~~~
+
+- Change \n to \r to allow running on non-Windows platforms.
+
+
+0.4.1
+~~~~~
+
+- Add ssc32yaml script runner
+- Don't change servo position on init (`Servo.is_changed` flag)
+
+
+0.4.0
+~~~~~
+
+- Servo script interface
+  with YAML serialization/deserialization
+- Move to package
+- Depends on PyYAML
+
+
+0.3.3
+~~~~~
+
+- Rename Servo properties `Servo.degree` -> `Servo.degrees`, `Servo.radian` -> `Servo.radians`
+- Use callback for Servo
+- Description for configuration (`SSC32.description`) line starts with `#~`
+
+
