eba2f916ebefc6479eabfe5301e3891082c90170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:02 +0000

    Add package gdown/3.6.0/gdown-3.6.0.tar.gz

---
+++ packages/gdown/gdown-3.6.0.tar.gz/gdown-3.6.0/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+from setuptools import find_packages
+from setuptools import setup
+import shlex
+import subprocess
+import sys
+
+
+version = '3.6.0'
+
+
+# release helper
+if sys.argv[-1] == 'release':
+    commands = [
+        'python setup.py sdist upload',
+        'git tag v{0}'.format(version),
+        'git push origin master --tag',
+    ]
+    for cmd in commands:
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+setup(
+    name='gdown',
+    version=version,
+    packages=find_packages(),
+    install_requires=['requests', 'six', 'tqdm'],
+    description='Google Drive direct download of big files.',
+    long_description=open('README.md').read(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    url='http://github.com/wkentaro/gdown',
+    license='MIT',
+    keywords='utility',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    entry_points={'console_scripts': ['gdown=gdown.cli:main']},
+)
