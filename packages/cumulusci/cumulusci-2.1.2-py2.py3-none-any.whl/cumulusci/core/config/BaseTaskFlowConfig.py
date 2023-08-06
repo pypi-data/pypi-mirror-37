414c9b1a5b4dd3b051b5510c2fb3866900819878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:51 +0000

    Add package cumulusci/2.1.2/cumulusci-2.1.2-py2.py3-none-any.whl

---
+++ packages/cumulusci/cumulusci-2.1.2-py2.py3-none-any.whl/cumulusci/core/config/BaseTaskFlowConfig.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+from cumulusci.core.config.BaseConfig import BaseConfig
+from cumulusci.core.config.FlowConfig import FlowConfig
+from cumulusci.core.config.TaskConfig import TaskConfig
+from cumulusci.core.exceptions import TaskNotFoundError, FlowNotFoundError
+
+
+def list_infos(infos):
+    rv = []
+    for info_name in infos:
+        info = infos[info_name]
+        if not info:
+            info = {}
+        rv.append({"name": info_name, "description": info.get("description", "")})
+    return rv
+
+
+class BaseTaskFlowConfig(BaseConfig):
+    """ Base class for all configs that contain tasks and flows """
+
+    def list_tasks(self):
+        """ Returns a list of task info dictionaries with keys 'name' and 'description' """
+        return list_infos(self.tasks)
+
+    def get_task(self, name):
+        """ Returns a TaskConfig """
+        config = getattr(self, "tasks__{}".format(name))
+        if not config:
+            raise TaskNotFoundError("Task not found: {}".format(name))
+        return TaskConfig(config)
+
+    def list_flows(self):
+        """ Returns a list of flow info dictionaries with keys 'name' and 'description' """
+        return list_infos(self.flows)
+
+    def get_flow(self, name):
+        """ Returns a FlowConfig """
+        config = getattr(self, "flows__{}".format(name))
+        if not config:
+            raise FlowNotFoundError("Flow not found: {}".format(name))
+        return FlowConfig(config)
