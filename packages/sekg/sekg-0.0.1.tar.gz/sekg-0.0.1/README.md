228c683b867c07dfe63c5d8b08f039fe54a4bd44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:59 +0000

    Add package sekg/0.0.1/sekg-0.0.1.tar.gz

---
+++ packages/sekg/sekg-0.0.1.tar.gz/sekg-0.0.1/README.md
@@ -0,0 +1,12 @@
+# sekg
+this is library for software engineering knowledge graph.
+
+## description
+1. including wrapper for neo4j by py2neo.
+2. the preprocess tool for stack overflow html.
+3. graph2vec library
+...
+todo
+
+## support
+python 2.7 / 3.5
---
+++ packages/sekg/sekg-0.0.1.tar.gz/sekg-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sekg",
+    version="0.0.1",
+    keywords=("pip", "kg", "se"),
+    description="knowledge graph util for software engineering",
+    long_description="knowledge graph util for software engineering",
+    license="MIT Licence",
+
+    url="https://github.com/FudanSELab/sekg",
+    author="FDSEKG",
+    author_email="lmwtclmwtc@outlook.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
