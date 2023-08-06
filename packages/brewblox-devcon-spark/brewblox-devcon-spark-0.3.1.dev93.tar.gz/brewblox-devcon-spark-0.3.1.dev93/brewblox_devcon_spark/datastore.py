ee1f6324b19011eace21881af87ed31f607dedc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:39 +0000

    Add package brewblox-devcon-spark/0.3.1.dev93/brewblox-devcon-spark-0.3.1.dev93.tar.gz

---
+++ packages/brewblox-devcon-spark/brewblox-devcon-spark-0.3.1.dev93.tar.gz/brewblox-devcon-spark-0.3.1.dev93/brewblox_devcon_spark/datastore.py
@@ -0,0 +1,64 @@
+"""
+Stores service-related data associated with objects.
+"""
+
+
+from aiohttp import web
+from brewblox_service import brewblox_logger, features
+
+from brewblox_devcon_spark import file_config, twinkeydict
+
+LOGGER = brewblox_logger(__name__)
+
+OBJECT_ID_START = 100
+SYS_OBJECTS = [
+    {
+        'keys': ['__profiles', 1],
+        'data': {}
+    },
+    {
+        'keys': ['__sysinfo', 2],
+        'data': {}
+    },
+    {
+        'keys': ['__time', 3],
+        'data': {}
+    },
+    {
+        'keys': ['__onewirebus', 4],
+        'data': {}
+    },
+]
+
+
+def setup(app: web.Application):
+    store = twinkeydict.TwinKeyFileDict(
+        app=app,
+        filename=app['config']['database'],
+        # System objects should not be serialized to file
+        filter=lambda k, v: k[1] >= OBJECT_ID_START
+    )
+
+    features.add(app, store, key='object_store')
+    add_system_objects(store)
+
+    config = file_config.FileConfig(app, app['config']['config'])
+    features.add(app, config)
+
+
+def get_datastore(app: web.Application) -> twinkeydict.TwinKeyDict:
+    return features.get(app, key='object_store')
+
+
+def add_system_objects(store: twinkeydict.TwinKeyDict):
+    for obj in SYS_OBJECTS:
+        store[obj['keys']] = obj['data']
+
+
+def clear_objects(store: twinkeydict.TwinKeyDict):
+    store.clear()
+    add_system_objects(store)
+
+
+def get_config(app: web.Application) -> file_config.FileConfig:
+    return features.get(app, file_config.FileConfig)
