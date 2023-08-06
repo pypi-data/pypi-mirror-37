a2d6dc65ffcc7e5e18654c06b5613195643cc06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:19 +0000

    Add package ml-ms4alg/0.1.15/ml_ms4alg-0.1.15.tar.gz

---
+++ packages/ml-ms4alg/ml_ms4alg-0.1.15.tar.gz/ml_ms4alg-0.1.15/ml_ms4alg/mountainsort4.py
@@ -0,0 +1,45 @@
+from .ms4alg import MountainSort4
+import tempfile
+import shutil
+import spikeinterface as si
+import numpy as np
+
+def mountainsort4(*,recording,detect_sign,clip_size=50,adjacency_radius=-1,detect_threshold=3,detect_interval=10):
+  MS4=MountainSort4()
+  MS4.setRecording(recording)
+  geom=_get_geom_from_recording(recording)
+  MS4.setGeom(geom)
+  MS4.setSortingOpts(
+    clip_size=clip_size,
+    adjacency_radius=adjacency_radius,
+    detect_sign=detect_sign,
+    detect_interval=detect_interval,
+    detect_threshold=detect_threshold
+  )
+  tmpdir = tempfile.mkdtemp()
+  MS4.setNumWorkers(1)
+  print('Using tmpdir: '+tmpdir)
+  MS4.setTemporaryDirectory(tmpdir)
+  try:
+    MS4.sort()
+  except:
+    print('Cleaning tmpdir: '+tmpdir)
+    shutil.rmtree(tmpdir)
+    raise
+  print('Cleaning tmpdir: '+tmpdir)
+  shutil.rmtree(tmpdir)
+  times,labels,channels=MS4.eventTimesLabelsChannels()
+  output=si.NumpySortingExtractor()
+  output.setTimesLabels(times=times,labels=labels)
+  return output
+
+
+def _get_geom_from_recording(recording):
+  M=recording.getNumChannels()
+  location0=recording.getChannelProperty(0,'location')
+  nd=len(location0)
+  geom=np.zeros((M,nd))
+  for i in range(M):
+    location_i=recording.getChannelProperty(i,'location')
+    geom[i,:]=location_i
+  return geom
