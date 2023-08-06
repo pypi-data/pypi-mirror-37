55870990ee1ec7b55f5bf4e813bbc90e8048d0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:18 +0000

    Add package edc-notification/0.1.2/edc_notification-0.1.2-py3-none-any.whl

---
+++ packages/edc-notification/edc_notification-0.1.2-py3-none-any.whl/edc_notification/mail/email_message.py
@@ -0,0 +1,38 @@
+import inspect
+
+from django.core import mail
+
+
+class EmailMessage:
+
+    def __init__(self, notification=None, instance=None, test=None):
+        self.instance = instance
+        self.notification = notification
+        self.test = test
+        self.email_from = self.notification.email_from
+        self.email_to = self.notification.email_to
+        self.template_opts = {
+            k: v or k.upper() for k, v in inspect.getmembers(self.notification)
+            if not inspect.ismethod(v)
+            and not k.startswith('_')
+            and not inspect.isclass(v)
+            and k not in self.__dict__}
+        self.template_opts.update(
+            subject_test_line=self.notification.subject_test_line if self.test else '',
+            body_test_line=self.notification.body_test_line if self.test else '')
+        self.subject = self.notification.subject_template.format(
+            **self.template_opts,
+            **self.__dict__)
+        self.body = self.notification.body_template.format(
+            **self.template_opts,
+            **self.__dict__)
+
+    def send(self):
+        connection = mail.get_connection()
+        args = [
+            self.subject,
+            self.body,
+            self.email_from,
+            self.email_to]
+        email = mail.EmailMessage(*args, connection=connection)
+        email.send()
