3218daa888c3a3a587d06b76f6f1735ad13c2fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:32 +0000

    Add package navio-aws/0.1.63/navio-aws-0.1.63.tar.gz

---
+++ packages/navio-aws/navio-aws-0.1.63.tar.gz/navio-aws-0.1.63/navio/aws/services/_cloudfront.py
@@ -0,0 +1,32 @@
+import boto3
+import botocore
+import os
+import uuid
+import fnmatch
+import re
+import sh
+import mimetypes
+import copy
+import uuid
+from navio.aws.services._session import AWSSession
+from concurrent import futures
+
+
+class AWSCloudFront(AWSSession):
+
+    def __init__(self, **kwargs):
+        super(self.__class__, self).__init__(kwargs['profile_name'])
+        self.distribution_id = kwargs.get('distribution_id')
+
+    def invalidate(self, *files):
+        cloudfront = self.client('cloudfront')
+        resp = cloudfront.create_invalidation(
+            DistributionId=self.distribution_id,
+            InvalidationBatch={
+                'CallerReference': uuid.uuid4().hex,
+                'Paths': {
+                    'Quantity': len(files),
+                    'Items': files
+                }
+            }
+        )
