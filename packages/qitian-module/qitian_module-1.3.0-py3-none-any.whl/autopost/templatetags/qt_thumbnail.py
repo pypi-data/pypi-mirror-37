228e815e5c75750e950cf713209273740b855816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:12 +0000

    Add package qitian-module/1.3.0/qitian_module-1.3.0-py3-none-any.whl

---
+++ packages/qitian-module/qitian_module-1.3.0-py3-none-any.whl/autopost/templatetags/qt_thumbnail.py
@@ -0,0 +1,38 @@
+from django import template
+import random
+import os
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter
+def auto_thumbnail(img_src, alias):
+    site, alias = site_image(alias)
+    if str(img_src) == '':
+        return settings.QINIU_RANDOM % random.randint(1, 15)
+    return qiniu_max_thumbnail(img_src, alias)
+
+
+def qiniu_max_thumbnail(image_src, alias):
+    if str(image_src) == '':
+        return ''
+    if hasattr(settings, 'QINIU_THUMBNAIL') and 'max_limit' in settings.QINIU_THUMBNAIL:
+        thumb_configure = settings.QINIU_THUMBNAIL['max_limit']
+    else:
+        return image_src
+    if alias not in thumb_configure:
+        return image_src
+    try:
+        return image_src.url + '?imageView2' + thumb_configure[alias]
+    except Exception as e:
+        return settings.MEDIA_URL + image_src + '?imageView2' + thumb_configure[alias]
+
+
+def site_image(alias):
+    site = ''
+    if alias.__contains__('__'):
+        args = str(alias).split(',')
+        site = args[0]
+        alias = args[1]
+    return site, alias
