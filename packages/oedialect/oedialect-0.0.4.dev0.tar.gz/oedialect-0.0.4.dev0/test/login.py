b035cac87e64e968d02d140e2ffa91fde681447b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:27 +0000

    Add package oedialect/0.0.4.dev0/oedialect-0.0.4.dev0.tar.gz

---
+++ packages/oedialect/oedialect-0.0.4.dev0.tar.gz/oedialect-0.0.4.dev0/test/login.py
@@ -0,0 +1,5 @@
+#secret_key = 'bb152815-5bf1-42a6-81bb-a3eb35d62241' # Server
+#OED_CREDS = 'user:12732997043ce2dd210b06e555fea0e39d0df4cc' # local
+OED_CREDS = 'user:3f1ec0807807b988eacd985d11c60f30203925f2' # local
+
+DB_CREDS = 'oedb_test:test'
