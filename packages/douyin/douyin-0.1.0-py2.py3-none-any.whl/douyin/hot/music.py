7f6a96222d8883b5e2a68288ce02e28978544b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:33 +0000

    Add package douyin/0.1.0/douyin-0.1.0-py2.py3-none-any.whl

---
+++ packages/douyin/douyin-0.1.0-py2.py3-none-any.whl/douyin/hot/music.py
@@ -0,0 +1,19 @@
+from douyin.config import hot_music_url
+from douyin.structures import HotMusic
+from douyin.utils import fetch
+
+# todo
+
+
+def music():
+    """
+    get hot music result
+    :return: HotMusic object
+    """
+    result = fetch(hot_music_url)
+    # process json data
+    active_time = result.get('active_time')
+    video_list = result.get('music_list', [])
+    data = [{'item': item.get('music_info'), 'hot_value': item.get('hot_value')} for item in video_list]
+    # construct HotMusic object and return
+    return HotMusic(active_time=active_time, data=data)
