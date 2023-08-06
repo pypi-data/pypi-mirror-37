4873fd978542f769a4a67b79ebfeed5b0bc34fd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:24 +0000

    Add package dsnd-probability-dw71/1/dsnd_probability_dw71-1.tar.gz

---
+++ packages/dsnd-probability-dw71/dsnd_probability_dw71-1.tar.gz/dsnd_probability_dw71-1/README.md
@@ -0,0 +1,11 @@
+# dsnd-probability-dw71 package
+
+Summary of the package
+
+# Files
+
+Explaination of the files in the package
+
+# Installation
+
+Install
---
+++ packages/dsnd-probability-dw71/dsnd_probability_dw71-1.tar.gz/dsnd_probability_dw71-1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_dw71',
+      version='1',
+      description='Gaussian and binominal distributions',
+      packages=['dsnd_probability_dw71'],
+      author = 'DataWorks71',
+      author_email = 'not@myhome.club',
+      url = 'http://www.example.com/~cschultz/bvote/',
+      zip_safe = False)
