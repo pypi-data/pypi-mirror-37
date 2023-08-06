e050f71fa1cbacc918839a0244183fed03edabef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:01 +0000

    Add package cite/0.2.0/cite-0.2.0-py2.py3-none-any.whl

---
+++ packages/cite/cite-0.2.0-py2.py3-none-any.whl/cite/__init__.py
@@ -0,0 +1,41 @@
+import argparse
+
+from habanero import cn
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+
+parser = argparse.ArgumentParser(description="cite - get citation for DOI.")
+parser.add_argument(
+    "-v",
+    "--version",
+    action="version",
+    version="%(prog)s {version}".format(version=__version__),
+)
+parser.add_argument(
+    "--format",
+    default="text",
+    help='Return citation data in specified format: "rdf-xml", "turtle", "citeproc-json", "citeproc-json-ish", "text" (Default), "ris", "bibtex" , "crossref-xml", "datacite-xml","bibentry", or "crossref-tdm"',
+)
+parser.add_argument("ids", nargs="+", help="One or more DOIs")
+
+args = parser.parse_args()
+
+
+def main():
+    items = []
+    for item in args.ids:
+        items.append(
+            item.replace("http://doi.org/", "")
+            .replace("https://doi.org/", "")
+            .replace("doi:", "")
+        )
+    results = cn.content_negotiation(items, format=args.format)
+    if type(results) is str:
+        print(results)
+    else:
+        for result in results:
+            print(result)
