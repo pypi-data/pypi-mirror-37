3159c799593fa54a0392224a7a6cb720010b401b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:24 +0000

    Add package code-record/0.0.2/code_record-0.0.2.tar.gz

---
+++ packages/code-record/code_record-0.0.2.tar.gz/code_record-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="code_record",
+    version="0.0.2",
+    author="Steven wang",
+    author_email="wangzhou8284@outlook.com",
+    description="useful codes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StevenLianaL/code_record",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
