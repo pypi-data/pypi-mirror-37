57c20fed86eb473b42c7f30bc3ec4ea58d579507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:06 +0000

    Add package njnuko/3.0.5/njnuko-3.0.5.tar.gz

---
+++ packages/njnuko/njnuko-3.0.5.tar.gz/njnuko-3.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '3.0.5'
+
+with open("README.md","r") as fh:
+      long_description=fh.read()
+
+
+
+setup(name="njnuko",
+      version="3.0.5",
+      description="file sorting",
+      long_description="file sorting project",
+      classifiers=[], 
+      keywords='njnuko',
+      author='njnuko',
+      author_email='njnuko@163.com',
+      url='https://github.com/njnuko/njnuko',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['psycopg2','pymysql'],
+      entry_points="",
+      py_modules=[],
+      )
