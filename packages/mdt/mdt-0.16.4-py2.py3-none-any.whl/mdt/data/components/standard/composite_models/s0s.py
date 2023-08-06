caf8326060435f2d3731415a8631d318556c6770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:44 +0000

    Add package mdt/0.16.4/mdt-0.16.4-py2.py3-none-any.whl

---
+++ packages/mdt/mdt-0.16.4-py2.py3-none-any.whl/mdt/data/components/standard/composite_models/s0s.py
@@ -0,0 +1,14 @@
+from mdt import CompositeModelTemplate
+
+__author__ = 'Robbert Harms'
+__date__ = "2015-06-22"
+__maintainer__ = "Robbert Harms"
+__email__ = "robbert.harms@maastrichtuniversity.nl"
+
+
+class S0(CompositeModelTemplate):
+    """Models the unweighted signal (aka. b0)."""
+    model_expression = 'S0'
+    volume_selection = {'unweighted_threshold': 250e6,
+                        'use_unweighted': True,
+                        'use_weighted': False}
