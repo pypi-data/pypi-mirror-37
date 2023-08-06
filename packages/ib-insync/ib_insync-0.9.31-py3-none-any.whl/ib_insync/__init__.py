08d53c8112e820e3748cbebea633c6d3fd649f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:16:22 +0000

    Add package ib-insync/0.9.31/ib_insync-0.9.31-py3-none-any.whl

---
+++ packages/ib-insync/ib_insync-0.9.31-py3-none-any.whl/ib_insync/__init__.py
@@ -0,0 +1,33 @@
+import sys
+import importlib
+
+from .version import __version__, __version_info__  # noqa
+from . import util
+
+
+if sys.version_info < (3, 6, 0):
+    raise RuntimeError('ib_insync requires Python 3.6 or higher')
+
+try:
+    import ibapi
+except ImportError:
+    raise RuntimeError(
+        'IB API from http://interactivebrokers.github.io is required')
+
+if util.ibapiVersionInfo() < (9, 73, 6):
+    raise RuntimeError(
+        f'Old version ({ibapi.__version__}) of ibapi package detected. '
+        'The newest version from http://interactivebrokers.github.io '
+        'is required')
+
+__all__ = ['util']
+for _name in (
+        'objects', 'event', 'contract', 'order', 'ticker', 'ib',
+        'client', 'wrapper', 'flexreport', 'ibcontroller'):
+    _mod = importlib.import_module(f'ib_insync.{_name}')
+    __all__ += _mod.__all__
+    locals().update((k, getattr(_mod, k)) for k in _mod.__all__)
+
+del sys
+del importlib
+del ibapi
