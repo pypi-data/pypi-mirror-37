ecd60d2c509826f0168ea79798b00c3df35ab8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:48 +0000

    Add package dash-building-blocks/0.1.1/dash_building_blocks-0.1.1.tar.gz

---
+++ packages/dash-building-blocks/dash_building_blocks-0.1.1.tar.gz/dash_building_blocks-0.1.1/setup.py
@@ -0,0 +1,47 @@
+import sys
+from dash_building_blocks import __version__
+from setuptools import setup
+
+def readme():
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+configuration = {
+    'name' : 'dash_building_blocks',
+    'version': __version__,
+    'description' : 'Lightweight Auxiliary Framework for Writing Object-Oriented Dash Code.',
+    'long_description' : readme(),
+    'long_description_content_type' : 'text/markdown',
+    'classifiers' : [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: MacOS',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    'keywords' : 'dash object-oriented-programming',
+    'url' : 'http://github.com/AlgorithmHub/dash_buildin_blocks',
+    'maintainer' : 'Marco de Lannoy Kobayashi',
+    'maintainer_email' : 'mdlkdev@gmail.com',
+    'license' : 'MIT',
+    'packages' : ['dash_building_blocks'],
+    'install_requires': [
+        'dash >= 0.22.0',
+        'dash-html-components',
+        'dash-core-components'
+    ],
+    'ext_modules' : [],
+    'cmdclass' : {},
+    'test_suite' : 'tests.test_suite',
+    'tests_require' : [],
+    'data_files' : ()
+    }
+
+setup(**configuration)
