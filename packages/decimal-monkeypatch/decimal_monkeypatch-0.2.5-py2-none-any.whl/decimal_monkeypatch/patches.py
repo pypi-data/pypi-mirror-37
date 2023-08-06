1be3fa23a7b4a6d4f7198ab940bd13c58e7dfd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:51 +0000

    Add package decimal-monkeypatch/0.2.5/decimal_monkeypatch-0.2.5-py2-none-any.whl

---
+++ packages/decimal-monkeypatch/decimal_monkeypatch-0.2.5-py2-none-any.whl/decimal_monkeypatch/patches.py
@@ -0,0 +1,17 @@
+from __future__ import print_function, division
+
+
+def autowrapt_decimal(module):
+    import sys
+    if 'decimal' not in sys.modules:
+        import cdecimal
+        sys.modules['decimal'] = cdecimal
+        print('module decimal patched to {}'.format(sys.modules['decimal'].__name__))
+        cdecimal.tmp_context = cdecimal.Context
+
+        def patched(*args, **kwargs):
+            if 'rounding' in kwargs and kwargs['rounding'] is None:
+                kwargs['rounding'] = 6
+            return cdecimal.tmp_context(*args, **kwargs)
+        cdecimal.Context = patched
+        print('cdecimal.Context monkey-patch for boto(if rounding=None, then 6 passed to Context instead\n)')
