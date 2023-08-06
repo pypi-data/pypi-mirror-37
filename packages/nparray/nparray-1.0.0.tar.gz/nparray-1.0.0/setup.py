3606adb96c9256a20085fea2a0208bb0c5d7c1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:42 +0000

    Add package nparray/1.0.0/nparray-1.0.0.tar.gz

---
+++ packages/nparray/nparray-1.0.0.tar.gz/nparray-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nparray',
+      version='1.0.0',
+      description='Numpy Array Helpers',
+      author='Kyle Conroy',
+      author_email='kyleconroy@gmail.com',
+      url='https://www.github.com/kecnry/nparray',
+      download_url = 'https://github.com/kecnry/nparray/tarball/1.0.0',
+      packages=['nparray'],
+     )
