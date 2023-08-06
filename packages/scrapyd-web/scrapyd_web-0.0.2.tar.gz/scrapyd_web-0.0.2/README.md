a8efb30adeedac8f6357ef9c783ba3c8cdd58529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:58 +0000

    Add package scrapyd-web/0.0.2/scrapyd_web-0.0.2.tar.gz

---
+++ packages/scrapyd-web/scrapyd_web-0.0.2.tar.gz/scrapyd_web-0.0.2/README.md
@@ -0,0 +1,5 @@
+This project it's fun with some customization index template of scrapyd web interface.
+
+
+
+
---
+++ packages/scrapyd-web/scrapyd_web-0.0.2.tar.gz/scrapyd_web-0.0.2/setup.py
@@ -0,0 +1,106 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'scrapyd_web'
+DESCRIPTION = 'scrapyd template customization'
+URL = 'https://github.com/serbernar/scrapyd-website'
+EMAIL = 'serbernar1@gmail.com'
+AUTHOR = 'Sergey Berebko'
+REQUIRES_PYTHON = '>=3.4.0'
+VERSION = '0.0.2'
+
+REQUIRED = [
+    'scrapyd>=1.2.0'
+]
+EXTRAS = {
+
+}
+PACKAGE_DATA = {
+    'scrapyd_website': ['templates/*.html']
+}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+about = dict()
+about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(
+            sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=bool(PACKAGE_DATA),
+    package_data=PACKAGE_DATA,
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
