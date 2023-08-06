f7d264302a23e8071057dadb7ce24f712516b964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:04 +0000

    Add package eric-demos-for/1.0.4/eric_demos_for-1.0.4.tar.gz

---
+++ packages/eric-demos-for/eric_demos_for-1.0.4.tar.gz/eric_demos_for-1.0.4/setup.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+# author:eric(2384363842@qq.com)
+# 创建于: 2018/10/24 19:55
+from setuptools import setup
+setup(
+    name='eric_demos_for',         # 应用名
+    version='1.0.4',        # 版本号
+    #packages=['demo_python_package_eric'],  # 包括在安装包内的Python包
+    author="hustcsxg",
+    author_email="hustcsxg@163.com",
+    url="http://www.py2ai.com"
+)
