99d2c61768604789dd01c936740bb79664d2a2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:51 +0000

    Add package proxy-6/1.1/proxy-6-1.1.tar.gz

---
+++ packages/proxy-6/proxy-6-1.1.tar.gz/proxy-6-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", ) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='proxy-6',
+    version='1.1',
+    description='Simple api for proxy6.net',
+    license="MIT",
+    long_description=long_description,
+    author='Valentine Bobrovsky',
+    author_email='vbabrouski@outlook.com',
+    url="https://github.com/vbxx3/proxy6_api",
+    packages=['proxy6_api'],  #same as name
+    install_requires=["setuptools", "requests"], #external packages as dependencies
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
