4b53de3ad4ab8c9966805dc48612f24504b4fb11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:22 +0000

    Add package django-postie/0.6.3/django-postie-0.6.3.tar.gz

---
+++ packages/django-postie/django-postie-0.6.3.tar.gz/django-postie-0.6.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+pkj_name = 'postie'
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='django-postie',
+    version='0.6.3',
+    install_requires=[
+        'django>=2',
+        'django-ckeditor',
+        'django-codemirror2',
+        'django-model-utils',
+        'django-parler'
+    ],
+    packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
+    url='https://gitlab.com/cyberbudy/django-postie',
+    license='MIT',
+    author='cyberbudy',
+    author_email='cyberbudy@gmail.com',
+    description='Django mailing through admin',
+    long_description=readme,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ]
+
+)
