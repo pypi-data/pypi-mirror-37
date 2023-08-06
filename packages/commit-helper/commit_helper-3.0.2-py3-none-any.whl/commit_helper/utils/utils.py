7e101e0ff2a595842730fb4f4d4caa1ca20b71d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:39 +0000

    Add package commit-helper/3.0.2/commit_helper-3.0.2-py3-none-any.whl

---
+++ packages/commit-helper/commit_helper-3.0.2-py3-none-any.whl/commit_helper/utils/utils.py
@@ -0,0 +1,79 @@
+import argparse
+from yaml import dump
+
+supported_conventions = [
+    "angular",
+    "karma",
+    "changelog",
+    "symphony",
+    "message",
+]
+
+menu = """
+    What type of commit convention are you using?
+
+    default: Just the message
+    1: Karma/Angular
+    2: Conventional changelog
+    3: Symfony CMF
+
+    """
+
+
+def get_text():
+    tag = str(input("type the tag: "))
+    msg = str(input("type the commit message: ")).lower()
+    return tag, msg
+
+
+def get_context():
+        context = str(input('type the context: ') or '').lower()
+        return context
+
+
+def gen_co_author(co_author):
+    if co_author is '':
+        return ''
+    return "\nCo-authored-by: %s" % co_author
+
+
+def create_file(convention_name, dont_create=False):    # pragma: no cover
+    if not dont_create:
+        data = dict(
+            convention=convention_name
+        )
+        with open('commiter.yml', 'w') as output_file:
+            dump(data, output_file, default_flow_style=False)
+        print('Successfully created the commiter file.')
+
+
+def parser_cli():
+    desc = "A commit formatter tool to help you follow commit conventions."
+    help_convention = \
+        """
+        Selects a convention to be used for the commit.
+        Required if there's no commiter.yml file.
+        """
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-ca", "--co-author",
+                        help="Make your friend an co-author to the commit",
+                        dest="co_author", default='')
+    parser.add_argument("-nf", "--no-file", dest="no_file",
+                        help="Disables the creation of a commiter.yml file",
+                        action="store_true")
+    parser.add_argument("-c", "--convention", choices=supported_conventions,
+                        dest="convention", default='', help=help_convention)
+    parser.add_argument("-d", "--debug", action="store_true", dest="debug",
+                        help="Toggles debug option")
+    return parser
+
+
+def sanitize_as_empty_string(string):
+    if string is None:
+        return ''
+    return string
+
+
+def debug(message, value, show=False):
+    if show:
+        print("DEBUG-> %s: %s" % (message, value))
---
+++ packages/commit-helper/commit_helper-3.0.2-py3-none-any.whl/test/test_utils.py
@@ -0,0 +1,70 @@
+import commit_helper.utils.utils as utils
+# import yaml
+
+
+def test_get_text():
+    inputs = [
+        "tag",
+        "message",
+    ]
+
+    def mock_input(s):
+        return inputs.pop(0)
+
+    utils.input = mock_input
+    a, b = utils.get_text()
+    if not a == 'tag':
+        raise AssertionError()
+    if not b == 'message':
+        raise AssertionError()
+
+
+def test_get_context():
+    inputs = [
+        "context",
+    ]
+
+    def mock_input(s):
+        return inputs.pop(0)
+    utils.input = mock_input
+    a = utils.get_context()
+    if not a == 'context':
+        raise AssertionError()
+
+
+def test_sanitize_as_empty_string():
+    string = 'asopdfha'
+    string = utils.sanitize_as_empty_string(string)
+    if not string == 'asopdfha':
+        raise AssertionError()
+
+    string2 = None
+    string2 = utils.sanitize_as_empty_string(string2)
+    if not string2 == '':
+        raise AssertionError()
+
+
+def test_gen_co_author():
+    arg = utils.gen_co_author('kiryto <black.swordsman@aincrad.com>')
+    if not arg == "\nCo-authored-by: kiryto <black.swordsman@aincrad.com>":
+        raise AssertionError()
+
+    arg2 = utils.gen_co_author('')
+    if not arg2 == '':
+        raise AssertionError()
+
+
+def test_debug(capsys):
+    utils.debug('msg', 666, show=True)
+    captured = capsys.readouterr()
+    if not captured.out == "DEBUG-> msg: 666\n":
+        raise AssertionError()
+
+# FIXME
+# def test_create_file(tmpdir):
+#     test_file = tmpdir.mkdir('test').join('commiter.yml')
+#     utils.create_file("changelog")
+#     stream = open('commiter.yml', 'r')
+#     convention = yaml.load(stream)
+#     if not convention['convention'] == 'changelog':
+#         raise AssertionError()
