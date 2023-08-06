10d5249d4f1efd84b6a5aa69f823707fb421187b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:33 +0000

    Add package lz/0.0.1/lz-0.0.1-py3-none-any.whl

---
+++ packages/lz/lz-0.0.1-py3-none-any.whl/lz/right.py
@@ -0,0 +1,43 @@
+import itertools
+from typing import (Callable,
+                    Iterable)
+
+from . import left
+from .functional import (compose,
+                         flip)
+from .hints import (Domain,
+                    Map,
+                    Range)
+from .iterating import (expand,
+                        reverse)
+
+
+def accumulator(function: Callable[[Domain, Range], Range],
+                initial: Range
+                ) -> Map[Iterable[Domain], Iterable[Iterable[Range]]]:
+    """
+    Returns function that yields cumulative results of given binary function
+    starting from given initial object in direction from right to left.
+    """
+    left_accumulator = left.accumulator(flip(function), initial)
+    return compose(left_accumulator, reverse)
+
+
+def attacher(object_: Domain) -> Map[Iterable[Domain], Iterable[Domain]]:
+    """
+    Returns function that appends given object to iterable.
+    """
+    def attach(iterable: Iterable[Domain]) -> Iterable[Domain]:
+        yield from itertools.chain(iterable, expand(object_))
+
+    return attach
+
+
+def folder(function: Callable[[Domain, Range], Range],
+           initial: Range) -> Map[Iterable[Domain], Range]:
+    """
+    Returns function that cumulatively applies given binary function
+    starting from given initial object in direction from right to left.
+    """
+    left_folder = left.folder(flip(function), initial)
+    return compose(left_folder, reverse)
---
+++ packages/lz/lz-0.0.1-py3-none-any.whl/lz/signatures/__init__.py
@@ -0,0 +1,11 @@
+import platform
+
+from .base import (Base,
+                   Parameter,
+                   Plain,
+                   factory)
+
+if platform.python_implementation() != 'PyPy':
+    from .base import Overloaded
+
+del platform
