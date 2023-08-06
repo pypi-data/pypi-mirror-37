dc3791f23af59cc564ad7c1bc5662f5501b111b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:55 +0000

    Add package py4js/0.1.0/py4js-0.1.0.tar.gz

---
+++ packages/py4js/py4js-0.1.0.tar.gz/py4js-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="py4js",
+    version="0.1.0",
+    keywords=("pip", "py4js"),
+    description="a fast and simple micro-framework for small web applications",
+#     long_description="""py4js is a fast and simple micro-framework for small web applications. Its goal is to enable you to develop
+# web applications in a simple and understandable way.
+#
+# With it, you don't need to know the HTTP protocol, or how Python communicates with JavaScript. You can use Python functions in JavaScript just like native JavaScript functions.
+# """,
+    license="MIT Licence",
+
+    url="https://github.com/lixk/py4js",
+    author="Xiangkui Li",
+    author_email="1749498702@qq.com",
+    py_modules=['py4js'],
+    # packages=find_packages(),
+    # include_package_data=True,
+    platforms="any",
+    install_requires=["bottle"]
+)
