a479aac6a4d35b245b2bbe573f5778adfd239126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:59 +0000

    Add package bzp/1.1.0/bzp-1.1.0-py3-none-any.whl

---
+++ packages/bzp/bzp-1.1.0-py3-none-any.whl/bzp/__version__.py
@@ -0,0 +1,11 @@
+# ICE TEMPLE
+
+__title__ = 'bzp'
+__description__ = 'Manager for bz2 pickle files'
+__url__ = 'https://github.com/ideascup/bzp/'
+__version__ = '1.1.0'
+__build__ = 0x010100
+__author__ = 'Dmitriy Pleshevskiy'
+__author_email__ = 'dmitriy@ideascup.me'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Dmitriy Pleshevskiy'
