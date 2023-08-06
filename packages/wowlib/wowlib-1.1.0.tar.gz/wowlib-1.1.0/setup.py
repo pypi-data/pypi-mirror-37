ab78df31a3713194b3fbdf1e1449c770329c3212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:23 +0000

    Add package wowlib/1.1.0/wowlib-1.1.0.tar.gz

---
+++ packages/wowlib/wowlib-1.1.0.tar.gz/wowlib-1.1.0/setup.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	name='wowlib',
+	version='1.1.0',
+	description='Excel convert tools for wow group',	
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	author='peakgao',
+	author_email='peakgao163@163.com',
+	license="MIT",
+	url='https://www.q1.com',
+	packages=['wowlib'],
+	#packages=setuptools.find_packages(),
+	install_requires=[
+        "xlrd",
+        ],	
+	classifiers=[
+	    "Programming Language :: Python :: 3",
+	    "License :: OSI Approved :: MIT License",
+	    "Operating System :: OS Independent",
+	],
+)
