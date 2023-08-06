8321c20d01eb4cc3b6ea963d7b37b127143e8f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:36 +0000

    Add package text-classifier-kv/0.0.8/text_classifier_kv-0.0.8.tar.gz

---
+++ packages/text-classifier-kv/text_classifier_kv-0.0.8.tar.gz/text_classifier_kv-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import io
+import os.path as op
+
+from setuptools import setup, find_packages
+
+here = op.abspath(op.dirname(__file__))
+
+# Get the long description from the README file
+with io.open(op.join(here, 'README.md'), mode='rt', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='text_classifier_kv',
+    version='0.0.8',
+    description='text classifier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kopylovvlad/text_classifier_kv',
+    author='Vladislav Kopylov',
+    author_email='kopylov.vlad@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.5',
+)
