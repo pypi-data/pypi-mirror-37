6bd9f929120962c5d55f14ed06efda4a73a941c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:34 +0000

    Add package pyterrier/0.1.2/pyterrier-0.1.2.tar.gz

---
+++ packages/pyterrier/pyterrier-0.1.2.tar.gz/pyterrier-0.1.2/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=6.0', 'jinja2>=2.10', ]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="Daniel Furtado",
+    author_email='daniel@dfurtado.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        ],
+    description="The web framework that favorite conventions over configurations",
+    entry_points={
+        'console_scripts': [
+            'pyterrier=pyterrier.pyterrier_cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='pyterrier',
+    name='pyterrier',
+    packages=find_packages(include=[
+        'pyterrier',
+        'pyterrier.app_template',
+        'pyterrier.cli',
+        'pyterrier.core',
+        'pyterrier.encoders',
+        'pyterrier.http',
+        'pyterrier.renderers',
+        'pyterrier.validators']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/dfurtado/pyterrier',
+    version='0.1.2',
+    zip_safe=False,
+)
