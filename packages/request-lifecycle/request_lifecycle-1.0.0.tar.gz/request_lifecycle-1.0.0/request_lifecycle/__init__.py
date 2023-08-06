75f8783f327c7d7427715ca46490e5a6a06833b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:38 +0000

    Add package request-lifecycle/1.0.0/request_lifecycle-1.0.0.tar.gz

---
+++ packages/request-lifecycle/request_lifecycle-1.0.0.tar.gz/request_lifecycle-1.0.0/request_lifecycle/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2018 Frank Lin. All Rights Reserved.
+# -*- coding: utf-8 -*-
+
+__version__ = '1.0.0'
+
+from .logger import RequestLifecycleLogger
+from .request_lifecycle import RequestLifecycleMiddleware
