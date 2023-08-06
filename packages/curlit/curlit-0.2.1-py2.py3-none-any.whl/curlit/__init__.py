c1342bc4379e4474f101752968bc2b32bee87078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:34 +0000

    Add package curlit/0.2.1/curlit-0.2.1-py2.py3-none-any.whl

---
+++ packages/curlit/curlit-0.2.1-py2.py3-none-any.whl/curlit/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from curlit.base import Curl
+
+from .django import *
+from .requests import *
+from .urllib import *
+
+
+def curlit(
+    request,
+    strify=True,
+    verbose=False,
+    indent=0,
+):
+    c = Curl(request)
+    if strify:
+        c = c.curl(
+            verbose=verbose,
+            indent=indent,
+        )
+    return c
