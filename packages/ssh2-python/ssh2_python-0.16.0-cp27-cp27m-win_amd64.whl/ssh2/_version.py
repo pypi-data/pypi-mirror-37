4319223591578a6c8a0baa47fd19b4cac8d5daa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:17 +0000

    Add package ssh2-python/0.16.0/ssh2_python-0.16.0-cp27-cp27m-win_amd64.whl

---
+++ packages/ssh2-python/ssh2_python-0.16.0-cp27-cp27m-win_amd64.whl/ssh2/_version.py
@@ -0,0 +1,10 @@
+
+import json
+
+version_json = '''
+{"date": "2018-10-23T22:08:54.731000", "full-revisionid": "26a372bb8afeb0fdac186d0767d6041ff941a0e0", "dirty": false, "version": "0.16.0", "error": null}'''  # END VERSION_JSON
+
+
+def get_versions():
+    return json.loads(version_json)
+
