ff369f3a98042f9ad002a2a845d3cf6603db1963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:40 +0000

    Add package photo-calendar/0.1.0/photo_calendar-0.1.0-py2-none-any.whl

---
+++ packages/photo-calendar/photo_calendar-0.1.0-py2-none-any.whl/photocalendar/__init__.py
@@ -0,0 +1,11 @@
+"""
+PhotoCalendar is a Python utility to create custom weekly/monthly/... photo calendars.
+This package provides a :class:`PhotoCalendar <photocalendar.PhotoCalendar>` class which represents the photo calendar (days, weeks, photos, backgrounds, holidays, ...) and creates the calendar in HTML format (which can be then converted to PDF).
+
+The appearance of the calendar is defined by the used template.
+a few simple :ref:`templates <section-templates>` is provided.
+"""
+from .photocalendar import PhotoCalendar
+
+name = "photocalendar"
+version = "0.1.0"
