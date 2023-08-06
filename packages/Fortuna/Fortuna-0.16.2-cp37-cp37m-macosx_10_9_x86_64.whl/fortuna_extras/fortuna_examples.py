9ae7ddf865380e8333fb5ffb3071e9acd449c609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:53 +0000

    Add package Fortuna/0.16.2/Fortuna-0.16.2-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/Fortuna/Fortuna-0.16.2-cp37-cp37m-macosx_10_9_x86_64.whl/fortuna_extras/fortuna_examples.py
@@ -0,0 +1,58 @@
+from Fortuna import CumulativeWeightedChoice, MultiCat, distribution_timer
+from collections import OrderedDict
+
+
+note = """
+Typical Magic Item Table from an RPG, based on 1d100 roll.
+
+     1-50   Potion of healing
+    51-60   Spell scroll (cantrip) - one of several cantrips
+    61-70   Potion of climbing
+    71-90   Spell scroll (level 1) - one of several level 1 spells
+    91-94   Spell scroll (level 2) - one of several level 2 spells
+    95-98   Potion of greater healing
+       99   Bag of holding
+      100   Driftglobe
+"""
+
+
+class RandomSpell:
+
+    def __call__(self, spell_level=None) -> str:
+        return self.random_spell(spell_level)
+
+    def __init__(self):
+        self.random_spell = MultiCat(OrderedDict({
+            "cantrip": ("Acid Splash", "Blade Ward", "Chill Touch", "Dancing Lights", "Fire Bolt"),  # ...
+            "level_1": ("Burning Hands", "Charm Person", "Chromatic Orb", "Detect Magic", "Find Familiar"),  # ...
+            "level_2": ("Blindness", "Blur", "Cloud of Daggers", "Continual Flame", "Gust of Wind"),  # ...
+            # ... up to level 9 spells
+        }))
+
+
+class MagicItemTable:
+
+    def __call__(self) -> str:
+        return self.magic_item_table()()
+
+    def __init__(self):
+        self.random_spell = RandomSpell()
+        self.magic_item_table = CumulativeWeightedChoice((
+            (50, lambda: f"Potion of healing"),
+            (60, lambda: f"Spell scroll (cantrip) {self.random_spell('cantrip')}"),
+            (70, lambda: f"Potion of climbing"),
+            (90, lambda: f"Spell scroll (level 1) {self.random_spell('level_1')}"),
+            (94, lambda: f"Spell scroll (level 2) {self.random_spell('level_2')}"),
+            (98, lambda: f"Potion of greater healing"),
+            (99, lambda: f"Bag of holding"),
+            (100, lambda: f"Driftglobe"),
+        ))
+
+
+if __name__ == "__main__":
+    print(note)
+
+    random_magic_item = MagicItemTable()
+
+    print("Magic Item Table Distribution:")
+    distribution_timer(random_magic_item, call_sig="random_magic_item()")
