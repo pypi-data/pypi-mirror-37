d0c6bb39bf50ada78ae28aac0b49b3c9af51280c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:26 +0000

    Add package streamstats/0.1.1/streamstats-0.1.1.tar.gz

---
+++ packages/streamstats/streamstats-0.1.1.tar.gz/streamstats-0.1.1/streamstats/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for StreamStats."""
+
+__author__ = """Maxwell B. Joseph"""
+__email__ = 'maxwell.b.joseph@colorado.edu'
+__version__ = '0.1.1'
+base_url = "https://streamstats.usgs.gov/streamstatsservices/"
