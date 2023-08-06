ac64b4c8fe9be6018b1b7fd176794915779ffaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:42 +0000

    Add package invogen/0.2.0/invogen-0.2.0-py3-none-any.whl

---
+++ packages/invogen/invogen-0.2.0-py3-none-any.whl/invogen/__init__.py
@@ -0,0 +1,13 @@
+"""
+    invogen
+    -------
+
+    A package to generate beautiful LaTeX invoices with Python
+
+    :copyright: © 2018 Samuel Searles-Bryant.
+    :license: MIT, see LICENSE for more details.
+"""
+__version__ = "0.2.0"
+
+from invogen.invoice import Invoice, InvoiceEntry
+from invogen.accounts import User, Customer
