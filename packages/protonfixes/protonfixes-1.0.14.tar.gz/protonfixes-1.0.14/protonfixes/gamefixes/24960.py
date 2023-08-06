4c923160f8d2649e82396606408cf099065fc95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:42 +0000

    Add package protonfixes/1.0.14/protonfixes-1.0.14.tar.gz

---
+++ packages/protonfixes/protonfixes-1.0.14.tar.gz/protonfixes-1.0.14/protonfixes/gamefixes/24960.py
@@ -0,0 +1,15 @@
+""" Game fix for Battlefield: Bad Company 2
+"""
+#pylint: disable=C0103
+
+from protonfixes import util
+
+def main():
+    """ Install corefonts, dx9. Set to win7 and override msdmo
+    """
+
+    # https://github.com/ValveSoftware/Proton/issues/200#issuecomment-415905979
+    util.protontricks('win7')
+    util.protontricks('corefonts')
+    util.protontricks('directx9')
+    util.set_environment('WINEDLLOVERRIDES', 'msdmo=b')
