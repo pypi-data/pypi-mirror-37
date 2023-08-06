ef34b4d6566d125710e0fbabc89a23bf1019a138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:51 +0000

    Add package servicer/0.8.1/servicer-0.8.1.tar.gz

---
+++ packages/servicer/servicer-0.8.1.tar.gz/servicer-0.8.1/servicer/builtin/service_adapters/service.py
@@ -0,0 +1,24 @@
+import os
+
+from servicer.run import run
+from servicer.tokens import interpolate_tokens
+
+class Service:
+    def __init__(self, config=None):
+        self.run = run
+
+        if config == None:
+            return
+
+        self.config = config
+
+        self.project = os.environ['PROJECT_NAME']
+        self.environment = os.getenv('SERVICE_ENVIRONMENT')
+
+        interpolate_tokens(self.config, os.environ)
+
+    def up(self):
+        pass
+
+    def down(self):
+        pass
