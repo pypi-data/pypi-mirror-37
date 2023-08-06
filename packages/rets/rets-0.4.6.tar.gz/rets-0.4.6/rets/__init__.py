a69704669d7ce95f45e6daf5af67c0afb07c6b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:59 +0000

    Add package rets/0.4.6/rets-0.4.6.tar.gz

---
+++ packages/rets/rets-0.4.6.tar.gz/rets-0.4.6/rets/__init__.py
@@ -0,0 +1,9 @@
+from .session import Session
+from .exceptions import RETSException
+
+__title__ = 'rets'
+__version__ = '0.4.6'
+__author__ = 'REfindly'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 REfindly'
+__all__ = ['Session', 'RETSException']
