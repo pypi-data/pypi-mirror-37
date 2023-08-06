3e6bec90827a961607994924a62178d8aa0cfe3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:00 +0000

    Add package zmail/0.2.0/zmail-0.2.0.tar.gz

---
+++ packages/zmail/zmail-0.2.0.tar.gz/zmail-0.2.0/zmail/exceptions.py
@@ -0,0 +1,21 @@
+"""
+zmail.exceptions
+~~~~~~~~~~~~~~~~
+Include all exceptions used within zmail.
+"""
+
+
+class ZmailException(RuntimeError):
+    """Base exception."""
+
+
+class InvalidProtocol(ZmailException, ValueError):
+    """Invalid protocol settings used."""
+
+
+class ParseError(ZmailException):
+    """Parse mail error."""
+
+
+class InvalidArguments(ZmailException):
+    """Invalid arguments."""
