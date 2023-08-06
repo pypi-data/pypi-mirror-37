a06a3729f4088d880f6b4ec3e955a30ee5e98af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:06 +0000

    Add package TM-CommonPy/0.2.16/TM_CommonPy-0.2.16-py3-none-any.whl

---
+++ packages/TM-CommonPy/TM_CommonPy-0.2.16-py3-none-any.whl/TM_CommonPy/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.10.0'
+#__all__ = ["CommandSet"]
+##region ImportThisModule
+from TM_CommonPy.Misc import *
+del TMLog #TMLog is in Misc, but shouldn't be exposed
+from TM_CommonPy.CommandSet import CommandSet
+from TM_CommonPy.CopyContext import CopyContext
+from TM_CommonPy.WorkspaceContext import WorkspaceContext
+from TM_CommonPy.ElementTreeContext import ElementTreeContext
+from TM_CommonPy.Narrator import Narrate
+##endregion
