bcdbab78cb3e29aaa533d6988424313eedf53a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:10 +0000

    Add package eric-4-1809-py/1.0/eric_4_1809_py-1.0.tar.gz

---
+++ packages/eric-4-1809-py/eric_4_1809_py-1.0.tar.gz/eric_4_1809_py-1.0/eric_4_1809_python/setup.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+# author:eric(2384363842@qq.com)
+# 创建于: 2018/10/24 19:55
+from setuptools import setup
+setup(
+    name='eric_4_1809_py',         # 应用名
+    version='1.0',        # 版本号
+    packages=['eric_4_1809_python'],  # 包括在安装包内的Python包
+    author="hustcsxg",
+    author_email="hustcsxg@163.com",
+    url="http://www.py2ai.com"
+)
