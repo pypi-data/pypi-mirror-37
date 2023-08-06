9ed4811eafdc7a0e0364ea374b4f18825d7d0517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:39 +0000

    Add package commit-helper/3.0.2/commit-helper-3.0.2.tar.gz

---
+++ packages/commit-helper/commit-helper-3.0.2.tar.gz/commit-helper-3.0.2/test/test_conventions.py
@@ -0,0 +1,41 @@
+import commit_helper.conventions.karma_angular as angular
+import commit_helper.conventions.changelog as changelog
+import commit_helper.conventions.symphony_cmf as symphony
+import commit_helper.conventions.no_convention as no_convention
+
+
+def test_angular_convention_with_context():
+    message = angular.angular_convention('TAG', 'message', 'context')
+    if not (message == 'tag(context): message\n'):
+        raise AssertionError()
+
+
+def test_angular_convention_without_context():
+    message = angular.angular_convention('tag', 'message', '')
+    if not (message == 'tag: message\n'):
+        raise AssertionError()
+
+
+def test_changelog_convention():
+    message = changelog.changelog_convention('tag', 'message')
+    if not (message == 'TAG: message\n'):
+        raise AssertionError()
+
+
+def test_symphony_convention():
+    message = symphony.symphony_convention('tag', 'message')
+    if not (message == '[Tag] message\n'):
+        raise AssertionError()
+
+
+def test_no_convention():
+    inputs = [
+        "message",
+    ]
+
+    def mock_input(s):
+        return inputs.pop(0)
+    no_convention.input = mock_input
+    message = no_convention.just_message()
+    if not message == 'Message\n':
+        raise AssertionError()
