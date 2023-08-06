95c3ab1a2e70751daad09108050401ebfcbabb80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:13 +0000

    Add package cloud-scanner-azure/0.1.2/cloud_scanner_azure-0.1.2.tar.gz

---
+++ packages/cloud-scanner-azure/cloud_scanner_azure-0.1.2.tar.gz/cloud_scanner_azure-0.1.2/cloud_scanner_azure/config/azure_resource_config.py
@@ -0,0 +1,10 @@
+from cloud_scanner_azure.config.azure_credential_config import (
+    AzureCredentialConfig)
+
+
+class AzureResourceServiceConfig:
+    """Configuration required for usage of AzureResourceService."""
+
+    def __init__(self, subscription_id, creds: AzureCredentialConfig):
+        self.credentials = creds
+        self.subscription_id = subscription_id
