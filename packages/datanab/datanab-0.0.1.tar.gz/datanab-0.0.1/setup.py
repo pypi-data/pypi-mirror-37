a6eaa42ca3afda1c6f7534590b62e97c5c04f9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:43 +0000

    Add package datanab/0.0.1/datanab-0.0.1.tar.gz

---
+++ packages/datanab/datanab-0.0.1.tar.gz/datanab-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='datanab',
+      version='0.0.1',
+      description='A Python wrapper around the datanab API',
+      long_description=long_description,
+      url='https://github.com/jmfernandes/datanab',
+      author='Josh Fernandes',
+      author_email='joshfernandes@mac.com',
+      keywords=['datanab'],
+      license='MIT',
+      python_requires='>=3',
+      packages=['datanab'],
+      requires=['requests'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
