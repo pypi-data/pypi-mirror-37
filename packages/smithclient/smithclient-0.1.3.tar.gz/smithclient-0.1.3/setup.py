196d4c8b846d9a08a89a9ca7c6235062b833cda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:52 +0000

    Add package smithclient/0.1.3/smithclient-0.1.3.tar.gz

---
+++ packages/smithclient/smithclient-0.1.3.tar.gz/smithclient-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='smithclient',
+      version='0.1.3',
+      description='Monitoring Smith smithclient',
+      url='https://github.com/dangavrilin/monitoring_smithclient.git',
+      author='Denis Gavrilin',
+      author_email='dangavrilin@gmail.com',
+      license='MIT',
+      packages=['smithclient'],
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
