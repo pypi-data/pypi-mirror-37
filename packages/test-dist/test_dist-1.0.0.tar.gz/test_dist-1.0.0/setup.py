13ef2bb7112c5e5e81f46e6b18d45e7a174c8473
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:49 +0000

    Add package test-dist/1.0.0/test_dist-1.0.0.tar.gz

---
+++ packages/test-dist/test_dist-1.0.0.tar.gz/test_dist-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name ='test_dist',
+    version = '1.0.0',
+    py_modules = ['test_dist'],
+    author = 'Kaibo Huang',
+    author_email = 'huangkaibochn@gmail.com',
+    description = 'test distribution',
+)
---
+++ packages/test-dist/test_dist-1.0.0.tar.gz/test_dist-1.0.0/test_dist.py
@@ -0,0 +1,4 @@
+def print_info():
+    print("Hello, my name is Kaibo Huang.")
+    print("I'm from China.")
+    print("My blog is huangkaibo.github.io")
