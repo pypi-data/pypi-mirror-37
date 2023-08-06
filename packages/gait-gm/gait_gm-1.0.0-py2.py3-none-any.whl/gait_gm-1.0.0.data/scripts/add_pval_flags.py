b4ffc5208f401cf27e427bd67e25b62f45e69f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:46 +0000

    Add package gait-gm/1.0.0/gait_gm-1.0.0-py2.py3-none-any.whl

---
+++ packages/gait-gm/gait_gm-1.0.0-py2.py3-none-any.whl/gait_gm-1.0.0.data/scripts/add_pval_flags.py
@@ -0,0 +1,185 @@
+#!python
+######################################################################################
+# AUTHOR: Francisco Huertas <f.huertas@ufl.edu>
+# CONTRIBUTORS: Alison Morse <ammorse@ufl.edu>, Oleksandr Moskalenko <om@rc.ufl.edu>
+#
+# DESCRIPTION: Add flags to a Differential Expression Analysis Dataset.
+# Output shows whether the feature belongs (1) or not (0) to a certain flag.
+#
+# VERSION: 1.0
+#######################################################################################
+import os
+import logging
+import argparse
+import linecache
+from argparse import RawDescriptionHelpFormatter
+import numpy as np
+from numpy import genfromtxt
+import keggPeaModules as modules
+from secimtools.dataManager import logger as sl
+
+
+def getOptions():
+    parser = argparse.ArgumentParser(
+        description="Add Pval Flags", formatter_class=RawDescriptionHelpFormatter
+    )
+    tool = parser.add_argument_group(description="Tool Input")
+    tool.add_argument(
+        "-de",
+        "--deaDataset",
+        dest="deaDataset",
+        action="store",
+        required=True,
+        help="Differential Expression Analysis Datset.",
+    )
+    tool.add_argument(
+        "-id",
+        "--uniqID",
+        dest="uniqID",
+        action="store",
+        required=True,
+        help="Name of the column with unique identifiers.",
+    )
+    tool.add_argument(
+        "-p",
+        "--pvalue",
+        dest="pvalue",
+        action="store",
+        required=True,
+        help="Name of the column with P-values.",
+    )
+    tool.add_argument(
+        "-t",
+        "--thres",
+        dest="thresholds",
+        action="store",
+        required=True,
+        help="P-value thresholds.",
+    )
+    output = parser.add_argument_group(description="Output")
+    output.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        action="store",
+        required=True,
+        help="Output file name.",
+    )
+    output.add_argument(
+        "-fl",
+        "--flags",
+        dest="flags",
+        action="store",
+        required=True,
+        help="Flags file name.",
+    )
+    args = parser.parse_args()
+
+    args.deaDataset = os.path.abspath(args.deaDataset)
+    args.output = os.path.abspath(args.output)
+    args.flags = os.path.abspath(args.flags)
+
+    return args
+
+
+def main():
+    """
+    Add binary flags (0/1) to a differential expression dataset depending on p-value thresholds.
+
+    Arguments:
+        :param deaDataset: Matrix with Differential Expression Analysis information
+        :type deaDataset: file
+
+        :param pvalue: Name of the column with the p-value information
+        :type pvalue: string
+
+        :param uniqid: Name of the column with the unique identifier
+        :type uniqid: string
+
+        :param thresholds: Desired flag thresholds. Must be separed with ",", no spaces allowed.
+        :type thresholds: string
+
+    Returns:
+        :return output: Table with input and added correspondent flags columns
+        :rtype output: file
+
+        :return flags: Table with only the correspondent flags columns
+        :rtype flags: file
+    """
+    args = getOptions()
+    logger = logging.getLogger()
+    sl.setLogger(logger)
+    logger.info(
+        u"""Importing data with following parameters: \
+        \n\tDEA Dataset: {0}\
+        \n\tUnique ID: {1}\
+        \n\tPvalues: {2}\
+        \n\tThresholds: {3}""".format(
+            args.deaDataset, args.uniqID, args.pvalue, args.thresholds
+        )
+    )
+
+    modules.checkForDuplicates(args.deaDataset, args.uniqID)
+
+    output = open(args.output, "w")
+    flags = open(args.flags, "w")
+
+    with open(args.deaDataset, "r") as data:
+        header = data.readline().strip().split("\t")
+
+    thresholds = args.thresholds.split(",")
+
+    header_list = []
+    for word in header:
+        if word == "":
+            output.write("NA")
+            header_list.append("NA")
+        elif header.index(word) == len(header) - 1:
+            word = word.replace('"', "")
+            output.write(word)
+            header_list.append(word)
+        else:
+            word = word.replace('"', "")
+            output.write(word + "\t")
+            header_list.append(word)
+
+    flags.write(str(args.uniqID))
+    for threshold in thresholds:
+        flags.write("\tFlag_" + threshold)
+        output.write("\tFlag_" + threshold)
+        header_list.append("\tFlag_" + threshold)
+
+    flags.write("\n")
+    output.write("\n")
+    # Get P value column from a DEA dataset
+    deaTable = genfromtxt(
+        args.deaDataset,
+        delimiter="\t",
+        usecols=header_list.index(args.pvalue),
+        dtype=None,
+    )
+    deaTable = np.delete(deaTable, 0)
+
+    # Add 1/0 if smaller/greater than threshold
+    i = 2
+    for pvalue in deaTable:
+        line = linecache.getline(args.deaDataset, i).strip()
+        pvalue = float(pvalue.strip())
+        flags.write(line.split("\t")[header_list.index(args.uniqID)])
+        output.write(line)
+        for threshold in thresholds:
+            if pvalue <= float(threshold):
+                flags.write("\t1")
+                output.write("\t1")
+            else:
+                flags.write("\t0")
+                output.write("\t0")
+        flags.write("\n")
+        output.write("\n")
+        i += 1
+
+    return args
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/gait-gm/gait_gm-1.0.0-py2.py3-none-any.whl/gait_gm-1.0.0.data/scripts/ensembl2symbol.py
@@ -0,0 +1,142 @@
+#!python
+######################################################################################
+# AUTHOR: Francisco Huertas <f.huertas@ufl.edu>
+# CONTRIBUTORS: Alison Morse <ammorse@ufl.edu>, Oleksandr Moskalenko <om@rc.ufl.edu>
+#
+# DESCRIPTION: Take a Gene Expression Annotation File with ENSEMBL IDs and return an Annotation
+# File with Gene Symbols
+#
+# VERSION: 1.0
+#######################################################################################
+import os
+import logging
+import argparse
+from argparse import RawDescriptionHelpFormatter
+import mygene
+import pandas as pd
+import keggPeaModules as modules
+from secimtools.dataManager import logger as sl
+
+
+def getOptions():
+    parser = argparse.ArgumentParser(
+        description="kegg_anno", formatter_class=RawDescriptionHelpFormatter
+    )
+    tool = parser.add_argument_group(title="Tool Specific Inputs")
+    tool.add_argument(
+        "-s",
+        "--species",
+        dest="species",
+        action="store",
+        required=True,
+        help="Species to download. One of rat, human, mouse or fruitfly",
+    )
+    tool.add_argument(
+        "-ga",
+        "--geneAnnot",
+        dest="geneAnnot",
+        action="store",
+        required=True,
+        help="Gene Expression Annotation File.",
+    )
+    tool.add_argument(
+        "-id",
+        "--uniqId",
+        dest="uniqId",
+        action="store",
+        required=True,
+        help="Name of the column with gene Unique Ids.",
+    )
+    tool.add_argument(
+        "-e",
+        "--ensemblId",
+        dest="ensemblId",
+        action="store",
+        required=True,
+        help="Name of the column with ENSEMBL IDs.",
+    )
+    output = parser.add_argument_group(description="Output")
+    output.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        action="store",
+        required=True,
+        help="Gene expression Annotation File with Gene Symbols.",
+    )
+
+    args = parser.parse_args()
+
+    args.geneAnnot = os.path.abspath(args.geneAnnot)
+    args.output = os.path.abspath(args.output)
+
+    return args
+
+
+def main():
+    """
+    Take a gene expression matrix and extract the column with ENSEMBL IDs. Then, translate ENSEMBL
+    IDs into Gene_Symbol needed for the rest of the pipeline. Create a table with Unique
+    Identifiers, ENSEMBL IDs, gene symbols, match scores, and selection resul, which is useful in
+    cases of multiple matches.
+
+    Arguments:
+        :param species: Species to download information from mygene
+        :type species: string
+
+        :param geneAnnot: Gene Expression Annotation file with ENSEMBL IDs column
+        :type geneAnnot: file
+
+        :param ensemblId: Name of the column with ENSEMBL IDs
+        :type ensemblId: string
+    """
+
+    args = getOptions()
+    logger = logging.getLogger()
+    sl.setLogger(logger)
+    logger.info(
+        u"""Importing data with following parameters: \
+        \n\tSpecies: {0}\
+        \n\tGene Annotation File: {1}\
+        \n\tUnique ID column: {2}\
+        \n\tENSEMBL ID Column: {3}""".format(
+            args.species, args.geneAnnot, args.uniqId, args.ensemblId
+        )
+    )
+    modules.checkForDuplicates(args.geneAnnot, args.uniqId)
+
+    # Original Gene Expression Annotation Dataset with ENSEMBL IDs
+    genesTable = pd.read_table(args.geneAnnot, delimiter="\t", header=0)
+
+    # Find Gene Symbol
+    mg = mygene.MyGeneInfo()
+    genes = genesTable[args.ensemblId].tolist()
+    genesTransformed = mg.querymany(
+        genes,
+        scopes="ensembl.gene",
+        fields="symbol",
+        species=args.species,
+        verbose=False,
+        returnall=True,
+        as_dataframe=True,
+        df_index=False,
+    )
+    genesTransformedTable = genesTransformed["out"]
+    genesTransformedTable.drop(labels=["_id"], axis=1, inplace=True)
+    genesTransformedTable = genesTransformedTable[["query", "symbol", "_score"]]
+    genesTransformedTable.columns = [args.ensemblId, "GeneSymbol", "Score"]
+
+    # Merge Both datasets
+    newGenesTable = pd.merge(genesTable, genesTransformedTable, on=args.ensemblId)
+
+    # In case of duplicated, select the first one (High score)
+    newGenesTable["Selected"] = "Yes"
+    isDup = newGenesTable.duplicated(subset=args.ensemblId, keep="first")
+    newGenesTable["Selected"][isDup] = "No"
+
+    # Write table
+    newGenesTable.to_csv(args.output, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    main()
