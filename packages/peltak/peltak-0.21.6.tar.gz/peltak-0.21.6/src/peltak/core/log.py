eabe0e96a230b547454f14740a25b95ecdb378f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:51 +0000

    Add package peltak/0.21.6/peltak-0.21.6.tar.gz

---
+++ packages/peltak/peltak-0.21.6.tar.gz/peltak-0.21.6/src/peltak/core/log.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Helpers for nice shell output formatting.
+"""
+from __future__ import absolute_import, unicode_literals
+
+# stdlib imports
+from typing import Any
+
+# local imports
+from . import shell
+
+
+def info(msg, *args, **kw):
+    # type: (str, *Any, **Any) -> None
+    """ Print sys message to stdout.
+
+    System messages should inform about the flow of the script. This should
+    be a major milestones during the build.
+    """
+    if len(args) or len(kw):
+        msg = msg.format(*args, **kw)
+
+    shell.cprint('-- <32>{}<0>'.format(msg))
+
+
+def err(msg, *args, **kw):
+    # type: (str, *Any, **Any) -> None
+    """ Per step status messages
+
+    Use this locally in a command definition to highlight more important
+    information.
+    """
+    if len(args) or len(kw):
+        msg = msg.format(*args, **kw)
+
+    shell.cprint('-- <31>{}<0>'.format(msg))
+
+
+# Used in docstrings only until we drop python2 support
+del Any
