e733ed9fbed5ee5090ef6e42b2a7b15802bfeb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:34 +0000

    Add package ansible-run/3.4/ansible-run-3.4.tar.gz

---
+++ packages/ansible-run/ansible-run-3.4.tar.gz/ansible-run-3.4/setup.py
@@ -0,0 +1,12 @@
+
+from subprocess import *
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name = 'ansible-run', version = '3.4',
+    install_requires=['yq','jq'],
+    url = 'https://www/github.com/moshloop/ansible-run',
+    author = 'Moshe Immerman', author_email = 'moshe.immerman@gmail.com',
+    scripts = ['ansible-run', 'ansible-test', 'ansible-role', 'ansible-vault-run']
+    )
