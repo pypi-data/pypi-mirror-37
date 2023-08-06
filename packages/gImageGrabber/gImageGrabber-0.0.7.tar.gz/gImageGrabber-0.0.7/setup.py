ba9569e1e532c5ba840a7ed2167a81bc4ac2b57e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:23 +0000

    Add package gImageGrabber/0.0.7/gImageGrabber-0.0.7.tar.gz

---
+++ packages/gImageGrabber/gImageGrabber-0.0.7.tar.gz/gImageGrabber-0.0.7/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+setup(
+    name='gImageGrabber',
+    version='0.0.7',
+    author='Saksham Sharma',
+    author_email='codeck313@gmail.com',
+    packages=['gimagegrabber', 'gimagegrabber.test'],
+    scripts=['bin/simpleScript.py'],
+    url='https://pypi.org/project/gImageGrabber',
+    project_url={
+        'Test Site': 'https://mysnappy.weebly.com/',
+    },
+    license='GNU General Public License version 3',
+    description='Tool to download orignal resolution images from Google search',
+    long_description=open('README.txt').read(),
+    keywords="google images extractor parser webpage gImageGrabber grabber extract",
+    install_requires=[
+        "urllib3 == 1.23",
+        "beautifulsoup4==4.6.3",
+        "selenium==3.14.1",
+
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Utilities",
+
+    ],
+)
