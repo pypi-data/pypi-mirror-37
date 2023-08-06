4ca5630fe2df3609dcb03aec31ad9f956a7e17fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:37 +0000

    Add package folderspy/0.0.5/folderspy-0.0.5.tar.gz

---
+++ packages/folderspy/folderspy-0.0.5.tar.gz/folderspy-0.0.5/folderspy/windows/spy.py
@@ -0,0 +1,27 @@
+import queue
+
+from .watcher import FolderWatcher
+from folderspy.spy import Spy
+
+
+class FolderSpy(Spy):
+
+    def _setup_watchers(self, folders):
+        """Setup watchers for all folders."""
+        
+        return {}
+
+    def _event_loop(self, folders, **kwargs):
+        """Kicks off the event loop that begins watching for events."""
+
+        event_queue = queue.Queue()
+        for folder in folders:
+            FolderWatcher(folder, queue=event_queue)
+
+        while True:
+            try:
+                folder, events = event_queue.get_nowait()
+                for event in events:
+                    getattr(folder, 'process_{0}'.format(event.maskname))(event)
+            except queue.Empty:
+                pass
