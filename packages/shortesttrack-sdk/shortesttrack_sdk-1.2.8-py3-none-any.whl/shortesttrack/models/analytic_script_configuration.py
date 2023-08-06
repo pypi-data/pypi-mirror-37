d1af2ef0a1e588d99d4dd57d8f0ed6aa29838e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:03 +0000

    Add package shortesttrack-sdk/1.2.8/shortesttrack_sdk-1.2.8-py3-none-any.whl

---
+++ packages/shortesttrack-sdk/shortesttrack_sdk-1.2.8-py3-none-any.whl/shortesttrack/models/analytic_script_configuration.py
@@ -0,0 +1,28 @@
+from shortesttrack.client import ISSConnector
+from shortesttrack.models.base.model import Model
+from shortesttrack.utils import getLogger
+
+logger = getLogger()
+
+
+class ISSConnectionInfo:
+    # TODO: move to a separate model
+    def __init__(self, info: dict) -> None:
+        self.url = info.get('url')
+        self.url = self.url if self.url[-1] != '/' else self.url[0:-1]
+        self.auth_custom_token = info.get('auth_custom_token')
+        self.id = info.get('iscript_service_id')
+
+
+class AnalyticScriptConfiguration(Model):
+    def __str__(self):
+        return f'ASEC({self.id})'
+
+    def get_iss_connection_info(self) -> list:
+        return [ISSConnectionInfo(info) for info in self.metadata['relations']]
+
+    @staticmethod
+    def get_iss_connector(connection_info: ISSConnectionInfo) -> ISSConnector:
+        return ISSConnector(
+            url=connection_info.url, auth_custom_token=connection_info.auth_custom_token
+        )
