0a90fa4ae0ad9586b528f806db3db9573e85e0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:29 +0000

    Add package apitaxdrivers/0.0.10/apitaxdrivers-0.0.10.tar.gz

---
+++ packages/apitaxdrivers/apitaxdrivers-0.0.10.tar.gz/apitaxdrivers-0.0.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='apitaxdrivers',
+    packages=find_packages(),  # this must be the same as the name above
+    version='0.0.10',
+    description='A set of example and micro drivers which can be used as the basis of more robust API drivers or combined together to form a robust application.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Shawn Clake',
+    author_email='shawn.clake@gmail.com',
+    url='https://github.com/ShawnClake/Apitax',  # use the URL to the github repo
+    keywords=['restful', 'api', 'commandtax', 'scriptax', 'apitax', 'drivers', 'plugins'],  # arbitrary keywords
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'requests',
+        'python-gitlab',
+        'pygithub',
+        'gitpython',
+        'apitaxcore==3.0.6',
+        'commanndtax==0.0.8',
+        'scriptax==0.0.4',
+    ],
+)
