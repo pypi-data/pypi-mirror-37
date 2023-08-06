29bc874b116d069348bd9af5fda9fa7af467bc36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:52 +0000

    Add package resync/1.0.9/resync-1.0.9.tar.gz

---
+++ packages/resync/resync-1.0.9.tar.gz/resync-1.0.9/resync/__init__.py
@@ -0,0 +1,15 @@
+"""Module config for resync."""
+
+from resync._version import __version__
+
+# Enable easy import for core classes, e.g.
+# from resync import Resource
+from resync.source_description import SourceDescription
+from resync.capability_list import CapabilityList
+from resync.resource_list import ResourceList
+from resync.change_list import ChangeList
+from resync.resource_dump import ResourceDump
+from resync.resource_dump_manifest import ResourceDumpManifest
+from resync.change_dump import ChangeDump
+from resync.archives import ResourceListArchive, ResourceDumpArchive, ChangeListArchive, ChangeDumpArchive
+from resync.resource import Resource
