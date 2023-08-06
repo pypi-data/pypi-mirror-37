26dd00b5a6c14f59251e6cc65e3d3ad65a441f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:22 +0000

    Add package collective.monkeypatcher/1.1.6/collective.monkeypatcher-1.1.6.tar.gz

---
+++ packages/collective.monkeypatcher/collective.monkeypatcher-1.1.6.tar.gz/collective.monkeypatcher-1.1.6/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '1.1.6'
+
+setup(
+    name='collective.monkeypatcher',
+    version=version,
+    description=("Support for applying monkey patches late in the startup "
+                 "cycle by using ZCML configuration actions"),
+    long_description='\n'.join([
+        open("README.rst").read(),
+        open("CHANGES.rst").read()]),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        ],
+    keywords='zope monkey patch',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://github.com/plone/collective.monkeypatcher',
+    license='BSD',
+    packages=find_packages(),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'six',
+    ],
+    )
