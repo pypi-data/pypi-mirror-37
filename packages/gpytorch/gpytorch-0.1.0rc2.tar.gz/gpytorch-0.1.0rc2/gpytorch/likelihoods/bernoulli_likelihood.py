9ecdf148fea2f2da87012f9f19ea30f1deb9d232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:52 +0000

    Add package gpytorch/0.1.0rc2/gpytorch-0.1.0rc2.tar.gz

---
+++ packages/gpytorch/gpytorch-0.1.0rc2.tar.gz/gpytorch-0.1.0rc2/gpytorch/likelihoods/bernoulli_likelihood.py
@@ -0,0 +1,66 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import torch
+from torch.distributions import Bernoulli
+
+from .. import settings
+from ..distributions import MultivariateNormal
+from ..functions import log_normal_cdf, normal_cdf
+from .likelihood import Likelihood
+
+
+class BernoulliLikelihood(Likelihood):
+    """
+    Implements the Bernoulli likelihood used for GP classification, using
+    Probit regression (i.e., the latent function is warped to be in [0,1]
+    using the standard Normal CDF \Phi(x)). Given the identity \Phi(-x) =
+    1-\Phi(x), we can write the likelihood compactly as:
+
+    p(Y=y|f)=\Phi(yf)
+    """
+
+    def forward(self, input):
+        """
+        Computes predictive distributions p(y|x) given a latent distribution
+        p(f|x). To do this, we solve the integral:
+
+            p(y|x) = \int p(y|f)p(f|x) df
+
+        Given that p(y=1|f) = \Phi(f), this integral is analytically tractable,
+        and if \mu_f and \sigma^2_f are the mean and variance of p(f|x), the
+        solution is given by:
+
+            p(y|x) = \Phi(\frac{\mu}{\sqrt{1+\sigma^2_f}})
+        """
+        if not isinstance(input, MultivariateNormal):
+            raise RuntimeError(
+                "BernoulliLikelihood expects a multi-variate normally distributed latent function to make predictions"
+            )
+
+        mean = input.mean
+        var = input.variance
+        link = mean.div(torch.sqrt(1 + var))
+        output_probs = normal_cdf(link)
+        return Bernoulli(probs=output_probs)
+
+    def variational_log_probability(self, latent_func, target):
+        """
+        Computes the log probability
+
+            \sum_{i} \log \Phi(y_{i}f_{i}),
+
+        where \Phi(y_{i}f_{i}) is computed by averaging over a set of s samples
+        of f_{i} drawn from p(f|x).
+        """
+        num_samples = settings.num_likelihood_samples.value()
+        samples = latent_func.rsample(torch.Size([num_samples])).view(-1)
+        target = target.unsqueeze(0).repeat(num_samples, 1).view(-1)
+        return log_normal_cdf(samples.mul(target)).sum().div(num_samples)
+
+    def pyro_sample_y(self, variational_dist_f, y_obs, sample_shape, name_prefix=""):
+        import pyro
+
+        f_samples = variational_dist_f(sample_shape)
+        y_prob_samples = torch.distributions.Normal(0, 1).cdf(f_samples)
+        y_dist = pyro.distributions.Bernoulli(y_prob_samples)
+        pyro.sample(name_prefix + "._training_labels", y_dist.independent(1), obs=y_obs)
---
+++ packages/gpytorch/gpytorch-0.1.0rc2.tar.gz/gpytorch-0.1.0rc2/gpytorch/likelihoods/softmax_likelihood.py
@@ -0,0 +1,76 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import torch
+from torch.distributions import Categorical
+
+from .. import settings
+from ..distributions import MultivariateNormal
+from .likelihood import Likelihood
+
+
+class SoftmaxLikelihood(Likelihood):
+    """
+    Implements the Softmax (multiclass) likelihood used for GP classification.
+    """
+
+    def __init__(self, num_features, n_classes, mixing_weights_prior=None):
+        super(SoftmaxLikelihood, self).__init__()
+        self.num_features = num_features
+        self.n_classes = n_classes
+        self.register_parameter(
+            name="mixing_weights",
+            parameter=torch.nn.Parameter(torch.ones(n_classes, num_features).fill_(1.0 / num_features)),
+            prior=mixing_weights_prior,
+        )
+
+    def forward(self, latent_func):
+        """
+        Computes predictive distributions p(y|x) given a latent distribution
+        p(f|x). To do this, we solve the integral:
+
+            p(y|x) = \int p(y|f)p(f|x) df
+
+        Given that p(y=1|f) = \Phi(f), this integral is analytically tractable,
+        and if \mu_f and \sigma^2_f are the mean and variance of p(f|x), the
+        solution is given by:
+
+            p(y|x) = \Phi(\frac{\mu}{\sqrt{1+\sigma^2_f}})
+        """
+        if not isinstance(latent_func, MultivariateNormal):
+            raise RuntimeError(
+                "SoftmaxLikelihood expects a multi-variate normally distributed latent function to make predictions"
+            )
+
+        n_samples = settings.num_likelihood_samples.value()
+        samples = latent_func.rsample(sample_shape=torch.Size((n_samples,)))
+        samples = samples.permute(1, 2, 0).contiguous()  # Now n_featuers, n_data, n_samples
+        if samples.ndimension() != 3:
+            raise RuntimeError("f should have 3 dimensions: features x data x samples")
+        num_features, n_data, _ = samples.size()
+        if num_features != self.num_features:
+            raise RuntimeError("There should be %d features" % self.num_features)
+
+        mixed_fs = self.mixing_weights.matmul(samples.view(num_features, n_samples * n_data))
+        softmax = torch.nn.functional.softmax(mixed_fs.t(), 1).view(n_data, n_samples, self.n_classes)
+        return Categorical(probs=softmax.mean(1))
+
+    def variational_log_probability(self, latent_func, target):
+        """
+        Computes the log probability \sum_{i} \log \Phi(y_{i}f_{i}), where
+        \Phi(y_{i}f_{i}) is computed by averaging over a set of s samples of
+        f_{i} drawn from p(f|x).
+        """
+        n_samples = settings.num_likelihood_samples.value()
+        samples = latent_func.rsample(sample_shape=torch.Size((n_samples,)))
+        samples = samples.permute(1, 2, 0).contiguous()  # Now n_featuers, n_data, n_samples
+        if samples.ndimension() != 3:
+            raise RuntimeError("f should have 3 dimensions: features x data x samples")
+        num_features, n_data, _ = samples.size()
+        if num_features != self.num_features:
+            raise RuntimeError("There should be %d features" % self.num_features)
+
+        mixed_fs = self.mixing_weights.matmul(samples.view(num_features, n_samples * n_data))
+        log_prob = -torch.nn.functional.cross_entropy(
+            mixed_fs.t(), target.unsqueeze(1).repeat(1, n_samples).view(-1), reduction="sum"
+        )
+        return log_prob.div(n_samples)
