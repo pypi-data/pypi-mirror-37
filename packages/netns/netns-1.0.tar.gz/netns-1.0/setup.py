a3cd8149beac27edfa796b87b231db010a385d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:54 +0000

    Add package netns/1.0/netns-1.0.tar.gz

---
+++ packages/netns/netns-1.0.tar.gz/netns-1.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    author='Lars Kellogg-Stedman',
+    author_email='lars@oddbit.com',
+    url='http://github.com/larsks/python-netns',
+    description='Wrapper for the Linux setns() system call',
+    name='netns',
+    license='GPLv3',
+    py_modules=['netns'],
+    version='1.0')
