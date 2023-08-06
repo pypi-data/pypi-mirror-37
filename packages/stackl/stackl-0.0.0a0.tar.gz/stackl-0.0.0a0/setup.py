a0fe763b5a39e39db1aa5008dd82219cbca35fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:27 +0000

    Add package stackl/0.0.0a0/stackl-0.0.0a0.tar.gz

---
+++ packages/stackl/stackl-0.0.0a0.tar.gz/stackl-0.0.0a0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stackl",
+    version="0.0.0a",
+    author="ArtOfCode",
+    author_email="hello@artofcode.co.uk",
+    description="Python library for connecting to Stack Exchange chat",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArtOfCode-/stat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
