4cae48b2494e84d878723cdf895c7ee406a30a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:04 +0000

    Add package daybit/0.0.2/daybit-0.0.2.tar.gz

---
+++ packages/daybit/daybit-0.0.2.tar.gz/daybit-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='daybit',
+      version='0.0.2',
+      description='an API wrapper for Daybit-exchange',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: Apache Software License",
+      ],
+      author='Daybit Developers',
+      url='https://github.com/daybit-exchange/pydaybit',
+      packages=['pydaybit'],
+      setup_requires=[
+          'pytest-runner',
+      ],
+      install_requires=[
+          'async_timeout',
+          'furl',
+          'websockets',
+
+          # Examples
+          'dateparser',
+          'pandas',
+          'numpy',
+          'tabulate',
+          'pytz',
+      ],
+      tests_require=[
+          'pytest',
+          'pytest-asyncio',
+      ],
+      zip_safe=False)
