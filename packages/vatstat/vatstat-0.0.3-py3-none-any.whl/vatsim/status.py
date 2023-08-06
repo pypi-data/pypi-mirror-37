be09d879b94ac8ce323a8741598a039aeb7c30a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:12 +0000

    Add package vatstat/0.0.3/vatstat-0.0.3-py3-none-any.whl

---
+++ packages/vatstat/vatstat-0.0.3-py3-none-any.whl/vatsim/status.py
@@ -0,0 +1,37 @@
+import http.client
+import random
+
+VATSIM_STATUS_HOST = "status.vatsim.net"
+
+
+def choose_server():
+    return random.choice(get_status_servers())
+
+
+def get_status_servers():
+    """
+    get a set of all available status servers live
+
+    heads up, vatsim gets angry if you hit this too often, so make sure you cache the result on your side
+
+    :return `set` of possible URLs that can be fetched to get the wazzup:
+    """
+    conn = http.client.HTTPSConnection(VATSIM_STATUS_HOST)
+
+    headers = {
+        'Cache-Control': "no-cache",
+        'User-Agent': "vatstat python library"
+    }
+
+    conn.request("GET", "", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    possible_servers = set()
+
+    for line in iter(data.splitlines()):
+        if line.startswith("url0="):
+            possible_servers.add(line.split("=")[1])
+
+    return possible_servers
