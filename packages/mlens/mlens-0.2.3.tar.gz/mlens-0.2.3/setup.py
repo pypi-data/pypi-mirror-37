5aa1558f341217427a8dcf831a6813c3bc5df16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:03 +0000

    Add package mlens/0.2.3/mlens-0.2.3.tar.gz

---
+++ packages/mlens/mlens-0.2.3.tar.gz/mlens-0.2.3/setup.py
@@ -0,0 +1,56 @@
+"""ML-ENSEMBLE
+
+:author: Sebastian Flennerhag
+:copyright: 2017-2018
+:license: MIT
+
+ML-Ensemble - a library for Ensemble Learning
+"""
+
+from setuptools import setup, find_packages
+import mlens
+
+VERSION = mlens.__version__
+
+setup(name='mlens',
+      version=VERSION,
+      description='Machine Learning Ensemble Library',
+      author='Sebastian Flennerhag',
+      author_email='sebastianflennerhag@hotmail.com',
+      url='https://github.com/flennerhag/mlens',
+      packages=find_packages(),
+      package_data={'': ['LICENSE.txt',
+                         'README.md',
+                         'requirements.txt'
+                         ]
+                    },
+      include_package_data=True,
+      install_requires=['numpy>=1.11',
+                        'scipy>=0.17'],
+      license='MIT',
+      platforms='any',
+      classifiers=['License :: OSI Approved :: MIT License',
+                   'Development Status :: 4 - Beta',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Scientific/Engineering :: Artificial Intelligence'
+                   ],
+      long_description="""
+A library for memory efficient parallelized Ensemble learning
+
+Documentation available at:
+    - http://mlens.readthedocs.io/en/latest/
+    - https://github.com/flennerhag/mlens
+
+Contact
+=======
+For questions and comments reach out to sebastianflennerhag@hotmail.com.
+
+This project is hosted at https://github.com/flennerhag/mlens
+""")
