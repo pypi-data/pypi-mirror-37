f891895cbeae81126288e5faf1020b924d90dc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:44 +0000

    Add package nester_DJuR/1.1.1/nester_DJuR-1.1.1.tar.gz

---
+++ packages/nester_DJuR/nester_DJuR-1.1.1.tar.gz/nester_DJuR-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'nester_DJuR',
+    version = '1.1.1',
+    py_modules = ['nester'],
+    author_email = 'ding@d.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
