0ef6d58e84f9a70fdcb4d9a6413b1b4e2f0f1e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:36 +0000

    Add package intermod-library/0.1.0/intermod-library-0.1.0.tar.gz

---
+++ packages/intermod-library/intermod-library-0.1.0.tar.gz/intermod-library-0.1.0/README.rst
@@ -0,0 +1,22 @@
+=========================
+Intermod Libray
+=========================
+
+Tools for finding and viewing intermodulation products and harmonics
+====================================================================
+
+Source can be found at:
+https://github.com/wboxx1/intermod-library
+
+Documentation can be found at:
+https://wboxx1.github.io/intermod-library/docs/build/html/index.html
+
+Installation:
+--------------
+
+Pip:
+
+.. code-block:: pypi
+
+    pip install intermod-library
+
---
+++ packages/intermod-library/intermod-library-0.1.0.tar.gz/intermod-library-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['intermod_library']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.0,<4.0',
+ 'numpy>=1.15,<2.0',
+ 'pandas>=0.23,<0.24',
+ 'scipy>=1.1,<2.0']
+
+setup_kwargs = {
+    'name': 'intermod-library',
+    'version': '0.1.0',
+    'description': 'A library of tools for finding and viewing intermodulation products and harmonics',
+    'long_description': '=========================\nIntermod Libray\n=========================\n\nTools for finding and viewing intermodulation products and harmonics\n====================================================================\n\nSource can be found at:\nhttps://github.com/wboxx1/intermod-library\n\nDocumentation can be found at:\nhttps://wboxx1.github.io/intermod-library/docs/build/html/index.html\n\nInstallation:\n--------------\n\nPip:\n\n.. code-block:: pypi\n\n    pip install intermod-library\n\n',
+    'author': 'Will Boxx',
+    'author_email': 'wboxx1@gmail.com',
+    'url': 'https://wboxx1.github.io/intermod-library/build/html/index.html',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
