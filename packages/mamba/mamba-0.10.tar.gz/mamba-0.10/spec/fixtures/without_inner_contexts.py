b3c52e9aa3978ac963f376c24a28637ef4f19d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:00 +0000

    Add package mamba/0.10/mamba-0.10.tar.gz

---
+++ packages/mamba/mamba-0.10.tar.gz/mamba-0.10/spec/fixtures/without_inner_contexts.py
@@ -0,0 +1,11 @@
+from mamba import description, it
+
+with description('Fixture#without_inner_contexts'):
+    with it('first example'):
+        pass
+
+    with it('second example'):
+        pass
+
+    with it('third example'):
+        pass
