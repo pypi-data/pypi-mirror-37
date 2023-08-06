50da4118004433338141fccec83ae9f7137452c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:29 +0000

    Add package pymortar/0.0.2/pymortar-0.0.2.tar.gz

---
+++ packages/pymortar/pymortar-0.0.2.tar.gz/pymortar-0.0.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymortar",
+    version="0.0.2",
+    author="Gabe Fierro",
+    author_email="gtfierro@cs.berkeley.edu",
+    description="Python3 Mortar",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mortar-frontend",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
