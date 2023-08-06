6198699b5aa0232512649ea5ef5d4677bc22b8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:13 +0000

    Add package esst/2018.10.25.1/esst-2018.10.25.1-py36-none-any.whl

---
+++ packages/esst/esst-2018.10.25.1-py36-none-any.whl/esst/discord_bot/logging_handler.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+"""
+Install a handler to redirect all INFO messages (and higher) to the Discord Channel
+"""
+
+import logging
+
+from esst import LOGGER
+from esst.commands import DISCORD
+
+
+class DiscordLoggingHandler(logging.Handler):
+    """
+    Install a handler to redirect all INFO messages (and higher) to the Discord Channel
+    """
+
+    def __init__(self):
+        logging.Handler.__init__(self, logging.INFO)
+
+    def emit(self, record: logging.LogRecord):
+        """
+        Redirects the record to the Discord channel if its level is INFO or higher
+
+        Args:
+            record: logging.record to emit
+        """
+        message = self.format(record)
+        DISCORD.say(str(message))
+
+
+def register_logging_handler():
+    """
+    Installs the handler to the main logger
+    """
+    LOGGER.debug('registering Discord logging handler')
+    LOGGER.addHandler(DiscordLoggingHandler())
