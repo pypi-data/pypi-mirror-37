05a59112eec82b0a28dee3666f49ec3cb5f66a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:40 +0000

    Add package qComputing/1.7.1/qComputing-1.7.1.tar.gz

---
+++ packages/qComputing/qComputing-1.7.1.tar.gz/qComputing-1.7.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='qComputing',
+    version='1.7.1',
+    py_modules=['Data', 'Gates', 'NoisyEvolution', 'Operations', 'QCircuit', 'qubit_class', 'SpecialStates', 'Graph'],
+    description='A package which deals with light weight routine quantum computing stuff',
+    author='Amara Katabarwa',
+    author_email="Henriamaa@gmail.com",
+    url='https://github.com/akataba/qComputing',
+    classifiers=[
+                  "Programming Language :: Python :: 3",
+                  "License :: OSI Approved :: MIT License",
+                  "Operating System :: OS Independent",
+              ],
+    )
