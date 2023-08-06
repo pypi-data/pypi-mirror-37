20f3cf0c15526a895c5567879fa217c601ab048e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:53 +0000

    Add package flask-fantasy/0.2.12/flask-fantasy-0.2.12.tar.gz

---
+++ packages/flask-fantasy/flask-fantasy-0.2.12.tar.gz/flask-fantasy-0.2.12/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+version = "0.2.12"
+
+setup(
+    name='flask-fantasy',
+    version=version,
+    packages=find_packages(exclude=["tests"]),
+    url='https://github.com/wangwenpei/fantasy',
+    download_url='https://github.com/wangwenpei/fantasy/tarball/master',
+    license='MIT',
+    author='WANG WENPEI',
+    zip_safe=False,
+    test_suite="tests",
+    author_email='stormxx@1024.engineer',
+    description='A bootstrap tool for Flask APP',
+    keywords='fantasy,flask',
+)
