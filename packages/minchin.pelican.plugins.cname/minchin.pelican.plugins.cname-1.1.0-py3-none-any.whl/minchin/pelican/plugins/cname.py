c05bf854461955277ecbeb597f1ee3204d578a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:10 +0000

    Add package minchin.pelican.plugins.cname/1.1.0/minchin.pelican.plugins.cname-1.1.0-py3-none-any.whl

---
+++ packages/minchin.pelican.plugins.cname/minchin.pelican.plugins.cname-1.1.0-py3-none-any.whl/minchin/pelican/plugins/cname.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*- #
+"""
+CName
+=============
+
+This plugin for Pelican add a *CNAME* file with contents based on ``SITEURL``.
+"""
+
+import os
+
+from pelican import signals
+
+__title__ = "minchin.pelican.plugins.cname"
+__version__ = "1.1.0"
+__description__ = "Pelican plugin that adds a `CNAME` file to the output root. Useful for publishing to Github Pages. Written in Python."
+__author__ = "William Minchin"
+__email__ = "w_minchin@hotmail.com"
+__url__ = "https://github.com/MinchinWeb/minchin.pelican.plugins.cname"
+__license__ = "AGPL-3.0"
+
+
+def add_cname(p):
+    """
+    :param p: pelican instance
+    :return: None
+    """
+
+    cname_path = os.path.join(p.output_path, 'CNAME')
+    siteurl = p.settings.get('SITEURL', "127.0.0.1:8000")
+    siteurl = siteurl.replace("http://", "")
+    siteurl = siteurl.replace("https://", "")
+    with open(cname_path, 'w') as cname_file:
+        cname_file.write(siteurl)
+
+
+def register():
+    signals.finalized.connect(add_cname)
