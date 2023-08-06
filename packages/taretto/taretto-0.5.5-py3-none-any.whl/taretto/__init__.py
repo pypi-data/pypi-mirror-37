f8c091764172a4789101e0a94875ed0802f12b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:23 +0000

    Add package taretto/0.5.5/taretto-0.5.5-py3-none-any.whl

---
+++ packages/taretto/taretto-0.5.5-py3-none-any.whl/taretto/__init__.py
@@ -0,0 +1,14 @@
+from apipkg import initpkg as __initpkg
+
+# fmt: off
+__initpkg(__name__, {
+    "navigate": "navmazing",
+    "ui": {
+        "core": "widgetastic.widget",  # todo: html widget library here, limit to base
+        "patternfly": "widgetastic_patternfly",  # dito
+
+        "View": "widgetastic.widget:View",
+        "Browser": "widgetastic.browser:Browser",
+        "DefaultPlugin": "widgetastic.browser:DefaultPlugin"
+    },
+})
