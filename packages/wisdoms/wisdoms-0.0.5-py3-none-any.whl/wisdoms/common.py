c5b4732401654cf7722575ba11785cc452dcc8cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:39 +0000

    Add package wisdoms/0.0.5/wisdoms-0.0.5-py3-none-any.whl

---
+++ packages/wisdoms/wisdoms-0.0.5-py3-none-any.whl/wisdoms/common.py
@@ -0,0 +1,26 @@
+# Created by Q-ays.
+# whosqays@gmail.com
+
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
+err_num = {
+    "OK": 0,
+    "DATAERR": 1,
+    "DSANF": 2,
+    "PARM": 3,
+    "PARGERR": 4,
+    "NOadmin": 5,
+    "NOAUTH": 6
+}
+
+err_msg = {
+    "OK": "OK",
+    "DATAERR": "数据库错误",
+    "DSANF": "第三方服务错误",
+    "PARM": "参数错误",
+    "PARGERR": "数据错误",
+    "NOadmin": "被管理者",
+    "NOAUTH": "没有权限"
+}
