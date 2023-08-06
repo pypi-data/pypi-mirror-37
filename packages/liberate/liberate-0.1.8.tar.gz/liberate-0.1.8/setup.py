b40b95c3701f4b8774f225004f9e1eea5c9c928c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:40 +0000

    Add package liberate/0.1.8/liberate-0.1.8.tar.gz

---
+++ packages/liberate/liberate-0.1.8.tar.gz/liberate-0.1.8/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+setup(
+  name='liberate',
+  version='0.1.8',
+  description='Video/Audio/Url liberation (to .ogg)',
+  author='Nichlas Severinsen',
+  author_email='ns@nsz.no',
+  url='https://notabug.org/necklace/liberate',
+  packages=['liberate'],
+  scripts=['liberate/liberate'],
+  install_requires=["youtube_dl", "colorama"]
+)
