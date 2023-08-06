3c69c7fa76f868fa72427b10fedbef38a97d2179
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:28 +0000

    Add package PeptideBuilder/1.0.4/PeptideBuilder-1.0.4-py3-none-any.whl

---
+++ packages/PeptideBuilder/PeptideBuilder-1.0.4-py3-none-any.whl/PeptideBuilder/__init__.py
@@ -0,0 +1,11 @@
+"""``PeptideBuilder`` package for creating peptide models in PDB format based on geometrical parameters
+Written by Matthew Z Tien.
+Python modules
+----------------
+The package consists of the following Python modules:
+* PeptideBuilder
+* Geometry
+"""
+__version__ = '1.0.4'
+from .PeptideBuilder import *
+from .Geometry import *
