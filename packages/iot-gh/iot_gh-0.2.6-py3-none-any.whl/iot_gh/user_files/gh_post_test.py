f51c4994a4d0c376d6eaa4b8c778ece20790fcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:13 +0000

    Add package iot-gh/0.2.6/iot_gh-0.2.6-py3-none-any.whl

---
+++ packages/iot-gh/iot_gh-0.2.6-py3-none-any.whl/iot_gh/user_files/gh_post_test.py
@@ -0,0 +1,19 @@
+from iot_gh.IoTGreenhouse import IoTGreenhouse
+from iot_gh.IoTGreenhouseService import IoTGreenhouseService
+
+def test_post():
+   
+    print("*** GH testing ***")   
+    ghs = IoTGreenhouseService()
+    gh = ghs.green_house
+    ghs.web_service.post_greenhouse()
+    print(gh.post_data.status_code)
+    print(gh.post_data.last_update)
+    print(gh.post_data.exception)
+    print("GH post test completed.")   
+
+if __name__ == "__main__":
+    test_post()
+
+
+
