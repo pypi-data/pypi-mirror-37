6b062bcc16419c1e8f2adbc40f87d2086ed4585d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:03 +0000

    Add package hemlock-io/0.0.13/hemlock_io-0.0.13.tar.gz

---
+++ packages/hemlock-io/hemlock_io-0.0.13.tar.gz/hemlock_io-0.0.13/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hemlock_io",
+    version="0.0.13",
+    author="John Alexander Harris",
+    author_email="john@hemlock.io",
+    description="Helper package for transferring data to server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hemlock-io/hemlock_io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
