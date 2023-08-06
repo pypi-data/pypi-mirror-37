fb6e4dd98ee1a82c7f32e2a6493b7791611f4961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:49 +0000

    Add package pykeygen/0.0.6/pykeygen-0.0.6.tar.gz

---
+++ packages/pykeygen/pykeygen-0.0.6.tar.gz/pykeygen-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='pykeygen',
+      version='0.0.6',
+      packages=find_packages(),
+      install_requires=['bip32utils', 'chainside-btcpy-multi', 'mnemonic'],
+      description='Tool to generate keys and addresses',
+      author='Oskar Hladky',
+      author_email='oskyks1@gmail.com',
+      url='https://github.com/oskyk/pykeygen',
+      python_requires='>=3',
+      download_url='https://github.com/oskyk/pykeygen/archive/0.0.1.tar.gz',
+      scripts=['pykeygen'],
+      keywords=['keygen', 'mnemonic', 'segwit', 'dash', 'litecoin', 'bitcoin']
+)
