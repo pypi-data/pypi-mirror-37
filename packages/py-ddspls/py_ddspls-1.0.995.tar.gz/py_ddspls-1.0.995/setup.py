9d1c2f8af123261cf88c02da663421b5358642da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:28 +0000

    Add package py-ddspls/1.0.995/py_ddspls-1.0.995.tar.gz

---
+++ packages/py-ddspls/py_ddspls-1.0.995.tar.gz/py_ddspls-1.0.995/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+    
+setup(name='py_ddspls',
+      version='1.0.995',
+      description='The multi data driven sparse pls package',
+      long_description=open('README.txt').read(),
+      url='http://github.com/hlorenzo/py_ddspls',
+      author='Hadrien Lorenzo',
+      author_email='hadrien.lorenzo.2015@gmail.com',
+      license='MIT',
+      packages=['py_ddspls'],
+      install_requires=['numpy','pandas','sklearn'],
+      dependency_links=['http://github.com/hlorenzo/py_ddspls'],
+      scripts=['bin/example.py'],
+      zip_safe=False,
+      include_package_data=True,
+      package_data={
+        'data':
+             ['data/penicilliumYES.npy',
+             'data/liverToxicity.npy']}
+)
