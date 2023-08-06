43c0877d0bc63c60361f87f59a3d9e81e92e7390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:21 +0000

    Add package ec2-metadata/1.8.0/ec2-metadata-1.8.0.tar.gz

---
+++ packages/ec2-metadata/ec2-metadata-1.8.0.tar.gz/ec2-metadata-1.8.0/setup.py
@@ -0,0 +1,57 @@
+# -*- encoding:utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import codecs
+import re
+
+from setuptools import setup
+
+
+def get_version(filename):
+    with codecs.open(filename, 'r', 'utf-8') as fp:
+        contents = fp.read()
+    return re.search(r"__version__ = ['\"]([^'\"]+)['\"]", contents).group(1)
+
+
+version = get_version('ec2_metadata.py')
+
+
+with codecs.open('README.rst', 'r', 'utf-8') as readme_file:
+    readme = readme_file.read()
+
+with codecs.open('HISTORY.rst', 'r', 'utf-8') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+
+setup(
+    name='ec2-metadata',
+    version=version,
+    description='An easy interface to query the EC2 metadata API, with caching.',
+    long_description=readme + '\n\n' + history,
+    author='Adam Johnson',
+    author_email='me@adamj.eu',
+    url='https://github.com/adamchainz/ec2-metadata',
+    py_modules=['ec2_metadata'],
+    include_package_data=True,
+    install_requires=[
+        'cached-property',
+        'requests',
+    ],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    license='ISC License',
+    zip_safe=False,
+    keywords='AWS, EC2, metadata',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
