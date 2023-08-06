c21adc6c71c2ee1ea1567e2e58cad88b31c271c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:24 +0000

    Add package pyenlone/0.1.4/pyenlone-0.1.4-py3-none-any.whl

---
+++ packages/pyenlone/pyenlone-0.1.4-py3-none-any.whl/pyenlone/__init__.py
@@ -0,0 +1,16 @@
+"""
+Implements enl.one APIs:
+- v.enl.one
+- tasks.enl.one (Soon)
+- status.enl.one (Not very soon, but soon)
+For more info: https://wiki.enl.one/doku.php?id=start
+
+All methods returns Munches. That only means you access them both as:
+- `some_result.some_property`
+- `some_result['some_property']`
+"""
+from .v import V, banned, Agent, DetailAgent, Team, TeamRole, TeamMember
+from .enloneexception import EnlOneException
+__all__ = ["V", "banned", "EnlOneException", "Agent", "DetailAgent", "Team",
+           "TeamMember", "TeamRole"]
+NAME = "pyenlone"
