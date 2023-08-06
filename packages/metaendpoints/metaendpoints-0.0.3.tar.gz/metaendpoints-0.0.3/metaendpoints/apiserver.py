84afae40202fe27c1966574dd237e51a62ec5464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:09 +0000

    Add package metaendpoints/0.0.3/metaendpoints-0.0.3.tar.gz

---
+++ packages/metaendpoints/metaendpoints-0.0.3.tar.gz/metaendpoints-0.0.3/metaendpoints/apiserver.py
@@ -0,0 +1,30 @@
+from concurrent import futures
+import time
+import grpc
+from grpc_health.v1.health import HealthServicer
+from grpc_health.v1 import health_pb2, health_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+def serve(register_servicers_callbacks = [], port: int = 50051, grace_period: int = 5):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server.add_insecure_port('[::]:{}'.format(port))
+
+    # Регистрируем api сервисы
+    for callback_ in register_servicers_callbacks:
+        callback_(server)
+
+    health = HealthServicer()
+    health.set("plugin", health_pb2.HealthCheckResponse.ServingStatus.Value('SERVING'))
+    health_pb2_grpc.add_HealthServicer_to_server(health, server)
+
+    server.start()
+
+    print("Server started...")
+
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(grace_period)
