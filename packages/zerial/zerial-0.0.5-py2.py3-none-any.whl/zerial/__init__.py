f8aed32263ff73529c20628e0103f9ccf374bf84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:22 +0000

    Add package zerial/0.0.5/zerial-0.0.5-py2.py3-none-any.whl

---
+++ packages/zerial/zerial-0.0.5-py2.py3-none-any.whl/zerial/__init__.py
@@ -0,0 +1,11 @@
+from ._core import deztructure, reztructure
+
+__author__ = 'Josh Reed'
+__email__ = 'jriddy@gmail.com'
+__version__ = '0.0.5'
+
+
+__all__ = [
+    'deztructure',
+    'reztructure',
+]
