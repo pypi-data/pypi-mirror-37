3feebcf4f8990562c76b1f0ee5e9e959e73baa59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:17 +0000

    Add package PowerPigeon/0.1.0/PowerPigeon-0.1.0.tar.gz

---
+++ packages/PowerPigeon/PowerPigeon-0.1.0.tar.gz/PowerPigeon-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().strip().splitlines()
+
+setup(
+    name='PowerPigeon',
+    version='0.1.0',
+    packages=['powerpigeon'],
+    url='https://github.com/JakeMakesStuff/PowerPigeon',
+    license='MPL-2.0',
+    author='Jake Gealer',
+    install_requires=requirements,
+    author_email='jake@gealer.email',
+    description='PowerPigeon is a model-based RethinkDB handler.'
+)
