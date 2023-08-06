05896cf59775751424f865d568c1042140273201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:50 +0000

    Add package ml-python/1.7/ml_python-1.7-py3-none-any.whl

---
+++ packages/ml-python/ml_python-1.7-py3-none-any.whl/ml/__init__.py
@@ -0,0 +1,11 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+
+from ml.k_means import *
+from ml.linear_regression import *
+from ml.logistic_regression import *
+from ml.nn import *
+from ml.random import *
+from ml.activation import *
+from ml.cnn import *
