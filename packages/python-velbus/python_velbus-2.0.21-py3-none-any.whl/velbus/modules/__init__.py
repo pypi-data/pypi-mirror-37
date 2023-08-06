7efbf660ba637824a4d631202e9f96d3c6e9823f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:19 +0000

    Add package python-velbus/2.0.21/python_velbus-2.0.21-py3-none-any.whl

---
+++ packages/python-velbus/python_velbus-2.0.21-py3-none-any.whl/velbus/modules/__init__.py
@@ -0,0 +1,13 @@
+"""
+:author: Thomas Delaet <thomas@delaet.org>
+"""
+
+from velbus.modules.vmb4ry import VMB4RYModule
+from velbus.modules.vmbin import VMB6INModule
+from velbus.modules.vmbin import VMB7INModule
+from velbus.modules.vmbgp import VMBGPxModule, VMBGPxDModule
+from velbus.modules.vmbpbn import VMB2PBNModule
+from velbus.modules.vmbpbn import VMB6PBNModule
+from velbus.modules.vmb1ts import VMB1TSModule
+from velbus.modules.vmbiro import VMBIROModule
+
