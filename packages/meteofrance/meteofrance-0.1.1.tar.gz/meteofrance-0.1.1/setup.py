bec61592c2f3c5e89956e6afc1fb44889ee4959a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:41 +0000

    Add package meteofrance/0.1.1/meteofrance-0.1.1.tar.gz

---
+++ packages/meteofrance/meteofrance-0.1.1.tar.gz/meteofrance-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import twine
+
+setup(
+    name='meteofrance',
+    version='0.1.1',
+    description = 'Extract Meteo-France weather forecast',
+    author = 'victorcerutti',
+    author_email = 'victorcerutti+meteofrance@gmail.com',
+    url = 'https://github.com/victorcerutti/meteofrance-py',
+    packages=['meteofrance',],
+    install_requires=[
+       'requests',
+       'beautifulsoup4',
+       're'
+    ],
+    license='MIT',
+    long_description='Extract Meteo-France current weather and rain forecast',
+)
