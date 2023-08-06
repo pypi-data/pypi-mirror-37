cfa0002b2020e4cdcb6af6308faf567b53574c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:30 +0000

    Add package termutils/0.1.2/termutils-0.1.2.tar.gz

---
+++ packages/termutils/termutils-0.1.2.tar.gz/termutils-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='termutils',
+    version='0.1.2',
+    packages=[''],
+    url='',
+    license='',
+    author='Steven',
+    author_email='stautonico@gmail.com',
+    description=''
+)
