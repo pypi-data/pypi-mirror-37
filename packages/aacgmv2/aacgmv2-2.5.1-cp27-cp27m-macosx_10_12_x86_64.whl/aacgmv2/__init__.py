d8312e841604e2143f709470b3b18a1658de5426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:36 +0000

    Add package aacgmv2/2.5.1/aacgmv2-2.5.1-cp27-cp27m-macosx_10_12_x86_64.whl

---
+++ packages/aacgmv2/aacgmv2-2.5.1-cp27-cp27m-macosx_10_12_x86_64.whl/aacgmv2/__init__.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""aacgmv2
+
+Modules
+---------------------------------------------------------------------------
+_aacgmv2 : Contains functions and variables from c code
+deprecated : Contains deprecated functions from previous versions
+wrapper : Contains current python functions
+---------------------------------------------------------------------------
+
+Parameters
+---------------------------------------------------------------------------
+AACGM_V2_DAT_PREFIX
+IGRF_12_COEFFS
+_aacgmv2.G2A
+_aacgmv2.A2G
+_aacgmv2.TRACE
+_aacgmv2.ALLOWTRACE
+_aacgmv2.BADIDEA
+_aacgmv2.GEOCENTRIC
+---------------------------------------------------------------------------
+
+Functions
+---------------------------------------------------------------------------
+convert_latlon_arr
+convert_str_to_bit
+convert_bool_to_bit
+get_aacgm_coord
+get_aacgm_coord_arr
+convert
+convert_mlt
+wrapper.set_coeff_path
+deprecated.subsol
+_aacgmv2.convert
+_aacgmv2.set_datetime
+_aacgmv2.mlt_convert
+_aacgmv2.mlt_convert_yrsec
+_aacgmv2.inv_mlt_convert
+_aacgmv2.inv_mlt_convert_yrsec
+---------------------------------------------------------------------------
+"""
+import os as _os
+from sys import stderr
+import logbook as logging
+
+__version__ = "2.5.1"
+
+# path and filename prefix for the IGRF coefficients
+AACGM_v2_DAT_PREFIX = _os.path.join(_os.path.realpath( \
+                                                _os.path.dirname(__file__)),
+                                    'aacgm_coeffs', 'aacgm_coeffs-12-')
+IGRF_COEFFS = _os.path.join(_os.path.realpath(_os.path.dirname(__file__)),
+                            'magmodel_1590-2015.txt')
+
+# If not defined, set the IGRF and AACGM environment variables
+reset_warn = False
+if 'IGRF_COEFFS' in _os.environ.keys():
+    stderr.write("resetting environment variable IGRF_COEFFS in python "
+                 + "script\n")
+    reset_warn = True
+_os.environ['IGRF_COEFFS'] = IGRF_COEFFS
+
+if 'AACGM_v2_DAT_PREFIX' in _os.environ.keys():
+    stderr.write("resetting environment variable AACGM_v2_DAT_PREFIX in " +
+                 "python script\n")
+    reset_warn = True
+_os.environ['AACGM_v2_DAT_PREFIX'] = AACGM_v2_DAT_PREFIX
+
+if reset_warn:
+    stderr.write("non-default coefficient files may be specified by running " +
+                 "aacgmv2.wrapper.set_coeff_path before any other functions\n")
+# Imports
+#---------------------------------------------------------------------
+
+try:
+    from aacgmv2.wrapper import (convert_latlon, convert_mlt, get_aacgm_coord)
+    from aacgmv2.wrapper import (convert_latlon_arr, get_aacgm_coord_arr)
+    from aacgmv2.wrapper import (convert_bool_to_bit, convert_str_to_bit)
+except Exception as err:
+    logging.exception(__file__ + ' -> aacgmv2: ' + str(err))
+
+try:
+    from aacgmv2 import (deprecated)
+    from aacgmv2.deprecated import (convert)
+except Exception as err:
+    logging.exception(__file__ + ' -> aacgmv2: ' + str(err))
+
+try:
+    from aacgmv2 import (_aacgmv2)
+except Exception as err:
+    logging.exception(__file__ + ' -> aacgmv2: ' + str(err))
