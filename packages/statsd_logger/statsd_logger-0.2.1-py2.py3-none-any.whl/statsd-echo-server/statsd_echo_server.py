3ece99b16023d850ba5a67bc60322e313a7b3d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:29 +0000

    Add package statsd_logger/0.2.1/statsd_logger-0.2.1-py2.py3-none-any.whl

---
+++ packages/statsd_logger/statsd_logger-0.2.1-py2.py3-none-any.whl/statsd-echo-server/statsd_echo_server.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+'''
+Simple StatsD Echo Server
+'''
+import socket
+from termcolor import colored
+
+
+class StatsDServer:
+    def __init__(self, host='127.0.0.1', port=8125):
+        self.port = port
+        self.host = host
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.host, self.port))
+
+    def run(self):
+        '''Main function.
+        Listen on port 8125 by default, parse StatsD messages
+        received on port and print on stdout'''
+        print("Server listening on {}:{}".format(self.host, self.port))
+        try:
+            while True:
+                data, _ = self.sock.recvfrom(1024)
+
+                data = data.decode('utf-8').split('\n')
+                for line in data:
+                    metric_val, metric_type, tag = line.split('|')
+                    metric, value = metric_val.split(':')
+                    metric = colored(metric, 'blue')
+                    value_str = colored("|".join([value, metric_type, tag]), 'green')
+                    print("StatsD Metric: {} {}".format(metric, value_str))
+        except KeyboardInterrupt:
+            print("Stopping StatsD Echo Server")
+            self.sock.close()
+
+
+if __name__ == '__main__':
+    '''Create statsd server and run it'''
+    server = StatsDServer()
+    server.run()
