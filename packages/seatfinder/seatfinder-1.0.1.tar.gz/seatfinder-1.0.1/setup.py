1e80212c41527b459e5dc8ec32a29aef3458377d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:29 +0000

    Add package seatfinder/1.0.1/seatfinder-1.0.1.tar.gz

---
+++ packages/seatfinder/seatfinder-1.0.1.tar.gz/seatfinder-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='seatfinder',
+    version='1.0.1',
+    url='https://github.com/soerface/py_seatfinder',
+    description='Easy access to the data of the KIT seatfinder. '
+                'This project is not affiliated in any way with the original author of https://www.seatfinder.de/',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    packages=['seatfinder'],
+    install_requires=['requests', 'pandas', 'matplotlib'],
+    tests_require=['nose', 'coverage'],
+    test_suite='nose.collector',
+)
