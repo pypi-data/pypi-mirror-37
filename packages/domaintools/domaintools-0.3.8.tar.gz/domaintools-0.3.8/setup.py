f140b64645c3bd4201c6e983ac02dffef1b9b752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:16 +0000

    Add package domaintools/0.3.8/domaintools-0.3.8.tar.gz

---
+++ packages/domaintools/domaintools-0.3.8.tar.gz/domaintools-0.3.8/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='domaintools',
+    version='0.3.8',
+    description='Domain parsing with python',
+    author='DevHub',
+    url='http://github.com/devhub/domaintools',
+    packages=find_packages(),
+)
