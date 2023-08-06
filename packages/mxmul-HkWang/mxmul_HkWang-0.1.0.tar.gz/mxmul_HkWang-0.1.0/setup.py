f1321c88c0c3cd39a2d5a7d22c6a57bfe36891bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:07 +0000

    Add package mxmul-HkWang/0.1.0/mxmul_HkWang-0.1.0.tar.gz

---
+++ packages/mxmul-HkWang/mxmul_HkWang-0.1.0.tar.gz/mxmul_HkWang-0.1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="mxmul_HkWang",
+    version="0.1.0",
+    author="Haokang Wang",
+    descriotion="This a common Python Package to test."
+)
