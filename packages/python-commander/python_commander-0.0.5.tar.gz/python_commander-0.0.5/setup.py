5a894a354e58af6e7ffcd2fc7bb519db38f0de55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:23 +0000

    Add package python-commander/0.0.5/python_commander-0.0.5.tar.gz

---
+++ packages/python-commander/python_commander-0.0.5.tar.gz/python_commander-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_commander",
+    version="0.0.5",
+    author="Bloodlister",
+    author_email="AsenJ.Mihaylov@gmail.com",
+    description="A spin-off from Django's manager.py",
+    license='MIT',
+    long_description=long_description,
+    url="https://github.com/Bloodlister/python_commander",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    dependency_links=[
+        'https://github.com/wong2/pick'
+    ]
+)
