8864045c3ac53e182cbde823d52f8d6be6a5174b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:49 +0000

    Add package SensorsAnalyticsSDK/1.10.0/SensorsAnalyticsSDK-1.10.0.tar.gz

---
+++ packages/SensorsAnalyticsSDK/SensorsAnalyticsSDK-1.10.0.tar.gz/SensorsAnalyticsSDK-1.10.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='SensorsAnalyticsSDK',
+    version='1.10.0',
+    author='gw',
+    author_email='guanwei@sensorsdata.cn',
+    url='http://www.sensorsdata.cn',
+    license='LICENSE.txt',
+    packages=['sensorsanalytics'],
+    description='This is the official Python SDK for Sensors Analytics.',
+    long_description=open('README.txt').read(),
+)
