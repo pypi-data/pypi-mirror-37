76366e38d78129acd46ddbd9fa5a42f59d2165d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:55 +0000

    Add package danke-devpy36/1.0.1/danke-devpy36-1.0.1.tar.gz

---
+++ packages/danke-devpy36/danke-devpy36-1.0.1.tar.gz/danke-devpy36-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/24 上午9:23
+# @Author  : Shark
+# @File    : setup.py
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+setup(
+    name="danke-devpy36",
+    version="1.0.1",
+    author="guohanhong",
+    author_email="guohanhong@dankegongyu.com",
+    license="MIT",
+    packages=[],
+    install_requires=[],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+       ## "Programming Language :: Python :: 2",
+       ## "Programming Language :: Python :: 2.6",
+       ## "Programming Language :: Python :: 2.7",
+    ],
+)
