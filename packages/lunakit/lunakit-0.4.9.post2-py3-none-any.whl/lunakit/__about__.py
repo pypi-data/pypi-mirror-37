aca50615e74f6f0aa4d32cc8d2135a2d27c53e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:01 +0000

    Add package lunakit/0.4.9.post2/lunakit-0.4.9.post2-py3-none-any.whl

---
+++ packages/lunakit/lunakit-0.4.9.post2-py3-none-any.whl/lunakit/__about__.py
@@ -0,0 +1,14 @@
+# Copyright 2018 miruka
+# This file is part of lunakit, licensed under LGPLv3.
+
+"Python API and CLI tools for Danbooru-based sites."
+
+__pkg_name__ = "lunakit"
+__version__  = "0.4.9-2"
+__status__   = "Development"
+# __status__ = "Production"
+
+__author__   = "miruka"
+__email__    = "miruka@disroot.org"
+
+__license__  = "LGPLv3"
