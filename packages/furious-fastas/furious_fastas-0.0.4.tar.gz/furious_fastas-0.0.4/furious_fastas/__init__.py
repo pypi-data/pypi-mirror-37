a4004ccad6ee1c3e8e2b80b34cc65441037b1380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:33 +0000

    Add package furious-fastas/0.0.4/furious_fastas-0.0.4.tar.gz

---
+++ packages/furious-fastas/furious_fastas-0.0.4.tar.gz/furious_fastas-0.0.4/furious_fastas/__init__.py
@@ -0,0 +1,8 @@
+# from os import path
+
+# class FuriousConfig(object):
+#     def __init__(self, db_path, contaminants_path=""):
+#         if contaminants_path:
+#             self.path_to_data_module = path.abspath(path.dirname(__file__))
+#             self.read(pjoin(here, "data/contaminants.fasta")) 
+
