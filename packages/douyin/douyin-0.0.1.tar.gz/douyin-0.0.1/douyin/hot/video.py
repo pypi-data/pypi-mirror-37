b6c46b91810b1ec475200948f90ab2b3735ff12a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:44 +0000

    Add package douyin/0.0.1/douyin-0.0.1.tar.gz

---
+++ packages/douyin/douyin-0.0.1.tar.gz/douyin-0.0.1/douyin/hot/video.py
@@ -0,0 +1,17 @@
+from douyin.config import hot_video_url
+from douyin.structure import HotVideo
+from douyin.utils import fetch
+
+
+def video():
+    """
+    get hot video result
+    :return: HotSearch object
+    """
+    result = fetch(hot_video_url)
+    # process json data
+    active_time = result.get('active_time')
+    video_list = result.get('aweme_list', [])
+    data = [{'item': item.get('aweme_info'), 'hot_value': item.get('hot_value')} for item in video_list]
+    # construct HotSearch object and return
+    return HotVideo(active_time=active_time, data=data)
