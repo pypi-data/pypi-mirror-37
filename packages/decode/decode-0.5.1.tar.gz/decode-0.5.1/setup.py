769a91c68ee7105bd785b5b034c39acda5d3b48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:47 +0000

    Add package decode/0.5.1/decode-0.5.1.tar.gz

---
+++ packages/decode/decode-0.5.1.tar.gz/decode-0.5.1/setup.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+"DESHIMA Code for data analysis"
+
+# standard library
+from setuptools import setup
+
+# module constants
+# INSTALL_REQUIRES = [
+#     'xarray >= 0.9.6',
+# ]
+
+PACKAGES = [
+    'decode',
+    'decode.core',
+    'decode.core.array',
+    'decode.core.cube',
+    'decode.io',
+    'decode.logging',
+    'decode.plot',
+    'decode.models',
+    'decode.joke',
+    'decode.utils',
+    'decode.utils.misc',
+    'decode.utils.ndarray'
+]
+
+
+
+# main
+setup(
+    name = 'decode',
+    description = __doc__,
+    version = '0.5.1',
+    author = 'DESHIMA software team',
+    author_email = 'tishida@ioa.s.u-tokyo.ac.jp',
+    url = 'https://github.com/deshima-dev/decode',
+    # install_requires = INSTALL_REQUIRES,
+    packages = PACKAGES,
+    package_data = {'decode': ['data/*.yaml']},
+)
