610c87ec8ec448238d5594e642ca30080a4cb34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:51 +0000

    Add package astrobox/0.1.4/astrobox-0.1.4.tar.gz

---
+++ packages/astrobox/astrobox-0.1.4.tar.gz/astrobox-0.1.4/astrobox/themes/default/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+PICTURES_PATH = os.path.dirname(__file__)
+
+BACKGROUND_IMAGE = os.path.join(PICTURES_PATH, 'background.png')
+
+FIELD_WIDTH = 1200
+FIELD_HEIGHT = 600
+
+METER_1_COLOR = (0, 255, 70)
+METER_2_COLOR = (232, 129, 31)
+
+TEAMS_COUNT = 4
+
+DEBUG = False
+
+MAX_HEALTH = 100
+STING_POWER = 50
+HEALTH_TOP_UP_SPEED = 0.5
+MATHERSHIP_SAFE_DISTANCE = 200
+
+LOAD_SPEED = 1
+LOAD_DISTANCE = 10
+
+MIN_ASTEROID_ELERIUM = 100
+MAX_ASTEROID_ELERIUM = 200
+
+MAX_DRON_ELERIUM = 100
+
+# For more see robogame_engine.constants
+
