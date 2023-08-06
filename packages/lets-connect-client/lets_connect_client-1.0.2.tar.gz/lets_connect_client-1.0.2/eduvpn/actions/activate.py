a5ba9433758c3e67b60e653687d6a9858aadbf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:33 +0000

    Add package lets-connect-client/1.0.2/lets_connect_client-1.0.2.tar.gz

---
+++ packages/lets-connect-client/lets_connect_client-1.0.2.tar.gz/lets_connect_client-1.0.2/eduvpn/actions/activate.py
@@ -0,0 +1,99 @@
+# python-eduvpn-client - The GNU/Linux eduVPN client and Python API
+#
+# Copyright: 2017, The Commons Conservancy eduVPN Programme
+# SPDX-License-Identifier: GPL-3.0+
+
+import logging
+import gi
+from gi.repository import GLib
+from eduvpn.util import error_helper, thread_helper
+from eduvpn.oauth2 import oauth_from_token
+from eduvpn.manager import update_config_provider, update_keys_provider, connect_provider, disconnect_all
+from eduvpn.remote import get_profile_config, create_keypair, user_info, check_certificate
+from eduvpn.steps.reauth import reauth
+from eduvpn.steps.two_way_auth import two_auth_step
+from eduvpn.notify import notify, init_notify
+from eduvpn.openvpn import parse_ovpn
+from eduvpn.exceptions import EduvpnAuthException, EduvpnException
+from eduvpn.crypto import common_name_from_cert
+from eduvpn.brand import get_brand
+
+logger = logging.getLogger(__name__)
+
+
+# ui thread
+def activate_connection(meta, builder, verifier, lets_connect):
+    """do the actual connecting action"""
+    logger.info("Connecting to {}".format(meta.display_name))
+    disconnect_all()
+    _, name = get_brand(lets_connect)
+    notification = init_notify(lets_connect)
+    notify(notification, "{} connecting...".format(name), "Connecting to '{}'".format(meta.display_name))
+    try:
+        if not meta.token:
+            logger.error("metadata for {} doesn't contain oauth2 token".format(meta.uuid))
+            connect_provider(meta.uuid)
+
+        else:
+            oauth = oauth_from_token(meta=meta, lets_connect=lets_connect)
+            thread_helper(lambda: _auth_check(oauth, meta, verifier, builder, lets_connect=lets_connect))
+
+    except Exception as e:
+        switch = builder.get_object('connect-switch')
+        GLib.idle_add(switch.set_active, False)
+        window = builder.get_object('eduvpn-window')
+        error_helper(window, "can't enable connection", "{}: {}".format(type(e).__name__, str(e)))
+        raise
+
+
+# background thread
+def _auth_check(oauth, meta, verifier, builder, lets_connect):
+    """quickly see if the can fetch messages, otherwise reauth"""
+    try:
+        info = user_info(oauth, meta.api_base_uri)
+        _cert_check(meta, oauth, builder, info)
+    except EduvpnAuthException:
+        GLib.idle_add(lambda: reauth(meta=meta, verifier=verifier, builder=builder, lets_connect=lets_connect))
+    except Exception as e:
+        error = e
+        window = builder.get_object('eduvpn-window')
+        GLib.idle_add(lambda: error_helper(window, "Can't check account status", "{}".format(str(error))))
+        raise
+
+
+# background thread
+def _cert_check(meta, oauth, builder, info):
+    common_name = common_name_from_cert(meta.cert.encode('ascii'))
+    cert_valid = check_certificate(oauth, meta.api_base_uri, common_name)
+
+    if not cert_valid['is_valid']:
+        logger.warning('client certificate not valid, reason: {}'.format(cert_valid['reason']))
+        if cert_valid['reason'] in ('certificate_missing', 'certificate_not_yet_valid', 'certificate_expired'):
+            logger.info('Going to try to fetch new keypair')
+            cert, key = create_keypair(oauth, meta.api_base_uri)
+            update_keys_provider(meta.uuid, cert, key)
+        elif cert_valid['reason'] == 'user_disabled':
+            raise EduvpnException('Your account has been disabled.')
+        else:
+            raise EduvpnException('Your client certificate is invalid ({})'.format(cert_valid['reason']))
+
+    _fetch_updated_config(oauth, meta, builder, info)
+
+
+# background thread
+def _fetch_updated_config(oauth, meta, builder, info):
+    config = get_profile_config(oauth, meta.api_base_uri, meta.profile_id)
+    meta.config = config
+    config_dict = parse_ovpn(meta.config)
+    update_config_provider(meta, config_dict)
+
+    _2fa_check(meta, builder, oauth, config_dict, info)
+
+
+# background thread
+def _2fa_check(meta, builder, oauth, config_dict, info):
+    if meta.two_factor and not info['two_factor_enrolled']:
+        # 2fa is required, but the user is not enroled anymore
+        two_auth_step(builder, oauth, meta, config_dict)
+    else:
+        connect_provider(meta.uuid)
---
+++ packages/lets-connect-client/lets_connect_client-1.0.2.tar.gz/lets_connect_client-1.0.2/eduvpn/steps/browser.py
@@ -0,0 +1,133 @@
+# python-eduvpn-client - The GNU/Linux eduVPN client and Python API
+#
+# Copyright: 2017, The Commons Conservancy eduVPN Programme
+# SPDX-License-Identifier: GPL-3.0+
+
+import logging
+import webbrowser
+from random import random
+import gi
+from gi.repository import GLib
+from eduvpn.util import error_helper, thread_helper
+from eduvpn.crypto import gen_code_verifier
+from eduvpn.oauth2 import get_open_port, create_oauth_session, get_oauth_token_code, oauth_from_token
+from eduvpn.remote import get_instance_info, get_auth_url
+from eduvpn.metadata import reuse_token_from_base_uri
+from eduvpn.steps.profile import fetch_profile_step
+
+
+logger = logging.getLogger(__name__)
+
+
+def browser_step(builder, meta, verifier, lets_connect, force_token_refresh=False):
+    """The notorious browser step. if no token, starts webserver, wait for callback, show token dialog"""
+    logger.info("opening token dialog")
+    dialog = builder.get_object('token-dialog')
+    thread_helper(lambda: _phase1_background(meta=meta, dialog=dialog, verifier=verifier, builder=builder,
+                                             force_token_refresh=force_token_refresh, lets_connect=lets_connect))
+    dialog.show_all()
+
+
+def _phase1_background(meta, dialog, verifier, builder, force_token_refresh, lets_connect):
+    try:
+        logger.info("starting token obtaining in background")
+        r = get_instance_info(instance_uri=meta.instance_base_uri, verifier=verifier)
+        meta.api_base_uri, meta.authorization_endpoint, meta.token_endpoint = r
+    except Exception as e:
+        error = e
+        GLib.idle_add(lambda: error_helper(dialog, "Can't fetch instance info", "{}".format(str(error))))
+        GLib.idle_add(lambda: dialog.hide())
+        raise
+
+    meta.refresh_token()
+
+    if not meta.token and not force_token_refresh:
+        # lets see if other profiles already have a token we can use
+        token = reuse_token_from_base_uri(meta.instance_base_uri)
+        if token:
+            meta.token = token
+
+    if not meta.token:
+        code_verifier = gen_code_verifier()
+        port = get_open_port()
+        try:
+            oauth = create_oauth_session(port, lets_connect=lets_connect, auto_refresh_url=meta.token_endpoint)
+            auth_url, state = get_auth_url(oauth, code_verifier, meta.authorization_endpoint)
+        except Exception as e:
+            error = e
+            GLib.idle_add(lambda: error_helper(dialog, "Can't create oauth session", "{}".format(str(error))))
+            GLib.idle_add(lambda: dialog.hide())
+            raise
+        else:
+            GLib.idle_add(lambda: _phase1_callback(meta, port, code_verifier, oauth, auth_url, dialog, builder, state,
+                                                   lets_connect=lets_connect))
+    else:
+        logger.info("we already have a token, skipping browser step")
+        oauth = oauth_from_token(meta=meta, lets_connect=lets_connect)
+        GLib.idle_add(lambda: _phase2_callback(meta=meta, oauth=oauth, dialog=dialog, builder=builder,
+                                               lets_connect=lets_connect))
+
+
+def _phase1_callback(meta, port, code_verifier, oauth, auth_url, dialog, builder, state, lets_connect):
+    thread_helper(lambda: _phase2_background(meta=meta, port=port, oauth=oauth, code_verifier=code_verifier,
+                                             auth_url=auth_url, dialog=dialog, builder=builder, state=state,
+                                             lets_connect=lets_connect))
+    _show_dialog(dialog, auth_url, builder)
+
+
+def _show_dialog(dialog, auth_url, builder):
+    url_field = builder.get_object('redirect-url-entry')
+    url_dialog = builder.get_object('redirecturl-dialog')
+    while True:
+        response = dialog.run()
+        if response == 0:  # cancel
+            logger.info("token dialog: cancel button pressed")
+            dialog.hide()
+            break
+        elif response == 1:
+            logger.info("token dialog: reopen browser button pressed, opening {} again".format(auth_url))
+            webbrowser.open(auth_url)
+        elif response == 2:
+            logger.info("token dialog: show redirect URL button pressed")
+            url_field.set_text(auth_url)
+            url_dialog.run()
+            logger.info("token dialog: url popup closed")
+            url_dialog.hide()
+        else:
+            logger.info("token dialog: window closed")
+            dialog.hide()
+            break
+
+
+def _phase2_background(meta, port, oauth, code_verifier, auth_url, dialog, builder, state, lets_connect):
+    session = random()
+    logger.info("opening browser with url {}".format(auth_url))
+    try:
+        webbrowser.open(auth_url)
+        dialog.session = session
+        code, other_state = get_oauth_token_code(port, lets_connect=lets_connect, timeout=120)
+        logger.info("control returned by browser")
+        if state != other_state:
+            logger.error("received from state, expected: {}, received: {}".format(state, other_state))
+            raise Exception("oauth state has been tampered with")
+        logger.info("setting oauth token for metadata")
+        meta.token = oauth.fetch_token(meta.token_endpoint, code=code, code_verifier=code_verifier)
+    except Exception as e:
+        error = e
+        if dialog.get_property("visible") and dialog.session == session:
+            GLib.idle_add(lambda: error_helper(dialog, "Can't obtain token", "{}".format(str(error))))
+            GLib.idle_add(lambda: dialog.hide())
+        else:
+            logging.error(error)
+        raise
+    else:
+        GLib.idle_add(lambda: _phase2_callback(meta=meta, oauth=oauth, dialog=dialog, builder=builder,
+                                               lets_connect=lets_connect))
+
+
+def _phase2_callback(meta, oauth, dialog, builder, lets_connect):
+    logger.info("hiding url and token dialog")
+    url_dialog = builder.get_object('redirecturl-dialog')
+    GLib.idle_add(lambda: url_dialog.hide())
+    GLib.idle_add(lambda: dialog.hide())
+    fetch_profile_step(meta=meta, oauth=oauth, builder=builder, lets_connect=lets_connect)
