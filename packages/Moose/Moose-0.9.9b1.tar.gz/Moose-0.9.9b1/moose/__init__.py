973eea6953280731690c84434f7b2349066a499d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:50 +0000

    Add package Moose/0.9.9b1/Moose-0.9.9b1.tar.gz

---
+++ packages/Moose/Moose-0.9.9b1.tar.gz/Moose-0.9.9b1/moose/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import unicode_literals
+from os.path import join, dirname
+
+def get_version():
+    with open(join(dirname(__file__), 'VERSION'), 'rb') as f:
+        version = f.read().decode('ascii').strip()
+    return version
+
+def setup(set_prefix=True):
+    """
+    Configure the settings (this happens as a side effect of accessing the
+    first setting), configure logging and populate the app registry.
+    Set the thread-local urlresolvers script prefix if `set_prefix` is True.
+    """
+    from moose.apps import apps
+    from moose.conf import settings
+
+    from moose.utils.log import configure_logging
+
+    configure_logging(settings.LOGGING_CONFIG, settings.LOGGING)
+
+    # apps.populate(settings.INSTALLED_APPS)
---
+++ packages/Moose/Moose-0.9.9b1.tar.gz/Moose-0.9.9b1/moose/actions/__init__.py
@@ -0,0 +1,3 @@
+from . import base, export, upload, review, operate, migrate
+
+__all__ = ['base', 'export', 'upload', 'review', 'operate', 'migrate']
