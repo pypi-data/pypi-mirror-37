1b4252cd2f94a90eb74acc2cb01e0a6405c800c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:50 +0000

    Add package django-cognito/1.1.9/django_cognito-1.1.9.tar.gz

---
+++ packages/django-cognito/django_cognito-1.1.9.tar.gz/django_cognito-1.1.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from django_cognito import __version__ as version
+
+setup(
+    name='django_cognito',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/Olorin92/django_cognito',
+    license='',
+    author='Alex Plant',
+    author_email='alex.c.plant@gmail.com',
+    description='Library for allowing the use of AWS Cognito security in Django projects',
+    install_requires=[
+        "boto3==1.9.0",
+        "botocore==1.12.0",
+        "Django>=1.10.0",
+        "djangorestframework==3.8.2",
+        "urllib3==1.23",
+        "jwt==0.5.4"
+    ]
+)
