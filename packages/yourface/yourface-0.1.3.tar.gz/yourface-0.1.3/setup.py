e82b1f9a3b6ae4bc6f2af8232ed59ea22cd19c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:35 +0000

    Add package yourface/0.1.3/yourface-0.1.3.tar.gz

---
+++ packages/yourface/yourface-0.1.3.tar.gz/yourface-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='yourface',
+
+    version='0.1.3',
+
+    license='MIT',
+
+    py_modules=['yourface'],
+
+    description="import your face",
+
+    author="Bren 'fraq' Briggs",
+
+    url="https://github.com/bbriggs/yourface",
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python'
+    ],
+
+    packages=['yourface']
+
+)
