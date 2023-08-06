6bb70162a3def93819e2a6abac57736cdaf73fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:29 +0000

    Add package pysurveygizmo/0.0.2/pysurveygizmo-0.0.2.tar.gz

---
+++ packages/pysurveygizmo/pysurveygizmo-0.0.2.tar.gz/pysurveygizmo-0.0.2/README.md
@@ -0,0 +1,3 @@
+# Pysurveygizmo
+
+Pysurveygizmo is a basic wrapper for the surveygizmo.com REST API client built for Python 3.6. The goal is to provide a simple, easy-to-use tool for exporting survey results from Surveygizmo.
---
+++ packages/pysurveygizmo/pysurveygizmo-0.0.2.tar.gz/pysurveygizmo-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysurveygizmo",
+    version="0.0.2",
+    author="Courtney Ferguson Lee",
+    author_email="cfergusonlee@venturewell.org",
+    description="A package for Survey Gizmo that pulls response and campaign data using your account API key and a valid survey ID number, returning a Pandas dataframe.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/venturewell/pysurveygizmo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
