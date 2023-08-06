9abe12cb039676d1fa0b42d63c57c0debc2bd6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:38 +0000

    Add package mmsplice/0.2.6/mmsplice-0.2.6-py2.py3-none-any.whl

---
+++ packages/mmsplice/mmsplice-0.2.6-py2.py3-none-any.whl/mmsplice/utils/__init__.py
@@ -0,0 +1,11 @@
+from .postproc import max_varEff, \
+    transform, \
+    predict_deltaLogitPsi, \
+    predict_pathogenicity
+
+__all__ = [
+    'max_varEff',
+    'transform',
+    'predict_deltaLogitPsi',
+    'predict_pathogenicity'
+]
