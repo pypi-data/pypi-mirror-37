59993b99745de0062ea54cf0d139f33c185af396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:39 +0000

    Add package osiftool/1.2.12/osiftool-1.2.12-py3-none-any.whl

---
+++ packages/osiftool/osiftool-1.2.12-py3-none-any.whl/osiftool/constants.py
@@ -0,0 +1,6 @@
+OSIF_SERVICE_PROFILE_NAME   = 'osif.json'
+
+URL_HOST    = 'http://osif.synctechno.com'
+URL_HOST_TEST    = 'http://192.168.0.109:8889'
+URL_LOGIN   = URL_HOST + '/auth/token'
+URL_MYSERVICE   = URL_HOST + '/myservice'
