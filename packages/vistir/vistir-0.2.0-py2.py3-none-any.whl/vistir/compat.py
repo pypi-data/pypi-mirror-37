5f7c4be935107da75fbe9958aae5849983888ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:54 +0000

    Add package vistir/0.2.0/vistir-0.2.0-py2.py3-none-any.whl

---
+++ packages/vistir/vistir-0.2.0-py2.py3-none-any.whl/vistir/compat.py
@@ -0,0 +1,144 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import errno
+import os
+import sys
+import warnings
+
+from tempfile import mkdtemp
+
+import six
+
+
+__all__ = [
+    "Path",
+    "get_terminal_size",
+    "finalize",
+    "partialmethod",
+    "JSONDecodeError",
+    "FileNotFoundError",
+    "ResourceWarning",
+    "FileNotFoundError",
+    "fs_str",
+    "lru_cache",
+    "TemporaryDirectory",
+    "NamedTemporaryFile",
+]
+
+if sys.version_info >= (3, 5):
+    from pathlib import Path
+    from functools import lru_cache
+else:
+    from pathlib2 import Path
+    from backports.functools_lru_cache import lru_cache
+
+from .backports.tempfile import NamedTemporaryFile as _NamedTemporaryFile
+if sys.version_info < (3, 3):
+    from backports.shutil_get_terminal_size import get_terminal_size
+    NamedTemporaryFile = _NamedTemporaryFile
+else:
+    from tempfile import NamedTemporaryFile
+    from shutil import get_terminal_size
+
+try:
+    from weakref import finalize
+except ImportError:
+    from backports.weakref import finalize
+
+try:
+    from functools import partialmethod
+except Exception:
+    from .backports.functools import partialmethod
+
+try:
+    from json import JSONDecodeError
+except ImportError:  # Old Pythons.
+    JSONDecodeError = ValueError
+
+if six.PY2:
+
+    class ResourceWarning(Warning):
+        pass
+
+    class FileNotFoundError(IOError):
+        """No such file or directory"""
+
+        def __init__(self, *args, **kwargs):
+            self.errno = errno.ENOENT
+            super(FileNotFoundError, self).__init__(*args, **kwargs)
+
+else:
+    from builtins import ResourceWarning, FileNotFoundError
+
+
+if not sys.warnoptions:
+    warnings.simplefilter("default", ResourceWarning)
+
+
+class TemporaryDirectory(object):
+    """Create and return a temporary directory.  This has the same
+    behavior as mkdtemp but can be used as a context manager.  For
+    example:
+
+        with TemporaryDirectory() as tmpdir:
+            ...
+
+    Upon exiting the context, the directory and everything contained
+    in it are removed.
+    """
+
+    def __init__(self, suffix="", prefix=None, dir=None):
+        if "RAM_DISK" in os.environ:
+            import uuid
+
+            name = uuid.uuid4().hex
+            dir_name = os.path.join(os.environ["RAM_DISK"].strip(), name)
+            os.mkdir(dir_name)
+            self.name = dir_name
+        else:
+            suffix = suffix if suffix else ""
+            if not prefix:
+                self.name = mkdtemp(suffix=suffix, dir=dir)
+            else:
+                self.name = mkdtemp(suffix, prefix, dir)
+        self._finalizer = finalize(
+            self,
+            self._cleanup,
+            self.name,
+            warn_message="Implicitly cleaning up {!r}".format(self),
+        )
+
+    @classmethod
+    def _cleanup(cls, name, warn_message):
+        from .path import rmtree
+        rmtree(name)
+        warnings.warn(warn_message, ResourceWarning)
+
+    def __repr__(self):
+        return "<{} {!r}>".format(self.__class__.__name__, self.name)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc, value, tb):
+        self.cleanup()
+
+    def cleanup(self):
+        from .path import rmtree
+        if self._finalizer.detach():
+            rmtree(self.name)
+
+
+def fs_str(string):
+    """Encodes a string into the proper filesystem encoding
+
+    Borrowed from pip-tools
+    """
+    if isinstance(string, str):
+        return string
+    assert not isinstance(string, bytes)
+    return string.encode(_fs_encoding)
+
+
+_fs_encoding = sys.getfilesystemencoding() or sys.getdefaultencoding()
---
+++ packages/vistir/vistir-0.2.0-py2.py3-none-any.whl/vistir/misc.py
@@ -0,0 +1,486 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import json
+import logging
+import locale
+import os
+import subprocess
+import sys
+
+from collections import OrderedDict
+from functools import partial
+from itertools import islice
+
+import six
+
+from .cmdparse import Script
+from .compat import Path, fs_str, partialmethod
+from .contextmanagers import spinner as spinner
+
+if os.name != "nt":
+    class WindowsError(OSError):
+        pass
+
+
+__all__ = [
+    "shell_escape",
+    "unnest",
+    "dedup",
+    "run",
+    "load_path",
+    "partialclass",
+    "to_text",
+    "to_bytes",
+    "locale_encoding",
+    "chunked",
+    "take",
+    "divide"
+]
+
+
+def _get_logger(name=None, level="ERROR"):
+    if not name:
+        name = __name__
+    if isinstance(level, six.string_types):
+        level = getattr(logging, level.upper())
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    formatter = logging.Formatter(
+        "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"
+    )
+    handler = logging.StreamHandler(stream=sys.stderr)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+def shell_escape(cmd):
+    """Escape strings for use in :func:`~subprocess.Popen` and :func:`run`.
+
+    This is a passthrough method for instantiating a :class:`~vistir.cmdparse.Script`
+    object which can be used to escape commands to output as a single string.
+    """
+    cmd = Script.parse(cmd)
+    return cmd.cmdify()
+
+
+def unnest(elem):
+    """Flatten an arbitrarily nested iterable
+
+    :param elem: An iterable to flatten
+    :type elem: :class:`~collections.Iterable`
+
+    >>> nested_iterable = (1234, (3456, 4398345, (234234)), (2396, (23895750, 9283798, 29384, (289375983275, 293759, 2347, (2098, 7987, 27599)))))
+    >>> list(vistir.misc.unnest(nested_iterable))
+    [1234, 3456, 4398345, 234234, 2396, 23895750, 9283798, 29384, 289375983275, 293759, 2347, 2098, 7987, 27599]
+    """
+
+    if _is_iterable(elem):
+        for item in elem:
+            if _is_iterable(item):
+                for sub_item in unnest(item):
+                    yield sub_item
+            else:
+                yield item
+    else:
+        raise ValueError("Expecting an iterable, got %r" % elem)
+
+
+def _is_iterable(elem):
+    if getattr(elem, "__iter__", False):
+        return True
+    return False
+
+
+def dedup(iterable):
+    """Deduplicate an iterable object like iter(set(iterable)) but
+    order-reserved.
+    """
+    return iter(OrderedDict.fromkeys(iterable))
+
+
+def _spawn_subprocess(script, env=None, block=True, cwd=None, combine_stderr=True):
+    from distutils.spawn import find_executable
+
+    if not env:
+        env = {}
+    command = find_executable(script.command)
+    options = {
+        "env": env,
+        "universal_newlines": True,
+        "stdout": subprocess.PIPE,
+        "stderr": subprocess.PIPE if not combine_stderr else subprocess.STDOUT,
+        "shell": False,
+    }
+    if not block:
+        options["stdin"] = subprocess.PIPE
+    if cwd:
+        options["cwd"] = cwd
+    # Command not found, maybe this is a shell built-in?
+    cmd = [command] + script.args
+    if not command:  # Try to use CreateProcess directly if possible.
+        cmd = script.cmdify()
+        options["shell"] = True
+
+    # Try to use CreateProcess directly if possible. Specifically catch
+    # Windows error 193 "Command is not a valid Win32 application" to handle
+    # a "command" that is non-executable. See pypa/pipenv#2727.
+    try:
+        return subprocess.Popen(cmd, **options)
+    except WindowsError as e:
+        if getattr(e, "winerror", 9999) != 193:
+            raise
+    options["shell"] = True
+    # Try shell mode to use Windows's file association for file launch.
+    return subprocess.Popen(script.cmdify(), **options)
+
+
+def _create_subprocess(
+    cmd,
+    env=None,
+    block=True,
+    return_object=False,
+    cwd=os.curdir,
+    verbose=False,
+    spinner=None,
+    combine_stderr=False,
+    display_limit=200
+):
+    if not env:
+        env = {}
+    try:
+        c = _spawn_subprocess(cmd, env=env, block=block, cwd=cwd,
+                                                    combine_stderr=combine_stderr)
+    except Exception as exc:
+        print("Error %s while executing command %s", exc, " ".join(cmd._parts))
+        raise
+    if not block:
+        c.stdin.close()
+        log_level = "DEBUG" if verbose else "ERROR"
+        logger = _get_logger(cmd._parts[0], level=log_level)
+        output = []
+        err = []
+        spinner_orig_text = ""
+        if spinner:
+            spinner_orig_text = getattr(spinner, "text", "")
+        streams = {
+            "stdout": c.stdout,
+            "stderr": c.stderr
+        }
+        while True:
+            stdout_line = None
+            stderr_line = None
+            for outstream in streams.keys():
+                stream = streams[outstream]
+                if not stream:
+                    continue
+                line = to_text(stream.readline())
+                if not line:
+                    continue
+                line = to_text("{0}".format(line.rstrip()))
+                if outstream == "stderr":
+                    stderr_line = line
+                else:
+                    stdout_line = line
+            if not (stdout_line or stderr_line):
+                break
+            if stderr_line:
+                err.append(stderr_line)
+                if verbose:
+                    if spinner:
+                        spinner.write_err(fs_str(stderr_line))
+                    else:
+                        logger.error(stderr_line)
+            if stdout_line:
+                output.append(stdout_line)
+                display_line = stdout_line
+                if len(stdout_line) > display_limit:
+                    display_line = "{0}...".format(stdout_line[:display_limit])
+                if verbose:
+                    if spinner:
+                        spinner.write_err(fs_str(display_line))
+                    else:
+                        logger.debug(display_line)
+                if spinner:
+                    spinner.text = fs_str("{0} {1}".format(spinner_orig_text, display_line))
+                continue
+        try:
+            c.wait()
+        finally:
+            if c.stdout:
+                c.stdout.close()
+            if c.stderr:
+                c.stderr.close()
+        if spinner:
+            if c.returncode > 0:
+                spinner.fail("Failed...cleaning up...")
+            else:
+                spinner.text = "Complete!"
+            if not os.name == "nt":
+                spinner.ok("✔")
+            else:
+                spinner.ok()
+        c.out = "\n".join(output)
+        c.err = "\n".join(err) if err else ""
+    else:
+        c.out, c.err = c.communicate()
+    if not block:
+        c.wait()
+    c.out = fs_str("{0}".format(c.out)) if c.out else fs_str("")
+    c.err = fs_str("{0}".format(c.err)) if c.err else fs_str("")
+    if not return_object:
+        return c.out.strip(), c.err.strip()
+    return c
+
+
+def run(
+    cmd,
+    env=None,
+    return_object=False,
+    block=True,
+    cwd=None,
+    verbose=False,
+    nospin=False,
+    spinner_name=None,
+    combine_stderr=True,
+    display_limit=200
+):
+    """Use `subprocess.Popen` to get the output of a command and decode it.
+
+    :param list cmd: A list representing the command you want to run.
+    :param dict env: Additional environment settings to pass through to the subprocess.
+    :param bool return_object: When True, returns the whole subprocess instance
+    :param bool block: When False, returns a potentially still-running :class:`subprocess.Popen` instance
+    :param str cwd: Current working directory contect to use for spawning the subprocess.
+    :param bool verbose: Whether to print stdout in real time when non-blocking.
+    :param bool nospin: Whether to disable the cli spinner.
+    :param str spinner_name: The name of the spinner to use if enabled, defaults to bouncingBar
+    :param bool combine_stderr: Optionally merge stdout and stderr in the subprocess, false if nonblocking.
+    :param int dispay_limit: The max width of output lines to display when using a spinner.
+    :returns: A 2-tuple of (output, error) or a :class:`subprocess.Popen` object.
+
+    .. Warning:: Merging standard out and standarad error in a nonblocking subprocess
+        can cause errors in some cases and may not be ideal. Consider disabling
+        this functionality.
+    """
+
+    _env = os.environ.copy()
+    if env:
+        _env.update(env)
+    env = _env
+    if six.PY2:
+        fs_encode = partial(to_bytes, encoding=locale_encoding)
+        _env = {fs_encode(k): fs_encode(v) for k, v in os.environ.items()}
+        for key, val in env.items():
+            _env[fs_encode(key)] = fs_encode(val)
+    else:
+        _env = {k: fs_str(v) for k, v in os.environ.items()}
+    if not spinner_name:
+        spinner_name = "bouncingBar"
+    if six.PY2:
+        if isinstance(cmd, six.string_types):
+            cmd = cmd.encode("utf-8")
+        elif isinstance(cmd, (list, tuple)):
+            cmd = [c.encode("utf-8") for c in cmd]
+    if not isinstance(cmd, Script):
+        cmd = Script.parse(cmd)
+    if block or not return_object:
+        combine_stderr = False
+    start_text = "Running..."
+    if nospin:
+        start_text = None
+    with spinner(spinner_name=spinner_name, start_text=start_text, nospin=nospin) as sp:
+        return _create_subprocess(
+            cmd,
+            env=_env,
+            return_object=return_object,
+            block=block,
+            cwd=cwd,
+            verbose=verbose,
+            spinner=sp,
+            combine_stderr=combine_stderr
+        )
+
+
+def load_path(python):
+    """Load the :mod:`sys.path` from the given python executable's environment as json
+
+    :param str python: Path to a valid python executable
+    :return: A python representation of the `sys.path` value of the given python executable.
+    :rtype: list
+
+    >>> load_path("/home/user/.virtualenvs/requirementslib-5MhGuG3C/bin/python")
+    ['', '/home/user/.virtualenvs/requirementslib-5MhGuG3C/lib/python37.zip', '/home/user/.virtualenvs/requirementslib-5MhGuG3C/lib/python3.7', '/home/user/.virtualenvs/requirementslib-5MhGuG3C/lib/python3.7/lib-dynload', '/home/user/.pyenv/versions/3.7.0/lib/python3.7', '/home/user/.virtualenvs/requirementslib-5MhGuG3C/lib/python3.7/site-packages', '/home/user/git/requirementslib/src']
+    """
+
+    python = Path(python).as_posix()
+    out, err = run([python, "-c", "import json, sys; print(json.dumps(sys.path))"],
+                        nospin=True)
+    if out:
+        return json.loads(out)
+    else:
+        return []
+
+
+def partialclass(cls, *args, **kwargs):
+    """Returns a partially instantiated class
+
+    :return: A partial class instance
+    :rtype: cls
+
+    >>> source = partialclass(Source, url="https://pypi.org/simple")
+    >>> source
+    <class '__main__.Source'>
+    >>> source(name="pypi")
+    >>> source.__dict__
+    mappingproxy({'__module__': '__main__', '__dict__': <attribute '__dict__' of 'Source' objects>, '__weakref__': <attribute '__weakref__' of 'Source' objects>, '__doc__': None, '__init__': functools.partialmethod(<function Source.__init__ at 0x7f23af429bf8>, , url='https://pypi.org/simple')})
+    >>> new_source = source(name="pypi")
+    >>> new_source
+    <__main__.Source object at 0x7f23af189b38>
+    >>> new_source.__dict__
+    {'url': 'https://pypi.org/simple', 'verify_ssl': True, 'name': 'pypi'}
+    """
+
+    name_attrs = [
+        n
+        for n in (getattr(cls, name, str(cls)) for name in ("__name__", "__qualname__"))
+        if n is not None
+    ]
+    name_attrs = name_attrs[0]
+    type_ = type(
+        name_attrs, (cls,), {"__init__": partialmethod(cls.__init__, *args, **kwargs)}
+    )
+    # Swiped from attrs.make_class
+    try:
+        type_.__module__ = sys._getframe(1).f_globals.get("__name__", "__main__")
+    except (AttributeError, ValueError):
+        pass
+    return type_
+
+
+# Borrowed from django -- force bytes and decode -- see link for details:
+# https://github.com/django/django/blob/fc6b90b/django/utils/encoding.py#L112
+def to_bytes(string, encoding="utf-8", errors="ignore"):
+    """Force a value to bytes.
+
+    :param string: Some input that can be converted to a bytes.
+    :type string: str or bytes unicode or a memoryview subclass
+    :param encoding: The encoding to use for conversions, defaults to "utf-8"
+    :param encoding: str, optional
+    :return: Corresponding byte representation (for use in filesystem operations)
+    :rtype: bytes
+    """
+
+    if not errors:
+        if encoding.lower() == "utf-8":
+            errors = "surrogateescape" if six.PY3 else "ignore"
+        else:
+            errors = "strict"
+    if isinstance(string, bytes):
+        if encoding.lower() == "utf-8":
+            return string
+        else:
+            return string.decode("utf-8").encode(encoding, errors)
+    elif isinstance(string, memoryview):
+        return bytes(string)
+    elif not isinstance(string, six.string_types):
+        try:
+            if six.PY3:
+                return six.text_type(string).encode(encoding, errors)
+            else:
+                return bytes(string)
+        except UnicodeEncodeError:
+            if isinstance(string, Exception):
+                return b" ".join(to_bytes(arg, encoding, errors) for arg in string)
+            return six.text_type(string).encode(encoding, errors)
+    else:
+        return string.encode(encoding, errors)
+
+
+def to_text(string, encoding="utf-8", errors=None):
+    """Force a value to a text-type.
+
+    :param string: Some input that can be converted to a unicode representation.
+    :type string: str or bytes unicode
+    :param encoding: The encoding to use for conversions, defaults to "utf-8"
+    :param encoding: str, optional
+    :return: The unicode representation of the string
+    :rtype: str
+    """
+
+    if not errors:
+        if encoding.lower() == "utf-8":
+            errors = "surrogateescape" if six.PY3 else "ignore"
+        else:
+            errors = "strict"
+    if issubclass(type(string), six.text_type):
+        return string
+    try:
+        if not issubclass(type(string), six.string_types):
+            if six.PY3:
+                if isinstance(string, bytes):
+                    string = six.text_type(string, encoding, errors)
+                else:
+                    string = six.text_type(string)
+            elif hasattr(string, "__unicode__"):
+                string = six.text_type(string)
+            else:
+                string = six.text_type(bytes(string), encoding, errors)
+        else:
+            string = string.decode(encoding, errors)
+    except UnicodeDecodeError as e:
+        string = " ".join(to_text(arg, encoding, errors) for arg in string)
+    return string
+
+
+def divide(n, iterable):
+    """
+    split an iterable into n groups, per https://more-itertools.readthedocs.io/en/latest/api.html#grouping
+
+    :param int n: Number of unique groups
+    :param iter iterable: An iterable to split up
+    :return: a list of new iterables derived from the original iterable
+    :rtype: list
+    """
+
+    seq = tuple(iterable)
+    q, r = divmod(len(seq), n)
+
+    ret = []
+    for i in range(n):
+        start = (i * q) + (i if i < r else r)
+        stop = ((i + 1) * q) + (i + 1 if i + 1 < r else r)
+        ret.append(iter(seq[start:stop]))
+
+    return ret
+
+
+def take(n, iterable):
+    """Take n elements from the supplied iterable without consuming it.
+
+    :param int n: Number of unique groups
+    :param iter iterable: An iterable to split up
+
+    from https://github.com/erikrose/more-itertools/blob/master/more_itertools/recipes.py
+    """
+
+    return list(islice(iterable, n))
+
+
+def chunked(n, iterable):
+    """Split an iterable into lists of length *n*.
+
+    :param int n: Number of unique groups
+    :param iter iterable: An iterable to split up
+
+    from https://github.com/erikrose/more-itertools/blob/master/more_itertools/more.py
+    """
+
+    return iter(partial(take, n, iter(iterable)), [])
+
+
+try:
+    locale_encoding = locale.getdefaultencoding()[1] or "ascii"
+except Exception:
+    locale_encoding = "ascii"
