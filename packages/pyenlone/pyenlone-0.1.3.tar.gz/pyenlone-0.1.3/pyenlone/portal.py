150cb6db6626091b1834730b68184f8695d54eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:23 +0000

    Add package pyenlone/0.1.3/pyenlone-0.1.3.tar.gz

---
+++ packages/pyenlone/pyenlone-0.1.3.tar.gz/pyenlone-0.1.3/pyenlone/portal.py
@@ -0,0 +1,16 @@
+class Portal:
+    @property
+    def guid():
+	    pass
+	
+	@property
+	def name():
+	    pass
+	    
+	@property
+	def lat():
+	    pass
+	    
+    @property
+    def lon():
+        pass
---
+++ packages/pyenlone/pyenlone-0.1.3.tar.gz/pyenlone-0.1.3/pyenlone/tasks/operation.py
@@ -0,0 +1,7 @@
+from .._proxy import ProxyFactory
+from .task import Task
+from .message import Message
+from .grant import Grant
+
+class Operation:
+    pass
