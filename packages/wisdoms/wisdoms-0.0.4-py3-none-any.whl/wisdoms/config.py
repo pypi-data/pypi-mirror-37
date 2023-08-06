e80ba58c7a87daa3b3065fa155541009313445e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:17 +0000

    Add package wisdoms/0.0.4/wisdoms-0.0.4-py3-none-any.whl

---
+++ packages/wisdoms/wisdoms-0.0.4-py3-none-any.whl/wisdoms/config.py
@@ -0,0 +1,49 @@
+# Created by Q-ays.
+# whosqays@gmail.com
+
+
+"""
+    :usage:
+        from wisdoms.config import c
+        c.get('name')
+"""
+
+import yaml
+
+
+class Config:
+    """
+    读取yml配置文件
+    """
+
+    def __init__(self):
+
+        try:
+            with open('../.env', 'r', encoding='utf-8') as f:
+                env = f.read().strip()
+        except Exception as e:
+            print('file path is ../../.env')
+            with open('../../.env', 'r', encoding='utf-8') as f:
+                env = f.read().strip()
+
+        if env:
+            try:
+                with open('./config/' + env + '.yml') as f:
+                    self.configuration = yaml.load(f)
+            except Exception as e:
+                print('file path is ../config')
+                with open('../config/' + env + '.yml') as f:
+                    self.configuration = yaml.load(f)
+
+    def get(self, key):
+        if self.configuration:
+            return self.configuration[key]
+        else:
+            return {'err_code': 'maybe environment variable is missed'}
+
+    def to_dict(self):
+        if self.configuration:
+            return self.configuration
+
+
+c = Config()
