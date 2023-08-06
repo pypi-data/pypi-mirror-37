54e81438f12924fa1b45a328b18760676a97b475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:45 +0000

    Add package flythings/1.0.0/flythings-1.0.0.zip

---
+++ packages/flythings/flythings-1.0.0.zip/flythings-1.0.0/flythings/__init__.py
@@ -0,0 +1,10 @@
+import os
+try:
+	import requests
+except ImportError:
+	print ("Trying to Install required module: requests\n")
+	os.system('python -m pip install requests')
+
+from flythings.client import search,sendObservation,sendObservations,login,setDevice,setSensor,setServer\
+	,setToken,setTimeout,getObservation,setCustomHeader,setWorkspace,sendSocket,findSeries,sendRecord\
+	,registerAction, registerActionForSeries,startActionListening, stopActionListening, ActionDataTypes
---
+++ packages/flythings/flythings-1.0.0.zip/flythings-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name='flythings',
+  packages=['flythings'],
+  version='1.0.0',
+  description='A python library to add ans search observations into flythings',
+  author='flythings',
+  author_email = 'tic@itg.es',
+  license='GPL-3.0',
+  url='https://github.com/flythings/python',
+  download_url='https://github.com/flythings/python/1.0.0.tar.gz',
+  keywords=['flythings', 'IoT'],
+  install_requires=['requests'],
+  classifiers=[]
+)
