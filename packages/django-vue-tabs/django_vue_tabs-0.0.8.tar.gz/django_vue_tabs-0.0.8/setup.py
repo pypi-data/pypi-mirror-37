e667ec4079126e64935d5afea546d0e45add16e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:31 +0000

    Add package django-vue-tabs/0.0.8/django_vue_tabs-0.0.8.tar.gz

---
+++ packages/django-vue-tabs/django_vue_tabs-0.0.8.tar.gz/django_vue_tabs-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.0.8'
+
+CLASSIFIERS = [
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Topic :: Software Development',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3',
+    'Environment :: Web Environment',
+    'Development Status :: 5 - Production/Stable',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+setup(
+    name='django_vue_tabs',
+    description='Django: Add tabs to ModelAdmin ',
+    version=VERSION,
+    author='Alexandre Busquets Triola',
+    author_email='abusquets@gmail.com',
+    license='MIT License',
+    platforms=['OS Independent'],
+    url='http://github.com/microdisseny/django-vue-tabs',
+    packages=find_packages(exclude=['__pycache__']),
+    include_package_data=True,
+    classifiers=CLASSIFIERS,
+    zip_safe=False,
+)
