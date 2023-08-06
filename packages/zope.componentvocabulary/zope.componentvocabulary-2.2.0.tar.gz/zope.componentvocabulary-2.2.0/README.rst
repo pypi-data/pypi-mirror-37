6e2247d1640ea31d844ef5911328dec03efc0b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:01 +0000

    Add package zope.componentvocabulary/2.2.0/zope.componentvocabulary-2.2.0.tar.gz

---
+++ packages/zope.componentvocabulary/zope.componentvocabulary-2.2.0.tar.gz/zope.componentvocabulary-2.2.0/README.rst
@@ -0,0 +1,12 @@
+``zope.componentvocabulary``
+============================
+
+.. image:: https://travis-ci.org/zopefoundation/zope.componentvocabulary.svg?branch=master
+        :target: https://travis-ci.org/zopefoundation/zope.componentvocabulary
+
+.. image:: https://coveralls.io/repos/github/zopefoundation/zope.componentvocabulary/badge.svg?branch=master
+        :target: https://coveralls.io/github/zopefoundation/zope.componentvocabulary?branch=master
+
+
+
+This package contains various vocabularies.
---
+++ packages/zope.componentvocabulary/zope.componentvocabulary-2.2.0.tar.gz/zope.componentvocabulary-2.2.0/setup.py
@@ -0,0 +1,80 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup for zope.app.catalog package
+"""
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+TEST_REQUIRES = [
+    'zope.configuration',
+    'zope.testing',
+    'zope.testrunner',
+]
+
+setup(name='zope.componentvocabulary',
+      version='2.2.0',
+      author='Zope Foundation and Contributors',
+      author_email='zope-dev@zope.org',
+      description='Component vocabularies',
+      long_description=(
+          read('README.rst')
+          + '\n\n' +
+          read('CHANGES.rst')
+      ),
+      keywords="zope component architecture vocabulary",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Zope Public License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Topic :: Internet :: WWW/HTTP',
+          'Framework :: Zope :: 3',
+      ],
+      url='http://github.com/zopefoundation/zope.componentvocabulary',
+      license='ZPL 2.1',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['zope'],
+      install_requires=[
+          'setuptools',
+          'six',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.schema',
+          'zope.security',
+      ],
+      extras_require={
+          'test': TEST_REQUIRES,
+      },
+      tests_require=TEST_REQUIRES,
+      include_package_data=True,
+      zip_safe=False,
+)
