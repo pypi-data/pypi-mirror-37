2e2aba7fd356042d910189837d4aa6d90872d700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:08 +0000

    Add package nflsim/1.0/nflsim-1.0.tar.gz

---
+++ packages/nflsim/nflsim-1.0.tar.gz/nflsim-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='nflsim',
+    url='https://github.com/dmparker0/nflsim/',
+    author='Dan Parker',
+    author_email='dan.m.parker0@gmail.com',
+    packages=['nflsim'],
+    install_requires=['numpy','pandas','scipy','bs4','requests','joblib'],
+    version='1.0',
+    license='MIT',
+    description='A tool for simulating the NFL regular season and playoffs',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    download_url = 'https://github.com/dmparker0/nflsim/archive/v1.0.tar.gz',
+    keywords = ['NFL', 'football', 'sports','simulation','statistics'], 
+)
+
