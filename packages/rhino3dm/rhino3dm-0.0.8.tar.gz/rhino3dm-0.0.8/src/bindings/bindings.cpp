2b9b64a4dc02d8b5be409f64711bac89356ee2b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:00 +0000

    Add package rhino3dm/0.0.8/rhino3dm-0.0.8.tar.gz

---
+++ packages/rhino3dm/rhino3dm-0.0.8.tar.gz/rhino3dm-0.0.8/src/bindings/bindings.cpp
@@ -0,0 +1,48 @@
+#include "bindings.h"
+
+#if defined(ON_PYTHON_COMPILE)
+namespace py = pybind11;
+PYBIND11_MODULE(_rhino3dm, m){
+  m.doc() = "rhino3dm python package. OpenNURBS wrappers with a RhinoCommon style";
+#endif
+
+#if defined(ON_WASM_COMPILE)
+using namespace emscripten;
+EMSCRIPTEN_BINDINGS(rhino3dm) {
+  void* m = nullptr;
+#endif
+
+  ON::Begin();
+  initDefines(m);
+  initPolylineBindings(m);
+  initObjectBindings(m);
+  init3dmAttributesBindings(m);
+  initLayerBindings(m);
+  initPointBindings(m);
+  initXformBindings(m);
+  initArcBindings(m);
+  initBoundingBoxBindings(m);
+  initBoxBindings(m);
+  initGeometryBindings(m);
+  initHatchBindings(m);
+  initCurveBindings(m);
+  initCurveProxyBindings(m);
+  initNurbsCurveBindings(m);
+  initMeshBindings(m);
+  initCircleBindings(m);
+  initConeBindings(m);
+  initCylinderBindings(m);
+  initArcCurveBindings(m);
+  initBezierBindings(m);
+  initLineCurveBindings(m);
+  initPolyCurveBindings(m);
+  initPolylineCurveBindings(m);
+  initSurfaceBindings(m);
+  initSurfaceProxyBindings(m);
+  initBrepBindings(m);
+  initExtrusionBindings(m);
+  initNurbsSurfaceBindings(m);
+  initSphereBindings(m);
+  initViewportBindings(m);
+  initExtensionsBindings(m);
+}
---
+++ packages/rhino3dm/rhino3dm-0.0.8.tar.gz/rhino3dm-0.0.8/src/bindings/bindings.h
@@ -0,0 +1,59 @@
+#pragma once
+
+// we don't need to export RH_C_FUNCTION in these libraries
+#define RH_C_FUNCTION
+
+#if defined(__EMSCRIPTEN__)
+#define ON_WASM_COMPILE
+#else
+#define ON_PYTHON_COMPILE
+#endif
+
+#if defined(ON_PYTHON_COMPILE)
+#include <pybind11/pybind11.h>
+#pragma comment(lib, "rpcrt4.lib")
+#pragma comment(lib, "shlwapi.lib")
+#endif
+
+#include "../lib/opennurbs/opennurbs.h"
+
+
+#if defined(ON_WASM_COMPILE)
+#include <emscripten/bind.h>
+#endif
+
+#include "bnd_color.h"
+#include "bnd_uuid.h"
+#include "bnd_defines.h"
+#include "bnd_boundingbox.h"
+#include "bnd_box.h"
+#include "bnd_point.h"
+#include "bnd_polyline.h"
+#include "bnd_plane.h"
+#include "bnd_xform.h"
+#include "bnd_circle.h"
+#include "bnd_cone.h"
+#include "bnd_cylinder.h"
+#include "bnd_object.h"
+#include "bnd_3dm_attributes.h"
+#include "bnd_layer.h"
+#include "bnd_geometry.h"
+#include "bnd_hatch.h"
+#include "bnd_arc.h"
+#include "bnd_curve.h"
+#include "bnd_curveproxy.h"
+#include "bnd_arccurve.h"
+#include "bnd_bezier.h"
+#include "bnd_linecurve.h"
+#include "bnd_polycurve.h"
+#include "bnd_polylinecurve.h"
+#include "bnd_nurbscurve.h"
+#include "bnd_mesh.h"
+#include "bnd_surface.h"
+#include "bnd_surfaceproxy.h"
+#include "bnd_brep.h"
+#include "bnd_beam.h"
+#include "bnd_nurbssurface.h"
+#include "bnd_sphere.h"
+#include "bnd_viewport.h"
+#include "bnd_extensions.h"
