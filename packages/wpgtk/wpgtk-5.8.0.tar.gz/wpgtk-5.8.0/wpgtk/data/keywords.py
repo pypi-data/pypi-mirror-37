312f0b4fa527a58d1a347499ca05195566cd0270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:05 +0000

    Add package wpgtk/5.8.0/wpgtk-5.8.0.tar.gz

---
+++ packages/wpgtk/wpgtk-5.8.0.tar.gz/wpgtk-5.8.0/wpgtk/data/keywords.py
@@ -0,0 +1,41 @@
+from .config import user_keywords, write_conf
+
+KEY_LENGTH = 5
+VAL_LENGTH = 2
+
+
+def update_keyword(old_keyword, new_keyword, save=False):
+    """validates and updates a keyword"""
+    if(len(new_keyword) < KEY_LENGTH):
+        raise Exception('Keyword must be longer than 5 characters')
+
+    user_keywords[new_keyword] = user_keywords[old_keyword]
+
+    if(old_keyword != new_keyword):
+        user_keywords.pop(old_keyword, None)
+
+    if save:
+        write_conf()
+
+
+def update_value(keyword, value, save=False):
+    if(len(value) < 2):
+        raise Exception('Value must be longer than 3 characters')
+
+    user_keywords[keyword] = value
+
+    if save:
+        write_conf()
+
+
+def create_pair(keyword, value, save=False):
+    if(len(value) < VAL_LENGTH):
+        raise Exception('Value must be longer than 3 characters')
+
+    if(len(keyword) < KEY_LENGTH):
+        raise Exception('Value must be longer than 3 characters')
+
+    user_keywords[keyword] = value
+
+    if save:
+        write_conf()
