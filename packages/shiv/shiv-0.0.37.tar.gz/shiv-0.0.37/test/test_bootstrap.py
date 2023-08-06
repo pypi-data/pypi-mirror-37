1f70c0bec08918e83f1f312e414c509aab254171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:57 +0000

    Add package shiv/0.0.37/shiv-0.0.37.tar.gz

---
+++ packages/shiv/shiv-0.0.37.tar.gz/shiv-0.0.37/test/test_bootstrap.py
@@ -0,0 +1,108 @@
+import os
+import sys
+
+from contextlib import contextmanager
+from pathlib import Path
+from site import addsitedir
+from code import interact
+from uuid import uuid4
+from zipfile import ZipFile
+
+import pytest
+
+from unittest import mock
+
+from shiv.bootstrap import import_string, current_zipfile, cache_path, _first_sitedir_index
+from shiv.bootstrap.environment import Environment
+
+
+@contextmanager
+def env_var(key, value):
+    os.environ[key] = value
+    yield
+    del os.environ[key]
+
+
+class TestBootstrap:
+    def test_various_imports(self):
+        assert import_string('site.addsitedir') == addsitedir
+        assert import_string('site:addsitedir') == addsitedir
+        assert import_string('code.interact') == interact
+        assert import_string('code:interact') == interact
+
+        # test things not already imported
+        func = import_string('os.path:join')
+        from os.path import join
+        assert func == join
+
+        # test something already imported
+        import shiv
+        assert import_string('shiv') == shiv == sys.modules['shiv']
+
+        # test bogus imports raise properly
+        with pytest.raises(ImportError):
+            import_string('this is bogus!')
+
+    def test_is_zipfile(self, zip_location):
+        with mock.patch.object(sys, 'argv', [zip_location]):
+            assert isinstance(current_zipfile(), ZipFile)
+
+    # When the tests are run via tox, sys.argv[0] is the full path to 'pytest.EXE',
+    # i.e. a native launcher created by pip to from console_scripts entry points.
+    # These are indeed a form of zip files, thus the following assertion could fail.
+    @pytest.mark.skipif(os.name == 'nt', reason="this may give false positive on win")
+    def test_argv0_is_not_zipfile(self):
+        assert not current_zipfile()
+
+    def test_cache_path(self):
+        mock_zip = mock.MagicMock(spec=ZipFile)
+        mock_zip.filename = "test"
+        uuid = str(uuid4())
+
+        assert cache_path(mock_zip, Path.cwd(), uuid) == Path.cwd() / f"test_{uuid}"
+
+    def test_first_sitedir_index(self):
+        with mock.patch.object(sys, 'path', ['site-packages', 'dir', 'dir', 'dir']):
+            assert _first_sitedir_index() == 0
+
+        with mock.patch.object(sys, 'path', []):
+            assert _first_sitedir_index() is None
+
+
+class TestEnvironment:
+    def test_overrides(self):
+        env = Environment()
+
+        assert env.entry_point is None
+        with env_var('SHIV_ENTRY_POINT', 'test'):
+            assert env.entry_point == 'test'
+
+        assert env.interpreter is None
+        with env_var('SHIV_INTERPRETER', '1'):
+            assert env.interpreter is not None
+
+        assert env.root is None
+        with env_var('SHIV_ROOT', 'tmp'):
+            assert env.root == Path('tmp')
+
+        assert env.force_extract is False
+        with env_var('SHIV_FORCE_EXTRACT', '1'):
+            assert env.force_extract is True
+
+        assert env.compile_pyc is True
+        with env_var("SHIV_COMPILE_PYC", "False"):
+            assert env.compile_pyc is False
+
+        assert env.compile_workers == 0
+        with env_var("SHIV_COMPILE_WORKERS", "1"):
+            assert env.compile_workers == 1
+
+        # ensure that non-digits are ignored
+        with env_var("SHIV_COMPILE_WORKERS", "one bazillion"):
+            assert env.compile_workers == 0
+
+    def test_serialize(self):
+        env = Environment()
+        env_as_json = env.to_json()
+        env_from_json = Environment.from_json(env_as_json)
+        assert env.__dict__ == env_from_json.__dict__
---
+++ packages/shiv/shiv-0.0.37.tar.gz/shiv-0.0.37/test/test_cli.py
@@ -0,0 +1,69 @@
+import os
+import subprocess
+import tempfile
+
+from pathlib import Path
+
+import pytest
+
+from click.testing import CliRunner
+
+from shiv.cli import main, _interpreter_path
+from shiv.constants import DISALLOWED_PIP_ARGS, NO_PIP_ARGS_OR_SITE_PACKAGES, NO_OUTFILE, BLACKLISTED_ARGS
+
+
+def strip_header(output):
+    return '\n'.join(output.splitlines()[1:])
+
+
+class TestCLI:
+    @pytest.fixture
+    def runner(self):
+        return lambda args: CliRunner().invoke(main, args)
+
+    def test_no_args(self, runner):
+        result = runner([])
+        assert result.exit_code == 1
+        assert strip_header(result.output) == NO_PIP_ARGS_OR_SITE_PACKAGES
+
+    def test_no_outfile(self, runner):
+        result = runner(['-e', 'test', 'flask'])
+        assert result.exit_code == 1
+        assert strip_header(result.output) == NO_OUTFILE
+
+    def test_find_interpreter(self):
+        interpreter = _interpreter_path()
+        assert Path(interpreter).exists()
+        assert Path(interpreter).is_file()
+
+    @pytest.mark.parametrize("arg", [arg for tup in BLACKLISTED_ARGS.keys() for arg in tup])
+    def test_blacklisted_args(self, runner, arg):
+        result = runner(['-o', 'tmp', arg])
+
+        # get the 'reason' message:
+        for tup in BLACKLISTED_ARGS:
+            if arg in tup:
+                reason = BLACKLISTED_ARGS[tup]
+
+        assert result.exit_code == 1
+
+        # assert we got the correct reason
+        assert strip_header(result.output) == DISALLOWED_PIP_ARGS.format(arg=arg, reason=reason)
+
+    @pytest.mark.parametrize('compile_option', ["--compile-pyc", "--no-compile-pyc"])
+    def test_hello_world(self, tmpdir, runner, package_location, compile_option):
+
+        with tempfile.TemporaryDirectory(dir=tmpdir) as tmpdir:
+            output_file = Path(tmpdir, 'test.pyz')
+
+            result = runner(['-e', 'hello:main', '-o', str(output_file), str(package_location), compile_option])
+
+            # check that the command successfully completed
+            assert result.exit_code == 0
+
+            # ensure the created file actually exists
+            assert output_file.exists()
+
+            # now run the produced zipapp
+            with subprocess.Popen([str(output_file)], stdout=subprocess.PIPE, shell=True) as proc:
+                assert proc.stdout.read().decode() == "hello world" + os.linesep
