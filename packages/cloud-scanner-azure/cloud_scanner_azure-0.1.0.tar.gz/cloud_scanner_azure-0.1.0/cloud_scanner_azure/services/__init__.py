128d15441d3e2c25c6dfcd90cb2009f52b7b6466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:53 +0000

    Add package cloud-scanner-azure/0.1.0/cloud_scanner_azure-0.1.0.tar.gz

---
+++ packages/cloud-scanner-azure/cloud_scanner_azure-0.1.0.tar.gz/cloud_scanner_azure-0.1.0/cloud_scanner_azure/services/__init__.py
@@ -0,0 +1,9 @@
+from cloud_scanner_azure.services.azure_cosmosdb import AzureCosmosDb
+from cloud_scanner_azure.services.azure_resource import AzureResource
+from cloud_scanner_azure.services.azure_resource_service import (
+    AzureResourceService)
+from cloud_scanner_azure.services.azure_storage_container import (
+    AzureStorageContainer)
+from cloud_scanner_azure.services.azure_storage_queue import AzureStorageQueue
+from cloud_scanner_azure.services.azure_subscription_service import (
+    AzureSubscriptionService)
---
+++ packages/cloud-scanner-azure/cloud_scanner_azure-0.1.0.tar.gz/cloud_scanner_azure-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+exec(open('cloud_scanner_azure/version.py').read())
+setup(name='cloud_scanner_azure',
+      version=__version__,
+      description='Core package for scanning Azure cloud resources',
+      url='https://microsoft.github.io/cloud-scanner-azure',
+      author='Microsoft',
+      author_email='tanner.barlow@microsoft.com,wallace.breza@microsoft.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'azure-mgmt-nspkg',
+          'azure-mgmt-resource',
+          'azure-mgmt-subscription',
+          'azure-servicebus',
+          'azure-storage',
+          'azure-storage-common',
+          'azure-cosmosdb-table',
+          'azure-functions'
+      ])
