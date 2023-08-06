5e73c6fedb0a39bcc4f4d902157a16d49dcd9438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:33 +0000

    Add package mw-aiohttp-babel/0.1.5/mw-aiohttp-babel-0.1.5.tar.gz

---
+++ packages/mw-aiohttp-babel/mw-aiohttp-babel-0.1.5.tar.gz/mw-aiohttp-babel-0.1.5/mw_aiohttp_babel/middlewares.py
@@ -0,0 +1,32 @@
+import asyncio
+from speaklater import is_lazy_string, make_lazy_string
+from mw_aiohttp_babel import locale
+from threading import local
+
+_thread_locals = local()
+
+def make_lazy_gettext(lookup_func):
+    def lazy_gettext(string, *args, **kwargs):
+        if is_lazy_string(string):
+            return string
+        return make_lazy_string(lookup_func(), string, *args, **kwargs)
+    return lazy_gettext
+
+laze_gettext = make_lazy_gettext(lambda: _thread_locals.locale.translate)
+
+def gettext(string,*args,**kwargs):
+    return get_current_locale().translate(string,*args,**kwargs)
+
+@asyncio.coroutine
+def babel_middleware(app, handler):
+    @asyncio.coroutine
+    def middleware(request):
+        # get locale from cookie
+        _code = locale.detect_locale(request)
+        _thread_locals.locale = request.locale = locale.get(_code)
+        response = yield from handler(request)
+        return response
+    return middleware
+
+def get_current_locale():
+    return getattr(_thread_locals, 'locale', None)
