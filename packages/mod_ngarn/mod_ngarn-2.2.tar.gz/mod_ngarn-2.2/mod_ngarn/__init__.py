46c47bb172713d6262687610e5a3129d8f4b592c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:41 +0000

    Add package mod_ngarn/2.2/mod_ngarn-2.2.tar.gz

---
+++ packages/mod_ngarn/mod_ngarn-2.2.tar.gz/mod_ngarn-2.2/mod_ngarn/__init__.py
@@ -0,0 +1,26 @@
+"""Simple async worker"""
+
+__version__ = "2.2"
+
+import asyncio
+
+import click
+
+from .worker import JobRunner
+
+global script
+global run
+
+@click.group()
+def script():
+    pass
+
+
+@click.command()
+def run():
+    job_runner = JobRunner()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(job_runner.run())
+
+
+script.add_command(run)
