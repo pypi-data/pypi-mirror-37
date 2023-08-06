6af8bbf555ec3c80640a06a403c75e843bf272d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:25 +0000

    Add package module1991/1.2.2/module1991-1.2.2.tar.gz

---
+++ packages/module1991/module1991-1.2.2.tar.gz/module1991-1.2.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+__version__ = "1.2.2"
+setup(name= "module1991",
+	version="1.2.2",
+	description="description",
+	long_description="long_description",
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"License :: OSI Approved :: Apache Software License",
+		"Programming Language :: Python :: 3.6"],
+	keywords='keywoards',
+	url='http://vk.com',
+	author='name lastname',
+	author_email='email@gmail.com',
+	license='Apache Software ',
+	packages=find_packages(),
+	install_requires=['opencv-python', 'bot_vk'],
+	include_package_data=True,
+	zip_safe=False
+)
