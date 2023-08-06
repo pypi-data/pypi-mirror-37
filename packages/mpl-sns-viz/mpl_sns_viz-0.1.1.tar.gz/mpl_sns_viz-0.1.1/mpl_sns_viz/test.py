34b34059b72a177eedf16ca85280421f675961a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:38 +0000

    Add package mpl-sns-viz/0.1.1/mpl_sns_viz-0.1.1.tar.gz

---
+++ packages/mpl-sns-viz/mpl_sns_viz-0.1.1.tar.gz/mpl_sns_viz-0.1.1/mpl_sns_viz/test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from mpl_sns_viz import Viz
+
+np.random.seed(42)
+
+data = np.random.rand(10,2)
+x = data[:, 0]
+y = data[:, 1]
+
+#fig, ax = plt.subplots(1, 1, figsize=(5,5))
+
+#Viz(1, 5, 5)
+
---
+++ packages/mpl-sns-viz/mpl_sns_viz-0.1.1.tar.gz/mpl_sns_viz-0.1.1/mpl_sns_viz/viz.py
@@ -0,0 +1,26 @@
+# Import libaries
+import matplotlib.pyplot as plt 
+
+class Viz():
+
+	def __init__(self, num_plots=1, fig_width=5, fig_height=5):
+
+		'''Generic Viz class for creating subplots.
+		
+		Attributes:
+			plot_num (int): number of plots needed in visualization 
+		'''
+
+		self.num_plots = num_plots
+		self.fig_height = fig_height
+		self.fig_width = fig_width
+
+	#def num_plots(self):
+
+		'''creates
+		
+		[description]
+		'''
+	
+
+
