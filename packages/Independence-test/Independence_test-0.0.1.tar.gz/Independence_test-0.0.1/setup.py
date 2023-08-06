62167248c089567075b8e7e6be302dbeebb10ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:25 +0000

    Add package Independence-test/0.0.1/Independence_test-0.0.1.tar.gz

---
+++ packages/Independence-test/Independence_test-0.0.1.tar.gz/Independence_test-0.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  3 16:50:29 2018
+
+@author: yili.peng
+"""
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='Independence_test'
+      ,version='0.0.1'
+      ,description='Time Series Independence test'
+      ,long_description=readme()
+      ,keywords='Time Series'
+      ,author='Yili Peng'
+      ,author_email='yili.peng@outlook.com'
+      ,packages=['Independence_test'])
