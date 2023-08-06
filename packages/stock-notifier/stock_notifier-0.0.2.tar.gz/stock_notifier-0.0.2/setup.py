6cda281581d1075e22c70aeb53eb50c33fa214f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:09 +0000

    Add package stock-notifier/0.0.2/stock_notifier-0.0.2.tar.gz

---
+++ packages/stock-notifier/stock_notifier-0.0.2.tar.gz/stock_notifier-0.0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stock_notifier",
+    version="0.0.2",
+    author="Saswat Raj",
+    author_email="saswatraj@outlook.com",
+    description="An application that provides system notifications for changes in stock values",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/saswatraj/stock_notifier",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: MacOS X",
+    ],
+    install_requires = [
+        'pync',
+        'BeautifulSoup'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'stock-notify=stock_notifier.command_line:main'
+        ]
+    }
+)
