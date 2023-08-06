8d4050646c17ece6819886e22bb7191246bba7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:07 +0000

    Add package umihico/0.2.8/umihico-0.2.8.tar.gz

---
+++ packages/umihico/umihico-0.2.8.tar.gz/umihico-0.2.8/umihico/scraping/lxml_.py
@@ -0,0 +1,8 @@
+from lxml.etree import ElementBase as _ElementBase
+
+
+def elements_to_strings_list(lxml_elements):
+    lxml_elements = lxml_elements if isinstance(
+        lxml_elements, list) else [lxml_elements, ]
+    return [str(element.text_content() if isinstance(
+        element, _ElementBase) else element) for element in lxml_elements]
