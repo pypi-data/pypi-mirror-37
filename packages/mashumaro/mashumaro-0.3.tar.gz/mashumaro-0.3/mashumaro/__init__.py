6e41a1669d6bb687d8e3563bafec9cf913c22e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:48 +0000

    Add package mashumaro/0.3/mashumaro-0.3.tar.gz

---
+++ packages/mashumaro/mashumaro-0.3.tar.gz/mashumaro-0.3/mashumaro/__init__.py
@@ -0,0 +1,5 @@
+from mashumaro.serializer.base.dict import DataClassDictMixin
+from mashumaro.serializer.msgpack import DataClassMessagePackMixin
+from mashumaro.serializer.yaml import DataClassYAMLMixin
+
+from mashumaro.exceptions import MissingField
