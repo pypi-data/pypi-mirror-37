4d2bf2c343e09bda0c3102e2f74f9543b781f70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:25 +0000

    Add package pymc-learn/0.0.1rc1/pymc_learn-0.0.1rc1-py3-none-any.whl

---
+++ packages/pymc-learn/pymc_learn-0.0.1rc1-py3-none-any.whl/pmlearn/naive_bayes/utils.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+
+def normalize(array):
+    """
+    Normalize values in the array to get probabilities.
+    Parameters
+    ----------
+    array : numpy array of shape [1,]
+    Returns
+    -------
+    A normalized array
+    """
+    return array/np.sum(array)
