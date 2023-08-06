9a507103cddab1853d401758a7ddfe58a8b65eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:48 +0000

    Add package telegraf-cloudflare/0.2.3/telegraf_cloudflare-0.2.3-py2-none-any.whl

---
+++ packages/telegraf-cloudflare/telegraf_cloudflare-0.2.3-py2-none-any.whl/cloudflare/parser.py
@@ -0,0 +1,60 @@
+import calendar
+from dateutil import parser
+from influx_line_protocol import Metric, MetricCollection
+
+
+class Parser(object):
+
+    NANOSECONDS_MULTIPILER = 10 ** 9
+
+    def parse_dashboard(self, zone_id, dashboard):
+        collections = MetricCollection()
+        [collections.append(self.parse_timeserie(zone_id, timeserie)) for timeserie in dashboard['timeseries']]
+
+        return collections
+
+    def parse_timeserie(self, zone_id, timeserie):
+        metric = Metric('cloudflare')
+        metric.with_timestamp(self.parse_time(timeserie['until']))
+        metric.add_tag('zone_id', zone_id)
+        metric.add_value('uniques', timeserie['uniques']['all'])
+        self.__parse_requests(metric, timeserie['requests'])
+        self.__parse_bandwidth(metric, timeserie['bandwidth'])
+
+        return metric
+
+    def __parse_requests(self, metric, requests):
+        metric.add_value('requests_all', requests['all'])
+        metric.add_value('requests_cached', requests['cached'])
+        metric.add_value('requests_uncached', requests['uncached'])
+        metric.add_value('requests_encrypted', requests['ssl']['encrypted'])
+        metric.add_value('requests_unencrypted', requests['ssl']['unencrypted'])
+
+        for status in requests['http_status']:
+            metric.add_value('requests_status_%s' % status, requests['http_status'][status])
+
+        for content_type in requests['content_type']:
+            metric.add_value('requests_content_type_%s' % content_type, requests['content_type'][content_type])
+
+        for country in requests['country']:
+            metric.add_value('requests_country_%s' % country.lower(), requests['country'][country])
+
+        for ip_class in requests['ip_class']:
+            metric.add_value('requests_ip_class_%s' % ip_class, requests['ip_class'][ip_class])
+
+    def __parse_bandwidth(self, metric, bandwidth):
+        metric.add_value('bandwidth_all', bandwidth['all'])
+        metric.add_value('bandwidth_cached', bandwidth['cached'])
+        metric.add_value('bandwidth_uncached', bandwidth['uncached'])
+        metric.add_value('bandwidth_encrypted', bandwidth['ssl']['encrypted'])
+        metric.add_value('bandwidth_unencrypted', bandwidth['ssl']['unencrypted'])
+
+        for content_type in bandwidth['content_type']:
+            metric.add_value('bandwidth_content_type_%s' % content_type, bandwidth['content_type'][content_type])
+
+        for country in bandwidth['country']:
+            metric.add_value('bandwidth_country_%s' % country.lower(), bandwidth['country'][country])
+
+    def parse_time(self, datetime):
+        d = parser.parse(datetime)
+        return int(calendar.timegm(d.timetuple())) * self.NANOSECONDS_MULTIPILER
---
+++ packages/telegraf-cloudflare/telegraf_cloudflare-0.2.3-py2-none-any.whl/cloudflare/test_parser.py
@@ -0,0 +1,118 @@
+import unittest
+from .parser import Parser
+from influx_line_protocol import Metric, MetricCollection
+
+
+class ParserTest(unittest.TestCase):
+    timeserie = {
+        "until": "2018-08-22T11:45:00Z",
+        "requests": {
+          "all": 258,
+          "cached": 142,
+          "uncached": 116,
+          "ssl": {
+            "encrypted": 255,
+            "unencrypted": 3
+          },
+          "http_status": {
+            "200": 229,
+            "499": 2
+          },
+          "content_type": {
+            "css": 10,
+            "html": 96,
+          },
+          "country": {
+            "PL": 183,
+            "US": 14
+          },
+          "ip_class": {
+            "monitoringService": 13,
+            "noRecord": 215,
+            "searchEngine": 30
+          }
+        },
+        "bandwidth": {
+          "all": 4607212,
+          "cached": 2985600,
+          "uncached": 1621612,
+          "ssl": {
+            "encrypted": 4606145,
+            "unencrypted": 1067
+          },
+          "content_type": {
+            "css": 273141,
+            "html": 1618653,
+          },
+          "country": {
+            "PL": 3712599,
+            "US": 231584
+          }
+        },
+        "uniques": {
+          "all": 116
+        }
+      }
+
+    def setUp(self):
+        self.metric = Metric('cloudflare')
+        self.metric.add_tag('zone_id', 'test')
+        self.metric.with_timestamp(15376167000000000000)
+        self.metric.values = {
+            'uniques': '116',
+            'requests_all': '258',
+            'requests_cached': '142',
+            'requests_uncached': '116',
+            'requests_encrypted': '255',
+            'requests_unencrypted': '3',
+            'requests_status_200': '229',
+            'requests_status_499': '2',
+            'requests_content_type_css': '10',
+            'requests_content_type_html': '96',
+            'requests_country_us': '14',
+            'requests_country_pl': '183',
+            'requests_ip_class_monitoringService': '13',
+            'requests_ip_class_noRecord': '215',
+            'requests_ip_class_searchEngine': '30',
+            'bandwidth_all': '4607212',
+            'bandwidth_cached': '2985600',
+            'bandwidth_uncached': '1621612',
+            'bandwidth_encrypted': '4606145',
+            'bandwidth_unencrypted': '1067',
+            'bandwidth_content_type_css': '273141',
+            'bandwidth_content_type_html': '1618653',
+            'bandwidth_country_pl': '3712599',
+            'bandwidth_country_us': '231584',
+
+        }
+
+    def test_only_one_serie(self):
+        expectedCollection = MetricCollection()
+        expectedCollection.append(self.metric)
+        timeseries = {'timeseries': [self.timeserie]}
+
+        a = Parser()
+        collection = a.parse_dashboard("test", timeseries)
+
+        self.maxDiff = None
+        self.assertDictEqual(expectedCollection.metrics[0].values, collection.metrics[0].values)
+
+    def test_multiple_series(self):
+        expectedCollection = MetricCollection()
+        expectedCollection.append(self.metric)
+        expectedCollection.append(self.metric)
+
+        timeseries = {'timeseries': [self.timeserie, self.timeserie]}
+
+        a = Parser()
+        collection = a.parse_dashboard("test", timeseries)
+
+        self.maxDiff = None
+        self.assertDictEqual(expectedCollection.metrics[0].values, collection.metrics[0].values)
+
+    def test_parse_time(self):
+        dtime = '2018-09-22T11:45:00Z'
+        expected_timestamp = 1537616700 * 10**9
+
+        p = Parser()
+        self.assertEqual(p.parse_time(dtime), expected_timestamp)
