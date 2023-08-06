4ec5bcb750680d17297a62a0948a1a8c0d37387d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package codes/0.0.1/codes-0.0.1.tar.gz

---
+++ packages/codes/codes-0.0.1.tar.gz/codes-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="codes",
+    version="0.0.1",
+    author="Steven wang",
+    author_email="wangzhou8284@outlook.com",
+    description="useful codes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StevenLianaL/codes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
