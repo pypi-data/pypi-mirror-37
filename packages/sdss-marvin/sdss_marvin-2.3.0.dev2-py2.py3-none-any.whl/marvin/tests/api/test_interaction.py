b2d2faa2275793ee83880a24d5159d8d4754e038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:27 +0000

    Add package sdss-marvin/2.3.0.dev2/sdss_marvin-2.3.0.dev2-py2.py3-none-any.whl

---
+++ packages/sdss-marvin/sdss_marvin-2.3.0.dev2-py2.py3-none-any.whl/marvin/tests/api/test_interaction.py
@@ -0,0 +1,14 @@
+# !usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+# Licensed under a 3-clause BSD license.
+#
+# @Author: Brian Cherinka
+# @Date:   2017-02-12 18:00:11
+# @Last modified by:   Brian Cherinka
+# @Last Modified time: 2017-07-05 20:37:53
+
+from __future__ import print_function, division, absolute_import
+from marvin.api.api import Interaction
+
+
