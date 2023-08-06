98322048baf1eb1a52a91c65d7359aa1c731258b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:59 +0000

    Add package cstruct/1.8/cstruct-1.8.tar.gz

---
+++ packages/cstruct/cstruct-1.8.tar.gz/cstruct-1.8/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+version = '1.8'
+
+def readme():
+    try:
+        f = open('README.md')
+        return f.read()
+    finally:
+        f.close()
+
+setup(name='cstruct',
+      version=version,
+      description="C-style structs for Python",
+      long_description="""\
+Convert C struct definitions into Python classes with methods for serializing/deserializing.""",
+      classifiers=[
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+      ],
+      keywords='struct',
+      author='Andrea Bonomi',
+      author_email='andrea.bonomi@gmail.com',
+      url='http://github.com/andreax79/python-cstruct',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
