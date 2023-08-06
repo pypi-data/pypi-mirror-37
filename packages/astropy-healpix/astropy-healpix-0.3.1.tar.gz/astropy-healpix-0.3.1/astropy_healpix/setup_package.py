e891aeec88e3a47dbd7f0a9eb40a330aeae116b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:50 +0000

    Add package astropy-healpix/0.3.1/astropy-healpix-0.3.1.tar.gz

---
+++ packages/astropy-healpix/astropy-healpix-0.3.1.tar.gz/astropy-healpix-0.3.1/astropy_healpix/setup_package.py
@@ -0,0 +1,39 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import os
+
+from distutils.core import Extension
+
+HEALPIX_ROOT = os.path.relpath(os.path.dirname(__file__))
+
+C_FILES = ['bl.c',
+           'healpix-utils.c',
+           'healpix.c',
+           'mathutil.c',
+           'permutedsort.c',
+           'qsort_reentrant.c',
+           'starutil.c']
+
+
+C_DIR = os.path.join('cextern', 'astrometry.net')
+
+
+def get_extensions():
+
+    libraries = []
+
+    sources = [os.path.join(C_DIR, filename) for filename in C_FILES]
+    sources.append(os.path.join(HEALPIX_ROOT, 'interpolation.c'))
+    sources.append(os.path.join(HEALPIX_ROOT, 'core_cython.pyx'))
+
+    include_dirs = ['numpy', C_DIR, HEALPIX_ROOT]
+
+    extension = Extension(
+        name="astropy_healpix.core_cython",
+        sources=sources,
+        include_dirs=include_dirs,
+        libraries=libraries,
+        language="c",
+        extra_compile_args=['-O2'])
+
+    return [extension]
