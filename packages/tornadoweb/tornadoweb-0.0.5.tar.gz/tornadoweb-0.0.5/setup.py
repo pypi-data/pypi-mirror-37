b4dba225b60bc8f297514ada582f1bd7a0e5311e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:33 +0000

    Add package tornadoweb/0.0.5/tornadoweb-0.0.5.tar.gz

---
+++ packages/tornadoweb/tornadoweb-0.0.5.tar.gz/tornadoweb-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+long_description = ""
+
+setup(
+    name="tornadoweb",
+    version="0.0.5",
+    author="9wfox",
+    author_email="568628130@qq.com",
+    description="tornado web",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/9wfox/tornadoweb",
+    packages=["tornadoweb"],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/tornadoweb/tornadoweb-0.0.5.tar.gz/tornadoweb-0.0.5/tornadoweb/__init__.py
@@ -0,0 +1,16 @@
+#-*- coding:utf-8 -*-
+
+"""
+    MVC package
+
+    只需 "from mvc import *" 即可，无需导入具体模块。
+"""
+from app import *
+from config import *
+#ConfigLoader.load()
+from utility import *
+from web import *
+
+set_default_encoding()
+
+
