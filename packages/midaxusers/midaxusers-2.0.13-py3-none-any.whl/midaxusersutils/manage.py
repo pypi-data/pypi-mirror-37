55369fb215c743ac99b51b09520a8418d036d8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:03 +0000

    Add package midaxusers/2.0.13/midaxusers-2.0.13-py3-none-any.whl

---
+++ packages/midaxusers/midaxusers-2.0.13-py3-none-any.whl/midaxusersutils/manage.py
@@ -0,0 +1,55 @@
+from midaxusers import create_app
+from midaxusers.models import db
+#from myapp.database import database_manager #sub manager for creating/dropping db
+from flask_migrate import Migrate, MigrateCommand
+import uuid
+import base64
+from midaxusers.models import db, User, UserAttributes, UserLogin
+
+def create_db():
+    app = create_app()  
+
+    # create the database and load test data
+    with app.app_context():            
+        db.create_all()     
+
+def create_test_user():
+    app = create_app()
+
+    with app.app_context():          
+        test_user = User()
+        test_user.domain = 'midax'
+        test_user.email = 'test@midax.com'
+        test_user.password = 'm1dax'
+        test_user.role = 1     
+
+        db.session.add(test_user)
+        db.session.commit()
+
+        #dbuser = UserLogin.get_user({'login_type': 'WEBSITE', 'login_key': 'test@midax.com'})
+
+        loginws = UserLogin()
+        loginws.login_type = 'WEBSITE'
+        loginws.login_key= 'test@midax.com'
+        loginws.password = 'm1dax'
+        loginws.user = test_user
+        logintm = UserLogin()
+        logintm.login_type = 'TERMINAL'
+        logintm.login_key= 'HONOLULU^1'
+        logintm.password = '123'
+        logintm.user = test_user
+
+        db.session.add(loginws)
+        db.session.add(logintm)
+
+        newuser_attributes = UserAttributes()
+
+        newuser_attributes.user = test_user
+        newuser_attributes.name = 'test'
+        newuser_attributes.value = 'attr'
+        db.session.add(newuser_attributes)
+
+        db.session.commit()     
+
+if __name__ == "__main__":
+    pass
