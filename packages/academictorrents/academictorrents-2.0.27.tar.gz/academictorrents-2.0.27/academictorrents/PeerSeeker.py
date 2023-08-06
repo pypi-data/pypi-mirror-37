738b52e26980a4fffd01584ebf7352e21fc204e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:28 +0000

    Add package academictorrents/2.0.27/academictorrents-2.0.27.tar.gz

---
+++ packages/academictorrents/academictorrents-2.0.27.tar.gz/academictorrents-2.0.27/academictorrents/PeerSeeker.py
@@ -0,0 +1,30 @@
+__author__ = 'alexisgallepe'
+
+import time
+from . import Peer
+from threading import Thread
+from pubsub import pub
+
+
+class PeerSeeker(Thread):
+    def __init__(self, newpeersQueue, torrent):
+        Thread.__init__(self)
+        self.stopRequested = False
+        self.newpeersQueue = newpeersQueue
+        self.torrent = torrent
+        self.peerFailed = [("", "")]
+        self.setDaemon(True)
+
+    def requestStop(self):
+        self.stopRequested = True
+
+    def run(self):
+        while not self.stopRequested:
+            peer = self.newpeersQueue.get()
+            if not (peer[0], peer[1]) in self.peerFailed:
+                p = Peer.Peer(self.torrent, peer[0], peer[1])
+                if not p.connectToPeer(3):
+                    self.peerFailed.append((peer[0], peer[1]))
+                else:
+                    print(p.ip + ": added new peer")
+                    pub.sendMessage('PeersManager.newPeer', peer=p)
