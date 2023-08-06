7ea48e6a8dcad56eb0329e4746ee79248851a311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:08 +0000

    Add package sublemon/0.0.2/sublemon-0.0.2.tar.gz

---
+++ packages/sublemon/sublemon-0.0.2.tar.gz/sublemon-0.0.2/README.md
@@ -0,0 +1,121 @@
+<p align="center">
+  <img width="300" height="200" src="static/logo.png" alt="bscan">
+</p>
+
+<h1 align="center">Sublemon</h1>
+
+<p align="center">
+  :melon: <em>local asynchronous subprocess control</em> :lemon:
+</p>
+<p align="center">
+  <a href="https://travis-ci.org/welchbj/sublemon">
+    <img src="https://img.shields.io/travis/welchbj/sublemon/master.svg?style=flat-square&label=travis" alt="travis status">
+  </a>
+  <a href="https://pypi.org/project/sublemon/">
+    <img src="https://img.shields.io/pypi/v/sublemon.svg?style=flat-square&label=pypi" alt="pypi">
+  </a>
+  <a href="https://www.python.org/">
+    <img src="https://img.shields.io/badge/python-3.6+-2a98ed.svg?style=flat-square" alt="python version">
+  </a>
+</p>
+
+---
+
+## Installation
+
+To get the latest version from PyPI, use:
+```sh
+pip install sublemon
+```
+
+Alternatively, install the latest version from version control:
+```sh
+pip install https://github.com/welchbj/sublemon/archive/master.tar.gz
+```
+
+
+## Basic Usage
+
+The below example shows some of the basic subprocess-spawning functionality of this library:
+```python
+import asyncio
+import time
+
+from sublemon import (
+    amerge,
+    crossplat_loop_run,
+    Sublemon)
+
+
+async def main():
+    """`sublemon` library example!"""
+    for c in (1, 2, 4,):
+        async with Sublemon(max_concurrency=c) as s:
+            start = time.perf_counter()
+            await asyncio.gather(one(s), two(s))
+            end = time.perf_counter()
+            print('Limiting to', c, 'concurrent subprocess(es) took',
+                  end-start, 'seconds\n')
+
+
+async def one(s: Sublemon):
+    """Spin up some subprocesses, sleep, and echo a message for this coro."""
+    shell_cmds = [
+        'sleep 1 && echo subprocess 1 in coroutine one',
+        'sleep 1 && echo subprocess 2 in coroutine one']
+    async for line in s.iter_lines(*shell_cmds):
+        print(line)
+
+
+async def two(s: Sublemon):
+    """Spin up some subprocesses, sleep, and echo a message for this coro."""
+    subprocess_1, subprocess_2 = s.spawn(
+        'sleep 1 && echo subprocess 1 in coroutine two',
+        'sleep 1 && echo subprocess 2 in coroutine two')
+    async for line in amerge(subprocess_1.stdout, subprocess_2.stdout):
+        print(line.decode('utf-8'), end='')
+
+
+if __name__ == '__main__':
+    crossplat_loop_run(main())
+
+```
+
+Running this example would yield the following console output, demonstrating `sublemon`'s ability to rate-limit pooled subprocess execution:
+```
+subprocess 1 in coroutine one
+subprocess 2 in coroutine one
+subprocess 1 in coroutine two
+subprocess 2 in coroutine two
+Limiting to 1 concurrent subprocess(es) took 4.251494415589895 seconds
+
+subprocess 1 in coroutine one
+subprocess 2 in coroutine one
+subprocess 2 in coroutine two
+subprocess 1 in coroutine two
+Limiting to 2 concurrent subprocess(es) took 2.1220036135871787 seconds
+
+subprocess 1 in coroutine one
+subprocess 2 in coroutine one
+subprocess 1 in coroutine two
+subprocess 2 in coroutine two
+Limiting to 4 concurrent subprocess(es) took 1.083995944693033 seconds
+```
+
+
+## License
+
+`sublemon` uses the [MIT License](https://opensource.org/licenses/MIT).
+
+
+## Development
+
+The development requirements can be installed with:
+```sh
+pip install -r dev-requirements.txt
+```
+
+Then try running the tests with:
+```sh
+python -m unittest discover
+```
---
+++ packages/sublemon/sublemon-0.0.2.tar.gz/sublemon-0.0.2/sublemon/subprocess.py
@@ -0,0 +1,150 @@
+"""Models for interacting with subprocesses."""
+
+import asyncio
+import uuid
+
+from datetime import datetime
+from typing import (
+    AsyncGenerator,
+    Optional,
+    TYPE_CHECKING)
+
+from sublemon.errors import SublemonLifetimeError
+
+if TYPE_CHECKING:
+    from sublemon.runtime import Sublemon  # noqa
+
+
+class SublemonSubprocess:
+
+    """Logical encapsulation of a subprocess."""
+
+    def __init__(self, server: 'Sublemon', cmd: str) -> None:
+        self._server = server
+        self._cmd = cmd
+        self._scheduled_at = datetime.now()
+        self._uuid = uuid.uuid4()
+        self._began_at: Optional[datetime] = None
+        self._exit_code: Optional[int] = None
+        self._subprocess: Optional[asyncio.subprocess.Process] = None
+        self._began_running_evt = asyncio.Event()
+        self._done_running_evt = asyncio.Event()
+
+    def __repr__(self) -> str:
+        return '<SublemonSubprocess [{}]>'.format(str(self))
+
+    def __str__(self) -> str:
+        return '{} -> `{}`'.format(self._scheduled_at, self._cmd)
+
+    def __hash__(self) -> int:
+        return hash((self._cmd, self._uuid,))
+
+    def __eq__(self, other) -> bool:
+        return (self._cmd == other._cmd and
+                self._scheduled_at == other._scheduled_at)
+
+    def __ne__(self, other) -> bool:
+        return not (self == other)
+
+    async def spawn(self):
+        """Spawn the command wrapped in this object as a subprocess."""
+        self._server._pending_set.add(self)
+        await self._server._sem.acquire()
+        self._subprocess = await asyncio.create_subprocess_shell(
+            self._cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        self._began_at = datetime.now()
+        if self in self._server._pending_set:
+            self._server._pending_set.remove(self)
+        self._server._running_set.add(self)
+        self._began_running_evt.set()
+
+    async def wait_running(self) -> None:
+        """Coroutine to wait for this subprocess to begin execution."""
+        await self._began_running_evt.wait()
+
+    async def wait_done(self) -> int:
+        """Coroutine to wait for subprocess run completion.
+
+        Returns:
+            The exit code of the subprocess.
+
+        """
+        await self._done_running_evt.wait()
+        if self._exit_code is None:
+            raise SublemonLifetimeError(
+                'Subprocess exited abnormally with `None` exit code')
+        return self._exit_code
+
+    def _poll(self) -> None:
+        """Check the status of the wrapped running subprocess.
+
+        Note:
+            This should only be called on currently-running tasks.
+
+        """
+        if self._subprocess is None:
+            raise SublemonLifetimeError(
+                'Attempted to poll a non-active subprocess')
+        elif self._subprocess.returncode is not None:
+            self._exit_code = self._subprocess.returncode
+            self._done_running_evt.set()
+            self._server._running_set.remove(self)
+            self._server._sem.release()
+
+    @property
+    async def stdout(self) -> AsyncGenerator[str, None]:
+        """Asynchronous generator for lines from subprocess stdout."""
+        await self.wait_running()
+        async for line in self._subprocess.stdout:  # type: ignore
+            yield line
+
+    @property
+    async def stderr(self) -> AsyncGenerator[str, None]:
+        """Asynchronous generator for lines from subprocess stderr."""
+        await self.wait_running()
+        async for line in self._subprocess.stderr:  # type: ignore
+            yield line
+
+    @property
+    def cmd(self) -> str:
+        """The shell command that this subprocess will/is/did run."""
+        return self._cmd
+
+    @property
+    def exit_code(self) -> Optional[int]:
+        """The exit code of this subprocess."""
+        return self._exit_code
+
+    @property
+    def is_pending(self) -> bool:
+        """Whether this subprocess is waiting to run."""
+        return not self._began_running_evt.is_set()
+
+    @property
+    def is_running(self) -> bool:
+        """Whether this subprocess is currently running."""
+        return (self._began_running_evt.is_set() and
+                not self._done_running_evt.is_set())
+
+    @property
+    def is_done(self) -> bool:
+        """Whether this subprocess has completed."""
+        return self._done_running_evt.is_set()
+
+    @property
+    def scheduled_at(self) -> datetime:
+        """The time this object was scheduled on the server."""
+        return self._scheduled_at
+
+    @property
+    def began_at(self) -> Optional[datetime]:
+        """The time the subprocess began execution.
+
+        Note:
+            This will be `None` until the subprocess has actually begun
+            execution.
+
+        """
+        return self._began_at
