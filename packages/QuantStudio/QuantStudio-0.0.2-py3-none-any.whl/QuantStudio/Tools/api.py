8919a408e6101fb1f9ab4db8590dfa64bc1d0580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:41 +0000

    Add package QuantStudio/0.0.2/QuantStudio-0.0.2-py3-none-any.whl

---
+++ packages/QuantStudio/QuantStudio-0.0.2-py3-none-any.whl/QuantStudio/Tools/api.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#from . import MailFun as Mail
+from . import MathFun as Math
+from . import DateTimeFun as DateTime
+from . import StrategyTestFun as Strategy
+from . import DataPreprocessingFun as Preprocess
+from . import FileFun as File
+try:
+    from .QtGUI import QtGUIFun as QtGUI
+except:
+    pass
+from .AuxiliaryFun import genAvailableName
