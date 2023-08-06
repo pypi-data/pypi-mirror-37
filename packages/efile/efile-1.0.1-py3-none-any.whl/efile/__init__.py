566cd43819b89896064e617d2c04956a403759d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:36 +0000

    Add package efile/1.0.1/efile-1.0.1-py3-none-any.whl

---
+++ packages/efile/efile-1.0.1-py3-none-any.whl/efile/__init__.py
@@ -0,0 +1,108 @@
+import base64
+import os.path
+import shutil
+import struct
+
+__VERSION__ = '1.0.1'
+
+
+def get_md5(input):
+    import hashlib
+    md5 = hashlib.md5()
+    md5.update(input.encode('utf-8'))
+    return md5.hexdigest()
+
+
+def get_name(file_path):
+    filename_len = 0
+    with open(file_path, mode='rb') as f:
+        old = f.read(4)
+        filename_len, _ = struct.unpack('HH', old)
+        filename = base64.standard_b64decode(
+            f.read(filename_len)).decode('utf-8')
+
+        print(filename)
+
+
+def encode(file_path):
+    filename = os.path.basename(file_path)
+    new_filename = get_md5(filename)
+
+    dfilename = base64.b64encode(filename.encode('utf-8'))
+    lfilename = len(dfilename)
+
+    shutil.copy(file_path, new_filename)
+
+    bdata = None
+    bdata_len = 0
+    with open(file_path, mode='rb') as f:
+        bdata = base64.b64encode(f.read(100))
+        bdata_len = len(bdata)
+
+    head = struct.pack('HH', lfilename, bdata_len)
+
+    with open(new_filename, mode='rb+') as f:
+        f.seek(100)
+        old = f.read()
+        f.seek(0)
+        f.write(head)
+        f.write(dfilename)
+        f.write(bdata)
+        f.write(old)
+
+    print('Encoded file is {}'.format(new_filename))
+
+
+def decode(file_path):
+    filename_len = 0
+    data_len = 0
+    with open(file_path, mode='rb') as f:
+        old = f.read(4)
+        filename_len, data_len = struct.unpack('HH', old)
+        filename = base64.b64decode(f.read(filename_len)).decode('utf-8')
+        head100 = base64.b64decode(f.read(data_len))
+        others = f.read()
+
+        if os.path.exists(filename):
+            print('{} has exists.'.format(filename))
+            filename = 'efile_' + filename
+            print('It will be rename to {}'.format(filename))
+        else:
+            print('Decoded file is {}'.format(filename))
+
+        with open(filename, 'wb+') as ff:
+            ff.write(head100)
+            ff.write(others)
+
+
+# -----
+
+def run(args):
+    if args.e:
+        print('encoding ... {}'.format(args.f))
+        encode(args.f)
+
+    elif args.d:
+        decode(args.f)
+    elif args.l:
+        if os.path.isdir(args.f):
+            for p, _, fs in os.walk(args.f):
+                for f in fs:
+                    get_name(os.path.join(p, f))
+        else:
+            get_name(args.f)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(prog='efile', description='')
+    parser.add_argument('-e', action='store_true', help='encode')
+    parser.add_argument('-d', action='store_true', help='decode')
+    parser.add_argument('-l', action='store_true', help='list name')
+    parser.add_argument('f', help='file/dir')
+    parser.add_argument('-V', '--version', action='version',
+                        version=__VERSION__)
+
+    args = parser.parse_args()
+    args = parser.parse_args()
+    run(args)
---
+++ packages/efile/efile-1.0.1-py3-none-any.whl/efile/__main__.py
@@ -0,0 +1,12 @@
+import argparse
+import sys
+
+import efile
+
+__VERSION__ = '1.0'
+
+
+
+
+if __name__ == '__main__':
+    sys.exit(main())
