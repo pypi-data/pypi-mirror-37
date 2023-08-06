b0012047b06296739d2b38a5d83f00a1d57f3b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:20 +0000

    Add package IBATSTrader/0.1.2/IBATSTrader-0.1.2.tar.gz

---
+++ packages/IBATSTrader/IBATSTrader-0.1.2.tar.gz/IBATSTrader-0.1.2/setup.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author  : MG
+@Time    : 2018/6/14 16:07
+@File    : setup.py
+@contact : mmmaaaggg@163.com
+@desc    : 
+"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as rm:
+    long_description = rm.read()
+
+setup(name='IBATSTrader',
+      version='0.1.2',
+      description='IBATS（Integration Backtest Analysis Trade System）的Trader端公共模块，所有 Trader均集成这个模块，并使用其提供的一些公共工具',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='MG',
+      author_email='mmmaaaggg@163.com',
+      url='https://github.com/mmmaaaggg/IBATS_Trader',
+      packages=find_packages(),
+      python_requires='>=3.6',
+      classifiers=(
+          "Programming Language :: Python :: 3 :: Only",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: Unix",
+          "Operating System :: POSIX",
+          "License :: OSI Approved :: MIT License",
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: No Input/Output (Daemon)",
+          "Intended Audience :: Developers",
+          "Natural Language :: Chinese (Simplified)",
+          "Topic :: Software Development",
+      ),
+      install_requires=[
+          'IBATSCommon',
+      ])
