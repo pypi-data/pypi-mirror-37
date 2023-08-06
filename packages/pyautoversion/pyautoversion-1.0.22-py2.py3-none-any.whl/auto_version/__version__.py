045d6e32dbfea4b6d6157d8a8b8bd2349c155f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:18 +0000

    Add package pyautoversion/1.0.22/pyautoversion-1.0.22-py2.py3-none-any.whl

---
+++ packages/pyautoversion/pyautoversion-1.0.22-py2.py3-none-any.whl/auto_version/__version__.py
@@ -0,0 +1,4 @@
+# This project's release version
+__version__ = "1.0.22"
+# This project's release commit hash
+COMMIT = "2b89864b09cca80517795e7df242886549792412"
---
+++ packages/pyautoversion/pyautoversion-1.0.22-py2.py3-none-any.whl/auto_version/tests/example.py
@@ -0,0 +1,4 @@
+LOCK = False
+RELEASE = True
+VERSION = "19.99.0.devX"
+VERSION_AGAIN = "19.99.0.devX"
