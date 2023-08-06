f6b79195039044e70f6480a557eab1ddfce604c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:54 +0000

    Add package pylintconfig/0.1/pylintconfig-0.1.tar.gz

---
+++ packages/pylintconfig/pylintconfig-0.1.tar.gz/pylintconfig-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# loading __version__
+exec(open('pylintconfig/version.py').read())
+
+with open('readme.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='pylintconfig',
+    version=__version__,
+    packages=find_packages(),
+    description='cli for easy configuration of pylint',
+    url='https://github.com/GliderGeek/pylintconfig',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        pylintconfig=pylintconfig.pylintconfig:pylintconfig
+    ''',
+)
