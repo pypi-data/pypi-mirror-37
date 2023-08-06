8bdb75777f412dd2ed40e43df410d17506d2187b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:40 +0000

    Add package grass-script/0.0.4/grass_script-0.0.4.tar.gz

---
+++ packages/grass-script/grass_script-0.0.4.tar.gz/grass_script-0.0.4/grass_script/help_me.py
@@ -0,0 +1,7 @@
+def help_me(*opt):
+    if opt == 'log':
+        print('log(obj)')
+        print('Prints the given object (obj) in the console')
+    else:
+        print('log(obj)')
+        print('Prints the given object (obj) in the console')
---
+++ packages/grass-script/grass_script-0.0.4.tar.gz/grass_script-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+__project__ = "grass_script"
+__version__ = "0.0.4"
+__description__ = "A Python module that is basically a programming language"
+__packages__ = ["grass_script"]
+__author__ = "TheOnlyWalrus"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Programming Language :: Python :: 3",
+]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+    classifiers = __classifiers__,
+)
