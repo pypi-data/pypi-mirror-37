557546962309cf12bb0db79af4d2fed6bb543c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:46 +0000

    Add package georinex/1.6.8/georinex-1.6.8.tar.gz

---
+++ packages/georinex/georinex-1.6.8.tar.gz/georinex-1.6.8/georinex/keplerian.py
@@ -0,0 +1,83 @@
+"""
+GPS Keplerian elements => ECEF
+Michael Hirsch, Ph.D.
+"""
+from datetime import datetime, timedelta
+import xarray
+import numpy as np
+from typing import Tuple
+
+
+def keplerian2ecef(sv: xarray.DataArray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    based on:
+    https://ascelibrary.org/doi/pdf/10.1061/9780784411506.ap03
+    """
+
+    if 'sv' in sv and sv['sv'] in ('R', 'S'):
+        return sv['X'], sv['Y'], sv['Z']
+
+    sv = sv.dropna(dim='time', how='all')
+
+    GM = 3.986005e14  # [m^3 s^-2]
+    omega_e = 7.292115e-5  # [rad s^-1]
+#    pi = 3.1415926535898  # definition
+
+    A = sv['sqrtA']**2
+
+    n0 = np.sqrt(GM/A**3)  # computed mean motion
+#    T = 2*pi / n0  # Satellite orbital period
+
+    n = n0 + sv['DeltaN']  # corrected mean motion
+
+    # from GPS Week 0
+    if sv.svtype[0] == 'E':
+        weeks = sv['GALWeek'].values - 1024
+        # TODO: need to add N*4096 for year 2058 and beyond
+    elif sv.svtype[0] == 'G':
+        weeks = sv['GPSWeek'].values
+    else:
+        raise ValueError(f'Unknown system type {sv.svtype[0]}')
+
+    T0 = [datetime(1980, 1, 6) + timedelta(weeks=week) for week in weeks]
+
+    tk = np.empty(sv['time'].size, dtype=float)
+
+    # FIXME: so ugly...
+    # time elapsed since reference epoch
+    # seems to be a bug in MyPy, this line computes "correctly"
+
+    for i, (t0, t1, t2) in enumerate(zip(T0, sv['time'], sv['Toe'])):
+        tsv = datetime.utcfromtimestamp(t1.item()/1e9)
+        toe = timedelta(seconds=t2.values.astype(int).item()) + t0  # type: ignore  # noqa
+        tk[i] = (tsv - toe).total_seconds()  # type: ignore  # noqa
+
+    Mk = sv['M0'] + n*tk  # Mean Anomaly
+    Ek = Mk + sv['Eccentricity'] * np.sin(Mk)  # FIXME: ok?
+
+    nuK = 2 * np.arctan2(np.sqrt(1 + sv['Eccentricity']) *
+                         np.sin(Ek/2), np.sqrt(1-sv['Eccentricity']) * np.cos(Ek/2))
+
+    PhiK = nuK + sv['omega']
+    dik = sv['Cic']*np.cos(2*PhiK) + sv['Cis']*np.sin(2*PhiK)
+
+    ik = sv['Io'] + sv['IDOT']*tk + dik  # corrected inclination
+
+    duk = sv['Cuc'] * np.cos(2*PhiK) + sv['Cus']*np.sin(2*PhiK)
+    uk = PhiK + duk
+
+    drk = sv['Crc']*np.cos(2*PhiK) + sv['Crs']*np.sin(2*PhiK)
+    rk = A*(1-sv['Eccentricity']*np.cos(Ek)) + drk
+    Xk1 = rk * np.cos(uk)
+
+    Yk1 = rk*np.sin(uk)
+
+    OmegaK = sv['Omega0'] + (sv['OmegaDot'] - omega_e)*tk - omega_e*sv['Toe']
+
+    X = Xk1 * np.cos(OmegaK) - Yk1 * np.sin(OmegaK) * np.cos(ik)
+
+    Y = Xk1*np.sin(OmegaK) + Yk1 * np.cos(OmegaK) * np.cos(ik)
+
+    Z = Yk1*np.sin(ik)
+
+    return X, Y, Z
