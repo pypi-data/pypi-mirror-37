0495d0b64edad376ed404bd9956b88acf6193c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:46 +0000

    Add package sbtab/0.9.47/sbtab-0.9.47.tar.gz

---
+++ packages/sbtab/sbtab-0.9.47.tar.gz/sbtab-0.9.47/sbtab/__init__.py
@@ -0,0 +1,10 @@
+from pkg_resources import resource_string
+__version__ = resource_string(__name__, 'VERSION').decode("utf-8")
+
+from . import misc
+from . import sbml2sbtab
+from . import sbtab2sbml
+from . import sbtab2html
+from . import SBtab
+from . import validatorSBtab
+
