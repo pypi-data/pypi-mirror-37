86c4b6a9597b79228cdb7b10bbf25eaa976e2d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:06 +0000

    Add package dpu-utils/0.1.17/dpu_utils-0.1.17.tar.gz

---
+++ packages/dpu-utils/dpu_utils-0.1.17.tar.gz/dpu_utils-0.1.17/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+      name='dpu_utils',
+      version='0.1.17',
+      license='MIT',
+      description='Python utilities used by Deep Procedural Intelligence',
+      url='https://github.com/microsoft/dpu-utils',
+      author='Deep Procedural Intelligence',
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'azure-storage', 'numpy'
+      ],
+      test_suite="tests",
+      zip_safe=False)
