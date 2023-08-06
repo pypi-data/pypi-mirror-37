49490d2e5f2cc4b9c3d8060bdef3e519b7cad865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:39 +0000

    Add package pickledonion/0.0.1/pickledonion-0.0.1.tar.gz

---
+++ packages/pickledonion/pickledonion-0.0.1.tar.gz/pickledonion-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pickledonion',
+    version='0.0.1',
+    author='Robert Gale',
+    author_email='rcgale@gmail.com',
+    packages=['pickledonion'],
+    url='https://github.com/rcgale/pickledonion',
+    install_requires=[],
+)
