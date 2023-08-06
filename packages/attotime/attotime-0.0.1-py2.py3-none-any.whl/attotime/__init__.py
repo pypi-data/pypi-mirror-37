2887e42197b300b055ebaa129c2df3362e69c0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:10 +0000

    Add package attotime/0.0.1/attotime-0.0.1-py2.py3-none-any.whl

---
+++ packages/attotime/attotime-0.0.1-py2.py3-none-any.whl/attotime/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2016, Brandon Nielsen
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the BSD license.  See the LICENSE file for details.
+
+#Expose the API
+from .attodatetime import attodatetime
+from .attotime import attotime
+from .attotimedelta import attotimedelta
