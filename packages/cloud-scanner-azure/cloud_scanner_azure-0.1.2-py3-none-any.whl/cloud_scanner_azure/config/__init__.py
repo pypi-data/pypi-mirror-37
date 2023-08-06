d9150f92dc24f19fab63808735b338b7e931cd53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:13 +0000

    Add package cloud-scanner-azure/0.1.2/cloud_scanner_azure-0.1.2-py3-none-any.whl

---
+++ packages/cloud-scanner-azure/cloud_scanner_azure-0.1.2-py3-none-any.whl/cloud_scanner_azure/config/__init__.py
@@ -0,0 +1,8 @@
+from cloud_scanner_azure.config.azure_config import AzureConfig
+from cloud_scanner_azure.config.azure_cosmosdb_config import (
+    AzureCosmosDbConfig)
+from cloud_scanner_azure.config.azure_credential_config import (
+    AzureCredentialConfig)
+from cloud_scanner_azure.config.azure_resource_config import (
+    AzureResourceServiceConfig)
+from cloud_scanner_azure.config.azure_storage_config import AzureStorageConfig
