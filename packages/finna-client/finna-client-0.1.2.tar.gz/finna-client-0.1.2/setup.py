1da11bee017cf5441e2b2917e8d7a80f6431bce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:39 +0000

    Add package finna-client/0.1.2/finna-client-0.1.2.tar.gz

---
+++ packages/finna-client/finna-client-0.1.2.tar.gz/finna-client-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='finna-client',
+    version='0.1.2',
+    url='https://github.com/NatLibFi/Finna-client',
+    author='Osma Suominen',
+    author_email='osma.suominen@helsinki.fi',
+    description='Python client library for accessing Finna REST API',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    py_modules=['finna_client'],
+    install_requires=['requests'],
+    extras_require={
+        'dev': ['bumpversion']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
