ecf23b4ce2fa9cc18b83ce44db6c65cbf18240fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:58 +0000

    Add package torchfun/0.0.168/torchfun-0.0.168-py3-none-any.whl

---
+++ packages/torchfun/torchfun-0.0.168-py3-none-any.whl/torchfun/functional.py
@@ -0,0 +1,16 @@
+from __future__ import division,print_function
+import torch
+from .torchfun import sort_args
+import torch.nn.functional as F
+
+def conv2d_dfs(x,weight,bias=None,stride=1,padding=0,dilation=1):
+    '''depth fully shared conv2d.
+    Argument:
+        x: input image with size: N x C x H x W
+        weight: shape shoule be : 1 x 1 x kernel-height x k-width
+        bias: contains only 1 number or None
+    '''
+    b,c,h,w = x.shape
+    x = x.contiguous().view(b*c,1,h,w)
+    x = F.conv2d(x,weight,bias,stride,padding,dilation,groups=1)
+    return x.contiguous().view(b,c,h,w)
