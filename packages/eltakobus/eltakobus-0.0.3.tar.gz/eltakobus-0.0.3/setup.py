74ad4fe469ae3fa31d1e795acebb3da272a92264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:59 +0000

    Add package eltakobus/0.0.3/eltakobus-0.0.3.tar.gz

---
+++ packages/eltakobus/eltakobus-0.0.3.tar.gz/eltakobus-0.0.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import setuptools
+
+setuptools.setup(
+    name="eltakobus",
+    version="0.0.3",
+    author="chrysn",
+    author_email="chrysn@fsfe.org",
+    description="Library for participating in the Eltako Series 14 RS485 bus",
+    url="https://gitlab.com/chrysn/eltakobus",
+    packages=setuptools.find_packages(),
+    extras_require={
+        'serial': ['pyserial_asyncio', 'pyserial >= 3.4'],
+        'coap': ['aiocoap == 0.4a1'],
+        # Not usable yet as it's not exported via an entry point
+        'eltakotool': ['pyyaml', 'pyxdg'],
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
