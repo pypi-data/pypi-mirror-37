d87f0f7513b70aff2d3c44422afbe3fe04ec0010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:48 +0000

    Add package annotationengine/1.0.5/annotationengine-1.0.5-py2.py3-none-any.whl

---
+++ packages/annotationengine/annotationengine-1.0.5-py2.py3-none-any.whl/annotationengine/errors.py
@@ -0,0 +1,21 @@
+class AnnotationEngineException(Exception):
+    ''' generic error in annotation engine '''
+    pass
+
+
+class UnknownAnnotationTypeException(Exception):
+    ''' error raised when an annotation type is not found '''
+    pass
+
+
+class AnnotationNotFoundException(AnnotationEngineException):
+    ''' error raised when an annotation is not found '''
+    pass
+
+
+class DataSetNotFoundException(AnnotationEngineException):
+    ''' error raised when a dataset is not found '''
+
+
+class SchemaServiceError(AnnotationEngineException):
+    ''' error raised when schema can't be loading from schema service '''
