10ff46c5d50184e5364dff05a7fa47f1fd82bf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:50 +0000

    Add package glorpen-di/1.4.1/glorpen_di-1.4.1-py2.py3-none-any.whl

---
+++ packages/glorpen-di/glorpen_di-1.4.1-py2.py3-none-any.whl/glorpen/di/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''Dependency injection component.
+
+.. moduleauthor:: Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
+
+'''
+
+from glorpen.di.container import Container, Kwargs
+
+__version__ = "1.4.1"
+__all__ = ['Container', 'Kwargs', '__version__']
