dd16f4a6e4b377dad515923ed6453f381b49505b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:44 +0000

    Add package flake8-rst/0.4.2/flake8-rst-0.4.2.tar.gz

---
+++ packages/flake8-rst/flake8-rst-0.4.2.tar.gz/flake8-rst-0.4.2/tests/result_py3/result_2.py
@@ -0,0 +1,5 @@
+('test_precisely',
+ [('F821', 9, 18, "undefined name 'LdaModel'", None),
+  ('F821', 9, 34, "undefined name 'mm'", None),
+  ('F821', 9, 46, "undefined name 'id2word'", None)],
+ {'logical lines': 2, 'physical lines': 2, 'tokens': 23})
