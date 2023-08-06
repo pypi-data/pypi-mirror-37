8e25585d607b54b62b16061d6642bdd5d3c6cd48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:51 +0000

    Add package hyperspy-gui-ipywidgets/1.1.1/hyperspy_gui_ipywidgets-1.1.1.tar.gz

---
+++ packages/hyperspy-gui-ipywidgets/hyperspy_gui_ipywidgets-1.1.1.tar.gz/hyperspy_gui_ipywidgets-1.1.1/hyperspy_gui_ipywidgets/__init__.py
@@ -0,0 +1,10 @@
+
+__version__ = "1.1.1"
+import hyperspy.api_nogui # necessary to register the toolkeys
+# Importing as below registers the widgets when importing the module
+import hyperspy_gui_ipywidgets.axes
+import hyperspy_gui_ipywidgets.model
+import hyperspy_gui_ipywidgets.tools
+import hyperspy_gui_ipywidgets.preferences
+import hyperspy_gui_ipywidgets.roi
+import hyperspy_gui_ipywidgets.microscope_parameters
