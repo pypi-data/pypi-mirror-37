c5f63023ae155d30c66aaddadb00039aa9ae5cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:13 +0000

    Add package amberai-ice-rpc/1.0.3/amberai-ice-rpc-1.0.3.tar.gz

---
+++ packages/amberai-ice-rpc/amberai-ice-rpc-1.0.3.tar.gz/amberai-ice-rpc-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='amberai-ice-rpc',
+    version='1.0.3',
+    description='AMBER AI group dedicated RPC framwork call sdk base rp',
+    author='chn1807',
+    author_email='chn1807@163.com',
+    maintainer='chn1807',
+    maintainer_email='chn1807@163.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'zeroc-ice==3.7.1'
+    ],
+    keywords='amberai ice rpc'
+)
