836927cc94f9a4d62c8afeedd24b5fcd69eecdcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:50 +0000

    Add package calculate-expression/1.0.0/calculate_expression-1.0.0.tar.gz

---
+++ packages/calculate-expression/calculate_expression-1.0.0.tar.gz/calculate_expression-1.0.0/setup.py
@@ -0,0 +1,18 @@
+#############################################
+# File Name: setup.py
+# Author: WEN
+# Created Time:  2018-10-20 00:16:21
+#############################################
+
+from setuptools import setup 
+
+setup(
+    name        = 'calculate_expression',
+    version     = '1.0.0',
+    py_modules  = ["calculate_expression"],
+    author      = 'WEN',
+    author_email    = '764499983@qq.com',
+    url     = 'https://github.com/GoodManWEN/calculate_expression',
+    description = 'simple tool to calculate expressions',
+    license = "MIT Licence"
+)
