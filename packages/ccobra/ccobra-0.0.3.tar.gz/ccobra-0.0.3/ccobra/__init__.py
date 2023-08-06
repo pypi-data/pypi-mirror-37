6c88ddd85cae8e771310b1be21bccac30e712bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:26 +0000

    Add package ccobra/0.0.3/ccobra-0.0.3.tar.gz

---
+++ packages/ccobra/ccobra-0.0.3.tar.gz/ccobra-0.0.3/ccobra/__init__.py
@@ -0,0 +1,15 @@
+""" ORCA Module
+
+.. autosummary::
+   :toctree: _autosummary
+
+   ccobra.data
+   ccobra.model
+   ccobra.syllogistic
+
+"""
+
+from . import syllogistic
+
+from .data import CCobraData
+from .model import CCobraModel
