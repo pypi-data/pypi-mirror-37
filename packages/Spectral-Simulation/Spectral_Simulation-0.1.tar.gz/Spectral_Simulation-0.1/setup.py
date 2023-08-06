bc93ddd7febaea491c8cebf2e7bd957765fc2c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:03 +0000

    Add package Spectral-Simulation/0.1/Spectral_Simulation-0.1.tar.gz

---
+++ packages/Spectral-Simulation/Spectral_Simulation-0.1.tar.gz/Spectral_Simulation-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Spectral_Simulation',
+      version='0.1',
+      description='Spectral_Simulation taking in H and R',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['Spectral_Simulation'],
+      zip_safe=False)
