96e81b48ca4bdec9618efdb39ffa524a9b0f6fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:13 +0000

    Add package loveyu/3.3.0/loveyu-3.3.0.tar.gz

---
+++ packages/loveyu/loveyu-3.3.0.tar.gz/loveyu-3.3.0/setup.py
@@ -0,0 +1,72 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+filepath = 'loveyu/README.md'
+
+setup(
+    name="loveyu",
+    version="3.3.0",
+    keywords=("tips", "image", "celebrate"),
+    description="little tips to celebrate something",
+    long_description='''the chars is produced by module named [image2char]
+    (https://github.com/cpak00/image2char)''',
+    long_description_content_type='text/markdown',
+    license="GPLv2 Licence",
+    author="cpak00",
+    author_email="cymcpak00@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[],
+    data_files=[filepath])
+
+print(
+    r'''
+    
+
+
+
+                                                                                                                '"+<>+-"'
+                       ._|i(llII1!1i:|\                                                                   '>\uxrr)!iii(r\ru{{[~\
+                .~>1xczmc/(l;[[[]<]!(xofj{;\                                                           .<rc(+'              \_]}/\I|
+            \>{ulrl?-'                .'_:\on]'                                         ."_~_,".     .<vl\                     . .<1)\
+          .(mn:\                            _}wx~                                 '<!i;;![>?>[;l!;;!+\n"                  \>.'"">1*z>.
+          "bw{~'                              '!mj[.  .''..                       ;-|!|'",,_\.'\",_,;wl|"               ._wl_~?~|xd".
+           "ib*/I;;[]}n}?~\.                   "]pBdmsdgapkdsu/!-.               ."}\!>-"..  .\-<;;~.,i?,>~   .\\.  ."~;vqx\.-_>nz+
+             >bo,.   .[fq;\                 '?xdfowbW#dmjn/l[[;}umql)>'       .:!!:-.           . '>!i_|('[!'  '~:((/nzp&n;1uk#n>.
+              _nq\+"   "x#gqr;|__+?]]|'.  'lm/>:vh8Bgqfwmcucv/i>".":vov(|.    .'                     .<1~l,'!"       \{gx1i;|jj
+                -xdkrl!|+v8kmjclv(:-.    <s)\-xghj{<~"'.   ."?:}cl\[\  ._\                             .[1l.'l;-<:1(cqc]';!"}d-
+                  "gMx/\\(vga1'        .}f~<zdr|.                .,[I>.                                 .<vl._j,_~?rh<.\")#pu-
+                   (*i'"'  +lkl;\.  .'_/d;j*k>                                                           "fq?,/[~]/f{?;)(pm\
+                   ./h(lj\  .<d8gwc/1;uMw*k*1.                                                           .>*v/lglxi\.\+{ou-
+                    .[o#8u+,' ,vp:.',|fB%qhd'                                                             .zo.'p!'+?!)I?'
+                      .?dWlx(]?+{amr!}&%Ws&s'                                                              ]d. /l
+                        ,lq\".   \;uwg%&kk&!                                                               ,f. ?(
+                         .<}ucx}[~" .~g*l\W<      .ua                               ,dw"  .!d?             -b\ -\
+                               ..    .sa_<M;      .lW       ~(r/l" .)<   '(~ .;/r1'  ,ph-.ra~  ]I   <(     |*~.?!
+                                      /g|_dv      .lW     .}&i",\&[.I&~  zq.\bc,,za'  's#dm\   v*   r&.    i*| {~
+                                      :M/'(p\     .lW     .*m.   *m. ob'I#,.i@luuzf_   .ha.    v*   r&.   .cp".u'
+                                       lx''u+     .lW     .nM? .!B1  \km&>  ,kl"  '.   .dp.    nW"':b&.   .1!  "
+                                       lb] ;l      1vnunn}  :uuux?    |ci    'lvuxr\    {}     "/jc!;u    ->> ~.
+                                       -dw".}?                                                           "l1''I
+                                        }#u,~c"                                                         .(:i ;?
+                                        "od(,;x"                                                       .!I1\[1.    .'
+                                         ?f{u~i\,                                                     '/i>]>\.      .
+                                          ]c:z!?u?                                                   "\[<(l(.
+                                           |{~I/[)l\                                                +x?>lx!.
+                                            -(],)(In1\                                            \1(,lbj?
+                                             .;{~_!ru/(_                                        .?(_,vq1'
+                                               '!1|\:xlcr|.                                   .|:>.?jx+
+                                                 .<{>\~\lbm)_                               .]/|._xl|
+                                                   .>(1_\_1mpjI_                          "[1]',lr<.
+                                                      ,]I:"._Imds(+.                   .~:>".+1:,
+                                                         ,;):,'>)ndw/;_.            "]I;|.+lc("
+                                                            -1/)>\\_<1ncr;_'. ."~>:)}[+!rzn:"
+                                                              .-[)([,. '-<1}ulmmxl1I(nnl]"
+                                                                  .\|[![<~\. '_|][l}I?'
+                                                                       \~><]?<]+-|~'
+
+
+
+    '''
+)
