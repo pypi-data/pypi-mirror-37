2882b2e633a105b036b53030d0b08e4b3fdc0ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:54 +0000

    Add package caf-verilog/0.0.2/caf_verilog-0.0.2.tar.gz

---
+++ packages/caf-verilog/caf_verilog-0.0.2.tar.gz/caf_verilog-0.0.2/caf_verilog/cpx_multiply.py
@@ -0,0 +1,34 @@
+from .quantizer import quantize
+
+
+class CpxMultiply:
+
+    def __init__(self, x, y, x_i_bits=12, x_q_bits=0, y_i_bits=12, y_q_bits=0, output_dir='.'):
+        self.x = x
+        self.y = y
+        self.x_i_bits = x_i_bits
+        self.x_q_bits = x_q_bits if x_q_bits else self.x_i_bits
+        self.y_i_bits = y_i_bits
+        self.y_q_bits = y_q_bits if y_q_bits else self.y_i_bits
+        self.x_quant = quantize(self.x, self.x_i_bits, self.x_q_bits)
+        self.y_quant = quantize(self.y, self.y_i_bits, self.y_q_bits)
+        self.output_dir = output_dir
+
+    def gen_tb(self):
+        """
+        Generate a test bench using quantized values.
+
+        :param output_dir: The output directory to place the testbench and quantized file.
+        :return:
+        """
+
+    def gen_quantized_output(self):
+        """
+        Perform the multiplication and then quantize to the closest representation of what the verilog module should
+        produce for the given bit length.
+
+        :return:
+        """
+        ideal_mult = self.x * self.y
+        q_out = quantize(ideal_mult, self.x_i_bits + self.y_i_bits, self.x_q_bits + self.y_q_bits)
+        return q_out
