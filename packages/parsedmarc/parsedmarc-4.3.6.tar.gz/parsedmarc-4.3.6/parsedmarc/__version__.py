5515b0d48745bb3fba55c7c6e59db72034ac8706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:54 +0000

    Add package parsedmarc/4.3.6/parsedmarc-4.3.6.tar.gz

---
+++ packages/parsedmarc/parsedmarc-4.3.6.tar.gz/parsedmarc-4.3.6/parsedmarc/__version__.py
@@ -0,0 +1,11 @@
+"""Sets global version values"""
+
+import platform
+
+__version__ = "4.3.6"
+
+USER_AGENT = "Mozilla/5.0 ((0 {1})) parsedmarc/{2}".format(
+            platform.system(),
+            platform.release(),
+            __version__
+        )
