905085c3776b258bf512642979d262a30f033a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:44 +0000

    Add package Axelrod/4.4.0/Axelrod-4.4.0.tar.gz

---
+++ packages/Axelrod/Axelrod-4.4.0.tar.gz/Axelrod-4.4.0/axelrod/__init__.py
@@ -0,0 +1,28 @@
+DEFAULT_TURNS = 200
+
+# The order of imports matters!
+from .version import __version__
+from .load_data_ import load_pso_tables, load_weights
+from . import graph
+from .action import Action
+from .random_ import random_choice, seed, Pdf
+from .plot import Plot
+from .game import DefaultGame, Game
+from .player import (
+    get_state_distribution_from_history,
+    is_basic,
+    obey_axelrod,
+    update_history,
+    update_state_distribution,
+    Player,
+)
+from .mock_player import MockPlayer
+from .match import Match
+from .moran import MoranProcess, ApproximateMoranProcess
+from .strategies import *
+from .deterministic_cache import DeterministicCache
+from .match_generator import *
+from .tournament import Tournament
+from .result_set import ResultSet
+from .ecosystem import Ecosystem
+from .fingerprint import AshlockFingerprint, TransitiveFingerprint
