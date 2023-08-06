8315f5e2bd9044bbb4ffd6ba77ae65b34730b8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:03 +0000

    Add package liebiaoxianshi/1.0.0/liebiaoxianshi-1.0.0.tar.gz

---
+++ packages/liebiaoxianshi/liebiaoxianshi-1.0.0.tar.gz/liebiaoxianshi-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'liebiaoxianshi',
+	version		= '1.0.0',
+	py_modules 	= ['nester'],
+	author 		= 'gaomy',
+	author_email	= '413570169@qq.com',
+	url 		= 'http://www.headfirstlabs.com',
+	description 	= 'A simple printer of liebiaoxianshi lists',
+)
