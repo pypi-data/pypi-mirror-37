41ba3f7edf0c6b510886b210a4ac633d16f308e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:33 +0000

    Add package kaihau/1.0.0/kaihau-1.0.0.tar.gz

---
+++ packages/kaihau/kaihau-1.0.0.tar.gz/kaihau-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+name = 'kaihau',
+version='1.0.0',
+author = 'yfx',
+author_email='2573179560@qq.com',
+# py_modules = 'my_module',
+url="https://pypi.org/"
+# py_modules = ['my_module'],
+)
