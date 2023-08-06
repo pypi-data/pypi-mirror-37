8911513ef7abf7c3ec34ff9940b4a34cac595409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:24 +0000

    Add package gpg-exchange/0.0.7/gpg_exchange-0.0.7-py2.py3-none-any.whl

---
+++ packages/gpg-exchange/gpg_exchange-0.0.7-py2.py3-none-any.whl/gpg_exchange/__init__.py
@@ -0,0 +1,22 @@
+"""
+Package setup script for GPG exchange.
+
+Copyright 2018 Leon Helwerda
+
+GPP exchange is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+GPG exchange is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <https://www.gnu.org/licenses/>.
+"""
+
+from .exchange import Exchange
+
+__version__ = '0.0.7'
