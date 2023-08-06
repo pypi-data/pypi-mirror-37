2d8d179605ed2fd0880d4c395d2394cddef05e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:08 +0000

    Add package mw-aiohttp-babel/0.1.1/mw-aiohttp-babel-0.1.1.tar.gz

---
+++ packages/mw-aiohttp-babel/mw-aiohttp-babel-0.1.1.tar.gz/mw-aiohttp-babel-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+    Babel localisation support for aiohttp
+    代码从 https://github.com/jie/aiohttp_babel 拷贝并修改
+"""
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = "mw-aiohttp-babel",
+    version = "0.1.1",
+    packages = find_packages(),
+    install_requires = [
+        "aiohttp",
+        "babel",
+        "speaklater",
+    ],
+    author = "cxhjet",
+    author_email = "cxhjet@qq.com",
+    description = "Babel localisation support for aiohttp",
+    license = "BSD",
+    keywords = "aiohttp locale babel localisation",
+    url = "https://bitbucket.org/maxwin-inc/mw-aiohttp-babel/src",
+)
