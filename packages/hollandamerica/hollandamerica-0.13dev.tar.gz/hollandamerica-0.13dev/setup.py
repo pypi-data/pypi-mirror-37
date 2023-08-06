f42a3f8dfe5899d52521465d099f16db85cc8687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:11 +0000

    Add package hollandamerica/0.13dev/hollandamerica-0.13dev.tar.gz

---
+++ packages/hollandamerica/hollandamerica-0.13dev.tar.gz/hollandamerica-0.13dev/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+	name='hollandamerica',
+	version='0.13dev',
+	packages=['holam',],
+	license='MIT',
+	long_description=open('README.txt').read(),	
+	install_requires=[
+		'beautifulsoup4',
+		'pandas',
+		'selenium',
+		'langdetect',
+		'urllib'
+	],
+)
