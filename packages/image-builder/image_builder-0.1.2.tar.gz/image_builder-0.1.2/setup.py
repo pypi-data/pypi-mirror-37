9009bd325001e4ca659615edb9858fec31bd3500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:54 +0000

    Add package image-builder/0.1.2/image_builder-0.1.2.tar.gz

---
+++ packages/image-builder/image_builder-0.1.2.tar.gz/image_builder-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='image_builder',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'shortesttrack-tools==0.1.2',
+    ]
+)
