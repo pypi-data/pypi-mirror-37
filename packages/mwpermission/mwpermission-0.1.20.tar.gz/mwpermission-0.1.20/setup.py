45fc42970045c328569fa5821a46d65fbc86a1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:32 +0000

    Add package mwpermission/0.1.20/mwpermission-0.1.20.tar.gz

---
+++ packages/mwpermission/mwpermission-0.1.20.tar.gz/mwpermission-0.1.20/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import os
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+setup(
+    name='mwpermission',
+    version='0.1.20',
+    description='maxwin permission ',
+    long_description= '\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+    url='https://bitbucket.org/maxwin-inc/mwpermission/src',  # Optional
+    author='cxhjet',  # Optional
+    author_email='cxhjet@qq.com',  # Optional
+    packages=['mwpermission'],
+    package_data={
+        '': ['*.*']
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        'flask>=0.11.1'
+    ]
+)
