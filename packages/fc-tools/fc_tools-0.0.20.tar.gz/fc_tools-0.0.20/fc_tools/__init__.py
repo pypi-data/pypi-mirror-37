98cabf3b5924b91b5b5c4ff65802c3d277d59ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:59 +0000

    Add package fc-tools/0.0.20/fc_tools-0.0.20.tar.gz

---
+++ packages/fc-tools/fc_tools-0.0.20.tar.gz/fc_tools-0.0.20/fc_tools/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2018 F. Cuvelier
+# License: GNU GPL version 3
+__version__ = '0.0.20'
+from . import others
+from . import git
+from . import colors
+from . import Sys
+from .git import gitinfo
---
+++ packages/fc-tools/fc_tools-0.0.20.tar.gz/fc_tools-0.0.20/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from os import path
+import sys
+here = path.abspath(path.dirname(__file__))
+
+
+if sys.version_info[0]>2:
+  with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+else:    
+  with open(path.join(here, 'README.rst')) as f:  
+    long_description = f.read().decode("UTF-8")
+
+
+setup_defaults = {  
+   'name'        : 'fc_tools',
+   'description' : 'Some functions used in fc_hypermesh, fc_bench, fc_simesh, ... packages',
+   'version'     : '0.0.20',
+   'url'         : 'http://www.math.univ-paris13.fr/~cuvelier/software',
+   'author'      : 'Francois Cuvelier',
+   'author_email': 'cuvelier@math.univ-paris13.fr',
+   'license'     : 'BSD',
+   'packages'    : ['fc_tools'],
+   'classifiers':['Topic :: Scientific/Engineering :: Mathematics'],
+   } 
+
+setup(name=setup_defaults['name'],
+      description = setup_defaults['description'],
+      long_description=long_description,
+      version=setup_defaults['version'],
+      url=setup_defaults['url'],
+      author=setup_defaults['author'],
+      author_email=setup_defaults['author_email'],
+      license = setup_defaults['license'],
+      packages=setup_defaults['packages'],
+      classifiers=setup_defaults['classifiers'],
+      install_requires=['numpy','screeninfo','appdirs']      
+     )
