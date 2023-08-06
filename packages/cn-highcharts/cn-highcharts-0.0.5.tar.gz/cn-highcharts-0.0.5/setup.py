5f93cdb9ec6dc870acd70ea4f48aac0ff66ec973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:31 +0000

    Add package cn-highcharts/0.0.5/cn-highcharts-0.0.5.tar.gz

---
+++ packages/cn-highcharts/cn-highcharts-0.0.5.tar.gz/cn-highcharts-0.0.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cn-highcharts',
+    version='0.0.5',
+    description=('Python Highcharts wrapper for china.'),
+    install_requires=["Jinja2", "future"],
+    # long_description=open('README.rst').read(),
+    author='liubola',
+    author_email='lby3523@gmail.com',
+    # maintainer='<维护人员的名字>',
+    # maintainer_email='<维护人员的邮件地址',
+    license='GNU General Public License v3.0',
+    packages=find_packages(),
+    package_data={
+        'highcharts.highcharts': ['templates/*.html'],
+        'highcharts.highmaps': ['templates/*.html'],
+        'highcharts.highstock': ['templates/*.html']
+    },
+    platforms=["all"],
+    keywords=[
+        'python', 'ipython', 'highcharts', 'chart', 'visualization', 'graph',
+        'javascript', 'html'
+    ],
+    url='https://github.com/liubola/cn-highcharts',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
