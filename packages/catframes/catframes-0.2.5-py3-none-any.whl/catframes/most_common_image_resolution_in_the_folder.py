532f37d6176ea9a14df0bd38255402379b5d53bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:50 +0000

    Add package catframes/0.2.5/catframes-0.2.5-py3-none-any.whl

---
+++ packages/catframes/catframes-0.2.5-py3-none-any.whl/catframes/most_common_image_resolution_in_the_folder.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3 
+
+import os
+import sys
+import operator
+import time
+from multiprocessing import Pool, cpu_count
+from catframes.utils import *
+
+
+def get_resolution(f):
+    if os.path.isdir(f):
+        return None
+    out = os.popen("identify -format '%wx%h' \"{}\"".format(f)).read()
+    if not out:
+        os.rename(f, f + '_corrupted')
+        return None
+    return out
+
+
+def most_common_image_resolution_in_the_folder(statistics=False):
+    filenames = list_of_files()
+    frequences_of_resolutions = {}
+
+    with Pool(processes=cpu_count()) as pool:
+        for out in pool.imap_unordered(get_resolution, filenames):
+            if out in frequences_of_resolutions:
+                frequences_of_resolutions[out] = frequences_of_resolutions[out] + 1
+            else:
+                frequences_of_resolutions[out] = 1
+
+    if statistics:
+        return frequences_of_resolutions
+    else:
+        result = max(frequences_of_resolutions, key=lambda key: frequences_of_resolutions[key])
+        return result
+
+
+def run():
+    usage = '\n    catframes_most_common_image_resolution_in_the_folder [--statistics|-s]\n'
+
+    if (len(sys.argv) > 1) and ((sys.argv[1] == '--help') or (sys.argv[1] == '-h')):
+        print(usage)
+
+    elif (len(sys.argv) > 1) and ((sys.argv[1] == '--statistics') or (sys.argv[1] == '-s')):
+        start_time = time.time()
+        stat = most_common_image_resolution_in_the_folder(statistics=True)
+        print()
+        for k, v in sorted(stat.items(), key=operator.itemgetter(1), reverse=True):
+            print("{} => {}".format(k, v))
+        print('-------------\nCompleted in {} seconds.\n'.format(time.time() - start_time))
+
+    elif len(sys.argv) > 1:
+        print('Bad argument.')
+        print(usage)
+
+    else:
+        print(most_common_image_resolution_in_the_folder(), end='')
