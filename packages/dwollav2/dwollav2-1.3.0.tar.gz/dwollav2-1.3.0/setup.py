b6b6afc78dd0d9a5dade65f67105da1fe5efe174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:29 +0000

    Add package dwollav2/1.3.0/dwollav2-1.3.0.tar.gz

---
+++ packages/dwollav2/dwollav2-1.3.0.tar.gz/dwollav2-1.3.0/setup.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import warnings
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='dwollav2',
+    version='1.3.0',
+    packages=['dwollav2'],
+    install_requires=[
+        'requests>=2.9.1',
+        'future>=0.15.2'
+    ],
+    test_suite='dwollav2.test.all',
+    url='https://docsv2.dwolla.com',
+    license='MIT',
+    author='Stephen Ausman',
+    author_email='stephen@dwolla.com',
+    long_description=open('README.rst').read(),
+    description='Official Dwolla V2 API client',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
