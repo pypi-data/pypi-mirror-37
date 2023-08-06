08b2d775cab98a19ae28346422991b0de2fef4c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:52 +0000

    Add package kurama/1.0.1/kurama-1.0.1-py2.py3-none-any.whl

---
+++ packages/kurama/kurama-1.0.1-py2.py3-none-any.whl/kurama/meme.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Credit goes to James Bennett (https://github.com/ubernostrum) 
+# for the original implementation
+
+
+def main():
+    import webbrowser
+    webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+
+if __name__ == '__main__':
+    main()
