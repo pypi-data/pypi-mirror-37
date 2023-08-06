297f684a1438e35d1983c103fc1f32f7d1a317cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:35 +0000

    Add package dask-kubernetes/0.6.0/dask-kubernetes-0.6.0.tar.gz

---
+++ packages/dask-kubernetes/dask-kubernetes-0.6.0.tar.gz/dask-kubernetes-0.6.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup, find_packages
+
+setup(
+    name='dask-kubernetes',
+    version='0.6.0',
+    description='Native Kubernetes integration for Dask',
+    url='https://github.com/dask/dask-kubernetes',
+    keywords='dask,kubernetes,distributed',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=(open('README.rst').read() if exists('README.rst') else ''),
+    zip_safe=False,
+    install_requires=list(open('requirements.txt').read().strip().split('\n')),
+)
