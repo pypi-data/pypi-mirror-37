846e0fd62f59c59d37fc62cdf2464bab9520f5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:16 +0000

    Add package twnews/0.1.8/twnews-0.1.8-py3-none-any.whl

---
+++ packages/twnews/twnews-0.1.8-py3-none-any.whl/twnews/tests/test_common.py
@@ -0,0 +1,37 @@
+"""
+分解程式共用單元測試
+"""
+
+import os
+import re
+import tempfile
+import unittest
+from twnews.soup import NewsSoup, get_cache_dir
+
+#@unittest.skip
+class TestCommon(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'https://tw.news.appledaily.com/local/realtime/20181025/1453825'
+        self.dtf = '%Y-%m-%d %H:%M:%S'
+
+    def test_01_cache(self):
+        """
+        測試快取機制
+        """
+
+        # 清除快取
+        cache_dir = get_cache_dir()
+        for cache_file in os.listdir(cache_dir):
+            if re.match(r'appledaily-mobile-.*\.html', cache_file):
+                cache_path = '{}/{}'.format(cache_dir, cache_file)
+                os.unlink(cache_path)
+
+        # 讀取新聞，計算快取檔案數
+        count = 0
+        nsoup = NewsSoup(self.url)
+        for cache_file in os.listdir(cache_dir):
+            if re.match(r'appledaily-mobile-.*\.html', cache_file):
+                count += 1
+
+        self.assertEqual(1, count)
