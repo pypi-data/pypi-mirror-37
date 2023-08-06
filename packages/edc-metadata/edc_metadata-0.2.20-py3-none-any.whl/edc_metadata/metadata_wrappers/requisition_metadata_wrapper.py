1c503bd2dea6fa2a8ff1ef4d6bb0fc3114d2ad09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:18 +0000

    Add package edc-metadata/0.2.20/edc_metadata-0.2.20-py3-none-any.whl

---
+++ packages/edc-metadata/edc_metadata-0.2.20-py3-none-any.whl/edc_metadata/metadata_wrappers/requisition_metadata_wrapper.py
@@ -0,0 +1,16 @@
+from .metadata_wrapper import MetadataWrapper
+
+
+class RequisitionMetadataWrapper(MetadataWrapper):
+
+    label = 'Requisition'
+
+    def __init__(self, metadata_obj=None, **kwargs):
+        self.panel_name = metadata_obj.panel_name
+        super().__init__(metadata_obj=metadata_obj, **kwargs)
+
+    @property
+    def options(self):
+        options = super().options
+        options.update(panel__name=self.panel_name)
+        return options
