46f3c1e396452ca4fd827a9e6289937d54d18d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:33 +0000

    Add package tkapi/0.5.1/tkapi-0.5.1-py3-none-any.whl

---
+++ packages/tkapi/tkapi-0.5.1-py3-none-any.whl/tkapi/besluit.py
@@ -0,0 +1,44 @@
+import tkapi
+
+
+class BesluitRelationFilter(tkapi.RelationFilter):
+
+    @property
+    def related_url(self):
+        return Besluit.url
+
+
+class BesluitFilter(tkapi.SoortFilter, tkapi.ZaakRelationFilter):
+
+    def __init__(self):
+        super().__init__()
+
+
+class Besluit(tkapi.TKItemRelated, tkapi.TKItem):
+    url = 'Besluit'
+
+    @staticmethod
+    def create_filter():
+        return BesluitFilter()
+
+    @property
+    def zaken(self):
+        from tkapi.zaak import Zaak
+        return self.related_items(Zaak)
+
+    @property
+    def stemmingen(self):
+        from tkapi.stemming import Stemming
+        return self.related_items(Stemming)
+
+    @property
+    def soort(self):
+        return self.get_property_or_empty_string('Soort')
+
+    @property
+    def status(self):
+        return self.get_property_or_empty_string('Status')
+
+    @property
+    def slottekst(self):
+        return self.get_property_or_empty_string('Slottekst')
