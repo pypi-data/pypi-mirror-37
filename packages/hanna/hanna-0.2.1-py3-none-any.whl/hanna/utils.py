5e3ad97314d93d63cc76ed169ec2962a0bcf5a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:29 +0000

    Add package hanna/0.2.1/hanna-0.2.1-py3-none-any.whl

---
+++ packages/hanna/hanna-0.2.1-py3-none-any.whl/hanna/utils.py
@@ -0,0 +1,15 @@
+import re
+
+
+def safe_math_eval(string, unsafe_error=ValueError, locals_dict=None):
+    is_unsafe = [
+        any(map(
+            lambda x: x in string,
+            ('__', '()', '[]', '{}', 'lambda', ',', ';', ':', '"', "'")
+        )),
+        # Allow a-z for numpy or math functions.
+        re.match(r'^[.*/+\-0-9a-zA-Z\s()_]+$', string) is None
+    ]
+    if any(is_unsafe):
+        raise unsafe_error(f'Evaluation of {repr(string)} is considered unsafe')
+    return eval(string, {'__builtins__': None}, locals_dict or {})
