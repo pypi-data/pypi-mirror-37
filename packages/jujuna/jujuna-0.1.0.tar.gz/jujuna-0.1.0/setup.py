ca6b1d83d1e627f8d4a34b1d7aeb7ad24d75748d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:00 +0000

    Add package jujuna/0.1.0/jujuna-0.1.0.tar.gz

---
+++ packages/jujuna/jujuna-0.1.0.tar.gz/jujuna-0.1.0/setup.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+here = Path(__file__).absolute().parent
+readme = here / 'README.md'
+changelog = here / 'CHANGELOG'
+reqs_file = here / 'requirements.txt'
+
+reqs = [line for line in reqs_file.read_text().strip().splitlines()
+        if not line.startswith('--')]
+version = here / 'VERSION'
+
+SETUP = {
+    'name': "jujuna",
+    'packages': find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    'version': version.read_text().strip(),
+    'author': "Matus Kosut",
+    'author_email': "matuskosut@gmail.com",
+    'maintainer': 'HUNT Data Center',
+    'maintainer_email': 'cloud@hunt.ntnu.no',
+    'url': "https://github.com/huntdatacenter/jujuna",
+    'long_description': open('README.md').read(),
+    'entry_points': {
+        'console_scripts': [
+            # Script invokation:
+            'jujuna = jujuna.__main__:main',
+        ]
+    },
+    'license': 'Apache 2',
+    'classifiers': [
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console"
+    ],
+    'install_requires': [
+        'pyyaml', 'async-timeout==2.0.1', 'juju>=0.10.2', 'argcomplete'
+    ],
+}
+
+
+if __name__ == '__main__':
+    setup(**SETUP)
