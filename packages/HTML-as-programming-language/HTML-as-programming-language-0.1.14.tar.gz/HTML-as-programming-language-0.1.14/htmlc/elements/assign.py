cc8f3ce2c9b38e0b635fa6ba9391c8f57c865104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:59 +0000

    Add package HTML-as-programming-language/0.1.14/HTML-as-programming-language-0.1.14.tar.gz

---
+++ packages/HTML-as-programming-language/HTML-as-programming-language-0.1.14.tar.gz/HTML-as-programming-language-0.1.14/htmlc/elements/assign.py
@@ -0,0 +1,44 @@
+from htmlc.diagnostics import Diagnostic, Severity
+from htmlc.elements.element import Element
+
+
+class Assign(Element):
+    """"
+    HTML: <assign a>5</assign>
+    C: a = 5;
+
+    or
+
+    HTML: <assign a><have nr0 of>myPile</have></assign>
+    C: a = myPile[0];
+    """
+    def __init__(self):
+        super().__init__()
+        self.var_name = None
+
+    def init(self):
+        for key in self.attributes:
+            self.var_name = key
+
+    def diagnostics(self):
+        d = []
+        if not self.var_name:
+            d.append(
+                Diagnostic(
+                    Severity.ERROR,
+                    self.code_range,
+                    "No variable name given (for example: <assign var-name>123</assign>)"
+                )
+            )
+        elif not self.data:
+            d.append(
+                Diagnostic(
+                    Severity.ERROR,
+                    self.code_range,
+                    "Cannot assign nothing to {}".format(self.var_name)
+                )
+            )
+        return d
+
+    def to_c(self, mapped_c):
+        mapped_c.add(f"{self.var_name} = {self.data.strip()};\n", self)
