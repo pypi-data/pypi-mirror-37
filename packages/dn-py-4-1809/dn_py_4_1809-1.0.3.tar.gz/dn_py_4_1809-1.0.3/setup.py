f119250af7bddb5aacf089f9c48c46d4ba41774e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:45 +0000

    Add package dn-py-4-1809/1.0.3/dn_py_4_1809-1.0.3.tar.gz

---
+++ packages/dn-py-4-1809/dn_py_4_1809-1.0.3.tar.gz/dn_py_4_1809-1.0.3/setup.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+# author:eric(2384363842@qq.com)
+# 创建于: 2018/10/24 19:55
+from setuptools import setup
+setup(
+    name='dn_py_4_1809',         # 应用名
+    version='1.0.3',        # 版本号
+    packages=['package_send2pypi'],  # 包括在安装包内的Python包
+    author="hustcsxg",
+    author_email="hustcsxg@163.com",
+    url="http://www.py2ai.com"
+)
