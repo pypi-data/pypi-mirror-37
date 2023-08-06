722e14c1ea06a6c16c6e66ee356f47e4a2f48714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:08 +0000

    Add package lumby-fbs/1.0.0/lumby_fbs-1.0.0.tar.gz

---
+++ packages/lumby-fbs/lumby_fbs-1.0.0.tar.gz/lumby_fbs-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='lumby_fbs',
+        version='1.0.0',
+        description='Lumby flatbuffer schema',
+        url='https://github.com/jean-airoldie/lumby-flatbuffers',
+        author='Maxence Caron',
+        author_email='maxence.caron.1@ulaval.ca',
+        license='MIT',
+        packages=['lumby_fbs'],
+        install_requires=[
+            'flatbuffers==1.10'
+        ],
+        zip_safe=False)
