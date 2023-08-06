ba05c39eb8b17c6bf3ce6ecd0cdc328f024a0ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:49 +0000

    Add package ml-python/1.6/ml_python-1.6-py3-none-any.whl

---
+++ packages/ml-python/ml_python-1.6-py3-none-any.whl/ml/__init__.py
@@ -0,0 +1,11 @@
+from ml.k_means import *
+from ml.linear_regression import *
+from ml.logistic_regression import *
+from ml.nn import *
+from ml.random import *
+from ml.activation import *
+from ml.cnn import *
+
+import warnings
+
+warnings.filterwarnings("always")
