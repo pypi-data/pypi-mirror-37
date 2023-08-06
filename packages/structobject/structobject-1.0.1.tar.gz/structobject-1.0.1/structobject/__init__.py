ddf98929511f46c69ca6cc8048e0b6080a34c7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:37 +0000

    Add package structobject/1.0.1/structobject-1.0.1.tar.gz

---
+++ packages/structobject/structobject-1.0.1.tar.gz/structobject-1.0.1/structobject/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .structObject import *
+from .structField import *
+from .compatibility import *
