47056c6e1ecb09156e8cbec4ff7cd08acf1f30c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:23 +0000

    Add package deathbycaptcha/0.1.0/deathbycaptcha-0.1.0.tar.gz

---
+++ packages/deathbycaptcha/deathbycaptcha-0.1.0.tar.gz/deathbycaptcha-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+version = "0.1.0"
+
+setup(name="deathbycaptcha",
+      version=version,
+      author="Sergey Kolchin",
+      author_email="qwang16@olivetuniversity.edu",
+      license="BSD License (New BSD License)",
+      description="python API for deathbycaptcha.com",
+      url="https://github.com/hyan15/python-deathbycaptcha",
+      packages=find_packages())
