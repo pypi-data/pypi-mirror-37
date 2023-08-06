270d7aecc2fc912e168572e3127508b5acf66285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:20 +0000

    Add package ssm-cache/2.6.1/ssm-cache-2.6.1.tar.gz

---
+++ packages/ssm-cache/ssm-cache-2.6.1.tar.gz/ssm-cache-2.6.1/setup.py
@@ -0,0 +1,24 @@
+""" Setup command """
+from setuptools import setup, find_packages
+
+#try:
+#    import pypandoc
+#    LONG_DESCRIPTION = pypandoc.convert('README.md', 'rst')
+#except(IOError, ImportError):
+LONG_DESCRIPTION = open('README.md').read()
+
+setup(
+    name='ssm-cache',
+    version='2.6.1',
+    description='AWS System Manager Parameter Store caching client for Python',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords=['aws', 'amazon-web-services', 'aws-lambda', 'aws-ssm', 'parameter-store'],
+    license="MIT",
+    author='Alex Casalboni',
+    author_email='alex@alexcasalboni.com',
+    url='https://github.com/alexcasalboni/ssm-cache-python',
+    download_url='https://github.com/alexcasalboni/ssm-cache-python/archive/2.6.tar.gz',
+    packages=find_packages(),
+    install_requires=['boto3', 'future'],
+)
