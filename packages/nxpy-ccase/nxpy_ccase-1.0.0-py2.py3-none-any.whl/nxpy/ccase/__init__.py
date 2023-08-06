184f3c4afb42f5698c70b123395cf6403e502a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:50 +0000

    Add package nxpy-ccase/1.0.0/nxpy_ccase-1.0.0-py2.py3-none-any.whl

---
+++ packages/nxpy-ccase/nxpy_ccase-1.0.0-py2.py3-none-any.whl/nxpy/ccase/__init__.py
@@ -0,0 +1,13 @@
+# nxpy.ccase package ---------------------------------------------------------
+
+# Copyright Nicola Musatti 2011 - 2018
+# Use, modification, and distribution are subject to the Boost Software
+# License, Version 1.0. (See accompanying file LICENSE.txt or copy at
+# http://www.boost.org/LICENSE_1_0.txt)
+
+# See https://github.com/nmusatti/nxpy_ccase. --------------------------------
+
+r"""
+Tools and utilities related to the ClearCase software configuration management product.
+
+"""
