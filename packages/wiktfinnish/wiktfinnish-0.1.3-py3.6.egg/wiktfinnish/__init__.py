12073ce16f975cb9dc9906bbeef0f12ba28a6286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:14 +0000

    Add package wiktfinnish/0.1.3/wiktfinnish-0.1.3-py3.6.egg

---
+++ packages/wiktfinnish/wiktfinnish-0.1.3-py3.6.egg/wiktfinnish/__init__.py
@@ -0,0 +1,24 @@
+# This file defines the public exports from the wiktfinnish module.
+#
+# Copyright (c) 2018 Tatu Ylonen.  See LICENSE and https://ylonen.org
+
+from wiktfinnish.formnames import COMP_FORMS, CASE_FORMS, POSSESSIVE_FORMS
+from wiktfinnish.formnames import VERB_FORMS, CLITIC_FORMS
+from wiktfinnish.formnames import all_forms_list, all_forms_iter
+from wiktfinnish.inflect import inflect
+from wiktfinnish.inflect import add_clitic
+from wiktfinnish.inflect import last_char_to_vowel, last_char_to_aou
+from wiktfinnish.inflect import word_to_aae
+
+
+__all__ = (
+    "inflect",
+    "add_clitic",
+    "COMP_FORMS",
+    "CASE_FORMS",
+    "POSSESSIVE_FORMS",
+    "VERB_FORMS",
+    "CLITIC_FORMS",
+    "all_forms_list",
+    "all_forms_iter",
+)
