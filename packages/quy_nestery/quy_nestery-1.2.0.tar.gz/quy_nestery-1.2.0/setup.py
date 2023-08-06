a82f8065dfe4967dab03b655ae665966ca33ce9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:03 +0000

    Add package quy_nestery/1.2.0/quy_nestery-1.2.0.tar.gz

---
+++ packages/quy_nestery/quy_nestery-1.2.0.tar.gz/quy_nestery-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name ='quy_nestery',
+        version = '1.2.0',
+        py_modules = ['quy_nestery'],
+        author = 'legiaquy',
+        author_email = 'legiaquy@gmail.com',
+        url = 'https://github.com/legiaquy/hfpython',
+        description = 'A simple printer of nested lists',
+        )
