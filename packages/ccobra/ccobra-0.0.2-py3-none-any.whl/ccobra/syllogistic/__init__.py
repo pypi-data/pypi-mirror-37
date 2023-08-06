5e3e2f48f9cab31941c0b3c84d85a131f9ac5320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:34 +0000

    Add package ccobra/0.0.2/ccobra-0.0.2-py3-none-any.whl

---
+++ packages/ccobra/ccobra-0.0.2-py3-none-any.whl/ccobra/syllogistic/__init__.py
@@ -0,0 +1,11 @@
+""" Syllogistic submodule.
+
+.. autosummary::
+   :toctree: _autosummary
+
+   ccobra.syllogistic.parsing
+
+"""
+
+from .parsing import decode_response, encode_response,encode_task, SYLLOGISMS, RESPONSES
+from .syllogism import Syllogism
