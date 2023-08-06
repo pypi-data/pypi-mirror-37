774f9ef046ca20ec96961c50f61d941d5d51f591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:42 +0000

    Add package ldbg-jsonrpc/1.0.1/ldbg_jsonrpc-1.0.1-py3-none-any.whl

---
+++ packages/ldbg-jsonrpc/ldbg_jsonrpc-1.0.1-py3-none-any.whl/ldbg_jsonrpc/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : __init__.py
+# @Author: harry
+# @Date  : 18-10-7 上午1:22
+# @Desc  : Description goes here
+
+"""
+Python3 Client SDK for Little Daemon Bot Gateway
+[jsonrpc-server-any](https://projectriri.github.io/bot-gateway/docs/Plugins.html#jsonrpc-server-any) Plugin.
+"""
+
+from . import client
+
+__author__ = 'Bittersweet(https://github.com/zhouziqunzzq)'
+__version__ = '1.0.1'
