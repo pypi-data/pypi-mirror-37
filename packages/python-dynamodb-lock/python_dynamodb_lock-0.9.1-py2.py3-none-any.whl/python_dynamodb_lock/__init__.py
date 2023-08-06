ec7ff1d71e1f570d68405a70ebf3c2d7487a4193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:29 +0000

    Add package python-dynamodb-lock/0.9.1/python_dynamodb_lock-0.9.1-py2.py3-none-any.whl

---
+++ packages/python-dynamodb-lock/python_dynamodb_lock-0.9.1-py2.py3-none-any.whl/python_dynamodb_lock/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""The package contains a single module - with the same name i.e. python_dynamodb_lock"""
+
+__author__ = """Mohan Kishore"""
+__email__ = 'mohankishore@yahoo.com'
+__version__ = '0.9.1'
+__copyright__ = 'Copyright (C) 2018 Mohan Kishore'
