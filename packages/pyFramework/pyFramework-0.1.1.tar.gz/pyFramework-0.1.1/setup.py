172def71925c6c0e547f7891d015d8d906de1952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:33 +0000

    Add package pyFramework/0.1.1/pyFramework-0.1.1.tar.gz

---
+++ packages/pyFramework/pyFramework-0.1.1.tar.gz/pyFramework-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyFramework',
+      version='0.1.1',
+      description='Entity Framework for Python',
+      license='mit',
+      author='SC van Nostrand',
+      author_email='scvannost@gmail.com',
+      download_url='https://github.com/scvannost/pyframework/archive/v_0.1.tar.gz',
+      url='https://scvannost.com',
+      packages=['pyframework'],
+      python_requires='>=3.0',
+      install_requires=[
+	'mysqlclient',
+	'passlib',
+      ],
+      keywords = ['DATABASE', 'USER MANAGEMENT'],
+      classifiers=[
+	'Development Status :: 3 - Alpha',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 3',
+	'Programming Language :: Python :: 3.5',
+      ]
+     )
