6dfc9a2ba1a52a916fbb664be1578581da0cc69c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:51 +0000

    Add package kurama/1.0/kurama-1.0-py2.py3-none-any.whl

---
+++ packages/kurama/kurama-1.0-py2.py3-none-any.whl/kurama/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Credit goes to James Bennett (https://github.com/ubernostrum)
+# for the original implementation
+
+from . import meme
+
+
+if __name__ == '__main__':
+    meme.main()
