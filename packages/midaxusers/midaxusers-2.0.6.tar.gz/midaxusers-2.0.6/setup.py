49e2043e9b83d0b9f49162e3d37e6fcbb8c77e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:03 +0000

    Add package midaxusers/2.0.6/midaxusers-2.0.6.tar.gz

---
+++ packages/midaxusers/midaxusers-2.0.6.tar.gz/midaxusers-2.0.6/setup.py
@@ -0,0 +1,46 @@
+import io
+
+from setuptools import find_packages, setup
+
+setup(
+    name='midaxusers',
+    version='2.0.6',
+    url='http://www.midax.com',
+    license='BSD',
+    maintainer='Midax',
+    maintainer_email='alex.r@midax.com',
+    description='Midax Users API',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'alembic',
+        'astroid',
+        'attrs',
+        'click',
+        'colorama',
+        'Flask',
+        'Flask-HTTPAuth',
+        'Flask-Migrate',
+        'Flask-SQLAlchemy',
+        'isort',
+        'itsdangerous',
+        'Jinja2',
+        'lazy-object-proxy',
+        'Mako',
+        'MarkupSafe',
+        'mccabe',
+        'pluggy',
+        'py',
+        'pylint',
+        'pyodbc',
+        'python-dateutil',
+        'python-editor',
+        'six',
+        'SQLAlchemy',
+        'Werkzeug',
+        'wrapt',
+        'cx_Oracle',
+    ]  
+)
