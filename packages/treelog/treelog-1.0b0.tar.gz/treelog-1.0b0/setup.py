c72847efbd89444245f2cc1c71e7827b7ea73d5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:40 +0000

    Add package treelog/1.0b0/treelog-1.0b0.tar.gz

---
+++ packages/treelog/treelog-1.0b0.tar.gz/treelog-1.0b0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os, re
+
+with open(os.path.join('treelog', '__init__.py')) as f:
+  version = next(filter(None, map(re.compile("^version = '([a-zA-Z0-9.]+)'$").match, f))).group(1)
+
+setup(
+  name = 'treelog',
+  version = version,
+  description = 'Logging framework that organizes messages in a tree structure',
+  author = 'Evalf',
+  author_email = 'info@evalf.com',
+  license = 'MIT',
+  python_requires = '>=3.5',
+)
