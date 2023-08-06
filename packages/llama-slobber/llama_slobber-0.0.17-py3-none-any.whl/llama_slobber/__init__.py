e63074f01b40959ea7e36f5eab7384e829576d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:44 +0000

    Add package llama-slobber/0.0.17/llama_slobber-0.0.17-py3-none-any.whl

---
+++ packages/llama-slobber/llama_slobber-0.0.17-py3-none-any.whl/llama_slobber/__init__.py
@@ -0,0 +1,12 @@
+from llama_slobber.ll_local_io import get_session
+from llama_slobber.ll_local_io import get_page_data
+from llama_slobber.ll_season import get_season
+from llama_slobber.ll_leagues import get_leagues
+from llama_slobber.ll_rundles import get_rundles
+from llama_slobber.ll_onedays import get_onedays
+from llama_slobber.ll_qhist import get_qhist
+from llama_slobber.ll_matchday import get_matchday
+from llama_slobber.ll_metrics1 import format_float
+from llama_slobber.ll_metrics1 import calc_hun
+
+name = "llama_slobber"
