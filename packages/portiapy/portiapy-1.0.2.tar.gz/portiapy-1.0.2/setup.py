a27bfa08e19cc2312581016070276d038c095354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:39 +0000

    Add package portiapy/1.0.2/portiapy-1.0.2.tar.gz

---
+++ packages/portiapy/portiapy-1.0.2.tar.gz/portiapy-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.0.2'
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='portiapy',
+    version=__version__,
+    author='Matheus Mota <matheus@agrinessedge.com>, Lucas Góes <lucas@agrinessedge.com>',
+    author_email='matheus@agrinessedge.com',
+    description='A small package for handling Agriness Edge\'s REST API',
+    long_description=long_description,
+    url='https://github.com/AgrinessEdgeIoT/portiapy',
+    packages=find_packages(),
+    install_requires=['requests>=2.19.1', 'pytz>=2018.5', 'python-dateutil>=2.7.3', 'plotly>=3.1.1', 'pandas>=0.23.4', 'arrow>=0.12.1'],
+    #scripts=['portia'],
+    classifiers=(
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ),
+)
