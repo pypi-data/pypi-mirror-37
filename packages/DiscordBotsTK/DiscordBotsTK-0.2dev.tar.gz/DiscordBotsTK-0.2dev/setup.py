4c373672d5bb893dddaacce762cb1fae6a2fb25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:18 +0000

    Add package DiscordBotsTK/0.2dev/DiscordBotsTK-0.2dev.tar.gz

---
+++ packages/DiscordBotsTK/DiscordBotsTK-0.2dev.tar.gz/DiscordBotsTK-0.2dev/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='DiscordBotsTK',
+    version='0.2dev',
+    author="iWeeti",
+    description="This is to update discordbots.tk guild count.",
+    packages=['discordbotstk'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown"
+)
