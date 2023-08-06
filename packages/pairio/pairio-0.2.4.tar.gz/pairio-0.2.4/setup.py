4f0c257482894d0ccfa79ce57b6dba699d274230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:08 +0000

    Add package pairio/0.2.4/pairio-0.2.4.tar.gz

---
+++ packages/pairio/pairio-0.2.4.tar.gz/pairio-0.2.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+pkg_name="pairio"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.2.4",
+    author="Jeremy Magland",
+    author_email="jmagland@flatironinstitute.org",
+    description="Python interface to pairio",
+    url="https://github.com/magland/pairio",
+    packages=setuptools.find_packages(),
+    package_data={},
+    install_requires=[
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    )
+)
