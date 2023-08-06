cdcfc1f53153c0317fbf8d93f43a01426249a120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:44 +0000

    Add package django-models-logging/0.9.9/django-models-logging-0.9.9.tar.gz

---
+++ packages/django-models-logging/django-models-logging-0.9.9.tar.gz/django-models-logging-0.9.9/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+files = ["templates/models_logging/*", "migrations/*"]
+
+setup(
+    name='django-models-logging',
+    version='0.9.9',
+    packages=['models_logging'],
+    url='https://bitbucket.org/legion_an/django-models-logging',
+    package_data = {'models_logging' : files},
+    license='',
+    author='legion',
+    author_email='legion.andrey.89@gmail.com',
+    description='Add logging of models from save, delete signals',
+    keywords=[
+        'django logging',
+        'django history',
+        'django logging models',
+        'django history models',
+    ],
+    install_requires=[
+        "django>=1.8,<2.0",
+        "python-dateutil",
+    ],
+)
