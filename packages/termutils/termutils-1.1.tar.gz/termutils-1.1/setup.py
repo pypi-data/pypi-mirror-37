aac66ae85e66c469c60933de177f67df41f52985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:56 +0000

    Add package termutils/1.1/termutils-1.1.tar.gz

---
+++ packages/termutils/termutils-1.1.tar.gz/termutils-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='termutils',
+    version='1.1',
+    packages=[''],
+    url='https://github.com/tman540/termutils',
+    license='MIT',
+    author='Steven',
+    author_email='stautonico@gmail.com',
+    description='',
+    install_requires=['requests']
+)
