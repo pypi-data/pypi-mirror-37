318837e829fea7a4e32aa0c240ec6c71eff67b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:44 +0000

    Add package UnleashClient/1.0.2/UnleashClient-1.0.2.tar.gz

---
+++ packages/UnleashClient/UnleashClient-1.0.2.tar.gz/UnleashClient-1.0.2/UnleashClient/constants.py
@@ -0,0 +1,12 @@
+# Library
+SDK_NAME = "unleash-client-python"
+SDK_VERSION = "1.0.2"
+REQUEST_TIMEOUT = 30
+
+# =Unleash=
+APPLICATION_HEADERS = {"Content-Type": "application/json"}
+
+# Paths
+REGISTER_URL = "/api/client/register"
+FEATURES_URL = "/api/client/features"
+METRICS_URL = "/api/client/metrics"
