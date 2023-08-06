e76086b133a18834dee086ba084a0298a956a272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:03 +0000

    Add package django-nextpage/1.0.1/django-nextpage-1.0.1.tar.gz

---
+++ packages/django-nextpage/django-nextpage-1.0.1.tar.gz/django-nextpage-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from nextpage import __version__
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-nextpage',
+    version=__version__,
+    description="django-nextpage",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Framework :: Django",
+        "Environment :: Web Environment",
+    ],
+    keywords='pagination,django',
+    author='tzangms',
+    author_email='tzangms@gmail.com',
+    url='http://github.com/StreetVoice/django-nextpage',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
