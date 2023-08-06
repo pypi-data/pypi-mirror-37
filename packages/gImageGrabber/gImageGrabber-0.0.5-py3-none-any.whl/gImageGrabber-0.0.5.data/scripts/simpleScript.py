273d970680cbdc8ab38c8041a6343f54679d6ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:04 +0000

    Add package gImageGrabber/0.0.5/gImageGrabber-0.0.5-py3-none-any.whl

---
+++ packages/gImageGrabber/gImageGrabber-0.0.5-py3-none-any.whl/gImageGrabber-0.0.5.data/scripts/simpleScript.py
@@ -0,0 +1,9 @@
+from gimagegrabber import imageScrapeBrowser as imgScrape
+
+# Search term
+search = 'Python Language'
+fType = ' '  # make sure if you dont want anything then a space is there
+testRun = False
+
+data = imgScrape.get_images(imgScrape.build_url(search), testRun)
+imgScrape.save_images(data, search, fType)
