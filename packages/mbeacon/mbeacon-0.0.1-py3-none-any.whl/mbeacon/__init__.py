cb5475116d54e3c3cf1fef06f76dbcfc9f272a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:05 +0000

    Add package mbeacon/0.0.1/mbeacon-0.0.1-py3-none-any.whl

---
+++ packages/mbeacon/mbeacon-0.0.1-py3-none-any.whl/mbeacon/__init__.py
@@ -0,0 +1,14 @@
+##############################################
+# The MIT License (MIT)
+# Copyright (c) 2018 Kevin Walchko
+# see LICENSE for full details
+##############################################
+from __future__ import absolute_import, print_function, division
+
+from mbeacon.mbeacon import BeaconServer
+from mbeacon.mbeacon import BeaconFinder
+# from mbeacon.mbeacon import Service
+
+__author__ = "Kevin Walchko"
+__license__ = "MIT"
+__version__ = "0.0.1"
