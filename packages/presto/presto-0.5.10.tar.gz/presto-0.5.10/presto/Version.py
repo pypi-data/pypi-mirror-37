a01cbdf0ec48c9c9374d7f541cf478a64a12df79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:30 +0000

    Add package presto/0.5.10/presto-0.5.10.tar.gz

---
+++ packages/presto/presto-0.5.10.tar.gz/presto-0.5.10/presto/Version.py
@@ -0,0 +1,8 @@
+"""
+Version and authorship information
+"""
+__author__    = 'Jason Anthony Vander Heiden'
+__copyright__ = 'Copyright 2017 Kleinstein Lab, Yale University. All rights reserved.'
+__license__   = 'Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)'
+__version__   = '0.5.10'
+__date__      = '2018.10.19'
