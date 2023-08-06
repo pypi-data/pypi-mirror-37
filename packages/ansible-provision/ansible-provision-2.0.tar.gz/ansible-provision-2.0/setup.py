c92a670c5e27ea519e226e3f84dd905424fda82f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:47 +0000

    Add package ansible-provision/2.0/ansible-provision-2.0.tar.gz

---
+++ packages/ansible-provision/ansible-provision-2.0.tar.gz/ansible-provision-2.0/setup.py
@@ -0,0 +1,16 @@
+from subprocess import *
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from ansible_provision import __version__
+import os
+
+__name__ = 'ansible-provision'
+
+setup(
+    name = __name__,
+    version = __version__,
+    install_requires = ['ansible-deploy'],
+    packages = [__name__.replace("-", "_")],
+    url = 'https://www/github.com/moshloop/ansible-provision',
+    author = 'Moshe Immerman', author_email = 'moshe.immerman@gmail.com'
+)
