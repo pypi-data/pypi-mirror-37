b9942e97dcbe9ea7f84094f576c7c25d00fc7ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:23 +0000

    Add package auth0-client/0.6.0/auth0_client-0.6.0-py3-none-any.whl

---
+++ packages/auth0-client/auth0_client-0.6.0-py3-none-any.whl/auth0_client/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+import pkg_resources
+from auth0_client.Auth0Client import Auth0Client #noqa
+
+try:
+    from auth0_client._build import __build__
+except ImportError:
+    pass
+else:
+    __version__ = '{}.{}'.format(__version__, __build__)
+
+__all__ = [
+]
+__title__ = 'auth0_client'
+__version__ = '0.6.0'
+__author__ = 'Will Rubel'
+__author_email__ = 'willrubel@gmail.com'
