69eea86da83a3220441bc0883255faa1f0a05338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:37 +0000

    Add package autofig/1.0.0/autofig-1.0.0.tar.gz

---
+++ packages/autofig/autofig-1.0.0.tar.gz/autofig-1.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='autofig',
+      version='1.0.0',
+      description='autofig: high-level wrapper for creating animations and figures with matplotlib',
+      author='Kyle Conroy',
+      author_email='kyleconroy@gmail.com',
+      url='https://www.github.com/kecnry/autofig',
+      download_url = 'https://github.com/kecnry/autofig/tarball/1.0.0',
+      packages=['autofig'],
+     )
