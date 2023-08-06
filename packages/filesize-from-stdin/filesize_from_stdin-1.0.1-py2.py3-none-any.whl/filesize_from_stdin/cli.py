7c62733512d2a6a74026588b7c23bdbd475af049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:25 +0000

    Add package filesize-from-stdin/1.0.1/filesize_from_stdin-1.0.1-py2.py3-none-any.whl

---
+++ packages/filesize-from-stdin/filesize_from_stdin-1.0.1-py2.py3-none-any.whl/filesize_from_stdin/cli.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for filesize_from_stdin."""
+import sys
+import click
+from .filesize_from_stdin import doit
+
+
+@click.command()
+def main():
+    """Console script for filesize_from_stdin."""
+    doit()
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
---
+++ packages/filesize-from-stdin/filesize_from_stdin-1.0.1-py2.py3-none-any.whl/filesize_from_stdin/filesize_from_stdin.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+"""Main module."""
+
+import operator
+import sys
+from pathlib import Path
+import humanize
+
+
+def doit():
+    """
+    Given a list of paths separated by newline, output
+    the list of files and their size in bytes sorted by size.
+    """
+
+    flist = {}
+
+    if sys.stdin.isatty():
+        print('fail')
+        sys.exit(1)
+
+    for line in sys.stdin.readlines():
+        path = Path(line.strip())
+        if path.exists():
+            flist[str(path.resolve())] = path.stat().st_size
+
+    # sort by size
+    for path, size in sorted(flist.items(), key=operator.itemgetter(1)):
+        print(("%s %s" % (humanize.naturalsize(size, gnu=True), path)))
