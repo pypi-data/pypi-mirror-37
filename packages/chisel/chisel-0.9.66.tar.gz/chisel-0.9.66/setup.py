4671b4e5f8b980fdac29d57820a63115b4d6de1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:06 +0000

    Add package chisel/0.9.66/chisel-0.9.66.tar.gz

---
+++ packages/chisel/chisel-0.9.66.tar.gz/chisel-0.9.66/setup.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2012-2018 Craig Hobbs
+#
+# Licensed under the MIT License
+# https://github.com/craigahobbs/chisel/blob/master/LICENSE
+
+from setuptools import setup
+
+import chisel
+
+setup(
+    name='chisel',
+    version=chisel.__version__,
+    author='Craig Hobbs',
+    author_email='craigahobbs@gmail.com',
+    description=('Schema-validated JSON web APIs for humans.'),
+    keywords='api json framework schema wsgi',
+    url='https://github.com/craigahobbs/chisel',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=['chisel'],
+    test_suite='chisel.tests'
+)
