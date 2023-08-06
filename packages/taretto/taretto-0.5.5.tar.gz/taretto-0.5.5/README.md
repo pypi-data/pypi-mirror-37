62a33a61c669e17183e5dd3ce8fa6dbd9e2d1844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:23 +0000

    Add package taretto/0.5.5/taretto-0.5.5.tar.gz

---
+++ packages/taretto/taretto-0.5.5.tar.gz/taretto-0.5.5/README.md
@@ -0,0 +1,2 @@
+# taretto
+Taretto Placeholder
---
+++ packages/taretto/taretto-0.5.5.tar.gz/taretto-0.5.5/testing/test_aliases.py
@@ -0,0 +1,27 @@
+import inspect
+import taretto
+import pytest
+
+
+@pytest.mark.parametrize(
+    "mod",
+    [taretto.navigate, taretto.ui, taretto.ui.core, taretto.ui.patternfly],
+    ids=lambda mod: mod.__name__,
+)
+def test_module_aliases(mod):
+    print(mod, dir(mod))
+
+
+@pytest.mark.parametrize(
+    "mod, classname",
+    [
+        (taretto.ui, "View"),
+        (taretto.ui, "Browser"),
+        (taretto.ui, "DefaultPlugin"),
+        (taretto.ui.patternfly, "VerticalNavigation"),
+    ],
+    ids=lambda mod: mod.__name__,
+)
+def test_aliased_classes(mod, classname):
+    klass = getattr(mod, classname)
+    assert inspect.isclass(klass)
