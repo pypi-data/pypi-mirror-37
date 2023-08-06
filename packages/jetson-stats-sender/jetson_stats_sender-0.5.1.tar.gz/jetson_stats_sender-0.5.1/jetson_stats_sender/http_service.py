1f3f8886abd65f89029dbd98d67efbde6a480e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:58 +0000

    Add package jetson-stats-sender/0.5.1/jetson_stats_sender-0.5.1.tar.gz

---
+++ packages/jetson-stats-sender/jetson_stats_sender-0.5.1.tar.gz/jetson_stats_sender-0.5.1/jetson_stats_sender/http_service.py
@@ -0,0 +1,83 @@
+import urllib2
+import json
+
+
+ENDPOINT_PATH = "/api/datareceival/datamessage"
+global_http_service = None
+
+
+def initialize_global_stats_sender(api_key, destination_address, camera_key=None):
+    global global_http_service
+    global_http_service = HttpService(api_key, destination_address, camera_key)
+
+
+def initialize_global_camera_key(camera_key):
+    global global_http_service
+    global_http_service.set_camera_key(camera_key)
+
+
+def send_per_second_stats_with_global_service(per_second_stats):
+    global global_http_service
+    global_http_service.send_per_second_stats(per_second_stats)
+
+
+def send_per_second_stat_with_global_service(per_second_stat):
+    global global_http_service
+    global_http_service.send_per_second_stat(per_second_stat)
+
+
+def get_global_http_service():
+    global global_http_service
+    return global_http_service
+
+
+class HttpService:
+    def __init__(self, api_key, destination_address, camera_key=None):
+        self.api_key = api_key
+        self.destination_address = destination_address
+        self.camera_key = camera_key
+
+    def set_camera_key(self, camera_key):
+        self.camera_key = camera_key
+
+    # Real implementation of the function that will send the passed in per_second_stat objects to the back-end
+    # Sends multiple stats
+    def send_per_second_stats(self, per_second_stats):
+        url = self.get_url_for_per_second_stats_post()
+        headers = self.get_headers_for_per_second_stats_post()
+        json_encoded_data = self.get_json_encoded_data_for_per_second_stats(per_second_stats)
+        try:
+            req = urllib2.Request(url=url, data=json_encoded_data, headers=headers)
+            response = urllib2.urlopen(req)
+            # Right now we do nothing with the response contents,
+            # but we could return what we got and compare it with what we sent
+            contents = response.read()
+            if 200 <= response.getcode() < 300:
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def send_per_second_stat(self, per_second_stat):
+        return self.send_per_second_stats([per_second_stat, ])
+
+    def get_url_for_per_second_stats_post(self):
+        return self.destination_address + ENDPOINT_PATH
+
+    def get_json_encoded_data_for_per_second_stats(self, per_second_stats):
+        json_stats = []
+        for stat in per_second_stats:
+            if stat.camera_key is None or stat.camera_key == "":
+                stat.camera_key = self.camera_key
+            json_stats.append(stat.to_json())
+        json_data = {
+            "api_key": self.api_key,
+            "RealTimeStats": json_stats
+        }
+        json_encoded_data = json.dumps(json_data)
+        return json_encoded_data
+
+    @staticmethod
+    def get_headers_for_per_second_stats_post():
+        return {'Content-type': 'application/json'}
