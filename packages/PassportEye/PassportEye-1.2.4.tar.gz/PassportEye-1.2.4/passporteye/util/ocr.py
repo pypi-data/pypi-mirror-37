4b69065456eb339351746fdffb38718787bb1683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:18 +0000

    Add package PassportEye/1.2.4/PassportEye-1.2.4.tar.gz

---
+++ packages/PassportEye/PassportEye-1.2.4.tar.gz/PassportEye-1.2.4/passporteye/util/ocr.py
@@ -0,0 +1,59 @@
+'''
+PassportEye::Util: Interface between SKImage and the PyTesseract OCR
+NB: You must have the "tesseract" tool present in your path for this to work.
+
+Author: Konstantin Tretyakov
+License: MIT
+'''
+
+from pytesseract import pytesseract
+from scipy.misc import imsave
+import sys
+import tempfile
+
+def ocr(img, mrz_mode=True, extra_cmdline_params=''):
+    """Runs Tesseract on a given image. Writes an intermediate tempfile and then runs the tesseract command on the image.
+
+    This is a simplified modification of image_to_string from PyTesseract, which is adapted to SKImage rather than PIL.
+
+    In principle we could have reimplemented it just as well - there are some apparent bugs in PyTesseract, but it works so far :)
+
+    :param mrz_mode: when this is True (default) the tesseract is configured to recognize MRZs rather than arbitrary texts.
+    :param extra_cmdline_params:extra parameters to the ocr.py
+    """
+    input_file_name = '%s.bmp' % _tempnam()
+    output_file_name_base = '%s' % _tempnam()
+    output_file_name = "%s.txt" % output_file_name_base
+    try:
+        imsave(input_file_name, img)
+
+        if mrz_mode:
+			# NB: Tesseract 4.0 does not seem to support tessedit_char_whitelist
+            config = "--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789>< -c load_system_dawg=F -c load_freq_dawg=F {}".format(extra_cmdline_params)
+        else:
+            config = ""
+
+        pytesseract.run_tesseract(input_file_name,
+                                 output_file_name_base,
+                                 'txt',
+                                 lang=None,
+                                 config=config)
+        
+        if sys.version_info.major == 3:
+            f = open(output_file_name, encoding='utf-8')
+        else:
+            f = open(output_file_name)
+        
+        try:
+            return f.read().strip()
+        finally:
+            f.close()
+    finally:
+        pytesseract.cleanup(input_file_name)
+        pytesseract.cleanup(output_file_name)
+
+
+def _tempnam():
+    '''TODO: Use the with(..) version for auto-deletion?'''
+    tmpfile = tempfile.NamedTemporaryFile(prefix="tess_")
+    return tmpfile.name
---
+++ packages/PassportEye/PassportEye-1.2.4.tar.gz/PassportEye-1.2.4/setup.py
@@ -0,0 +1,54 @@
+'''
+PassportEye: Python tools for image processing of identification documents
+
+Note that "python setup.py test" invokes pytest on the package. With appropriately
+configured setup.cfg, this will check both xxx_test modules and docstrings.
+
+Author: Konstantin Tretyakov
+License: MIT
+'''
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+# This is a plug-in for setuptools that will invoke py.test
+# when you run python setup.py test
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest  # import here, because outside the required eggs aren't loaded yet
+        sys.exit(pytest.main(self.test_args))
+
+setup(name='PassportEye',
+      version=[ln for ln in open("passporteye/__init__.py") if ln.startswith("__version__")][0].split('"')[1],
+      description="Extraction of machine-readable zone information from passports, visas and id-cards via OCR",
+      long_description=open("README.rst").read(),
+      classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Financial and Insurance Industry'
+      ],
+      keywords='id-card passport image-processing mrz machine-readable-zone',
+      author='Konstantin Tretyakov',
+      author_email='kt@ut.ee',
+      url='https://github.com/konstantint/PassportEye',
+      license='MIT',
+      packages=find_packages(exclude=['examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['numpy', 'scipy', 'scikit-image >= 0.14.1', 'scikit-learn', 'matplotlib', 'pytesseract >= 0.2.0', 
+                        'pdfminer' if sys.version_info.major == 2 else 'pdfminer3k'],
+      entry_points={
+          'console_scripts': ['evaluate_mrz=passporteye.mrz.scripts:evaluate_mrz',
+                              'mrz=passporteye.mrz.scripts:mrz']
+      }
+)
