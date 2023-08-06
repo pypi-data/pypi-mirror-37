0fae7e4d81fbc765f58ef57364787b38d336470e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:58 +0000

    Add package replacement/0.2.6/replacement-0.2.6.tar.gz

---
+++ packages/replacement/replacement-0.2.6.tar.gz/replacement-0.2.6/setup.py
@@ -0,0 +1,33 @@
+'''setup.py
+PyPI build for replacement package
+(c) 2018 Anthony Soenen and Sirio Balmelli
+'''
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    LONG = fh.read()
+
+setup(
+    name="replacement",
+    version="0.2.6",
+    packages=find_packages(),
+
+    install_requires=['ruamel.yaml'],
+    python_requires='>=3.5',
+
+    entry_points={'console_scripts': ['replacement = replacement:main']},
+
+    author="Sirio Balmelli",
+    author_email="sirio.bm@gmail.com",
+    description="Replacement is a python utility that parses a yaml template and outputs text.",
+    license='Apache',
+    keywords='replacement template substitute compile',
+    url="https://github.com/siriobalmelli/replacement",
+    long_description=LONG,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
