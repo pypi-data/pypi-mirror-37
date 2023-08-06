d6c9fce014c581dff41b1784251d1e84df5d51b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:03 +0000

    Add package ybc-tuya/1.1.2/ybc_tuya-1.1.2.tar.gz

---
+++ packages/ybc-tuya/ybc_tuya-1.1.2.tar.gz/ybc_tuya-1.1.2/ybc_tuya/tt.py
@@ -0,0 +1,32 @@
+
+import ybc_tuya as ybc_tt
+
+
+ybc_tt.canvas()
+# ybc_tt.ruler(size=50)
+
+
+ybc_tt.pen_speed(6)
+
+
+# ybc_tt.fill_rect(350,250,100,100,'orange')
+
+
+# ybc_tt.fill_circle(400,300,40,'black')
+
+
+# ybc_tt.fill_circle(387,293,15,'white')
+
+
+# ybc_tt.fill_circle(413,293,15,'white')
+
+
+# ybc_tt.fill_circle(400,313,15,'white')
+
+ybc_tt.jiqimao()
+
+# ybc_tt.xzpq()
+
+ybc_tt.save('1.png')
+
+ybc_tt.stop()
