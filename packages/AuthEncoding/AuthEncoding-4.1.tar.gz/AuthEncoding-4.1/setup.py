8a8d5d53319816fc0180aacad01449e682920465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:38 +0000

    Add package AuthEncoding/4.1/AuthEncoding-4.1.tar.gz

---
+++ packages/AuthEncoding/AuthEncoding-4.1.tar.gz/AuthEncoding-4.1/setup.py
@@ -0,0 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2010-2015 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name='AuthEncoding',
+    version='4.1',
+    url='https://github.com/zopefoundation/AuthEncoding',
+    license='ZPL 2.1',
+    description="Framework for handling LDAP style password hashes.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=(open('README.txt').read() + '\n' +
+                      open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: System :: Systems Administration :: Authentication/Directory :: LDAP",  # noqa: E501
+    ],
+    install_requires=[
+        'six',
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+        'bcrypt': [
+            'bcrypt',
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
