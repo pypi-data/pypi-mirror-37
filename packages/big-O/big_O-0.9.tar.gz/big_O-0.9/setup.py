a8aaaab41ce5449a7ecfcce94c62c461e1e85656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:30 +0000

    Add package big-O/0.9/big_O-0.9.tar.gz

---
+++ packages/big-O/big_O-0.9.tar.gz/big_O-0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+with open('README.rst') as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name='big_O',
+    version='0.9',
+    description='Empirical estimation of time complexity from execution time',
+    author='Pietro Berkes',
+    author_email='pietro.berkes@googlemail.com',
+    url='https://github.com/pberkes/big_O',
+    license='LICENSE.txt',
+    long_description=long_description,
+    packages=['big_o', 'big_o.test'],
+    install_requires=['numpy']
+)
