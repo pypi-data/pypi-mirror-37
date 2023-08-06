b83ae6ba76b94b6877cd60814d1a1ba18bc61742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:16 +0000

    Add package subzone/0.2.8/subzone-0.2.8-py3-none-any.whl

---
+++ packages/subzone/subzone-0.2.8-py3-none-any.whl/subzone/__version__.py
@@ -0,0 +1,11 @@
+
+
+
+__title__ = 'subzone'
+__description__ = 'SubZone - Subdomains, Dns records, & more!'
+__url__ = 'https://github.com/ncorbuk/SubZone'
+__version__ = '0.2.8'
+__author__ = 'Nathan Corbin'
+__author_email__ = 'ncorbuk@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018 Nathan Corbin'
