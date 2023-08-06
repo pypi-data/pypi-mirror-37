8697b696bb058b734a8e4086bc328f06d2882fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:05 +0000

    Add package gwcelery/0.1.4/gwcelery-0.1.4.tar.gz

---
+++ packages/gwcelery/gwcelery-0.1.4.tar.gz/gwcelery-0.1.4/gwcelery/conf/playground.py
@@ -0,0 +1,9 @@
+"""Application configuration for ``gracedb-playground.ligo.org``."""
+
+from . import *  # noqa: F401, F403
+
+lvalert_host = 'lvalert-playground.cgca.uwm.edu'
+"""LVAlert host."""
+
+gracedb_host = 'gracedb-playground.ligo.org'
+"""GraceDb host."""
