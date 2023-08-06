d73967e0b82f2fd1e5c7e2ee8835fdd1dce25c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:40 +0000

    Add package pastepwn/1.0.7/pastepwn-1.0.7.tar.gz

---
+++ packages/pastepwn/pastepwn-1.0.7.tar.gz/pastepwn-1.0.7/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+packages = find_packages(exclude=['tests*'])
+
+
+# Taken from https://github.com/python-telegram-bot/python-telegram-bot/blob/9d99660ba95b103b3e1dc80414a5ce2fd805260b/setup.py#L9
+def requirements():
+    """Build the requirements list for this project"""
+    requirements_list = []
+
+    with open('requirements.txt') as reqs:
+        for install in reqs:
+            requirements_list.append(install.strip())
+
+        return requirements_list
+
+
+with open("README.md", "r", encoding="utf-8") as file:
+    readme = file.read()
+
+
+setup(name='pastepwn',
+      version='1.0.7',
+      install_requires=requirements(),
+      keywords='python pastebin scraping osint framework',
+      description='Python framework to scrape PasteBin pastes and analyze them',
+      long_description=readme,
+      url='https://github.com/d-Rickyy-b/pastepwn',
+      author='d-Rickyy-b',
+      author_email='pastepwn@rickyy.de',
+      license='MIT',
+      packages=packages,
+      include_package_data=True)
