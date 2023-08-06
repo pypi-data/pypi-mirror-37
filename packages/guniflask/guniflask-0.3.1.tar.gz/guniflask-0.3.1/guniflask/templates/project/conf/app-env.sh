13529ec37adc73930bbf268a34ba98c4d5d326eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:57 +0000

    Add package guniflask/0.3.1/guniflask-0.3.1.tar.gz

---
+++ packages/guniflask/guniflask-0.3.1.tar.gz/guniflask-0.3.1/guniflask/templates/project/conf/app-env.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# The command of Python
+export PYTHON=python3
+
+# The home directory of virtual environment
+export VIRTUAL_ENV=${VIRTUAL_ENV}
+
+# The directory where configuration files are stored
+export GUNIFLASK_CONF_DIR=${GUNIFLASK_CONF_DIR}
+
+# The directory where log files are stored
+export GUNIFLASK_LOG_DIR=${GUNIFLASK_LOG_DIR}
+
+# The directory where pid files are stored
+export GUNIFLASK_PID_DIR=${GUNIFLASK_PID_DIR}
+
+# A string representing this instance
+export GUNIFLASK_ID_STRING=${GUNIFLASK_ID_STRING}
---
+++ packages/guniflask/guniflask-0.3.1.tar.gz/guniflask-0.3.1/guniflask/templates/project/{{project_name}}/hooks.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+
+def make_settings(app, settings):
+    """
+    This function is invoked before initializing app.
+    """
+
+
+def init_app(app, settings):
+    """
+    This function is invoked before running app.
+    """
