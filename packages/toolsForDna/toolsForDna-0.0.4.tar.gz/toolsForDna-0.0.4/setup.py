492b02521cedca3b9d3e0c729826495bb98a77df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:47 +0000

    Add package toolsForDna/0.0.4/toolsForDna-0.0.4.tar.gz

---
+++ packages/toolsForDna/toolsForDna-0.0.4.tar.gz/toolsForDna-0.0.4/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="toolsForDna",
+    version="0.0.4",
+    author="TheOnlyWalrus",
+    description="Tools for DNA",
+    packages=setuptools.find_packages(),
+)
