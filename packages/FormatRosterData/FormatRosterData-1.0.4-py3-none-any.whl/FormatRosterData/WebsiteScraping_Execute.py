4d9ccb65e20827d3badbf7883ba6395a24f4c091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:24 +0000

    Add package FormatRosterData/1.0.4/FormatRosterData-1.0.4-py3-none-any.whl

---
+++ packages/FormatRosterData/FormatRosterData-1.0.4-py3-none-any.whl/FormatRosterData/WebsiteScraping_Execute.py
@@ -0,0 +1,25 @@
+##region Setttings
+bPause = True
+##endregion
+##region Imports
+import os
+import FormatRosterData as FRD
+import openpyxl
+import TM_CommonPy as TM
+import traceback
+##endregion
+
+def Main():
+    vRoster = FRD.GetRoster('http://www.espn.com/mens-college-basketball/team/roster/_/id/84')
+    print(vRoster.content)
+
+try:
+    Main()
+except PermissionError:
+    print("PERMISSION_ERROR\n\tI'd recommend to just try again.\n\tOtherwise, close all extra programs and then retry.")
+    TM.DisplayDone()
+except Exception as e:
+    TM.DisplayException(e)
+else:
+    if bPause:
+        TM.DisplayDone()
