2ad82cb74699b3ea4dab33e2e86ce50a880e10ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:09 +0000

    Add package PySysrev/0.9/PySysrev-0.9.tar.gz

---
+++ packages/PySysrev/PySysrev-0.9.tar.gz/PySysrev-0.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='PySysrev',
+      version='0.9',
+      description='Gets annotations from Sysrev API',
+      url='https://github.com/sysrev/PySysrev',
+      author='nole-lin',
+      author_email='nole@insilica.co',
+      packages=['PySysrev'],
+      install_requires=[
+          'pandas',
+          'requests',
+          'spacy',
+          'plac',
+          'pathlib',
+          'boto3'
+      ],
+      zip_safe=False)
