e8e27960a0560c336ae22e43a3e59be52035f4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:36 +0000

    Add package archery/1.2.2/archery-1.2.2.tar.gz

---
+++ packages/archery/archery-1.2.2.tar.gz/archery-1.2.2/archery/test_archery.py
@@ -0,0 +1,380 @@
+#/usr/bin/env python
+import os
+import sys
+
+import warnings
+import unittest
+from  archery.bow import Hankyu, Daikyu, sdict, vdict, mdict, edict
+from archery.barrack import bowyer, Path, make_from_path, paired_row_iter
+
+from math import sqrt, cos, pi, sin, acos
+
+class TestDeprecation(unittest.TestCase):
+    def test_deprec_edict(self):
+        """ edict (ExpDict is becoming searchable """
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always')
+            edict(x=1)
+            self.assertTrue(
+                issubclass(w[-1].category, DeprecationWarning),
+            )
+
+class TestKyleExpectation(unittest.TestCase):
+
+    def test_commutativity(self):
+        a = mdict({"a": 1, "y": {"b": 4, "c": [5]}})
+        b=  mdict({"a": 1, "x": 5, "y": {"b": 6, "c": [12]}})
+        expected_ab = {"a": 2, "x": 5, "y": {"b": 10, "c": [5, 12]}}
+        expected_ba = {"a": 2, "x": 5, "y": {"b": 10, "c": [12, 5]}}
+        self.assertEqual(a + b, expected_ab)
+        a = mdict({"a": 1, "y": mdict({"b": 4, "c": [5]})})
+        b=  mdict({"a": 1, "x": 5, "y": mdict({"b": 6, "c": [12]})})
+        self.assertEqual(b + a, expected_ba)
+        a = mdict({"a": 1, "y": mdict({"b": 4, "c": [5]})})
+        b=  mdict({"a": 1, "x": 5, "y": mdict({"b": 6, "c": [12]})})
+        a += b
+        self.assertEqual(a, expected_ab)
+
+
+    def test_bug_iadd(self):
+        a = mdict({"a": 1, "y": mdict({"b": 4, "c": [5]})})
+        b=  mdict({"a": 1, "x": 5, "y": mdict({"b": 6, "c": [12]})})
+        expected_ab = {"a": 2, "x": 5, "y": {"b": 10, "c": [5, 12]}}
+        expected_ba = {"a": 2, "x": 5, "y": {"b": 10, "c": [12, 5]}}
+        self.assertEqual(a + b, expected_ab)
+        self.assertEqual(b + a, expected_ba)
+        a += b
+        self.assertEqual(a, expected_ab)
+
+
+class TestVectorDict(unittest.TestCase):
+    """test a dict with Vector properies (cos, dot, abs)"""
+
+    def setUp(self):
+        self.point = vdict( x=3, y=3, z=0)
+        self.point2 = vdict( x=1, y=1, z=1)
+    
+    def testNorm(self):
+        self.assertAlmostEqual(
+                abs(self.point),
+                sqrt(3*3+3*3)
+        )
+    def testCos(self):
+        self.assertAlmostEqual(
+                vdict(x=1,y=0).cos(vdict(x=1,y=1)),
+                cos(pi/4)
+        )
+
+class TestBarrack(unittest.TestCase):
+    def test_path(self):
+        self.assertEqual(
+            Path(("a","b",1)).key(),
+            ("a","b")
+        )
+        self.assertFalse(
+            Path(("a","b",1)).contains("a","b","c")
+        )
+        self.assertEqual(
+            Path(("a","b",1)).value(),
+            1
+        )
+        self.assertTrue(Path(("x", "y", 1)).startswith("x"))
+        self.assertTrue(Path(("s", "x", "y", 1)).contains("x","y"))
+
+    def test_paired_row_iter(self):
+        self.assertEqual(set(paired_row_iter(dict(x=1, a=dict(b=dict(c=1))))),
+            { (("x",),1), (("a","b","c"), 1)}
+        )
+
+
+class TestBugWeired(unittest.TestCase):
+
+    def test_stay_same(self):
+
+        class Matrix(mdict):
+            def __call__(self, other):
+                other = other.copy()
+                res= vdict()
+                for (src, dst), functor in self.items():
+                    res += mdict({ dst: functor(other[src])})
+                return res
+
+
+        theta = pi/6
+
+        u = mdict(x=1, y=2)
+        v = mdict(x=1, y=0)
+        alien = vdict(x=u, y=v)
+
+        def rotation_maker(theta):
+            """"Matrix takes as key (SRC, DST) (which is the opposite of "actual notation")
+            """
+            return Matrix({
+                ("x", "x") : lambda v:1.0 *  v * cos(theta),
+                ("y", "x") : lambda v:1.0 * -v * sin(theta),
+                ("x", "y") : lambda v:1.0 *  v * sin(theta),
+                ("y", "y") : lambda v:1.0 *  v * cos(theta)
+            })
+
+        rotation = rotation_maker(pi/6)
+        self.assertAlmostEqual(
+            rotation(u),
+            {'x': -0.13397459621556118, 'y': 2.232050807568877}
+            )
+# OUT:{'x': 1, 'y': 0}
+        self.assertAlmostEqual(rotation(v),
+            {'x': 0.8660254037844387, 'y': 0.49999999999999994}
+        )
+        self.assertAlmostEqual(
+            acos(vdict(u).cos(vdict(rotation(u))))/2 / pi * 360,
+            29.999999999999993
+        )
+        self.assertEqual(u, dict(x=1, y=2))
+        self.assertAlmostEqual(acos(vdict(v).cos(vdict(rotation_maker(pi/3)(v))))/2 / pi * 360,
+60.0)
+        self.assertAlmostEqual(
+            acos(vdict(v).cos(vdict(rotation_maker(pi/5)(v))))/2 / pi * 360,
+            36.0
+        )
+        self.assertEqual(
+            alien,
+            {'x': {'x': 1, 'y': 2}, 'y': {'x': 1, 'y': 0}}
+        )
+        """
+        undefined behaviour until I can make a sense of this
+
+
+
+        self.assertAlmostEqual(
+            rotation_maker(pi/4)(alien),
+            {
+                'x': {'x': 1.1102230246251565e-16, 'y': 1.4142135623730951},
+                'y': {'x': -1.1102230246251565e-16, 'y': 1.414213562373095}
+            }
+        )
+        self.assertAlmostEqual(
+            acos(alien.cos(rotation_maker(pi/4)(alien)))/2 / pi * 360,
+            54.73561031724534
+        )
+
+        self.assertAlmostEqual(rotation_maker(pi/4)(alien),
+            {'x':
+                {'x': 1.1102230246251565e-16, 'y': 1.4142135623730951},
+             'y': {'x': -1.1102230246251565e-16, 'y': 1.414213562373095}})
+        """
+
+
+class TestSearchableDict(unittest.TestCase):
+    def setUp(self):
+        self.easy = vdict( x=1, y=1, z=0)
+        
+        self.tree = sdict(
+            a = 1,
+            b = dict(
+                c = 3.0,
+                d = dict(e=True)
+            ),
+            point = self.easy
+        )
+
+    def test_rec_type(self):
+        """test that if created with another mutale mapping
+        does not converts it on the fly to himself"""
+        self.assertNotEqual(
+            type(self.tree["point"]),
+            sdict
+        )
+
+    def test_search(self):
+        self.assertEqual(
+                set([ x for x in self.tree.search(
+                    lambda x : Path(x).contains("point","x") 
+                        or Path(x).endswith(3.0)
+                    )
+                ]),
+                set([('b', 'c', 3.0), ('point', 'x', 1)])
+        )
+        self.assertEqual(
+                set([ x for x in self.tree.search(
+                    lambda x : Path(x).startswith("a")
+                    )
+                ]),
+                set([('a', 1)])
+        )
+
+    def test_leaf_search(self):
+        self.assertEqual(
+                [ i for i in self.tree.leaf_search(lambda x : type(x) is  float)],
+                [ 3.0 ]
+        )
+        self.assertEqual(
+                [ i for i in self.tree.leaf_search(lambda x : type(x) is  bool)],
+                [ True ]
+        )
+        
+
+
+        
+
+class TestHankyu(unittest.TestCase):
+    def setUp(self):
+        self.easy = Hankyu( x=1, y=1, z=0)
+        self.easy_too = Hankyu( x=0, y=-2, o=0)
+        
+        self.a_tree = dict(
+            a = 1,
+            b = dict(
+                c = 3.0,
+                d = dict(e=True)
+            ),
+            point = self.easy
+        )
+        self.cplx = bowyer(Hankyu,self.a_tree)
+
+    def test_convert(self):
+        self.assertEqual(
+            bowyer(Hankyu, {'a': {'b': 1}}),
+            Hankyu( {'a': Hankyu(b=1)})
+            )
+        
+
+    def test_add_not_exists(self):
+        self.easy += dict(a=1)
+        self.assertEqual(self.easy['a'], 1)
+        self.assertEqual(
+                self.easy + self.easy_too, 
+                sum([ self.easy_too, self.easy])
+        )
+
+    def test_add_exists(self):
+        self.easy += dict(x=1)
+        self.assertEqual(self.easy['x'] , 2)
+
+    def test_number_inc(self):
+        self.easy += 1
+        self.assertEqual(self.easy['x'] , 2)
+
+
+    def test_bug_convert_empty_mapping(self):
+        """bug #8 : empty mapping intialisation dont work"""
+        bug = bowyer( Hankyu,dict( a={}))
+        expected = Hankyu( {'a': {}})
+        self.assertEqual(
+            expected,
+            bug
+        )
+
+class TestDaikyu(unittest.TestCase):
+    def setUp(self):
+        self.easy = Daikyu( x=1, y=1, z=0)
+        self.easy_too = Daikyu( x=0, y=-2, o=0)
+        
+        self.a_tree = dict(
+            a = 1,
+            b = dict(
+                c = 3.0,
+                d = dict(e=True)
+            ),
+            point = self.easy
+        )
+        self.cplx = bowyer(Daikyu,self.a_tree)
+
+
+    def test_bug_1(self):
+        """bug  #1 : * returns a copy where a reference is needed"""
+        pt = self.easy
+        pt *= -1
+        self.assertEqual(
+            self.easy,
+            pt
+        )
+    def test_bug_2(self):
+        """bug #2 : self * other without affectation should not modify self.
+        """
+        self.easy *= -1
+        a_copy = self.easy.copy()
+        self.easy * a_copy
+        self.assertEqual(
+            self.easy,
+            a_copy
+        )
+
+
+    def test_bug_4(self):
+        """bug #4 : self / other without affectation should not modify self.
+        """
+        del(self.easy['z'])
+        self.easy /= -1
+        a_copy = self.easy.copy()
+        self.easy / a_copy
+        self.assertEqual(
+            self.easy,
+            a_copy
+        )
+
+    def test_bug_5(self):
+        """bug #5 : int * dict without affectation should not modify self.
+        """
+        a_copy = self.easy.copy()
+        2 * self.easy
+        self.assertEqual(
+            self.easy,
+            a_copy
+        )
+
+    def test_bug_6(self):
+        """bug #6 : int / dict without affectation should not modify self.
+        """
+        del(self.easy['z'])
+        a_copy = self.easy.copy()
+        2.0 / self.easy
+        self.assertEqual(
+            self.easy,
+            a_copy
+        )
+
+
+    def test_for_fun(self):
+        """just for fun """
+        a = self.easy
+        b = self.easy_too
+        aa = a.copy()
+        bb = b.copy()
+        self.assertEqual(
+            (a - b) * (a + b),
+            aa * aa - bb * bb
+        )
+
+
+    def test_for_fun2(self):
+        """just for fun """
+        a = self.easy
+        b = self.easy_too
+        aa = a.copy()
+        bb = b.copy()
+        self.assertEqual(
+            -(a + b) * (a + b),
+            -1 * ((aa * aa) + 2 * aa * bb + (bb * bb))
+        )
+    
+    def test_dyslexia_bug4(self):
+        """counfonding right & left in all r(div/add/sub/mul) operator """
+        a = self.easy
+        self.assertEqual(
+            2/Daikyu({'a':1}),
+            {'a' : 2}
+        )
+        self.assertEqual(
+            2-a,
+            -(a-2)
+        )
+    def test_inplace_bug5(self):
+        """a/b in place"""
+        a = self.easy*4
+        self.assertEqual(
+            a/{'x':2},
+            {'x' : 2}
+        )
+
+if __name__ == '__main__':
+    unittest.main(verbosity=4)
---
+++ packages/archery/archery-1.2.2.tar.gz/archery-1.2.2/archery/trait.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""traits of wits for  MutableMapping
+(any class that quacks like a dict, key like dict, and fly like a dict).
+You'll make your code perspire smartness by all its pore(c)(tm)(r).
+"""
+from __future__ import division
+from collections import MutableMapping, Mapping
+from .barrack import paired_row_iter, mapping_row_iter, bowyer
+__all__ = [ 'InclusiveAdder', 'InclusiveSubber', "Vector",
+    'ExclusiveMuler', 'TaintedExclusiveDiver',
+    'Copier', 'Iterator', 'Searchable']
+from copy import deepcopy
+class Copier(object):
+    def copy(self):
+        try:
+            return deepcopy(
+                bowyer(
+                    globals()[type(self).__name__], 
+                    {k:v for k,v in self.items()}
+                )
+            )
+        except KeyError:
+            pass
+        if hasattr(self,"_asdict"):
+            return self._asdict().copy()
+        if hasattr(super(Copier, self),"copy"):
+            return self.__class__(super(Copier, self).copy())
+        return deepcopy(self)
+
+class Vector(object):
+
+    def dot(u, v):
+        """
+        scalar product of two MappableMappings (recursive)
+        https://en.wikipedia.org/wiki/Dot_product
+
+        """
+        return sum(v for i,v in paired_row_iter(u*v))
+
+
+    def __abs__(v):
+        """return the absolute value (hence >=0)
+        aka the distance from origin as defined in Euclidean geometry.
+        Keys of the dict are the dimension, values are the metrics
+        https://en.wikipedia.org/wiki/Euclidean_distance
+        """
+        return v.dot(v)**.5
+
+    def cos(u, v):
+        """
+        returns the cosine similarity of 2 mutable mappings (recursive)
+        https://en.wikipedia.org/wiki/Cosine_similarity
+        dict().cos(dict(x=....)) will logically yield division by 0 exception.
+        http://math.stackexchange.com/a/932454
+        """
+        return u.dot(v) / abs(u) / abs(v)
+
+from copy import deepcopy
+
+class InclusiveAdder(object):
+    """ making dict able to add 
+
+ >>> from archery.trait import Adder
+ >>> from collections import defaultdict
+ >>> class dad(defaultdict,Adder,Subber):pass
+ ... 
+ >>> tata = dad( int, dict(a = 1, b = 0, c = -1 ) )
+ >>> 
+ >>> toto = dad( int, dict(a = 1 ) )
+ >>> print toto+tata
+ defaultdict(<type 'int'>, {'a': 2, 'c': -1, 'b': 0})
+ >>> toto+=1
+ defaultdict(<type 'int'>, {'a': 2}
+
+"""
+
+    def __add__(self, other):
+        """adder"""
+        copy = self.copy()
+        copy += other
+        return copy
+        
+
+    def __iinc__(self, number):
+        """in place increment"""
+        for k in self.keys():
+            self[k] += number
+        return self
+
+    def __iadd__(self, other):
+        if not isinstance(other, MutableMapping):
+            return self.__iinc__(other)
+        for k, v in other.items():
+            if k in self:
+                self[k] += v
+            else:
+                self[k] = v
+        return self
+    def __radd__(self, other):
+        copy = deepcopy(self)
+        return copy + other
+
+
+class TaintedExclusiveDiver(object):
+    """Making dict able to truedivide (you need to provide a muler)
+    This operator is tainted thanks to my inability to make neither
+    from __future__ import truedivision
+    nor
+    from operator import truetruediv
+    works. 
+    So as a result I use implicit 1.0 cast
+    
+    Why don't I stick to regular (broken) python truedivision ? 
+    Don't you think this  : 
+    0.5 * a == a / 2
+    less surprising than :
+    a/2 = something that is part /2 (if result is int),
+    and sometimes something else
+
+    )"""
+    def __div__(self, other):
+        return self.__truediv__(other)
+
+    def __idiv__(self, other):
+        return self.__itruediv__(other)
+
+    def __rdiv__(self, other):
+        return self.__rtruediv__(other)
+
+    def __truediv__(self, other):
+        """truediver"""
+        copy = self.copy()
+        if not isinstance(other, MutableMapping):
+            return copy.__iscalmul__(1 / other)
+        copy /= other
+        return copy
+
+    def __itruediv__(self, other):
+        if not isinstance(other, MutableMapping):
+            self.__iscalmul__(1 / other)
+            return self
+        todel = []
+        for k in self:
+            if k in other:
+                self[k] /=  other[k]
+            else:
+                todel += [k]
+        for k in todel:
+            del(self[k])
+        return self
+
+    def __iinv__(self):
+        """in place inversion 1/a"""
+        for k, v in self.items():
+            self[k] = 1/v
+        return self
+
+    def __rtruediv__(self, other):
+        copy = self.copy()
+        if not isinstance(other, MutableMapping):
+            return copy.__iinv__().__iscalmul__(other)
+        return copy / other
+
+
+class ExclusiveMuler(object):
+    """Making dict able to multiply"""
+
+    def __mul__(self, other):
+        """muler"""
+        copy = self.copy()
+        copy *= other
+        return copy 
+
+    def __iscalmul__(self, number):
+        """in place imul"""
+        for k in self.keys():
+            self[k] *= number
+        return self
+
+    def __imul__(self, other):
+        if not isinstance(other, MutableMapping):
+            self.__iscalmul__(other)
+            return self
+        todel = set()
+        for k in self:
+            if k in other:
+                self[k] *= other[k]
+            else:
+                todel |= {k,}
+        for k in todel:
+            del(self[k])
+        return self
+
+    def __rmul__(self, other):
+        copy = self.copy()
+        if not isinstance(other, MutableMapping):
+            return copy.__iscalmul__(other)
+        return copy.__mul__(other)
+
+    def __lmul__(self, other):
+        copy = self.copy()
+        if not isinstance(other, MutableMapping):
+            return copy.__iscalmul__(other)
+        return copy.__mul__(other)
+
+class Iterator(object):
+
+    def __iter__(self):
+        self.__iter = mapping_row_iter(self)
+        return self.__iter
+
+    def __next__(self):
+        return self.__iter()
+
+class InclusiveSubber(object):
+    def __sub__(self, other):
+        """suber"""
+        copy = self.copy()
+        copy -= other
+        return copy
+
+    def __isub__(self, other):
+# breaks consistency
+#        for k in self.keys():
+#            self[k] -= other
+        if not isinstance(other, MutableMapping):
+            self.__iinc__(-other)
+            return self
+        for k, v in other.items():
+            self[k] = self[k] - v if k in self else -v
+
+        return self
+
+    def __rsub__(self, other):
+        copy = self.copy()
+        if not isinstance(other, MutableMapping):
+            return copy.__neg__().__iinc__(other)
+        return copy.__sub__(other)
+
+    def __neg__(self):
+        """in place negation"""
+        for k in self:
+            self[k] *= -1
+        return self
+
+
+class Searchable(Iterator):
+
+
+    def search(self, predicate):
+        """Return a generator of all tuples made of :
+        - all keys leading to a value
+        - and the value itself
+        that match the predicate on the `Path`_"""
+        for el in self:
+            if predicate(el):
+                yield el
+
+    def leaf_search(self, predicate):
+        """Return a generator all all values matching 
+        the predicates"""
+        for el in self:
+            value = el[-1]
+            if predicate(value):
+                yield value
