4d873146efcab5e4b1770592a19865ccbe47db74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:12 +0000

    Add package mbpls/1.0.0a1/mbpls-1.0.0a1-py3-none-any.whl

---
+++ packages/mbpls/mbpls-1.0.0a1-py3-none-any.whl/mbpls/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# -*- coding: utf-8 -*-
+#
+# author: Laurent Vermue
+# author_email: lauve@dtu.dk
+#
+#
+# License: 3-clause BSD
+
+from . import mbpls, data
+
+__all__ = ["mbpls", "data"]
+
+__version__ = "1.0.0a1"
