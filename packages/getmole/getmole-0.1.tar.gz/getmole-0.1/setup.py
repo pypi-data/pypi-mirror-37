e5da97ac4fcadb35777af11241db436bc31e13e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:10 +0000

    Add package getmole/0.1/getmole-0.1.tar.gz

---
+++ packages/getmole/getmole-0.1.tar.gz/getmole-0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name='getmole',version='0.1',description='get molecule file',url='http://github.com/maiziex/getmole',author='maiziex',author_email='maiziex@gmail.com',license='MIT',packages=['getmole'],zip_safe=False)
