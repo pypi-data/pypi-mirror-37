34c4a248c076357325e0975dfb4a55bab749a0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:59 +0000

    Add package cobrakbase/0.0.5/cobrakbase-0.0.5.tar.gz

---
+++ packages/cobrakbase/cobrakbase-0.0.5.tar.gz/cobrakbase-0.0.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='cobrakbase',
+      version='0.0.5',
+      description='KBase interface for COBRApy',
+      url='https://github.com/Fxe/cobrakbase',
+      author='Filipe Liu',
+      author_email='fliu@anl.gov',
+      license='MIT',
+      packages=['cobrakbase'],
+      install_requires=[
+          "cobra >= 0.13.4"
+      ],
+      zip_safe=False)
