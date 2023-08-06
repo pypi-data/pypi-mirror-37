bfb10a840a5a444218a528e708b2b55065183b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:12 +0000

    Add package mbpls/1.0.0a1/mbpls-1.0.0a1.tar.gz

---
+++ packages/mbpls/mbpls-1.0.0a1.tar.gz/mbpls-1.0.0a1/mbpls/data/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# -*- coding: utf-8 -*-
+#
+# Author: Laurent Vermue <lauve@dtu.dk>
+#
+#
+# License: 3-clause BSD
+
+from .get_data import data_path
+from .get_data import orthogonal_data
+
+__all__ = ["data_path",
+           "orthogonal_data",
+           ]
