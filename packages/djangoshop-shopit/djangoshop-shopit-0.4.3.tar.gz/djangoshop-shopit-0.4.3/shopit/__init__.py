ad4179cad08ff8dca1229474914797d55e6465bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:08 +0000

    Add package djangoshop-shopit/0.4.3/djangoshop-shopit-0.4.3.tar.gz

---
+++ packages/djangoshop-shopit/djangoshop-shopit-0.4.3.tar.gz/djangoshop-shopit-0.4.3/shopit/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+"""
+Release logic:
+ 1. Bump the __version__.
+ 2. git add shopit/__init__.py
+ 3. git commit -m 'Bump to <version>'
+ 4. git push
+ 5. Make sure all tests pass on https://travis-ci.org/dinoperovic/djangoshop-shopit
+ 6. git tag <version>
+ 7. git push --tags
+ 8. python setup.py sdist
+ 9. twine upload dist/djangoshop-shopit-<version>.tar.gz
+10. Done!
+"""
+__version__ = '0.4.3'
+
+default_app_config = 'shopit.apps.ShopitConfig'
