0ec9c891e9469549254468daacd3568f22e29668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:26 +0000

    Add package filesize-from-stdin/1.0.3/filesize_from_stdin-1.0.3.tar.gz

---
+++ packages/filesize-from-stdin/filesize_from_stdin-1.0.3.tar.gz/filesize_from_stdin-1.0.3/filesize_from_stdin/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for filesize-from-stdin."""
+
+__author__ = """Taylor Monacelli"""
+__email__ = 'taylormonacelli@gmail.com'
+__version__ = '1.0.3'
---
+++ packages/filesize-from-stdin/filesize_from_stdin-1.0.3.tar.gz/filesize_from_stdin-1.0.3/tests/context.py
@@ -0,0 +1,10 @@
+import filesize_from_stdin
+import os
+import sys
+
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            '..')))
