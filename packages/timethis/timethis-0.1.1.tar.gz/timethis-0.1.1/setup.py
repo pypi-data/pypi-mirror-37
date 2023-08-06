4e2caa8fea0fcbb662239cbfcc3c235bc730cf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:55 +0000

    Add package timethis/0.1.1/timethis-0.1.1.tar.gz

---
+++ packages/timethis/timethis-0.1.1.tar.gz/timethis-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md') as file_object:
+    long_description = file_object.read()
+
+setuptools.setup(
+    name='timethis',
+    version='0.1.1',
+    author='Lukas Waymann',
+    author_email='meribold@gmail.com',
+    description='Context manager for measuring execution times',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/meribold/timethis',
+    py_modules=['timethis'],
+    python_requires='~=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
