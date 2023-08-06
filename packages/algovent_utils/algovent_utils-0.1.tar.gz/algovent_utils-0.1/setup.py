e64284d57f4047975c4e1621cd2816a3c4a57017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:59 +0000

    Add package algovent_utils/0.1/algovent_utils-0.1.tar.gz

---
+++ packages/algovent_utils/algovent_utils-0.1.tar.gz/algovent_utils-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='algovent_utils',
+      version='0.1',
+      description='utils to support django-algovent framework',
+      url='http://github.com/storborg/funniest',
+      author='Naveen Bollimpalli',
+      author_email='naveenbollimpalli@gmail.com',
+      license='MIT',
+      packages=['algovent_utils'],
+      zip_safe=False)
