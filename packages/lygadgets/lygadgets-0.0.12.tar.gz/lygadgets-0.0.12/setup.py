c5ec41c5f8c98f2725aa4d688a32828d4e6a84c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:48 +0000

    Add package lygadgets/0.0.12/lygadgets-0.0.12.tar.gz

---
+++ packages/lygadgets/lygadgets-0.0.12.tar.gz/lygadgets-0.0.12/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md', 'r') as fx:
+      return fx.read()
+
+
+setup(name='lygadgets',
+      version='0.0.12',
+      description='Tools to make klayout and python work better together',
+      long_description=readme(),
+      author='Alex Tait',
+      author_email='alexander.tait@nist.gov',
+      license='MIT',
+      packages=['lygadgets'],
+      install_requires=[],
+      entry_points={'console_scripts': ['lygadget_link=lygadgets.command_line:cm_link_any']},
+      )
