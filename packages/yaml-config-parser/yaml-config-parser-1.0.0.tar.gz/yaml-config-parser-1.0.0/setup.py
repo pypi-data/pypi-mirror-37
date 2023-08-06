d2863b6d6c2f8efcf4bdaf0652086a2d3254104e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:38 +0000

    Add package yaml-config-parser/1.0.0/yaml-config-parser-1.0.0.tar.gz

---
+++ packages/yaml-config-parser/yaml-config-parser-1.0.0.tar.gz/yaml-config-parser-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '1.0.0'
+
+setup(name='yaml-config-parser',
+      version=version,
+      description="A simple yaml config parser tool",
+      long_description="""\
+A simple yaml config parser tool""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='config parser',
+      author='luserv',
+      author_email='vasterlu@gmail.com',
+      url='https://github.com/luserv/config_parser',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      py_modules=['config_parser'],
+      zip_safe=False,
+      install_requires=[
+          "pyyaml"
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
