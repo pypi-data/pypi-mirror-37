032187e925d9aa1214e4565daa72f546cfe8b2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:46 +0000

    Add package Products.contentmigration/2.2.0/Products.contentmigration-2.2.0.tar.gz

---
+++ packages/Products.contentmigration/Products.contentmigration-2.2.0.tar.gz/Products.contentmigration-2.2.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+version = '2.2.0'
+
+setup(
+    name='Products.contentmigration',
+    version=version,
+    description="A generic content migration framework for Plone.",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.2",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Plone Archetypes ATContentTypes',
+    author='Martin Aspeli (and others)',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.org/project/Products.contentmigration',
+    license='LGPL',
+    packages=find_packages(),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
+        test=[
+            'archetypes.schemaextender',
+            'plone.app.testing',
+            'Products.CMFPlone',
+            'six',
+            'zope.testing',
+            'Zope2',
+        ],
+    ),
+    install_requires=[
+        'setuptools',
+    ],
+)
